1a7ae1045f8f97311bc76ac9571457da6fd39f6e
redshiftzero <jen@penumbra.zone>
2021-10-18 12:11:09 -0400

    refactor: move `Spend::new`, `Output::new` to the tx builder

--- crypto/src/action/spend.rs
+++ crypto/src/action/spend.rs
@@ -4,47 +4,14 @@ use rand_core::{CryptoRng, RngCore};
 use crate::Fr;
 
 use crate::{
-    keys::SpendKey,
-    merkle::proof::MerklePath,
     proofs::transparent::SpendProof,
     rdsa::{Signature, SigningKey, SpendAuth, VerificationKey},
-    value, Note, Nullifier,
+    value, Nullifier,
 };
 
 pub struct Spend {
     pub body: Body,
     pub auth_sig: Signature<SpendAuth>,
-}
-
-impl Spend {
-    pub fn new<R: RngCore + CryptoRng>(
-        mut rng: R,
-        spend_key: SpendKey,
-        _merkle_path: MerklePath,
-        note: Note,
-    ) -> Self {
-        // TODO: Derive nullifier from note commitment, note position, and
-        // nullifier deriving key
-        // See p.55 ZCash spec
-        let nullifier = Nullifier::new();
-
-        let v_blinding = Fr::rand(&mut rng);
-        let value_commitment = note.value.commit(v_blinding);
-
-        let body = Body::new(
-            &mut rng,
-            value_commitment,
-            nullifier,
-            *spend_key.spend_auth_key(),
-        );
-
-        let spend_auth_randomizer = Fr::rand(&mut rng);
-        let rsk = spend_key.spend_auth_key().randomize(&spend_auth_randomizer);
-
-        let auth_sig = rsk.sign(rng, &body.serialize());
-
-        Spend { body, auth_sig }
-    }
 }
 
 pub struct Body {
@@ -57,14 +24,15 @@ pub struct Body {
 
 impl Body {
     pub fn new<R: RngCore + CryptoRng>(
-        mut rng: R,
+        rng: &mut R,
         value_commitment: value::Commitment,
         nullifier: Nullifier,
         ask: SigningKey<SpendAuth>,
+        spend_auth_randomizer: Fr,
     ) -> Body {
-        let a = Fr::rand(&mut rng);
+        let a = Fr::rand(rng);
         let rk = ask.randomize(&a).into();
-        let proof = SpendProof::new();
+        let proof = SpendProof::new(spend_auth_randomizer);
         Body {
             value_commitment,
             nullifier,
--- crypto/src/proofs/transparent.rs
+++ crypto/src/proofs/transparent.rs
@@ -1,10 +1,16 @@
 //! Transparent proofs for `MVP1` of the Penumbra system.
+use crate::Fr;
 
-pub struct SpendProof {}
+pub struct SpendProof {
+    _spend_auth_randomizer: Fr,
+    // more TK
+}
 
 impl SpendProof {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(spend_auth_randomizer: Fr) -> Self {
+        Self {
+            _spend_auth_randomizer: spend_auth_randomizer,
+        }
     }
 }
 
