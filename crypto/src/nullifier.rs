6da8a3bbd6a9c88930bb79e721bc00f610a27cfe
redshiftzero <jen@penumbra.zone>
2021-10-26 17:32:09 -0400

    feat: fail if nullifier has been revealed

--- crypto/src/nullifier.rs
+++ crypto/src/nullifier.rs
@@ -6,7 +6,7 @@ use once_cell::sync::Lazy;
 
 use crate::Fq;
 
-#[derive(PartialEq, Eq, Clone)]
+#[derive(PartialEq, Eq, Clone, Hash)]
 pub struct Nullifier(pub Fq);
 
 /// The domain separator used to derive nullifiers.
