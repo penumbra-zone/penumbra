92f868fa22b49cce3f9ccd6491c65c43e25c31b1
redshiftzero <jen@penumbra.zone>
2021-10-27 12:17:00 -0400

    feat(pcli): basic client storage
    Sketch based on Section 4.21 of the ZCash spec

--- penumbra/src/bin/pcli.rs
+++ penumbra/src/bin/pcli.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
 use structopt::StructOpt;
 
+use penumbra_crypto::{memo::MemoPlaintext, merkle, note, Note, Nullifier};
+use std::collections::{BTreeMap, HashSet};
+
+const MAX_MERKLE_CHECKPOINTS_CLIENT: usize = 10;
+
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "pcli",
@@ -9,7 +14,7 @@ use structopt::StructOpt;
 )]
 struct Opt {
     /// The address of the Tendermint node.
-    #[structopt(short, long, default_value = "127.0.0.1:26657")]
+    #[structopt(short, long, default_value = "127.0.0.1:26658")]
     addr: std::net::SocketAddr,
     #[structopt(subcommand)]
     cmd: Command,
@@ -21,6 +26,46 @@ enum Command {
     Tx { key: String, value: String },
     /// Queries the Penumbra state.
     Query { key: String },
+}
+
+struct ClientState {
+    // The first block height we've scanned from.
+    first_block_height: i64,
+    // The last block height we've scanned to.
+    last_block_height: i64,
+    // Note commitment tree.
+    note_commitment_tree: merkle::BridgeTree<note::Commitment, { merkle::DEPTH as u8 }>,
+    // Our nullifiers and the notes they correspond to.
+    nullifier_map: BTreeMap<Nullifier, Note>,
+    // Notes that we have received.
+    received_set: HashSet<(Note, MemoPlaintext)>,
+    // Notes that we have spent.
+    spent_set: HashSet<Note>,
+}
+
+impl Default for ClientState {
+    fn default() -> Self {
+        Self {
+            first_block_height: 0,
+            last_block_height: 0,
+            note_commitment_tree: merkle::BridgeTree::new(MAX_MERKLE_CHECKPOINTS_CLIENT),
+            nullifier_map: BTreeMap::new(),
+            received_set: HashSet::new(),
+            spent_set: HashSet::new(),
+        }
+    }
+}
+
+impl ClientState {
+    // TODO: For each output in incoming transactions, try to decrypt the note ciphertext.
+    // If the note decrypts, we:
+    // * add the (note plaintext, memo) to the received_set.
+    // * compute and add the nullifier to the nullifier map.
+    // * add the note commitment to the note commitment tree.
+    // * witness the note commitment value.
+    //
+    // For each spend, if the revealed nf is in our nullifier_map, then
+    // we add nullifier_map[nf] to spent_set.
 }
 
 #[tokio::main]
