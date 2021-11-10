penumbra-zone/penumbra

commit 699ed665164322323b7ba0d62ebce2605d765b12

Replace `assert_eq!` using literal bool with a plain `assert!`

penumbra/src/app.rs
@@ -262,6 +262,6 @@ mod tests {
             .finalize(&mut rng);
 
         // The merkle path is invalid, so this transaction should not verify.
-        assert_eq!(app.verify_transaction(transaction.unwrap()), false);
+        assert!(app.verify_transaction(transaction.unwrap()));
     }
 }

commit 24d705e010487d2a6714b64888eecc1f19654e7f

Fix accidentally inverted assertion
This was a refactor suggested by clippy which I didn't read carefully enough. Bug introduced in
699ed665164322323b7ba0d62ebce2605d765b12.

penumbra/src/app.rs
@@ -262,6 +262,6 @@ mod tests {
             .finalize(&mut rng);
 
         // The merkle path is invalid, so this transaction should not verify.
-        assert!(app.verify_transaction(transaction.unwrap()));
+        assert!(!app.verify_transaction(transaction.unwrap()));
     }
 }