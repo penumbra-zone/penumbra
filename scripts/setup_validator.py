71b3eee523691c920d60b43fc70c61a38538d8dd
Chris Czub <chris@penumbra.zone>
2021-11-29 14:02:57 -0500

    Genesis loading

--- scripts/setup_validator.py
+++ scripts/setup_validator.py
@@ -2,9 +2,45 @@
 
 import json
 
-# This script will patch an existing genesis.json file
-# with hardcoded genesis notes for ease of spinning up nodes.
+import docker
+
+
+# This script will handle configuring a Penumbra docker-compose validator deployment
+# by initializing the Tendermint node and patching the genesis.json (stored in the
+# Docker volume).
 def main():
+    client = docker.from_env()
+
+    # First initialize the validator (no change if already exists)
+    init_validator(client)
+
+    patch_genesis(client)
+
+
+# This method will run the Tendermint validator initialization,
+# creating keys and configuration files.
+def init_validator(client: docker.DockerClient):
+    print(
+        client.containers.run(
+            "penumbra_tendermint", "init validator", stderr=True
+        ).decode("utf-8")
+    )
+
+
+# This method will patch an existing genesis.json file
+# with hardcoded genesis notes for ease of spinning up nodes.
+def patch_genesis(client: docker.DockerClient):
+    # Copy the genesis file to the local machine...
+    print(
+        client.containers.run(
+            "penumbra_tendermint",
+            "/tendermint/config/genesis.json",
+            entrypoint="cat",
+            stderr=True,
+        ).decode("utf-8")
+    )
+    return
+
     with open("/tendermint/config/genesis.json") as f:
         existing_genesis = json.load(f)
 
