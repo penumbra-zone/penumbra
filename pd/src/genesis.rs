cd6e512b9102c59672fdd664c302e6a2cee013cb
Kenny Foner <kwf@very.science>
2021-12-08 18:25:49 -0500

    Fix old call to `Note::new`

--- pd/src/genesis.rs
+++ pd/src/genesis.rs
@@ -116,7 +116,7 @@ impl TryFrom<&GenesisNote> for Note {
         let transmission_key = ka::Public(genesis_note.transmission_key);
         let diversifier = Diversifier(genesis_note.diversifier);
 
-        let note = Note::new(
+        let note = Note::from_parts(
             diversifier,
             transmission_key,
             Value {
