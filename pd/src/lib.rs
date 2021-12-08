4d0568b3cef98233bd882c2ec53cf171d257809d
Henry de Valence <hdevalence@penumbra.zone>
2021-12-07 17:32:30 -0800

    pd: consistent spans for ABCI request processing
    Previously, we had a bunch of ad-hoc spans created with `#[instrument]`,
    leading to inconsistent and less-than-helpful debug info.  This commit adds a
    `RequestExt` trait with a `create_span` method that allows us to create spans
    consistently, then uses it to instrument ABCI requests and removes all the
    other spans.

--- pd/src/lib.rs
+++ pd/src/lib.rs
@@ -4,16 +4,16 @@ mod app;
 mod db;
 mod pd_metrics;
 mod pending_block;
+mod request_ext;
 mod state;
 mod verify;
 mod wallet;
 
-pub use pd_metrics::register_all_metrics;
-
 pub mod genesis;
-
-pub use pending_block::PendingBlock;
-pub use state::State;
 
 pub use app::App;
 pub use genesis::GenesisNote;
+pub use pd_metrics::register_all_metrics;
+pub use pending_block::PendingBlock;
+pub use request_ext::RequestExt;
+pub use state::State;
