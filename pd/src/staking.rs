a888681592a5bf03c5928405864a0384859e5e02
Ava Howell <ava@penumbra.zone>
2021-12-09 13:23:07 -0800

    update docstrings

--- pd/src/staking.rs
+++ pd/src/staking.rs
@@ -7,20 +7,19 @@ const PENUMBRA_BECH32_VALIDATOR_PREFIX: &str = "penumbravalpub";
 /// voting power.
 #[derive(Deserialize, Serialize, Debug, Eq, Clone)]
 pub struct Validator {
-    /// tm_pubkey is the validator's long-term Tendermint public key, where the private component
+    /// The validator's long-term Tendermint public key, where the private component
     /// is used to sign blocks.
     tm_pubkey: PublicKey,
 
-    /// voting_power is the validator's current voting power.
+    /// The validator's current voting power.
     pub voting_power: vote::Power,
 
-    /// commission_address is the validator's shielded commission address, where they receive their
-    /// portion of the staking rewards.
+    /// The validator's shielded commission address, where they receive their portion of the
+    /// staking rewards.
     #[serde(with = "serde_with::rust::display_fromstr")]
     pub commission_address: Address,
 
-    /// commission_rate is the portion of staking rewards that go to the validator (as opposed to
-    /// the delegators).
+    /// The portion of staking rewards that go to the validator (as opposed to the delegators).
     pub commission_rate_bps: u16,
     // TODO: track unclaimed reward
 }
