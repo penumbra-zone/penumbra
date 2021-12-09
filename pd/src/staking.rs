1a95a476eba35e215c9bd1e627664641db8ec92a
Ava Howell <ava@penumbra.zone>
2021-12-08 17:13:09 -0800

    add commission address, comission rate, unclaimed reward fields to validator

--- pd/src/staking.rs
+++ pd/src/staking.rs
@@ -1,12 +1,30 @@
+use num::bigint;
+use num::rational;
+use penumbra_crypto::{Address, Value};
 use tendermint::{vote, PublicKey};
 
 const PENUMBRA_BECH32_VALIDATOR_PREFIX: &str = "penumbravalpub";
 /// Validator tracks the Penumbra validator's long-term consensus key (tm_pubkey), as well as their
 /// voting power.
-#[derive(Debug, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct Validator {
+    /// tm_pubkey is the validator's long-term Tendermint public key, where the private component
+    /// is used to sign blocks.
     tm_pubkey: PublicKey,
+
+    /// voting_power is the validator's current voting power.
     pub voting_power: vote::Power,
+
+    /// commission_address is the validator's shielded commission address, where they receive their
+    /// portion of the staking rewards.
+    pub commission_address: Address,
+
+    /// commission_rate is the portion of staking rewards that go to the validator (as opposed to
+    /// the delegators).
+    pub commision_rate: rational::Ratio<bigint::BigInt>,
+
+    /// unclaimed_reward is the amount of commission that the validator has yet to claim.
+    pub unclaimed_reward: Value,
 }
 
 impl PartialEq for Validator {
