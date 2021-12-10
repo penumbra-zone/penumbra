de22afde65a053cd698adb50a3c476f5082845c1
redshiftzero <jen@penumbra.zone>
2021-12-10 14:30:03 -0500

    fix: remove unclaimed_rewards field from validators table (not used)

--- pd/src/db/validators.sql
+++ pd/src/db/validators.sql
@@ -2,6 +2,5 @@ CREATE TABLE IF NOT EXISTS validators (
     tm_pubkey bytea NOT NULL PRIMARY KEY,
     voting_power bigint NOT NULL,
     commission_address varchar NOT NULL,
-    commission_rate bigint NOT NULL,
-    unclaimed_reward bytea NOT NULL
+    commission_rate bigint NOT NULL
 )
