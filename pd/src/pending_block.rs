dbd9657b81d8f14e5a3700ce8fa9d9b0097dfd50
Henry de Valence <hdevalence@penumbra.zone>
2021-12-10 11:47:03 -0800

    pd: obtain latest NCT position without requiring an auth path
    Previously, we tried to get a hold of the position by getting a full
    authentication path, which is only possible for witnessed leaves.
    
    This invocation might be useful to upstream as a method on BridgeTree?

--- pd/src/pending_block.rs
+++ pd/src/pending_block.rs
@@ -2,7 +2,7 @@ use std::collections::{BTreeMap, BTreeSet};
 
 use penumbra_crypto::{
     asset,
-    merkle::{Frontier, NoteCommitmentTree, Tree},
+    merkle::{Frontier, NoteCommitmentTree},
     note, Nullifier,
 };
 
@@ -42,18 +42,16 @@ impl PendingBlock {
         for (note_commitment, data) in transaction.new_notes {
             self.note_commitment_tree.append(&note_commitment);
 
-            let (position, _) = self
+            let position = self
                 .note_commitment_tree
-                .authentication_path(&note_commitment)
-                .expect("we just appended this commitment");
+                .bridges()
+                .last()
+                .map(|b| b.frontier().position().into())
+                // If there are no bridges, the tree is empty
+                .unwrap_or(0u64);
 
-            self.notes.insert(
-                note_commitment,
-                PositionedNoteData {
-                    position: u64::from(position),
-                    data,
-                },
-            );
+            self.notes
+                .insert(note_commitment, PositionedNoteData { position, data });
         }
 
         for nullifier in transaction.spent_nullifiers {
