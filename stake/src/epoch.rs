e3efb3c723bf2d255b5035c58c4a6945310bb763
Chris Czub <chris@penumbra.zone>
2021-12-14 17:53:13 -0500

    Expose Epoch type via stake module

--- stake/src/epoch.rs
+++ stake/src/epoch.rs
@@ -1,6 +1,8 @@
+use anyhow::{anyhow, Error};
+
 /// Epoch represents a given epoch for Penumbra and is used
 /// for calculation of staking exchange rates.
-#[derive(Debug, Eq)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct Epoch {
     epoch: u64,
 }
@@ -10,13 +12,14 @@ impl Epoch {
     /// block height. Due to the implementation in tendermint using
     /// signed representation for block height, we provide this
     /// as well as an unsigned implemention (`from_blockheight_unsigned`)
-    pub fn from_blockheight(block_height: i64, epoch_duration: u64) -> Result<Self> {
+    pub fn from_blockheight(block_height: i64, epoch_duration: u64) -> Result<Self, Error> {
         if block_height < 0 {
-            return Err(anyhow::anyhow!("block height should never be negative"));
+            return Err(anyhow!("block height should never be negative"));
         }
 
         Ok(Epoch::from_blockheight_unsigned(
             block_height.unsigned_abs(),
+            epoch_duration,
         ))
     }
 
--- stake/src/lib.rs
+++ stake/src/lib.rs
@@ -1,5 +1,7 @@
+mod epoch;
 mod funding_stream;
 mod validator;
 
+pub use epoch::Epoch;
 pub use funding_stream::FundingStream;
 pub use validator::Validator;
