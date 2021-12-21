3c268e3a1204872cea12e088cde07455b0337c40
Henry de Valence <hdevalence@penumbra.zone>
2021-12-20 19:03:44 -0800

    pcli: clean up options for tx send
    This removes all of the short options (they're already short, so this just
    increases readability) and renames `source_address_id` to `from` for clarity.

--- pcli/src/opt.rs
+++ pcli/src/opt.rs
@@ -129,18 +129,18 @@ pub enum TxCmd {
     /// Send transaction to the node.
     Send {
         /// The destination address to send funds to.
-        #[structopt(short, long)]
+        #[structopt(long)]
         to: String,
         /// The amounts to send, written as typed values 1.87penumbra, 12cubes, etc.
         values: Vec<String>,
         /// The transaction fee (paid in upenumbra).
-        #[structopt(short, long, default_value = "0")]
+        #[structopt(long, default_value = "0")]
         fee: u64,
-        /// Optional. Only spend funds originally received by a specific address.
-        #[structopt(short, long)]
-        source_address_id: Option<u64>,
+        /// Optional. Only spend funds originally received by the given address index.
+        #[structopt(long)]
+        from: Option<u64>,
         /// Optional. Set the transaction's memo field to the provided text.
-        #[structopt(short, long)]
+        #[structopt(long)]
         memo: Option<String>,
     },
 }
