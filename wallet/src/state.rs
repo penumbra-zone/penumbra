1499fec34cd44f4ea871576902a784baa8603556
plaidfinch <finch@plaidfinch.net>
2021-12-25 12:12:49 -0600

    Fix errors in state helpers

--- wallet/src/state.rs
+++ wallet/src/state.rs
@@ -1,17 +1,22 @@
 use std::{
     collections::{BTreeMap, HashMap},
+    future::Future,
     mem,
+    pin::Pin,
     time::{Duration, SystemTime},
 };
 
 use anyhow::Context;
+use futures::stream::StreamExt;
 use penumbra_crypto::{
     asset::{self, Denom},
     memo,
     merkle::{Frontier, NoteCommitmentTree, Tree, TreeExt},
-    note, Address, FieldExt, Note, Nullifier, Transaction, Value, CURRENT_CHAIN_ID,
+    note, Address, Note, Nullifier, Transaction, Value, CURRENT_CHAIN_ID,
 };
-use penumbra_proto::light_wallet::{CompactBlock, StateFragment};
+use penumbra_proto::light_wallet::{
+    light_wallet_client::LightWalletClient, CompactBlock, CompactBlockRangeRequest, StateFragment,
+};
 use rand::seq::SliceRandom;
 use rand_core::{CryptoRng, RngCore};
 use serde::{Deserialize, Serialize};
@@ -570,6 +575,50 @@ impl ClientState {
         self.last_block_height = Some(height);
         tracing::debug!(self.last_block_height, "finished scanning block");
 
+        Ok(())
+    }
+
+    #[instrument(skip(self, for_each_block), fields(start_height = self.last_block_height()))]
+    pub async fn sync<F, Fut>(
+        &mut self,
+        wallet_uri: String,
+        mut for_each_block: F,
+    ) -> anyhow::Result<()>
+    where
+        F: FnMut(bool, u32, &Self) -> Fut,
+        Fut: Future<Output = anyhow::Result<()>>,
+    {
+        tracing::info!("starting wallet sync");
+        let mut client = LightWalletClient::connect(wallet_uri).await?;
+
+        let start_height = self.last_block_height().map(|h| h + 1).unwrap_or(0);
+        let mut stream = client
+            .compact_block_range(tonic::Request::new(CompactBlockRangeRequest {
+                start_height,
+                end_height: 0,
+            }))
+            .await?
+            .into_inner()
+            .peekable();
+
+        while let Some(result) = stream.next().await {
+            let block = result?;
+            let height = block.height;
+            self.scan_block(block)?;
+
+            // On the final block, prune the timeouts before invoking the callback
+            let final_block = Pin::new(&mut stream).peek().await.is_none();
+            if final_block {
+                self.prune_timeouts();
+            }
+
+            // Invoke the callback, passing it a bool indicating whether or not this is the final
+            // block, the height of the block, and `&self`, which enables checkpointing the wallet
+            // during synchronization in a client-determined way
+            for_each_block(final_block, height, self).await?;
+        }
+
+        tracing::info!(end_height = ?self.last_block_height().unwrap(), "finished wallet sync");
         Ok(())
     }
 }
