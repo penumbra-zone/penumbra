5493eff79691b54a12962a0a32bc32519d420b8a
Henry de Valence <hdevalence@penumbra.zone>
2022-01-03 15:04:25 -0800

    pcli: stub work on delegate command
    Co-Authored-By: Ava Howell <ava@penumbra.zone>

--- pcli/src/command/stake.rs
+++ pcli/src/command/stake.rs
@@ -54,6 +54,12 @@ pub enum StakeCmd {
     },
     /// Display this wallet's delegations and their value.
     Show,
+    /// Display all of the validators participating in the chain.
+    ListValidators {
+        /// Whether to show validators that are not currently part of the consensus set.
+        #[structopt(long)]
+        show_inactive: bool,
+    },
 }
 
 impl StakeCmd {
@@ -63,7 +69,30 @@ impl StakeCmd {
 
     pub async fn exec(&self, _opt: &Opt, state: &ClientState) -> Result<()> {
         match self {
-            StakeCmd::Delegate { .. } => {
+            StakeCmd::Delegate {
+                to,
+                amount,
+                fee,
+                source,
+            } => {
+                let unbonded_amount = amount;
+                // Steps:
+                //
+                // - parse amount, check that it's staking token
+                // - parse the to address, check that it's a validator identity
+                // - check that we have at least `amount` of staking token to spend
+                // - obtain the rate data for the current epoch
+                // - use the rata data to compute the correct amount of delegation token
+                // - construct a spend description that releases unbonded_amount + fee
+                // - construct a delegate description that consumes unbonded_amount staking token, produces delegation_amount of delegation token
+                // - construct an output description that records the new delegation tokens
+                // - construct an output description that records the change
+                // - finalize transaction
+                // - submit a transaction to the rpc endpoint
+
+                // outsource computation to staking crate? wallet crate?
+                // overlap with tx command?
+
                 todo!()
             }
             StakeCmd::Undelegate { .. } => {
@@ -73,6 +102,9 @@ impl StakeCmd {
                 todo!()
             }
             StakeCmd::Show => {
+                todo!()
+            }
+            StakeCmd::ListValidators { .. } => {
                 todo!()
             }
         }
