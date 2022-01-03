6b0d0f0220fc6b05526a9874422990bbc4a622c4
Henry de Valence <hdevalence@penumbra.zone>
2022-01-02 23:00:20 -0800

    stake: update DelegationToken to use IdentityKey

--- stake/src/token.rs
+++ stake/src/token.rs
@@ -2,27 +2,23 @@ use std::str::FromStr;
 
 use penumbra_crypto::asset;
 use regex::Regex;
-use tendermint::PublicKey;
 
-use crate::VALIDATOR_IDENTITY_BECH32_PREFIX;
+use crate::IdentityKey;
 
 /// Delegation tokens represent a share of a particular validator's delegation pool.
 pub struct DelegationToken {
-    validator_pubkey: PublicKey,
+    validator_identity: IdentityKey,
     base_denom: asset::Denom,
 }
 
 impl DelegationToken {
-    pub fn new(pk: PublicKey) -> Self {
+    pub fn new(validator_identity: IdentityKey) -> Self {
         // This format string needs to be in sync with the asset registry
         let base_denom = asset::REGISTRY
-            .parse_denom(&format!(
-                "udelegation_{}",
-                pk.to_bech32(VALIDATOR_IDENTITY_BECH32_PREFIX)
-            ))
+            .parse_denom(&format!("udelegation_{}", validator_identity))
             .expect("base denom format is valid");
         DelegationToken {
-            validator_pubkey: pk,
+            validator_identity,
             base_denom,
         }
     }
@@ -43,60 +39,35 @@ impl DelegationToken {
     }
 
     /// Get the identity key of the validator this delegation token is associated with.
-    pub fn validator(&self) -> PublicKey {
-        self.validator_pubkey.clone()
+    pub fn validator(&self) -> IdentityKey {
+        self.validator_identity.clone()
     }
 }
 
 impl TryFrom<asset::Denom> for DelegationToken {
     type Error = anyhow::Error;
-    fn try_from(value: asset::Denom) -> Result<Self, Self::Error> {
-        // Almost all of this code is devoted to parsing Bech32 in the
-        // tendermint-specific way, perhaps we could upstream a Bech32 decoding
-        // function to tendermint-rs
-
-        let (hrp, data, variant) = bech32::decode(
-            // Note: this regex must be in sync with both asset::REGISTRY
-            // and VALIDATOR_IDENTITY_BECH32_PREFIX
+    fn try_from(base_denom: asset::Denom) -> Result<Self, Self::Error> {
+        // Note: this regex must be in sync with both asset::REGISTRY
+        // and VALIDATOR_IDENTITY_BECH32_PREFIX
+        let validator_identity =
             Regex::new("udelegation_(?P<data>penumbravalid1[a-zA-HJ-NP-Z0-9]+)")
                 .expect("regex is valid")
-                .captures(&value.to_string())
+                .captures(&base_denom.to_string())
                 .ok_or_else(|| {
-                    anyhow::anyhow!("base denom {} is not a delegation token", value.to_string())
+                    anyhow::anyhow!(
+                        "base denom {} is not a delegation token",
+                        base_denom.to_string()
+                    )
                 })?
                 .name("data")
                 .expect("data is a named capture")
-                .as_str(),
-        )?;
+                .as_str()
+                .parse()?;
 
-        if variant != bech32::Variant::Bech32 {
-            return Err(anyhow::anyhow!(
-                "delegation token denom uses wrong Bech32 variant"
-            ));
-        }
-        if hrp != VALIDATOR_IDENTITY_BECH32_PREFIX {
-            return Err(anyhow::anyhow!(
-                "wrong Bech32 HRP {}, expected {}",
-                hrp,
-                VALIDATOR_IDENTITY_BECH32_PREFIX
-            ));
-        }
-
-        use bech32::FromBase32;
-        let data = Vec::<u8>::from_base32(&data).expect("bech32 decoding produces valid base32");
-
-        // some custom tendermint thing (????)
-        // see PublicKey::to_bech32 impl
-        if &data[0..5] != &[0x16, 0x24, 0xDE, 0x64, 0x20] {
-            return Err(anyhow::anyhow!(
-                "invalid backward_compatible_amino_prefix bytes"
-            ));
-        }
-
-        Ok(DelegationToken::new(
-            PublicKey::from_raw_ed25519(&data[5..])
-                .ok_or_else(|| anyhow::anyhow!("invalid Ed25519 verification key"))?,
-        ))
+        Ok(Self {
+            base_denom,
+            validator_identity,
+        })
     }
 }
 
@@ -138,16 +109,17 @@ impl std::hash::Hash for DelegationToken {
 
 #[cfg(test)]
 mod tests {
+    use penumbra_crypto::rdsa::{SigningKey, SpendAuth};
+
     use super::*;
 
     #[test]
     fn delegation_token_denomination_round_trip() {
-        use ed25519_consensus::SigningKey;
         use rand_core::OsRng;
 
-        let vk = PublicKey::Ed25519(SigningKey::new(OsRng).verification_key());
+        let ik = IdentityKey(SigningKey::<SpendAuth>::new(OsRng).into());
 
-        let token = DelegationToken::new(vk);
+        let token = DelegationToken::new(ik);
 
         let denom = token.to_string();
         let token2 = DelegationToken::from_str(&denom).unwrap();
