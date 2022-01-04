9abb3119988a9314e5ab4cdc2f0b28652b5cf5cf
Henry de Valence <hdevalence@penumbra.zone>
2022-01-03 16:09:54 -0800

    pcli: stub work on delegate command
    Co-Authored-By: Ava Howell <ava@penumbra.zone>

--- pcli/src/command/stake.rs
+++ pcli/src/command/stake.rs
@@ -1,5 +1,7 @@
-use anyhow::Result;
-use penumbra_stake::IdentityKey;
+use anyhow::{anyhow, Result};
+use penumbra_crypto::Value;
+use penumbra_proto::thin_wallet::{thin_wallet_client::ThinWalletClient, ValidatorRateRequest};
+use penumbra_stake::{Epoch, IdentityKey, RateData, STAKING_TOKEN_ASSET_ID};
 use penumbra_wallet::ClientState;
 use structopt::StructOpt;
 
@@ -67,7 +69,7 @@ impl StakeCmd {
         true
     }
 
-    pub async fn exec(&self, _opt: &Opt, state: &ClientState) -> Result<()> {
+    pub async fn exec(&self, opt: &Opt, state: &ClientState) -> Result<()> {
         match self {
             StakeCmd::Delegate {
                 to,
@@ -75,14 +77,43 @@ impl StakeCmd {
                 fee,
                 source,
             } => {
-                let unbonded_amount = amount;
+                let unbonded_amount = {
+                    let Value { amount, asset_id } = amount.parse::<Value>()?;
+                    if asset_id != *STAKING_TOKEN_ASSET_ID {
+                        return Err(anyhow!("staking can only be done with the staking token"));
+                    }
+                    amount
+                };
+
+                let to = to.parse::<IdentityKey>()?;
+
+                // FIXME! need some kind of structure for recording chain
+                // parameters - connected with having protos for genesis data
+                // (though not all genesis data is chain parameters)
+                let epoch_duration = 10;
+                let current_epoch =
+                    Epoch::from_height(state.last_block_height().unwrap() as u64, epoch_duration);
+
+                let mut client = ThinWalletClient::connect(format!(
+                    "http://{}:{}",
+                    opt.node, opt.thin_wallet_port
+                ))
+                .await?;
+
+                let rate_data: RateData = client
+                    .validator_rate(tonic::Request::new(ValidatorRateRequest {
+                        identity_key: Some(to.into()),
+                        epoch_index: current_epoch.index,
+                    }))
+                    .await?
+                    .into_inner()
+                    .try_into()?;
+
+                let delegation_amount = rate_data.delegation_amount(unbonded_amount);
+
                 // Steps:
                 //
-                // - parse amount, check that it's staking token
-                // - parse the to address, check that it's a validator identity
                 // - check that we have at least `amount` of staking token to spend
-                // - obtain the rate data for the current epoch
-                // - use the rata data to compute the correct amount of delegation token
                 // - construct a spend description that releases unbonded_amount + fee
                 // - construct a delegate description that consumes unbonded_amount staking token, produces delegation_amount of delegation token
                 // - construct an output description that records the new delegation tokens
