eb828f51a1fcf44fa38512a11ce782fcbe7fc773
Henry de Valence <hdevalence@penumbra.zone>
2022-01-03 16:08:51 -0800

    stake: add constants for the staking token
    Co-Authored-By: Ava Howell <ava@penumbra.zone>

--- stake/src/lib.rs
+++ stake/src/lib.rs
@@ -1,3 +1,6 @@
+use once_cell::sync::Lazy;
+use penumbra_crypto::asset;
+
 mod delegate;
 mod epoch;
 mod funding_stream;
@@ -20,3 +23,10 @@ pub use validator::{Validator, ValidatorDefinition};
 pub const VALIDATOR_CONSENSUS_BECH32_PREFIX: &str = "penumbravalconspub";
 
 pub use penumbra_proto::serializers::bech32str::validator_identity_key::BECH32_PREFIX as VALIDATOR_IDENTITY_BECH32_PREFIX;
+
+// TODO: go through the source tree and use these instead of hardcoding "upenumbra"
+
+pub static STAKING_TOKEN_DENOM: Lazy<asset::Denom> =
+    Lazy::new(|| asset::REGISTRY.parse_denom("upenumbra").unwrap());
+pub static STAKING_TOKEN_ASSET_ID: Lazy<asset::Id> =
+    Lazy::new(|| asset::REGISTRY.parse_denom("upenumbra").unwrap().id());
