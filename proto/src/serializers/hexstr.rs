4bae38c06716edadbdd918f8511d6ff9bb45c153
Kenny Foner <kwf@very.science>
2022-01-12 16:16:53 -0500

    Undo removal of AS_HEX, fix actual root problem
    The root problem was deserializing as `Option<String>` instead of `String` in the `deserialize`
    function of `hexstr`.

--- proto/src/serializers/hexstr.rs
+++ proto/src/serializers/hexstr.rs
@@ -5,7 +5,7 @@ pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let string = Option::<String>::deserialize(deserializer)?.unwrap_or_default();
+    let string = String::deserialize(deserializer)?;
     hex::decode(&string).map_err(serde::de::Error::custom)
 }
 
