9e6c2bbf49da374061d62d32f82ef42423d1557d
Henry de Valence <hdevalence@penumbra.zone>
2022-01-19 02:08:57 -0800

    pcli: implement stake show

--- pcli/src/command/stake.rs
+++ pcli/src/command/stake.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use anyhow::{anyhow, Context, Result};
 use comfy_table::{presets, Table};
 use futures::stream::TryStreamExt;
@@ -5,6 +7,7 @@ use penumbra_crypto::Value;
 use penumbra_proto::{light_wallet::ValidatorInfoRequest, thin_wallet::ValidatorRateRequest};
 use penumbra_stake::{
     DelegationToken, Epoch, IdentityKey, RateData, ValidatorInfo, STAKING_TOKEN_ASSET_ID,
+    STAKING_TOKEN_DENOM,
 };
 use rand_core::OsRng;
 use structopt::StructOpt;
@@ -170,7 +173,99 @@ impl StakeCmd {
                 todo!()
             }
             StakeCmd::Show => {
-                todo!()
+                let mut client = opt.light_wallet_client().await?;
+
+                let validators = client
+                    .validator_info(ValidatorInfoRequest {
+                        show_inactive: true,
+                    })
+                    .await?
+                    .into_inner()
+                    .try_collect::<Vec<_>>()
+                    .await?
+                    .into_iter()
+                    .map(TryInto::try_into)
+                    .collect::<Result<Vec<ValidatorInfo>, _>>()?;
+
+                let notes = state.unspent_notes_by_denom_and_address();
+                let mut total = 0;
+
+                let mut table = Table::new();
+                table.load_preset(presets::NOTHING);
+                table.set_header(vec!["Name", "Value", "Exch. Rate", "Tokens"]);
+                table
+                    .get_column_mut(1)
+                    .unwrap()
+                    .set_cell_alignment(comfy_table::CellAlignment::Right);
+
+                for (denom, notes_by_address) in notes.iter() {
+                    let dt = if let Ok(dt) = DelegationToken::try_from(denom.clone()) {
+                        dt
+                    } else {
+                        continue;
+                    };
+
+                    let info = validators
+                        .iter()
+                        .find(|v| v.validator.identity_key == dt.validator())
+                        .unwrap();
+
+                    let delegation = Value {
+                        amount: notes_by_address
+                            .values()
+                            .flat_map(|notes| notes.iter().map(|n| n.as_ref().amount()))
+                            .sum::<u64>(),
+                        asset_id: dt.id(),
+                    };
+
+                    let unbonded = Value {
+                        amount: info.rate_data.unbonded_amount(delegation.amount),
+                        asset_id: *STAKING_TOKEN_ASSET_ID,
+                    };
+
+                    let rate = info.rate_data.validator_exchange_rate as f64 / 1_0000_0000.0;
+
+                    table.add_row(vec![
+                        info.validator.name.clone(),
+                        unbonded.try_format(state.asset_cache()).unwrap(),
+                        format!("{:.4}", rate),
+                        delegation.try_format(state.asset_cache()).unwrap(),
+                    ]);
+
+                    total += unbonded.amount;
+                }
+
+                let unbonded = Value {
+                    amount: notes
+                        .get(&*STAKING_TOKEN_DENOM)
+                        .unwrap_or(&BTreeMap::default())
+                        .values()
+                        .flat_map(|notes| notes.iter().map(|n| n.as_ref().amount()))
+                        .sum::<u64>(),
+                    asset_id: *STAKING_TOKEN_ASSET_ID,
+                };
+
+                total += unbonded.amount;
+
+                table.add_row(vec![
+                    "Unbonded Stake".to_string(),
+                    unbonded.try_format(state.asset_cache()).unwrap(),
+                    format!("{:.4}", 1.0),
+                    unbonded.try_format(state.asset_cache()).unwrap(),
+                ]);
+
+                let total = Value {
+                    amount: total,
+                    asset_id: *STAKING_TOKEN_ASSET_ID,
+                };
+
+                table.add_row(vec![
+                    "Total".to_string(),
+                    total.try_format(state.asset_cache()).unwrap(),
+                    String::new(),
+                    String::new(),
+                ]);
+                println!("{}", table);
             }
             StakeCmd::ListValidators {
                 show_inactive,
