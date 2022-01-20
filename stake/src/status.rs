d3134e9d1ff9321e257322f25a89e8f942f4e6de
Kenny Foner <kwf@very.science>
2022-01-20 17:01:52 -0500

    Sort out validator status serialization

--- stake/src/status.rs
+++ stake/src/status.rs
@@ -3,60 +3,33 @@ use serde::{Deserialize, Serialize};
 
 use crate::IdentityKey;
 
-#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
-#[serde(try_from = "pb::ValidatorState", into = "pb::ValidatorState")]
-pub enum ValidatorState {
-    Inactive,
-    Active,
-    Unbonding,
-    Slashed,
-}
-
-impl Protobuf<pb::validator_status::ValidatorState> for ValidatorState {}
-
-impl From<ValidatorState> for pb::validator_status::ValidatorState {
-    fn from(msg: ValidatorState) -> Self {
-        match msg {
-            ValidatorState::Inactive => pb::validator_status::ValidatorState {
-                validator_state: Some(pb::validator_status::ValidatorState::Inactive),
-            },
-            ValidatorState::Active => pb::validator_status::ValidatorState {
-                validator_state: Some(pb::validator_status::ValidatorState::Active),
-            },
-            ValidatorState::Unbonding => pb::validator_status::ValidatorState {
-                validator_state: Some(pb::validator_status::ValidatorState::Unbonding),
-            },
-            ValidatorState::Slashed => pb::validator_status::ValidatorState {
-                validator_state: Some(pb::validator_status::ValidatorState::Slashed),
-            },
-        }
-    }
-}
-
-impl TryFrom<pb::validator_status::ValidatorState> for ValidatorState {
-    type Error = anyhow::Error;
-
-    fn try_from(proto: pb::validator_status::ValidatorState) -> anyhow::Result<Self, Self::Error> {
-        if proto.validator_state.is_none() {
-            return Err(anyhow::anyhow!("missing validator_state content"));
-        }
-
-        match proto.validator_state.unwrap() {
-            pb::validator_status::ValidatorState::Inactive => Ok(ValidatorState::Inactive),
-            pb::validator_status::ValidatorState::Active => Ok(ValidatorState::Active),
-            pb::validator_status::ValidatorState::Unbonding => Ok(ValidatorState::Unbonding),
-            pb::validator_status::ValidatorState::Slashed => Ok(ValidatorState::Slashed),
-        }
-    }
-}
-
+/// The current status of a validator, including its identity, voting power, and state in the
+/// validator state machine.
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 #[serde(try_from = "pb::ValidatorStatus", into = "pb::ValidatorStatus")]
 pub struct ValidatorStatus {
+    /// The validator's identity.
     pub identity_key: IdentityKey,
+    /// The validator's voting power.
     pub voting_power: u64,
+    /// The validator's current state.
     pub state: ValidatorState,
-    pub unbonding_epoch: Option<u64>,
+}
+
+/// The state of a validator in the validator state machine.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum ValidatorState {
+    /// The validator is not currently a part of the consensus set, but could become so if it
+    /// acquired enough voting power.
+    Inactive,
+    /// The validator is an active part of the consensus set.
+    Active,
+    /// The validator has been removed from the consensus set, and all stake will finish unbonding
+    /// at the epoch `unbonding_epoch`.
+    Unbonding { unbonding_epoch: u64 },
+    /// The validator has been slashed, and undelegations will occur immediately with no unbonding
+    /// period.
+    Slashed,
 }
 
 impl Protobuf<pb::ValidatorStatus> for ValidatorStatus {}
@@ -66,8 +39,16 @@ impl From<ValidatorStatus> for pb::ValidatorStatus {
         pb::ValidatorStatus {
             identity_key: Some(v.identity_key.into()),
             voting_power: v.voting_power,
-            state: Some(v.state.into()),
-            unbonding_epoch: v.unbonding_epoch,
+            state: match v.state {
+                ValidatorState::Inactive => pb::validator_status::ValidatorState::Inactive,
+                ValidatorState::Active => pb::validator_status::ValidatorState::Active,
+                ValidatorState::Unbonding { .. } => pb::validator_status::ValidatorState::Unbonding,
+                ValidatorState::Slashed => pb::validator_status::ValidatorState::Slashed,
+            } as i32,
+            unbonding_epoch: match v.state {
+                ValidatorState::Unbonding { unbonding_epoch } => Some(unbonding_epoch),
+                _ => None,
+            },
         }
     }
 }
@@ -75,15 +56,17 @@ impl From<ValidatorStatus> for pb::ValidatorStatus {
 impl TryFrom<pb::ValidatorStatus> for ValidatorStatus {
     type Error = anyhow::Error;
     fn try_from(v: pb::ValidatorStatus) -> Result<Self, Self::Error> {
-        let state = match v.state.unwrap() {
-            pb::ValidatorState { validator_state } => match validator_state.unwrap() {
-                pb::validator_state::ValidatorState::Inactive => ValidatorState::Inactive,
-                pb::validator_state::ValidatorState::Active => ValidatorState::Active,
-                pb::validator_state::ValidatorState::Unbonding => {
-                    ValidatorState::Unbonding { epoch_index }
-                }
-                pb::validator_state::ValidatorState::Slashed => ValidatorState::Slashed,
+        let state = match pb::validator_status::ValidatorState::from_i32(v.state)
+            .ok_or_else(|| anyhow::anyhow!("missing validator state"))?
+        {
+            pb::validator_status::ValidatorState::Inactive => ValidatorState::Inactive,
+            pb::validator_status::ValidatorState::Active => ValidatorState::Active,
+            pb::validator_status::ValidatorState::Unbonding => ValidatorState::Unbonding {
+                unbonding_epoch: v
+                    .unbonding_epoch
+                    .ok_or_else(|| anyhow::anyhow!("missing unbonding epoch"))?,
             },
+            pb::validator_status::ValidatorState::Slashed => ValidatorState::Slashed,
         };
 
         Ok(ValidatorStatus {
