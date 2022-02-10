44ab43f62bafa861608ac3f2e6deabb456c43983
redshiftzero <jen@penumbra.zone>
2022-02-10 17:02:50 -0500

    feat(metrics): add counter of notes

--- pd/src/pd_metrics.rs
+++ pd/src/pd_metrics.rs
@@ -3,5 +3,12 @@ use metrics::register_counter;
 /// Registers all metrics tracked by `pd`.
 pub fn register_all_metrics() {
     register_counter!("node_spent_nullifiers_total");
+    register_counter!("node_notes_total");
     register_counter!("node_transactions_total");
+}
+
+/// Represents a bundle of structured metrics data.
+pub struct MetricsData {
+    pub nullifier_count: u64,
+    pub note_count: u64,
 }
