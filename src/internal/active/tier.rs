7e423544efadc4836903b530fec1d4034c4bab21
plaidfinch <finch@plaidfinch.net>
2022-03-16 11:34:27 -0400

    Size-check Tier rather than Inner

--- src/internal/active/tier.rs
+++ src/internal/active/tier.rs
@@ -321,6 +321,6 @@ mod test {
 
     #[test]
     fn check_inner_size() {
-        static_assertions::assert_eq_size!(Inner<crate::Item>, [u8; 56]);
+        static_assertions::assert_eq_size!(Tier<Tier<Tier<crate::Item>>>, [u8; 64]);
     }
 }
