9323f72b63872d42851941fdc40a4eafcc281cad
Henry de Valence <hdevalence@penumbra.zone>
2022-03-17 23:16:05 -0700

    Sketch an application/component interface.
    Notes from Discord are below -- though the actual design ended up being
    slightly different, handling init_chain as a special case, and splitting out
    separate check_tx_stateless and check_tx_stateful methods.
    
    Another addition to the design in the notes is the WriteOverlayExt trait; if
    we're encoding data into the tree, it's much easier if we encode it using our
    existing, proto-based encoding framework.
    
    > some thoughts on a new design for our state handling:
    > as a prerequisite, we'd shift all of our persistent state to be recorded in a rocksdb-backed jellyfish tree, eliminating postgres entirely
    > this has a bunch of advantages, but one really important one is that it means that our apphashes are actually hashes of the entire application state, which improves replicability. another is that we don't have to worry about latency for state queries as much.
    > next, we'd restructure the penumbra application logic as an Application containing a bunch of Components, and a jmt::WriteOverlay
    > https://rustdoc.penumbra.zone/main/jmt/struct.WriteOverlay.html
    > the WriteOverlay is a wrapper over a JMT that maintains a cache of pending writes, and reads through that cache, so that it reflects the pending changes.  it also has a commit method that takes a writer and commits all the pending writes to the backing store.
    > each Component shares access to its application's WriteOverlay (how? maybe a RefCell, doesn't really matter), and uses that for reading and writing application state
    > and each Component is responsible for doing some area of the application logic, like the shielded pool, ibc, staking, etc.
    > there'd be a trait Component that has something like the following methods:
    > - check_tx(&self, tx: &Transaction) -> Result<(), Error>: this performs all transaction checks relevant to that component against the current state;
    > - begin_block(&mut self, begin_block: &BeginBlock): executes any necessary steps as part of beginning a block;
    > - execute_tx(&mut self, tx: &Transaction): executes the transaction against the current state. this is infallible (errors should panic), and unlike our current deliver_tx functionality, it doesn't do any checking. rather, we'd implement deliver_tx as a check_tx(); execute_tx() sequence;
    > - end_block(&mut self, end_block: &EndBlock): executes any necessary steps as part of ending a block
    > notice that there's no commit anywhere, because the Components aren't responsible for managing when stuff is committed, they just have to write it to the shared WriteOverlay, and actually committing the state just requires having the Application call self.overlay.commit() and return the result
    > now, the thing that's really cool about this is that there is no requirement for the Application to be unique!!! (edited)
    > because the components get all of their state from the write overlay, and the write overlay is a wrapper around the persistent state, you can create multiple Application instances, and do stuff to them independently, as long as only one of them is actually committing changes to the persistent state
    > and this means that we can have total code reuse between the mempool (pre-consensus transaction handling) and the consensus worker
    > how does that work? well, both the consensus worker and the mempool have separate Application instances, so they have different sets of un-committed state on top of some common finalized state.
    > when transactions go to the mempool with check_tx, the mempool uses check_tx to check the proposed transaction against the mempool state, then execute_tx to execute it, writing to its un-committed overlay on top of the finalized state. later, when the worker commits a new block, it notifies the mempool, which wipes its Application copy and creates a new one (edited)
    > this means that we don't need to have any special casing of "is there a duplicate nullifier?" "what about duplicate validator definitions?" "what about ...." in the mempool

--- pd/src/components/shielded_pool.rs
+++ pd/src/components/shielded_pool.rs
@@ -1,1 +1,43 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use penumbra_transaction::Transaction;
+use tendermint::abci;
 
+use super::{Component, Overlay};
+use crate::genesis;
+
+// Stub component
+pub struct ShieldedPool {
+    overlay: Overlay,
+}
+
+#[async_trait]
+impl Component for ShieldedPool {
+    fn new(overlay: Overlay) -> Self {
+        Self { overlay }
+    }
+
+    fn init_chain(&self, _app_state: &genesis::AppState) {
+        todo!()
+    }
+
+    async fn begin_block(&self, _begin_block: &abci::request::BeginBlock) {
+        todo!()
+    }
+
+    fn check_tx_stateless(_tx: &Transaction) -> Result<()> {
+        todo!()
+    }
+
+    async fn check_tx_stateful(&self, _tx: &Transaction) -> Result<()> {
+        todo!()
+    }
+
+    async fn execute_tx(&self, _tx: &Transaction) {
+        todo!()
+    }
+
+    async fn end_block(&self, _end_block: &abci::request::EndBlock) {
+        todo!()
+    }
+}
