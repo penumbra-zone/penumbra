9f214c6345981634f1125dfa8d96120aceb1ca69
plaidfinch <finch@plaidfinch.net>
2022-03-25 00:36:26 -0400

    Flesh out spec for epoch and block

--- tct/tests/spec/block.rs
+++ tct/tests/spec/block.rs
@@ -1,11 +1,93 @@
-use hash_hasher::{HashedMap, HashedSet};
+use hash_hasher::HashedMap;
 use penumbra_tct::{
-    block::InsertError, internal::hash::Hash, Commitment, Position, Proof, Witness,
+    block::{Position, Proof},
+    internal::{active::Insert, hash::Hash},
+    Commitment, Witness,
 };
 
-use super::{Tier, Tree};
+use super::{tree::Tree, InsertError, Tier};
 
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct Builder {
-    pub tiers: Tier<Commitment>,
+    pub block: Tier<Commitment>,
+}
+
+impl Builder {
+    pub fn insert(
+        &mut self,
+        witness: Witness,
+        commitment: Commitment,
+    ) -> Result<Position, InsertError> {
+        let insert = match witness {
+            Witness::Keep => Insert::Keep(commitment),
+            Witness::Forget => Insert::Hash(Hash::of(commitment)),
+        };
+
+        // Fail if block is full
+        if self.block.len() >= 4usize.pow(8) {
+            return Err(InsertError::BlockFull);
+        }
+
+        // Insert the item into the block
+        self.block.push_back(insert);
+        // Calculate the item's position
+        let position = self.block.len() as u16 - 1;
+        // Return the position
+        Ok(position.into())
+    }
+
+    pub fn forget(&mut self, commitment: Commitment) -> bool {
+        let mut forgotten = false;
+        for insert_commitment in self.block.iter_mut() {
+            if let Insert::Keep(c) = insert_commitment {
+                if commitment == *c {
+                    *insert_commitment = Insert::Hash(Hash::of(commitment));
+                    forgotten = true;
+                }
+            }
+        }
+        forgotten
+    }
+
+    pub fn build(self) -> Block {
+        let tree = Tree::from_block(self.block);
+        let mut index = HashedMap::default();
+        tree.index_with(|commitment, position| {
+            index.insert(commitment, (position as u16).into());
+        });
+        Block { index, tree }
+    }
+}
+
+pub struct Block {
+    index: HashedMap<Commitment, Position>,
+    tree: Tree,
+}
+
+impl Block {
+    pub fn root(&self) -> Hash {
+        self.tree.root()
+    }
+
+    pub fn position(&self) -> Position {
+        (self.tree.position(8) as u16).into()
+    }
+
+    pub fn witnessed_count(&self) -> usize {
+        self.index.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        if let Tree::Node { ref children, hash } = self.tree {
+            hash == Hash::default() && children.is_empty()
+        } else {
+            false
+        }
+    }
+
+    pub fn witness(&self, commitment: Commitment) -> Option<Proof> {
+        let position = *self.index.get(&commitment)?;
+        let auth_path = self.tree.witness(u16::from(position) as u64);
+        Some(Proof::new(commitment, position, auth_path))
+    }
 }
--- tct/tests/spec/epoch.rs
+++ tct/tests/spec/epoch.rs
@@ -1,11 +1,148 @@
-use hash_hasher::{HashedMap, HashedSet};
+use std::collections::VecDeque;
+
+use hash_hasher::HashedMap;
 use penumbra_tct::{
-    block::InsertError, internal::hash::Hash, Commitment, Position, Proof, Witness,
+    epoch::{Position, Proof},
+    internal::{active::Insert, hash::Hash},
+    Commitment, Witness,
 };
 
-use super::{Tier, Tree};
+use super::{block, tree::Tree, InsertError, Tier};
 
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct Builder {
-    pub tiers: Tier<Tier<Commitment>>,
+    pub epoch: Tier<Tier<Commitment>>,
+}
+
+impl Builder {
+    pub fn insert(
+        &mut self,
+        witness: Witness,
+        commitment: Commitment,
+    ) -> Result<Position, InsertError> {
+        let insert = match witness {
+            Witness::Keep => Insert::Keep(commitment),
+            Witness::Forget => Insert::Hash(Hash::of(commitment)),
+        };
+
+        // Fail if epoch is full
+        if self.epoch.len() >= 4usize.pow(8) {
+            return Err(InsertError::EpochFull);
+        }
+
+        // Ensure epoch is not empty
+        if self.epoch.is_empty() {
+            self.epoch.push_back(Insert::Keep(VecDeque::new()))
+        }
+
+        match self
+            .epoch
+            .back_mut()
+            .expect("a new block is added if tiers are empty")
+        {
+            Insert::Hash(_) => Err(InsertError::BlockForgotten),
+            Insert::Keep(block) => {
+                // Fail if block is full
+                if block.len() >= 4usize.pow(8) {
+                    return Err(InsertError::BlockFull);
+                }
+
+                // Insert the item into the block
+                block.push_back(insert);
+                // Calculate the item's position
+                let position = (block.len() as u32 - 1) | ((self.epoch.len() as u32 - 1) << 16);
+                // Return the position
+                Ok(position.into())
+            }
+        }
+    }
+
+    pub fn forget(&mut self, commitment: Commitment) -> bool {
+        let mut forgotten = false;
+        for insert_block in self.epoch.iter_mut() {
+            if let Insert::Keep(block) = insert_block {
+                for insert_commitment in block.iter_mut() {
+                    if let Insert::Keep(c) = insert_commitment {
+                        if commitment == *c {
+                            *insert_commitment = Insert::Hash(Hash::of(commitment));
+                            forgotten = true;
+                        }
+                    }
+                }
+            }
+        }
+        forgotten
+    }
+
+    pub fn insert_block(&mut self, block: block::Builder) -> Result<(), InsertError> {
+        if self.epoch.len() < 4usize.pow(8) {
+            self.epoch.push_back(Insert::Keep(block.block));
+            Ok(())
+        } else {
+            Err(InsertError::EpochFull)
+        }
+    }
+
+    pub fn insert_block_root(&mut self, block_root: Hash) -> Result<(), InsertError> {
+        if self.epoch.len() < 4usize.pow(8) {
+            self.epoch.push_back(Insert::Hash(block_root));
+            Ok(())
+        } else {
+            Err(InsertError::EpochFull)
+        }
+    }
+
+    pub fn build(self) -> Epoch {
+        let tree = Tree::from_epoch(self.epoch);
+        let mut index = HashedMap::default();
+        tree.index_with(|commitment, position| {
+            index.insert(commitment, (position as u32).into());
+        });
+        Epoch { index, tree }
+    }
+}
+
+pub struct Epoch {
+    index: HashedMap<Commitment, Position>,
+    tree: Tree,
+}
+
+impl Epoch {
+    pub fn root(&self) -> Hash {
+        self.tree.root()
+    }
+
+    pub fn current_block_root(&self) -> Option<Hash> {
+        let mut tree = &self.tree;
+        for _ in 0..8 {
+            if let Tree::Node { children, .. } = tree {
+                tree = children.last()?;
+            } else {
+                return None;
+            }
+        }
+        Some(tree.root())
+    }
+
+    pub fn position(&self) -> Position {
+        (self.tree.position(16) as u32).into()
+    }
+
+    pub fn witnessed_count(&self) -> usize {
+        self.index.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        if let Tree::Node { ref children, hash } = self.tree {
+            hash == Hash::default() && children.is_empty()
+        } else {
+            false
+        }
+    }
+
+    pub fn witness(&self, commitment: Commitment) -> Option<Proof> {
+        let position = *self.index.get(&commitment)?;
+        let auth_path = self.tree.witness(u32::from(position) as u64);
+        Some(Proof::new(commitment, position, auth_path))
+    }
 }
