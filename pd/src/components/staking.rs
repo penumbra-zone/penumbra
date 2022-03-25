66df9bb04cb310989a39b72c2f53a9545c77bdcb
Chris Czub <chris@penumbra.zone>
2022-03-25 16:20:18 -0400

    Add skeleton staking component

---
+++ pd/src/components/staking.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use penumbra_transaction::Transaction;
+use tendermint::abci;
+
+use super::{Component, Overlay};
+use crate::genesis;
+
+// Stub component
+pub struct Staking {
+    overlay: Overlay,
+}
+
+#[async_trait]
+impl Component for Staking {
+    async fn new(overlay: Overlay) -> Result<Self> {
+        Ok(Self { overlay })
+    }
+
+    fn init_chain(&mut self, _app_state: &genesis::AppState) -> Result<()> {
+        todo!()
+    }
+
+    async fn begin_block(&mut self, _begin_block: &abci::request::BeginBlock) -> Result<()> {
+        todo!()
+    }
+
+    fn check_tx_stateless(_tx: &Transaction) -> Result<()> {
+        todo!()
+    }
+
+    async fn check_tx_stateful(&self, _tx: &Transaction) -> Result<()> {
+        todo!()
+    }
+
+    async fn execute_tx(&mut self, _tx: &Transaction) -> Result<()> {
+        todo!()
+    }
+
+    async fn end_block(&mut self, _end_block: &abci::request::EndBlock) -> Result<()> {
+        todo!()
+    }
+}
