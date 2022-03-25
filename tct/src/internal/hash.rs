765b990a596663ff6fb67682fd5820b99a414aee
plaidfinch <finch@plaidfinch.net>
2022-03-25 13:12:02 -0400

    Add more docs to spec and use actual roots

--- tct/src/internal/hash.rs
+++ tct/src/internal/hash.rs
@@ -63,8 +63,8 @@ impl<T: GetHash> GetHash for &mut T {
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Default, Serialize, Deserialize)]
 /// The hash of an individual item, tree root, or intermediate node.
+#[derive(Clone, Copy, PartialEq, Eq, Default, Serialize, Deserialize)]
 pub struct Hash(#[serde(with = "crate::serialize::fq")] pub(crate) Fq);
 
 impl From<Hash> for Fq {
