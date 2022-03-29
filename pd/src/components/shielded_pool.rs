bea03c13071c88cbbe37ea2594c220c4979c0dc9
Henry de Valence <hdevalence@penumbra.zone>
2022-03-28 23:39:51 -0700

    wip

--- pd/src/components/shielded_pool.rs
+++ pd/src/components/shielded_pool.rs
@@ -9,11 +9,12 @@ use penumbra_crypto::{
     asset::{Denom, Id},
     ka,
     merkle::{self, Frontier, NoteCommitmentTree, TreeExt},
-    Note, One, Value,
+    Address, Note, One, Value,
 };
 use penumbra_proto::light_wallet::{CompactBlock, StateFragment};
 use penumbra_transaction::Transaction;
 use tendermint::abci;
+use tracing::instrument;
 
 use super::{Component, Overlay};
 use crate::{genesis, WriteOverlayExt};
@@ -22,7 +23,11 @@ use crate::{genesis, WriteOverlayExt};
 pub struct ShieldedPool {
     overlay: Overlay,
     note_commitment_tree: NoteCommitmentTree,
+    // TODO: change the on-chain registry to just store asset id -> amount
+    // and asset id -> asset id separately
     supply_updates: BTreeMap<asset::Id, i64>,
+    /// The in-progress CompactBlock representation of the ShieldedPool changes
+    compact_block: CompactBlock,
 }
 
 #[async_trait]
@@ -34,13 +39,11 @@ impl Component for ShieldedPool {
             overlay,
             note_commitment_tree,
             supply_updates: Default::default(),
+            compact_block: Default::default(),
         })
     }
 
     fn init_chain(&mut self, app_state: &genesis::AppState) -> Result<()> {
-        let mut fragments = Vec::new();
-        let mut genesis_supply = BTreeMap::new();
-
         for allocation in &app_state.allocations {
             tracing::info!(?allocation, "processing allocation");
 
@@ -60,34 +63,6 @@ impl Component for ShieldedPool {
                     )
                 })?;
 
-            // These notes are public, so we don't need a blinding factor for privacy,
-            // but since the note commitments are determined by the note contents, we
-            // want to have unique (deterministic) blinding factors for each one.
-            let blinding_factor = blake2b_simd::Params::default()
-                .personal(b"genesis_note")
-                .to_state()
-                .update(app_state.chain_params.chain_id.as_bytes())
-                .update(&(fragments.len() as u64).to_le_bytes())
-                .finalize();
-
-            let note = Note::from_parts(
-                *allocation.address.diversifier(),
-                *allocation.address.transmission_key(),
-                Value {
-                    amount: allocation.amount,
-                    asset_id: base_denom.id(),
-                },
-                Fq::from_le_bytes_mod_order(blinding_factor.as_bytes()),
-            )?;
-            let note_commitment = note.commit();
-
-            // Scanning assumes that notes are encrypted, so we need to create
-            // note ciphertexts, even if the plaintexts are known.  Use the key
-            // "1" to ensure we have contributory behaviour in note encryption.
-            let esk = ka::Secret::new_from_field(Fr::one());
-            let epk = esk.diversified_public(&note.diversified_generator());
-            let encrypted_note = note.encrypt(&esk);
-
             // A transaction ID is either a hash of a transaction, or special data.
             // Special data is encoded with 23 leading 0 bytes, followed by a nonzero code byte,
             // followed by 8 data bytes.
@@ -101,55 +76,24 @@ impl Component for ShieldedPool {
                 .try_into()
                 .unwrap();
 
-            // Now record the note:
-
-            // 1. Insert it into the NCT
-            self.note_commitment_tree.append(&note_commitment);
-            // 2. Record its source in the JMT
-            self.overlay.lock().unwrap().put(
-                format!("shielded_pool/note_commitment_source/{}", note_commitment).into(),
+            self.mint_note(
+                Value {
+                    amount: allocation.amount,
+                    asset_id: base_denom.id(),
+                },
+                &allocation.address,
                 transaction_id,
-            );
-            // 3. Queue it for inclusion into the genesis CompactBlock
-            fragments.push(StateFragment {
-                note_commitment: note_commitment.0.to_bytes().to_vec().into(),
-                ephemeral_key: epk.0.to_vec().into(),
-                encrypted_note: encrypted_note.to_vec().into(),
-            });
-            // 4. Update the genesis token supply tracking.
-            genesis_supply
-                .entry(base_denom.id())
-                .or_insert((base_denom, 0))
-                .1 += allocation.amount;
+            )?;
         }
 
-        // Finally, write the genesis CompactBlock:
-        let compact_block = CompactBlock {
-            height: 0,
-            nullifiers: Vec::new(),
-            fragments,
-        };
-        self.overlay.put_proto(
-            format!("shielded_pool/compact_block/{}", compact_block.height).into(),
-            compact_block,
-        );
-        // and the note commitment tree data and anchor:
-        let nct_anchor = self.note_commitment_tree.root2();
-        let nct_data = bincode::serialize(&self.note_commitment_tree)?;
-        self.overlay.lock().unwrap().put(
-            b"shielded_pool/nct_anchor".into(),
-            nct_anchor.to_bytes().to_vec(),
-        );
-        self.overlay
-            .lock()
-            .unwrap()
-            .put(b"shielded_pool/nct_data".into(), nct_data);
+        self.compact_block.height = 0;
+        self.write_block()?;
 
         Ok(())
     }
 
     async fn begin_block(&mut self, _begin_block: &abci::request::BeginBlock) -> Result<()> {
-        todo!()
+        Ok(())
     }
 
     fn check_tx_stateless(_tx: &Transaction) -> Result<()> {
@@ -157,7 +101,7 @@ impl Component for ShieldedPool {
         todo!()
     }
 
-    async fn check_tx_stateful(&self, _tx: &Transaction) -> Result<()> {
+    async fn check_tx_stateful(&self, tx: &Transaction) -> Result<()> {
         todo!()
     }
 
@@ -171,8 +115,13 @@ impl Component for ShieldedPool {
 }
 
 impl ShieldedPool {
-    #[tracing::instrument(skip(self))]
-    fn mint_tokens(&mut self, value: Value, transaction_id: [u8; 32]) -> StateFragment {
+    #[instrument(skip(self))]
+    fn mint_note(
+        &mut self,
+        value: Value,
+        address: &Address,
+        transaction_id: [u8; 32],
+    ) -> Result<()> {
         // These notes are public, so we don't need a blinding factor for privacy,
         // but since the note commitments are determined by the note contents, we
         // need to have unique (deterministic) blinding factors for each note, so they
@@ -180,12 +129,110 @@ impl ShieldedPool {
         //
         // Hashing the current NCT root is sufficient, since it will change every time
         // we insert a new note.
-        let blinding_factor = blake2b_simd::Params::default()
-            .personal(b"PenumbraMint")
-            .to_state()
-            .update(&self.note_commitment_tree.root2().to_bytes())
-            .finalize();
+        let blinding_factor = Fq::from_le_bytes_mod_order(
+            blake2b_simd::Params::default()
+                .personal(b"PenumbraMint")
+                .to_state()
+                .update(&self.note_commitment_tree.root2().to_bytes())
+                .finalize()
+                .as_bytes(),
+        );
 
-        todo!()
+        let note = Note::from_parts(
+            *address.diversifier(),
+            *address.transmission_key(),
+            value,
+            blinding_factor,
+        )?;
+        let note_commitment = note.commit();
+
+        tracing::debug!(?note_commitment, "minted tokens");
+
+        // Scanning assumes that notes are encrypted, so we need to create
+        // note ciphertexts, even if the plaintexts are known.  Use the key
+        // "1" to ensure we have contributory behaviour in note encryption.
+        let esk = ka::Secret::new_from_field(Fr::one());
+        let epk = esk.diversified_public(&note.diversified_generator());
+        let encrypted_note = note.encrypt(&esk);
+
+        // Now record the note:
+
+        // 1. Insert it into the NCT
+        self.note_commitment_tree.append(&note_commitment);
+        // 2. Record its source in the JMT
+        self.overlay.lock().unwrap().put(
+            format!("shielded_pool/note_commitment_source/{}", note_commitment).into(),
+            transaction_id.to_vec(),
+        );
+        // 3. Update the token supply tracking.
+        *self.supply_updates.entry(value.asset_id).or_insert(0) += value.amount as i64;
+        // 4. Finally, record it in the pending compact block.
+        self.compact_block.fragments.push(StateFragment {
+            note_commitment: note_commitment.0.to_bytes().to_vec().into(),
+            ephemeral_key: epk.0.to_vec().into(),
+            encrypted_note: encrypted_note.to_vec().into(),
+        });
+
+        Ok(())
+    }
+
+    fn put_nct_anchor(&mut self) {
+        let nct_anchor = self.note_commitment_tree.root2();
+        // Write the NCT anchor both as a value, so we can look it up,
+        self.overlay.lock().unwrap().put(
+            b"shielded_pool/current_anchor".into(),
+            nct_anchor.to_bytes().to_vec(),
+        );
+        // and as a key, so we can query for it.
+        self.overlay.lock().unwrap().put(
+            [
+                b"shielded_pool/valid_anchors/".to_vec(),
+                nct_anchor.to_bytes().to_vec(),
+            ]
+            .concat()
+            .into(),
+            Vec::new(),
+        );
+    }
+
+    async fn check_nct_anchor(&self, anchor: &merkle::Root) -> Result<()> {
+        if self
+            .overlay
+            .lock()
+            .unwrap()
+            .get(
+                [
+                    b"shielded_pool/valid_anchors/".to_vec(),
+                    anchor.to_bytes().to_vec(),
+                ]
+                .concat()
+                .into(),
+            )
+            .await?
+            .is_some()
+        {
+            Ok(())
+        } else {
+            Err(anyhow!("provided anchor is not a valid NCT root"))
+        }
+    }
+
+    // TODO individial methods
+    #[instrument(skip(self))]
+    fn write_block(&mut self) -> Result<()> {
+        // Write the CompactBlock:
+        self.overlay.put_proto(
+            format!("shielded_pool/compact_block/{}", self.compact_block.height).into(),
+            std::mem::take(&mut self.compact_block),
+        );
+        // and the note commitment tree data and anchor:
+        self.put_nct_anchor();
+        let nct_data = bincode::serialize(&self.note_commitment_tree)?;
+        self.overlay
+            .lock()
+            .unwrap()
+            .put(b"shielded_pool/nct_data".into(), nct_data);
+
+        Ok(())
     }
 }
