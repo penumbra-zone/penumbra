27e196f1830f74f4b22ee3d3aa139d2295cdf4a0
Henry de Valence <hdevalence@penumbra.zone>
2022-03-31 12:25:58 -0700

    replace text comments with spans, skip double-logging in proto* methods by only logging domain methods

--- pd/src/storage/write_overlay_ext.rs
+++ pd/src/storage/write_overlay_ext.rs
@@ -5,6 +5,7 @@ use penumbra_proto::{Message, Protobuf};
 use std::fmt::Debug;
 use std::sync::Arc;
 use tokio::sync::Mutex;
+use tracing::instrument;
 
 /// An extension trait that allows writing proto-encoded domain types to
 /// a shared [`WriteOverlay`].
@@ -13,23 +14,17 @@ pub trait WriteOverlayExt {
     /// Reads a domain type from the overlay, using the proto encoding.
     async fn get_domain<D, P>(&self, key: KeyHash) -> Result<Option<D>>
     where
-        D: Protobuf<P>,
+        D: Protobuf<P> + TryFrom<P> + Clone + Debug,
         // TODO: does this get less awful if P is an associated type of D?
-        P: Message + Default,
-        P: From<D>,
-        D: TryFrom<P> + Clone,
-        D: Debug,
+        P: Message + Default + From<D>,
         <D as TryFrom<P>>::Error: Into<anyhow::Error>;
 
     /// Puts a domain type into the overlay, using the proto encoding.
     async fn put_domain<D, P>(&self, key: KeyHash, value: D)
     where
-        D: Protobuf<P> + Send,
+        D: Protobuf<P> + Send + TryFrom<P> + Clone + Debug,
         // TODO: does this get less awful if P is an associated type of D?
-        P: Message + Default,
-        P: From<D>,
-        D: TryFrom<P> + Clone,
-        D: Debug,
+        P: Message + Default + From<D>,
         <D as TryFrom<P>>::Error: Into<anyhow::Error>;
 
     /// Reads a proto type from the overlay.
@@ -38,7 +33,7 @@ pub trait WriteOverlayExt {
     /// but there are cases where it's convenient to use the proto directly.
     async fn get_proto<P>(&self, key: KeyHash) -> Result<Option<P>>
     where
-        P: Message + Default;
+        P: Message + Default + Debug;
 
     /// Puts a proto type into the overlay.
     ///
@@ -46,62 +41,56 @@ pub trait WriteOverlayExt {
     /// but there are cases where it's convenient to use the proto directly.
     async fn put_proto<P>(&self, key: KeyHash, value: P)
     where
-        P: Message;
+        P: Message + Debug;
 }
 
 #[async_trait]
 impl<R: TreeReader + Sync> WriteOverlayExt for Arc<Mutex<WriteOverlay<R>>> {
+    #[instrument(skip(self, key))]
     async fn get_domain<D, P>(&self, key: KeyHash) -> Result<Option<D>>
     where
         D: Protobuf<P>,
         // TODO: does this get less awful if P is an associated type of D?
         P: Message + Default,
         P: From<D>,
-        D: TryFrom<P> + Clone,
-        D: Debug,
+        D: TryFrom<P> + Clone + Debug,
         <D as TryFrom<P>>::Error: Into<anyhow::Error>,
     {
-        tracing::debug!(keyhash = ?key, "Requested KeyHash");
-
         match self.get_proto(key).await {
             Ok(Some(p)) => match D::try_from(p) {
                 Ok(d) => {
-                    tracing::debug!(domain = ?d, "Retrieved domain");
-
+                    tracing::debug!(?key, value = ?d);
                     Ok(Some(d))
                 }
                 Err(e) => Err(e.into()),
             },
-            Ok(None) => Ok(None),
+            Ok(None) => {
+                tracing::debug!(?key, "no entry in tree");
+                Ok(None)
+            }
             Err(e) => Err(e.into()),
         }
     }
 
+    #[instrument(skip(self, key, value))]
     async fn put_domain<D, P>(&self, key: KeyHash, value: D)
     where
         D: Protobuf<P>,
         // TODO: does this get less awful if P is an associated type of D?
         P: Message + Default,
         P: From<D>,
-        D: TryFrom<P> + Clone,
-        D: std::marker::Send,
+        D: TryFrom<P> + Clone + Send + Debug,
         <D as TryFrom<P>>::Error: Into<anyhow::Error>,
     {
-        tracing::debug!(keyhash = ?key, "Inserted KeyHash");
-
-        let v: P = value.try_into().unwrap();
-
-        tracing::debug!(val = ?v, "Inserted value");
-
-        self.put_proto(key, v).await;
+        tracing::debug!(?key, ?value);
+        self.put_proto(key, P::from(value)).await;
     }
 
+    #[instrument(skip(self, key))]
     async fn get_proto<P>(&self, key: KeyHash) -> Result<Option<P>>
     where
-        P: Message + Default,
+        P: Message + Default + Debug,
     {
-        tracing::debug!(keyhash = ?key, "Requested KeyHash");
-
         let bytes = match self.lock().await.get(key).await? {
             None => return Ok(None),
             Some(bytes) => bytes,
@@ -109,20 +98,14 @@ impl<R: TreeReader + Sync> WriteOverlayExt for Arc<Mutex<WriteOverlay<R>>> {
 
         Message::decode(bytes.as_slice())
             .map_err(|e| anyhow!(e))
-            .map(|v| {
-                tracing::debug!(val = ?v, "Retrieved proto");
-                Some(v)
-            })
+            .map(|v| Some(v))
     }
 
+    #[instrument(skip(self, key, value))]
     async fn put_proto<P>(&self, key: KeyHash, value: P)
     where
-        P: Message,
+        P: Message + Debug,
     {
-        tracing::debug!(keyhash = ?key, "Inserted KeyHash");
-
-        tracing::debug!(val = ?value, "Inserted value");
-
         self.lock().await.put(key, value.encode_to_vec());
     }
 }
