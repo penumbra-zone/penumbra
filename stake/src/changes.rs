2f148b62e2847931b8e9f4bb9f94a59e1312124f
Henry de Valence <hdevalence@penumbra.zone>
2022-04-04 00:43:53 -0700

    stake: add a DelegationChanges struct and proto
    In the previous staking code, we relied on the existence of
    ```
    Reader::delegation_changes() -> BTreeMap<IdentityKey, i64>
    ```
    that aggregated the delegation changes over an entire epoch at the database level.
    
    This won't work going forward, because we won't have access to the individual
    (un)delegation values, they'll be protected with flow encryption.  So we need a
    way to record and then later extract a list of (un)delegation actions, which
    the `DelegationChanges` struct now provides.

---
+++ stake/src/changes.rs
@@ -0,0 +1,50 @@
+use anyhow::Result;
+use penumbra_proto::{stake as pb, Protobuf};
+
+use crate::{Delegate, Undelegate};
+
+#[derive(Debug, Clone, Default)]
+pub struct DelegationChanges {
+    pub delegations: Vec<Delegate>,
+    pub undelegations: Vec<Undelegate>,
+}
+
+impl Protobuf<pb::DelegationChanges> for DelegationChanges {}
+
+impl From<DelegationChanges> for pb::DelegationChanges {
+    fn from(changes: DelegationChanges) -> pb::DelegationChanges {
+        pb::DelegationChanges {
+            delegations: changes.delegations.into_iter().map(Into::into).collect(),
+            undelegations: changes.undelegations.into_iter().map(Into::into).collect(),
+        }
+    }
+}
+
+impl TryFrom<pb::DelegationChanges> for DelegationChanges {
+    type Error = anyhow::Error;
+    fn try_from(changes: pb::DelegationChanges) -> Result<DelegationChanges> {
+        Ok(DelegationChanges {
+            delegations: changes
+                .delegations
+                .into_iter()
+                .map(TryInto::try_into)
+                .collect::<Result<_>>()?,
+            undelegations: changes
+                .undelegations
+                .into_iter()
+                .map(TryInto::try_into)
+                .collect::<Result<_>>()?,
+        })
+    }
+}
+
+impl std::iter::Sum for DelegationChanges {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        let mut sum = DelegationChanges::default();
+        for changes in iter {
+            sum.delegations.extend(changes.delegations);
+            sum.undelegations.extend(changes.undelegations);
+        }
+        sum
+    }
+}
