f4c84519a364c03edf01a1850ab1df6364814b1e
plaidfinch <finch@plaidfinch.net>
2022-04-13 20:40:25 -0400

    Cache the result of hashes in fast_hash mode

--- tct/src/internal/hash/fast_hash.rs
+++ tct/src/internal/hash/fast_hash.rs
@@ -3,23 +3,44 @@
 //! of this crate; it is only useful to accelerate testing, when we don't care about producing
 //! zero-knowledge proofs.**
 
+use std::sync::Arc;
+
 use ark_ff::fields::PrimeField;
+use dashmap::DashMap;
 use decaf377::FieldExt;
+use hash_hasher::HashBuildHasher;
+use once_cell::sync::Lazy;
 use poseidon377::Fq;
 
+#[allow(clippy::type_complexity)]
+static HASH_1_CACHE: Lazy<Arc<DashMap<(Fq, Fq), Fq, HashBuildHasher>>> =
+    Lazy::new(|| Arc::new(DashMap::with_hasher(HashBuildHasher::default())));
+
 pub fn hash_1(domain_separator: &Fq, value: Fq) -> Fq {
-    let mut state = blake2b_simd::State::new();
-    state.update(&domain_separator.to_bytes());
-    state.update(&value.to_bytes());
-    Fq::from_le_bytes_mod_order(state.finalize().as_bytes())
+    *HASH_1_CACHE
+        .entry((*domain_separator, value))
+        .or_insert_with(|| {
+            let mut state = blake2b_simd::State::new();
+            state.update(&domain_separator.to_bytes());
+            state.update(&value.to_bytes());
+            Fq::from_le_bytes_mod_order(state.finalize().as_bytes())
+        })
 }
 
+#[allow(clippy::type_complexity)]
+static HASH_4_CACHE: Lazy<Arc<DashMap<(Fq, (Fq, Fq, Fq, Fq)), Fq, HashBuildHasher>>> =
+    Lazy::new(|| Arc::new(DashMap::with_hasher(HashBuildHasher::default())));
+
 pub fn hash_4(domain_separator: &Fq, value: (Fq, Fq, Fq, Fq)) -> Fq {
-    let mut state = blake2b_simd::State::new();
-    state.update(&domain_separator.to_bytes());
-    state.update(&value.0.to_bytes());
-    state.update(&value.1.to_bytes());
-    state.update(&value.2.to_bytes());
-    state.update(&value.3.to_bytes());
-    Fq::from_le_bytes_mod_order(state.finalize().as_bytes())
+    *HASH_4_CACHE
+        .entry((*domain_separator, value))
+        .or_insert_with(|| {
+            let mut state = blake2b_simd::State::new();
+            state.update(&domain_separator.to_bytes());
+            state.update(&value.0.to_bytes());
+            state.update(&value.1.to_bytes());
+            state.update(&value.2.to_bytes());
+            state.update(&value.3.to_bytes());
+            Fq::from_le_bytes_mod_order(state.finalize().as_bytes())
+        })
 }
