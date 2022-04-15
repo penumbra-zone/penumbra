1f42e7bb0b65a8c4efeb86c2eac06240d8fdc706
Henry de Valence <hdevalence@penumbra.zone>
2022-04-15 10:10:58 +0200

    cargo fix

--- pd/src/mempool.rs
+++ pd/src/mempool.rs
@@ -7,24 +7,3 @@ pub use service::Mempool;
 use worker::Worker;
 
 // Old code below
-
-use std::{
-    collections::BTreeSet,
-    future::Future,
-    pin::Pin,
-    sync::Arc,
-    task::{Context, Poll},
-};
-
-use futures::FutureExt;
-use penumbra_crypto::Nullifier;
-
-use tendermint::abci::{
-    request::CheckTx as CheckTxRequest, response::CheckTx as CheckTxResponse, MempoolRequest,
-    MempoolResponse,
-};
-use tokio::sync::Mutex as AsyncMutex;
-use tower_abci::BoxError;
-use tracing::Instrument;
-
-use crate::RequestExt;
