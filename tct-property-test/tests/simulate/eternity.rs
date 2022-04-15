5d1f45acd3d0eefd2a8e8b0c9f9e7580837d271b
Kenny Foner <kwf@very.science>
2022-04-15 15:41:51 -0400

    Add `position_of` to property test suite

--- tct-property-test/tests/simulate/eternity.rs
+++ tct-property-test/tests/simulate/eternity.rs
@@ -89,11 +89,12 @@ impl Simulate for Action {
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Arbitrary)]
 #[proptest(params("Vec<Commitment>"))]
 pub enum Observation {
-    Witness(#[proptest(strategy = "CommitmentStrategy::one_of(params)")] Commitment),
+    Witness(#[proptest(strategy = "CommitmentStrategy::one_of(params.clone())")] Commitment),
     Root,
     CurrentEpochRoot,
     CurrentBlockRoot,
     Position,
+    PositionOf(#[proptest(strategy = "CommitmentStrategy::one_of(params)")] Commitment),
     WitnessedCount,
     IsEmpty,
 }
@@ -146,6 +147,11 @@ impl Simulate for Observation {
                 spec.position(),
                 real.position(),
                 "result mismatch from `Eternity::position`"
+            ),
+            PositionOf(commitment) => assert_eq!(
+                spec.position_of(commitment),
+                real.position_of(commitment),
+                "result mismatch from `Eternity::position_of`"
             ),
             WitnessedCount => assert_eq!(
                 spec.witnessed_count(),
