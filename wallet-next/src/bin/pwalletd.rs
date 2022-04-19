ea14e017fdfbfd7475b9326d39958b5d59162bb9
Chris Czub <chris@penumbra.zone>
2022-04-19 16:38:33 -0400

    Create Storage struct to encapsulate wallet sql interactions (#652)

--- wallet-next/src/bin/pwalletd.rs
+++ wallet-next/src/bin/pwalletd.rs
@@ -2,16 +2,17 @@ use anyhow::Result;
 use sqlx::sqlite::SqlitePool;
 use std::env;
 
-use penumbra_wallet_next::{insert_table, read_table};
+use penumbra_wallet_next::Storage;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+    let storage = Storage::new(pool);
 
-    // TODO: weird chicken & egg problem w/ database existing or not
-    sqlx::migrate!().run(&pool).await?;
-    let row = insert_table(&pool).await?;
-    let x = read_table(&pool).await?;
+    storage.migrate().await?;
+
+    let row = storage.insert_table().await?;
+    let x = storage.read_table().await?;
 
     println!(
         "Hello, pwalletd! I got stuff from sqlite: row {} value {}",
