5d6024877e798be11a555085d244a71b6337e011
plaidfinch <finch@plaidfinch.net>
2022-04-22 09:50:56 -0400

    Add pruning for trees in TCT spec

--- tct-spec/src/tree.rs
+++ tct-spec/src/tree.rs
@@ -8,16 +8,16 @@ use parking_lot::Mutex;
 use penumbra_tct::{
     internal::{
         active::Insert,
-        hash::{CachedHash, Hash},
+        hash::{CachedHash, GetHash, Hash},
     },
     Commitment,
 };
 
-type Parent = Weak<Wrapped>;
-
 pub struct Tree {
     inner: Arc<Wrapped>,
 }
+
+type Parent = Weak<Wrapped>;
 
 struct Wrapped {
     parent: Parent,
@@ -63,14 +63,22 @@ impl Tree {
         Arc::ptr_eq(&self.inner, &other.inner)
     }
 
+    fn parent(&self) -> Option<Tree> {
+        self.inner.parent.upgrade().map(|inner| Tree { inner })
+    }
+
     pub fn from_block(block: impl IntoIterator<Item = Insert<Commitment>>) -> Tree {
-        build::block(block).with_parent(Parent::new())
+        let mut block = build::block(block).with_parent(Parent::new());
+        block.prune();
+        block
     }
 
     pub fn from_epoch(
         epoch: impl IntoIterator<Item = Insert<impl IntoIterator<Item = Insert<Commitment>>>>,
     ) -> Tree {
-        build::epoch(epoch).with_parent(Parent::new())
+        let mut epoch = build::epoch(epoch).with_parent(Parent::new());
+        epoch.prune();
+        epoch
     }
 
     pub fn from_eternity(
@@ -80,15 +88,37 @@ impl Tree {
             >,
         >,
     ) -> Tree {
-        build::eternity(eternity).with_parent(Parent::new())
+        let mut eternity = build::eternity(eternity).with_parent(Parent::new());
+        eternity.prune();
+        eternity
     }
 
-    pub fn inner(&self) -> &Inner {
+    fn inner(&self) -> &Inner {
         &self.inner.inner
     }
 
-    pub fn parent(&self) -> Option<Tree> {
-        self.inner.parent.upgrade().map(|inner| Tree { inner })
+    pub fn as_leaf(&self) -> &Leaf {
+        if let Inner::Leaf(leaf) = self.inner() {
+            leaf
+        } else {
+            panic!("expected tree to be a leaf")
+        }
+    }
+
+    pub fn as_node(&self) -> &Node {
+        if let Inner::Node(node) = self.inner() {
+            node
+        } else {
+            panic!("expected tree to be a node")
+        }
+    }
+
+    pub fn as_tier(&self) -> &Tier {
+        if let Inner::Tier(tier) = self.inner() {
+            tier
+        } else {
+            panic!("expected tree to be a tier")
+        }
     }
 
     pub fn is_frontier(&self) -> bool {
@@ -110,7 +140,7 @@ impl Tree {
         self.inner.height
     }
 
-    pub fn hash(&self) -> Hash {
+    pub fn root(&self) -> Hash {
         self.inner.hash.set_if_empty(|| {
             let padding = if self.is_frontier() {
                 Hash::default()
@@ -121,13 +151,8 @@ impl Tree {
             let hash = match self.inner() {
                 Inner::Leaf(Leaf { commitment }) => Hash::of(*commitment),
                 Inner::Node(Node { children }) => {
-                    let children_hashes: Vec<Hash> = children
-                        .iter()
-                        .map(|child| match &*child.lock() {
-                            Insert::Keep(child) => child.hash(),
-                            Insert::Hash(hash) => *hash,
-                        })
-                        .collect();
+                    let children_hashes: Vec<Hash> =
+                        children.iter().map(|child| child.lock().hash()).collect();
 
                     match children_hashes.as_slice() {
                         [] => unreachable!("nodes never have zero children"),
@@ -151,7 +176,48 @@ impl Tree {
         })
     }
 
-    pub fn prune(&self) -> bool {
-        todo!("implement pruning")
+    fn prune(&mut self) -> bool {
+        fn prune_insert(insert: &mut Insert<Tree>) -> bool {
+            match insert {
+                Insert::Keep(tree) => {
+                    // Determine whether to keep the tree, in the process pruning everything beneath
+                    // its topmost level
+                    let keep = tree.prune();
+                    if !keep {
+                        // If the tree should not be kept, replace it with its hash
+                        *insert = Insert::Hash(tree.hash());
+                    }
+                    keep
+                }
+                // A hashed tree should not be kept
+                Insert::Hash(_) => false,
+            }
+        }
+
+        match self.inner() {
+            // A leaf containing a commitment is witnessed, so should be kept:
+            Inner::Leaf(_) => true,
+            // A node should be kept if any of its children are kept:
+            Inner::Node(node) => node
+                .children
+                .iter()
+                .any(|child| prune_insert(&mut child.lock())),
+            // A tier should be kept if it is not empty and its root is kept:
+            Inner::Tier(tier) => match tier.root {
+                None => false,
+                Some(ref root) => prune_insert(&mut root.lock()),
+            },
+        }
+    }
+}
+
+// This impl allows us to take the hash of `Insert<Tree>`, which is useful.
+impl GetHash for Tree {
+    fn hash(&self) -> Hash {
+        self.root()
+    }
+
+    fn cached_hash(&self) -> Option<Hash> {
+        self.inner.hash.get()
     }
 }
