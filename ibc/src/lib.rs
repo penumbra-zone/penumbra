f808db10174e7629c1633e51e1be9cbad416e85b
Henry de Valence <hdevalence@penumbra.zone>
2022-04-24 12:08:24 -0700

    ibc: suppress warnings of unreachable patterns

--- ibc/src/lib.rs
+++ ibc/src/lib.rs
@@ -1,3 +1,8 @@
+// Many of the IBC message types are enums, where the number of variants differs
+// depending on the build configuration, meaning that the fallthrough case gets
+// marked as unreachable only when not building in test configuration.
+#![allow(unreachable_patterns)]
+
 mod client;
 mod ibcaction;
 
