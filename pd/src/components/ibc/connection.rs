b8b329497cd178f8f040e1937365ef8b65c65369
Ava Howell <ava@penumbra.zone>
2022-04-26 19:08:25 -0700

    wip: add connectionopentry handling

--- pd/src/components/ibc/connection.rs
+++ pd/src/components/ibc/connection.rs
@@ -1,12 +1,19 @@
+use crate::{components::app::View as _, components::ibc::client::View as _};
 use anyhow::Result;
 use async_trait::async_trait;
-use ibc::core::ics03_connection::connection::{self, ConnectionEnd};
+use ibc::clients::ics07_tendermint::client_state::ClientState as TendermintClientState;
+use ibc::core::ics02_client::client_state::AnyClientState;
+use ibc::core::ics02_client::client_type::ClientType;
+use ibc::core::ics03_connection::connection::Counterparty;
+use ibc::core::ics03_connection::connection::{ConnectionEnd, State as ConnectionState};
 use ibc::core::ics03_connection::msgs::conn_open_ack::MsgConnectionOpenAck;
 use ibc::core::ics03_connection::msgs::conn_open_confirm::MsgConnectionOpenConfirm;
 use ibc::core::ics03_connection::msgs::conn_open_init::MsgConnectionOpenInit;
 use ibc::core::ics03_connection::msgs::conn_open_try::MsgConnectionOpenTry;
 use ibc::core::ics03_connection::version::Version;
 use ibc::core::ics24_host::identifier::ConnectionId;
+use ibc::downcast;
+use ibc::Height as IBCHeight;
 use penumbra_chain::genesis;
 use penumbra_component::Component;
 use penumbra_ibc::{Connection, ConnectionCounter};
@@ -90,8 +97,9 @@ fn validate_ibc_action_stateless(ibc_action: &IbcAction) -> Result<(), anyhow::E
             return Ok(());
         }
 
-        Some(ConnectionOpenTry(msg)) => {
-            let _msg_connection_open_try = MsgConnectionOpenTry::try_from(msg.clone())?;
+        // process a notice of a connection attempt on a counterparty chain
+        ConnectionOpenTry(msg) => {
+            let _ = MsgConnectionOpenTry::try_from(msg.clone())?;
         }
 
         Some(ConnectionOpenAck(msg)) => {
@@ -118,8 +126,9 @@ impl ConnectionComponent {
                     .await;
             }
 
-            Some(ConnectionOpenTry(msg)) => {
-                let _msg_connection_open_try = MsgConnectionOpenTry::try_from(msg.clone()).unwrap();
+            Some(ConnectionOpenTry(raw_msg)) => {
+                let msg = MsgConnectionOpenTry::try_from(raw_msg.clone()).unwrap();
+                self.execute_connection_open_try(&msg).await;
             }
 
             Some(ConnectionOpenAck(msg)) => {
@@ -141,7 +150,7 @@ impl ConnectionComponent {
         let compatible_versions = vec![Version::default()];
 
         let new_connection_end = ConnectionEnd::new(
-            connection::State::Init,
+            ConnectionState::Init,
             msg.client_id.clone(),
             msg.counterparty.clone(),
             compatible_versions,
@@ -155,7 +164,115 @@ impl ConnectionComponent {
             .unwrap();
     }
 
-    async fn validate_ibc_action_stateful(&self, ibc_action: &IbcAction) -> Result<()> {
+    async fn execute_connection_open_try(&mut self, msg: &MsgConnectionOpenTry) {
+        // todo: construct a connection object with the TryOpen state and save it to the state
+    }
+
+    // validate the client state given to us in a MsgConnectionOpenTry, verifying that the state
+    // that the counterparty chain stored is valid
+    async fn validate_client_state(
+        &self,
+        client_state: AnyClientState,
+    ) -> Result<(), anyhow::Error> {
+        let tm_client_state =
+            downcast!(client_state => AnyClientState::Tendermint).ok_or_else(|| {
+                anyhow::anyhow!("invalid client state: not a Tendermint client state")
+            })?;
+
+        if tm_client_state.frozen_height.is_some() {
+            return Err(anyhow::anyhow!("invalid client state: frozen"));
+        }
+
+        if self.state.get_chain_id().await? != tm_client_state.chain_id.as_str() {
+            return Err(anyhow::anyhow!(
+                "invalid client state: chain id does not match"
+            ));
+        }
+
+        // TODO:
+        // - check client latest height
+        // - check client proof specs match penumbra proof specs
+        // - check trust level matches penumbra trust level
+        // - check unbonding time matches penumbra unbonding time
+        // - check unbonding period is less than trusting period
+        // - check upgrade path
+
+        Ok(())
+    }
+    async fn validate_connection_open_try(
+        &self,
+        msg: &MsgConnectionOpenTry,
+    ) -> Result<(), anyhow::Error> {
+        // verify the consensus height is correct
+        if msg.consensus_height()
+            > IBCHeight::zero().with_revision_height(self.state.get_block_height().await?)
+        {
+            return Err(anyhow::anyhow!(
+                "consensus height is greater than the current block height",
+            ));
+        }
+
+        //
+        // TODO: store our earliest non-pruned block height and check against the consensus height
+        //
+
+        // verify the client state
+        let provided_cs = msg
+            .client_state
+            .clone()
+            .ok_or_else(|| anyhow::anyhow!("client state not provided in MsgConnectionOpenTry"))?;
+
+        self.validate_client_state(provided_cs).await?;
+
+        let mut new_conn = ConnectionEnd::new(
+            ConnectionState::Init,
+            msg.client_id.clone(),
+            msg.counterparty.clone(),
+            msg.counterparty_versions.clone(),
+            msg.delay_period,
+        );
+
+        if let Some(prev_conn_id) = &msg.previous_connection_id {
+            // check that we have a valid connection with the given ID
+            let prev_connection = self
+                .state
+                .get_connection(prev_conn_id)
+                .await?
+                .ok_or_else(|| anyhow::anyhow!("no connection with the given ID"))?
+                .0;
+
+            // check that the existing connection matches the incoming connectionOpenTry
+            if !(prev_connection.state_matches(&ConnectionState::Init)
+                && prev_connection.counterparty_matches(&msg.counterparty)
+                && prev_connection.client_id_matches(&msg.client_id)
+                && prev_connection.delay_period() == msg.delay_period)
+            {
+                return Err(anyhow::anyhow!(
+                    "connection with the given ID is not in the correct state",
+                ));
+            }
+
+            new_conn = prev_connection;
+        }
+
+        let expected_conn = ConnectionEnd::new(
+            ConnectionState::Init,
+            msg.counterparty.client_id().clone(),
+            Counterparty::new(
+                msg.client_id.clone(),
+                None,
+                self.state.get_commitment_prefix().await?,
+            ),
+            msg.counterparty_versions.clone(),
+            msg.delay_period,
+        );
+
+        // TODO: use Client verification methods to verify that the connection was included on the
+        // counterparty chain
+
+        Ok(())
+    }
+    async fn validate_ibc_action_stateful(&self, ibc_action: &IBCAction) -> Result<()> {
         match &ibc_action.action {
             Some(ConnectionOpenInit(raw_msg)) => {
                 // check that the client id exists
@@ -165,8 +282,9 @@ impl ConnectionComponent {
                 return Ok(());
             }
 
-            Some(ConnectionOpenTry(msg)) => {
-                let _msg_connection_open_try = MsgConnectionOpenTry::try_from(msg.clone())?;
+            ConnectionOpenTry(raw_msg) => {
+                let msg = MsgConnectionOpenTry::try_from(raw_msg.clone())?;
+                self.validate_connection_open_try(&msg).await?;
             }
 
             Some(ConnectionOpenAck(msg)) => {
@@ -224,6 +342,17 @@ pub trait View: StateExt + Send + Sync {
             .await?;
 
         return Ok(());
+    }
+
+    async fn get_connection(&self, connection_id: &ConnectionId) -> Result<Option<Connection>> {
+        self.get_domain(
+            format!(
+                "ibc/ics03-connection/connections/{}",
+                connection_id.as_str()
+            )
+            .into(),
+        )
+        .await
     }
 }
 
