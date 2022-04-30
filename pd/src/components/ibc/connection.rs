e67ead549c8c104b35d88b53909f8a90bf98b6bc
Ava Howell <ava@penumbra.zone>
2022-04-30 15:47:13 -0700

    move penumbra client state validation to IBC crate

--- pd/src/components/ibc/connection.rs
+++ pd/src/components/ibc/connection.rs
@@ -4,9 +4,7 @@ use async_trait::async_trait;
 use ibc::core::ics02_client::client_consensus::ConsensusState;
 use ibc::core::ics02_client::client_def::AnyClient;
 use ibc::core::ics02_client::client_def::ClientDef;
-use ibc::core::ics02_client::client_state::AnyClientState;
 use ibc::core::ics02_client::client_state::ClientState;
-use ibc::core::ics02_client::trust_threshold::TrustThreshold;
 use ibc::core::ics03_connection::connection::Counterparty;
 use ibc::core::ics03_connection::connection::{ConnectionEnd, State as ConnectionState};
 use ibc::core::ics03_connection::msgs::conn_open_ack::MsgConnectionOpenAck;
@@ -14,14 +12,14 @@ use ibc::core::ics03_connection::msgs::conn_open_confirm::MsgConnectionOpenConfi
 use ibc::core::ics03_connection::msgs::conn_open_init::MsgConnectionOpenInit;
 use ibc::core::ics03_connection::msgs::conn_open_try::MsgConnectionOpenTry;
 use ibc::core::ics03_connection::version::{pick_version, Version};
-use ibc::core::ics23_commitment::specs::ProofSpecs;
-use ibc::core::ics24_host::identifier::ChainId;
 use ibc::core::ics24_host::identifier::ConnectionId;
-use ibc::downcast;
 use ibc::Height as IBCHeight;
 use penumbra_chain::genesis;
 use penumbra_component::Component;
-use penumbra_ibc::{Connection, ConnectionCounter, COMMITMENT_PREFIX, SUPPORTED_VERSIONS};
+use penumbra_ibc::{
+    validate_penumbra_client_state, Connection, ConnectionCounter, COMMITMENT_PREFIX,
+    SUPPORTED_VERSIONS,
+};
 use penumbra_proto::ibc::{
     ibc_action::Action::{
         ConnectionOpenAck, ConnectionOpenConfirm, ConnectionOpenInit, ConnectionOpenTry,
@@ -116,32 +114,6 @@ fn validate_ibc_action_stateless(ibc_action: &IbcAction) -> Result<(), anyhow::E
 
     Ok(())
 }
-
-// Check that the trust threshold is:
-//
-// a) non-zero
-// b) greater or equal to 1/3
-// c) strictly less than 1
-fn validate_trust_threshold(trust_threshold: TrustThreshold) -> Result<(), anyhow::Error> {
-    if trust_threshold.denominator() == 0 {
-        return Err(anyhow::anyhow!(
-            "trust threshold denominator cannot be zero"
-        ));
-    }
-
-    if trust_threshold.numerator() * 3 < trust_threshold.denominator() {
-        return Err(anyhow::anyhow!("trust threshold must be greater than 1/3"));
-    }
-
-    if trust_threshold.numerator() >= trust_threshold.denominator() {
-        return Err(anyhow::anyhow!(
-            "trust threshold must be strictly less than 1"
-        ));
-    }
-
-    Ok(())
-}
-
 impl ConnectionComponent {
     async fn execute_ibc_action(&mut self, ibc_action: &IbcAction) {
         match &ibc_action.action {
@@ -214,70 +186,6 @@ impl ConnectionComponent {
             .unwrap();
     }
 
-    // validate the client state given to us in a MsgConnectionOpenTry, verifying that the state
-    // that the counterparty chain stored is valid
-    //
-    // TODO: this should probably be a static function in the client crate
-    async fn validate_penumbra_client_state(
-        &self,
-        client_state: AnyClientState,
-    ) -> Result<(), anyhow::Error> {
-        let tm_client_state =
-            downcast!(client_state => AnyClientState::Tendermint).ok_or_else(|| {
-                anyhow::anyhow!("invalid client state: not a Tendermint client state")
-            })?;
-
-        if tm_client_state.frozen_height.is_some() {
-            return Err(anyhow::anyhow!("invalid client state: frozen"));
-        }
-
-        // NOTE: Chain ID validation is actually not standardized yet. see
-        // https://github.com/informalsystems/ibc-rs/pull/304#discussion_r503917283
-        let chain_id = ChainId::from_string(&self.state.get_chain_id().await?);
-        if chain_id != tm_client_state.chain_id {
-            return Err(anyhow::anyhow!(
-                "invalid client state: chain id does not match"
-            ));
-        }
-
-        // check that the revision number is the same as our chain ID's version
-        if tm_client_state.latest_height.revision_number != chain_id.version() {
-            return Err(anyhow::anyhow!(
-                "invalid client state: revision number does not match"
-            ));
-        }
-
-        // check that the latest height isn't gte the current block height
-        if tm_client_state.latest_height.revision_height >= self.state.get_block_height().await? {
-            return Err(anyhow::anyhow!(
-                "invalid client state: latest height is greater than or equal to the current block height"
-            ));
-        }
-
-        // check client proof specs match penumbra proof specs
-        let penumbra_proof_specs: ProofSpecs = vec![jmt::ics23_spec()].into();
-        if penumbra_proof_specs != tm_client_state.proof_specs {
-            return Err(anyhow::anyhow!(
-                "invalid client state: proof specs do not match"
-            ));
-        }
-
-        // check that the trust level is correct
-        validate_trust_threshold(tm_client_state.trust_level)?;
-
-        // TODO: check that the unbonding period is correct
-        //
-        // - check unbonding period is greater than trusting period
-        if tm_client_state.unbonding_period < tm_client_state.trusting_period {
-            return Err(anyhow::anyhow!(
-                "invalid client state: unbonding period is less than trusting period"
-            ));
-        }
-        // TODO: check upgrade path
-
-        Ok(())
-    }
-
     async fn validate_connection_open_try(
         &self,
         msg: &MsgConnectionOpenTry,
@@ -301,7 +209,9 @@ impl ConnectionComponent {
             .clone()
             .ok_or_else(|| anyhow::anyhow!("client state not provided in MsgConnectionOpenTry"))?;
 
-        self.validate_penumbra_client_state(provided_cs).await?;
+        let height = self.state.get_block_height().await?;
+        let chain_id = self.state.get_chain_id().await?;
+        validate_penumbra_client_state(provided_cs, &chain_id, height)?;
 
         let mut previous_conn: Option<ConnectionEnd> = None;
         if let Some(prev_conn_id) = &msg.previous_connection_id {
