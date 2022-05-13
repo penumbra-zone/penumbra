penumbra-zone/penumbra

commit 36f651d9766b274b75c50a707e20549c474ff009

Store the NCT in `Storage`, not `State` (#796)
* Move put_nct and get_nct to Storage

* Change ShieldedPool::new method Storage, read the NCT from it with get_nct

* Add NCT accessor to the shielded pool component

* Store the NCT in the storage on app commit

pd/src/components/app.rs
@@ -25,6 +25,20 @@ pub struct App {
 }
 
 impl App {
+    #[instrument(skip(storage))]
+    pub async fn new(storage: Storage) -> Self {
+        let staking = Staking::new(storage.state().await.unwrap()).await;
+        let ibc = IBCComponent::new(storage.state().await.unwrap()).await;
+        let shielded_pool = ShieldedPool::new(storage.clone()).await;
+
+        Self {
+            state: storage.state().await.unwrap(),
+            shielded_pool,
+            staking,
+            ibc,
+        }
+    }
+
     /// Commits the application state to persistent storage,
     /// returning the new root hash and storage version.
     ///
@@ -32,13 +46,18 @@ impl App {
     /// as an empty state over top of the newly written storage.
     #[instrument(skip(self, storage))]
     pub async fn commit(&mut self, storage: Storage) -> Result<(RootHash, Version)> {
+        // Store the latest NCT in the storage (this is not in the JMT State because we don't want
+        // the serialization format to be consensus critical; only the root is stored in the State)
+        storage
+            .put_nct(self.shielded_pool.note_commitment_tree())
+            .await?;
         // Commit the pending writes, clearing the state.
-        let (root_hash, version) = self.state.write().await.commit(storage).await?;
+        let (root_hash, version) = self.state.write().await.commit(storage.clone()).await?;
         tracing::debug!(?root_hash, version, "finished committing state");
         // Now re-instantiate all of the components:
         self.staking = Staking::new(self.state.clone()).await;
         self.ibc = IBCComponent::new(self.state.clone()).await;
-        self.shielded_pool = ShieldedPool::new(self.state.clone()).await;
+        self.shielded_pool = ShieldedPool::new(storage).await;
 
         Ok((root_hash, version))
     }
@@ -51,20 +70,6 @@ impl App {
 
 #[async_trait]
 impl Component for App {
-    #[instrument(skip(state))]
-    async fn new(state: State) -> Self {
-        let staking = Staking::new(state.clone()).await;
-        let ibc = IBCComponent::new(state.clone()).await;
-        let shielded_pool = ShieldedPool::new(state.clone()).await;
-
-        Self {
-            state,
-            shielded_pool,
-            staking,
-            ibc,
-        }
-    }
-
     #[instrument(skip(self, app_state))]
     async fn init_chain(&mut self, app_state: &genesis::AppState) {
         self.state

pd/src/consensus/worker.rs
@@ -29,7 +29,7 @@ impl Worker {
         queue: mpsc::Receiver<Message>,
         height_tx: watch::Sender<block::Height>,
     ) -> Result<Self> {
-        let app = App::new(storage.state().await?).await;
+        let app = App::new(storage.clone()).await;
 
         Ok(Self {
             queue,

pd/src/mempool/worker.rs
@@ -25,7 +25,7 @@ impl Worker {
         queue: mpsc::Receiver<Message>,
         height_rx: watch::Receiver<block::Height>,
     ) -> Result<Self> {
-        let app = App::new(storage.state().await?).await;
+        let app = App::new(storage.clone()).await;
 
         Ok(Self {
             queue,
@@ -59,7 +59,7 @@ impl Worker {
                     if let Ok(()) = change {
                         let height = self.height_rx.borrow().value();
                         tracing::info!(?height, "resetting ephemeral mempool state");
-                        self.app = App::new(self.storage.state().await?).await;
+                        self.app = App::new(self.storage.clone()).await;
                     } else {
                         tracing::info!("consensus worker shut down, shutting down mempool worker");
                         // The consensus worker shut down, we should too.

shielded-pool/src/component.rs
@@ -12,7 +12,7 @@ use penumbra_crypto::{
     merkle::{self, Frontier, NoteCommitmentTree, TreeExt},
     note, Address, Note, Nullifier, One, Value, STAKING_TOKEN_ASSET_ID,
 };
-use penumbra_storage::{State, StateExt};
+use penumbra_storage::{State, StateExt, Storage};
 use penumbra_transaction::{action::output, Action, Transaction};
 use tendermint::abci;
 use tracing::instrument;
@@ -27,19 +27,27 @@ pub struct ShieldedPool {
     compact_block: CompactBlock,
 }
 
-#[async_trait]
-impl Component for ShieldedPool {
-    #[instrument(name = "shielded_pool", skip(state))]
-    async fn new(state: State) -> Self {
-        let note_commitment_tree = Self::get_nct(&state).await.unwrap();
+impl ShieldedPool {
+    #[instrument(name = "shielded_pool", skip(storage))]
+    pub async fn new(storage: Storage) -> Self {
+        let note_commitment_tree = storage.get_nct().await.unwrap();
 
         Self {
-            state,
+            state: storage.state().await.unwrap(),
             note_commitment_tree,
             compact_block: Default::default(),
         }
     }
 
+    /// Get the current note commitment tree (this may not yet have been committed to the underlying
+    /// storage).
+    pub fn note_commitment_tree(&self) -> &NoteCommitmentTree {
+        &self.note_commitment_tree
+    }
+}
+
+#[async_trait]
+impl Component for ShieldedPool {
     #[instrument(name = "shielded_pool", skip(self, app_state))]
     async fn init_chain(&mut self, app_state: &genesis::AppState) {
         for allocation in &app_state.allocations {
@@ -124,7 +132,7 @@ impl Component for ShieldedPool {
                         .verify(
                             tx.transaction_body().merkle_root,
                             spend.body.value_commitment,
-                            spend.body.nullifier.clone(),
+                            spend.body.nullifier,
                             spend.body.rk,
                         )
                         .is_err()
@@ -138,7 +146,7 @@ impl Component for ShieldedPool {
                         return Err(anyhow::anyhow!("Double spend"));
                     }
 
-                    spent_nullifiers.insert(spend.body.nullifier.clone());
+                    spent_nullifiers.insert(spend.body.nullifier);
                 }
                 Action::Delegate(_delegate) => {
                     // Handled in the `Staking` component.
@@ -354,38 +362,8 @@ impl ShieldedPool {
         self.state
             .set_nct_anchor(self.compact_block.height, self.note_commitment_tree.root2())
             .await;
-        self.put_nct().await?;
 
         Ok(())
-    }
-
-    /// This is not part of the View trait because the NCT isn't a domain
-    /// type, and we'll be replacing it anyways, so there's not much point
-    /// implementing one now.  When switching to the TCT we should revisit.
-    async fn put_nct(&mut self) -> Result<()> {
-        let nct_data = bincode::serialize(&self.note_commitment_tree)?;
-        self.state
-            .write()
-            .await
-            .put(b"shielded_pool/nct_data".into(), nct_data);
-        Ok(())
-    }
-
-    /// This is an associated function rather than a method,
-    /// so that we can call it in the constructor to get the NCT.
-    /// NOTE: we may not need that any more now that we can use an
-    /// State on an empty database.
-    async fn get_nct(state: &State) -> Result<NoteCommitmentTree> {
-        if let Ok(Some(bytes)) = state
-            .read()
-            .await
-            .get(b"shielded_pool/nct_data".into())
-            .await
-        {
-            bincode::deserialize(&bytes).map_err(Into::into)
-        } else {
-            Ok(NoteCommitmentTree::new(0))
-        }
     }
 }
 

ibc/src/component.rs
@@ -23,10 +23,9 @@ pub struct IBCComponent {
     enabled: bool,
 }
 
-#[async_trait]
-impl Component for IBCComponent {
+impl IBCComponent {
     #[instrument(name = "ibc", skip(state))]
-    async fn new(state: State) -> Self {
+    pub async fn new(state: State) -> Self {
         let client = Ics2Client::new(state.clone()).await;
         let connection = connection::ConnectionComponent::new(state.clone()).await;
 
@@ -36,7 +35,10 @@ impl Component for IBCComponent {
             enabled: false,
         }
     }
+}
 
+#[async_trait]
+impl Component for IBCComponent {
     #[instrument(name = "ibc", skip(self, app_state))]
     async fn init_chain(&mut self, app_state: &genesis::AppState) {
         self.enabled = app_state.chain_params.ibc_enabled;

stake/src/component.rs
@@ -46,6 +46,15 @@ pub struct Staking {
 }
 
 impl Staking {
+    #[instrument(name = "staking", skip(state))]
+    pub async fn new(state: State) -> Self {
+        Self {
+            state,
+            delegation_changes: Default::default(),
+            tm_validator_updates: Default::default(),
+        }
+    }
+
     /// Updates the state of the given validator, performing all necessary state transitions.
     ///
     /// This method errors on illegal state transitions; since execution must be infallible,
@@ -618,7 +627,7 @@ impl Staking {
             .get(&delegation_denom)
             .copied()
             .unwrap_or(0);
-        let power = cur_rate_data.voting_power(total_delegation_tokens, &genesis_base_rate);
+        let power = cur_rate_data.voting_power(total_delegation_tokens, genesis_base_rate);
 
         // Update the validator to return its power to Tendermint for this block.
         self.tm_validator_updates
@@ -737,15 +746,6 @@ impl Staking {
 
 #[async_trait]
 impl Component for Staking {
-    #[instrument(name = "staking", skip(state))]
-    async fn new(state: State) -> Self {
-        Self {
-            state,
-            delegation_changes: Default::default(),
-            tm_validator_updates: Default::default(),
-        }
-    }
-
     #[instrument(name = "staking", skip(self, app_state))]
     async fn init_chain(&mut self, app_state: &genesis::AppState) {
         let starting_height = self.state.get_block_height().await.unwrap();
@@ -1212,7 +1212,7 @@ pub trait View: StateExt {
         slashing_penalty_bps: u64,
     ) -> Result<()> {
         let mut cur_rate = self
-            .current_validator_rate(&identity_key)
+            .current_validator_rate(identity_key)
             .await?
             .ok_or_else(|| {
                 anyhow::anyhow!("validator to be slashed did not have current rate in JMT")
@@ -1227,7 +1227,7 @@ pub trait View: StateExt {
             rate
         };
 
-        self.set_validator_rates(&identity_key, cur_rate, next_rate)
+        self.set_validator_rates(identity_key, cur_rate, next_rate)
             .await;
 
         Ok(())

chain/src/note_source.rs
@@ -34,7 +34,7 @@ impl NoteSource {
 impl TryFrom<[u8; 32]> for NoteSource {
     type Error = anyhow::Error;
     fn try_from(bytes: [u8; 32]) -> Result<Self> {
-        if &bytes[..CODE_INDEX] != &[0u8; CODE_INDEX][..] {
+        if bytes[..CODE_INDEX] != [0u8; CODE_INDEX][..] {
             Ok(Self::Transaction { id: bytes })
         } else {
             match (bytes[CODE_INDEX], &bytes[CODE_INDEX + 1..]) {
@@ -59,9 +59,9 @@ impl Protobuf<pb::NoteSource> for NoteSource {}
 impl TryFrom<pb::NoteSource> for NoteSource {
     type Error = anyhow::Error;
     fn try_from(note_source: pb::NoteSource) -> Result<Self> {
-        Ok(<[u8; 32]>::try_from(note_source.inner)
+        <[u8; 32]>::try_from(note_source.inner)
             .map_err(|_| anyhow!("expected 32 bytes"))?
-            .try_into()?)
+            .try_into()
     }
 }
 

component/Cargo.toml
@@ -6,7 +6,6 @@ edition = "2021"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
-penumbra-storage = { path = "../storage" }
 penumbra-transaction = { path = "../transaction" }
 penumbra-chain = { path = "../chain" }
 async-trait = "0.1.52"

commit 61dd192723d150c86c9706c8f44311907c853ae7

pd: ensure components have shared state
This commit fixes a bug introduced in #796, which removed the `new` method from
the `Component` trait, in order to be able to "sideload" a copy of the note
commitment tree kept in a sidecar part of the underlying storage, rather than
in the (merklized) application state.

In that change, the `ShieldedPool` component was changed to take a raw
`Storage`, rather than a `State`, so that it would be able to read the NCT out
on its own.  But it's important that all of the components use the *same*
shared state, rather than each creating their own fork off of the version
currently in persistent storage.

Co-Authored-By: Ava Howell <ava@penumbra.zone>

pd/src/components/app.rs
@@ -25,14 +25,23 @@ pub struct App {
 }
 
 impl App {
-    #[instrument(skip(storage))]
     pub async fn new(storage: Storage) -> Self {
-        let staking = Staking::new(storage.state().await.unwrap()).await;
-        let ibc = IBCComponent::new(storage.state().await.unwrap()).await;
-        let shielded_pool = ShieldedPool::new(storage.clone()).await;
+        tracing::info!("initializing App instance");
+
+        // The NCT (and *only* the NCT) is stored outside of the main state,
+        // so that the backing format for the NCT isn't consensus-critical.
+        // (The NCT data is already committed to by the NCT root, which is in the state).
+        let note_commitment_tree = storage.get_nct().await.unwrap();
+
+        // All of the components need to use the *same* shared state.
+        let state = storage.state().await.unwrap();
+
+        let staking = Staking::new(state.clone()).await;
+        let ibc = IBCComponent::new(state.clone()).await;
+        let shielded_pool = ShieldedPool::new(state.clone(), note_commitment_tree).await;
 
         Self {
-            state: storage.state().await.unwrap(),
+            state,
             shielded_pool,
             staking,
             ibc,
@@ -46,18 +55,26 @@ impl App {
     /// as an empty state over top of the newly written storage.
     #[instrument(skip(self, storage))]
     pub async fn commit(&mut self, storage: Storage) -> Result<(RootHash, Version)> {
-        // Store the latest NCT in the storage (this is not in the JMT State because we don't want
-        // the serialization format to be consensus critical; only the root is stored in the State)
+        // We want to store the latest NCT in a sidecar part of the storage,
+        // rather than the Penumbra state, because the serialization format for
+        // the NCT should not be consensus-critical.  We need to grab a copy of
+        // the entire NCT, so we can use it to re-instantiate the ShieldedPool.
+        let note_commitment_tree = self.shielded_pool.note_commitment_tree().clone();
         storage
             .put_nct(self.shielded_pool.note_commitment_tree())
             .await?;
+
         // Commit the pending writes, clearing the state.
         let (root_hash, version) = self.state.write().await.commit(storage.clone()).await?;
         tracing::debug!(?root_hash, version, "finished committing state");
-        // Now re-instantiate all of the components:
+
+        // Get the latest version of the state, now that we've committed it.
+        self.state = storage.state().await?;
+
+        // Now re-instantiate all of the components so they all have the same shared state.
         self.staking = Staking::new(self.state.clone()).await;
         self.ibc = IBCComponent::new(self.state.clone()).await;
-        self.shielded_pool = ShieldedPool::new(storage).await;
+        self.shielded_pool = ShieldedPool::new(self.state.clone(), note_commitment_tree).await;
 
         Ok((root_hash, version))
     }

pd/src/consensus/worker.rs
@@ -11,7 +11,7 @@ use tendermint::{
     block,
 };
 use tokio::sync::{mpsc, watch};
-use tracing::Instrument;
+use tracing::{instrument, Instrument};
 
 use super::Message;
 use crate::App;
@@ -24,6 +24,7 @@ pub struct Worker {
 }
 
 impl Worker {
+    #[instrument(skip(storage, queue, height_tx), name = "consensus::Worker::new")]
     pub async fn new(
         storage: Storage,
         queue: mpsc::Receiver<Message>,

pd/src/mempool/worker.rs
@@ -7,7 +7,7 @@ use penumbra_storage::Storage;
 use penumbra_transaction::Transaction;
 use tendermint::block;
 use tokio::sync::{mpsc, watch};
-use tracing::Instrument;
+use tracing::{instrument, Instrument};
 
 use super::Message;
 use crate::App;
@@ -20,6 +20,7 @@ pub struct Worker {
 }
 
 impl Worker {
+    #[instrument(skip(storage, queue, height_rx), name = "mempool::Worker::new")]
     pub async fn new(
         storage: Storage,
         queue: mpsc::Receiver<Message>,

shielded-pool/src/component.rs
@@ -12,7 +12,7 @@ use penumbra_crypto::{
     merkle::{self, Frontier, NoteCommitmentTree, TreeExt},
     note, Address, Note, NotePayload, Nullifier, One, Value, STAKING_TOKEN_ASSET_ID,
 };
-use penumbra_storage::{State, StateExt, Storage};
+use penumbra_storage::{State, StateExt};
 use penumbra_transaction::{Action, Transaction};
 use tendermint::abci;
 use tracing::instrument;
@@ -28,12 +28,10 @@ pub struct ShieldedPool {
 }
 
 impl ShieldedPool {
-    #[instrument(name = "shielded_pool", skip(storage))]
-    pub async fn new(storage: Storage) -> Self {
-        let note_commitment_tree = storage.get_nct().await.unwrap();
-
+    #[instrument(name = "shielded_pool", skip(state, note_commitment_tree))]
+    pub async fn new(state: State, note_commitment_tree: NoteCommitmentTree) -> Self {
         Self {
-            state: storage.state().await.unwrap(),
+            state,
             note_commitment_tree,
             compact_block: Default::default(),
         }