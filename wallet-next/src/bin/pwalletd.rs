20c9214730f08012ac86dc87b6ae9c695e8703c7
aubrey <aubrey@penumbra.zone>
2022-05-16 18:16:36 -0700

    Add wallet service init/start to pwalletd

--- wallet-next/src/bin/pwalletd.rs
+++ wallet-next/src/bin/pwalletd.rs
@@ -1,8 +1,14 @@
 #![allow(clippy::clone_on_copy)]
 use anyhow::Result;
+use penumbra_chain::params::ChainParams;
+use penumbra_crypto::FullViewingKey;
+use penumbra_proto::client::oblivious::oblivious_query_client::ObliviousQueryClient;
+use penumbra_proto::wallet::wallet_protocol_server::WalletProtocolServer;
+use penumbra_wallet_next::WalletService;
 use std::env;
-use std::path::PathBuf;
+use std::str::FromStr;
 use structopt::StructOpt;
+use tonic::transport::Server;
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "pwalletd",
@@ -21,24 +27,21 @@ enum Command {
     Init {
         /// The path used to store the SQLite state database.
         #[structopt(short, long)]
-        sqlite_path: PathBuf,
+        sqlite_path: String,
         /// The full viewing key
         #[structopt(short, long)]
         fvk: String,
     },
     Start {
+        /// The path used to store the SQLite state database.
+        #[structopt(short, long)]
+        sqlite_path: String,
         /// Bind the services to this host.
         #[structopt(short, long, default_value = "127.0.0.1")]
         host: String,
         /// Bind the wallet gRPC server to this port.
         #[structopt(short, long, default_value = "8081")]
         wallet_port: u16,
-        /// The address of the pd+tendermint node.
-        #[structopt(short, long, default_value = "testnet.penumbra.zone")]
-        node: String,
-        /// The port to use to speak to pd.
-        #[structopt(short, long, default_value = "8080")]
-        pd_port: u16,
     },
 }
 #[tokio::main]
@@ -47,19 +50,40 @@ async fn main() -> Result<()> {
     let opt = Opt::from_args();
 
     match opt.cmd {
-        Command::Init {
-            sqlite_path: _,
-            fvk: _,
-        } => todo!(),
+        Command::Init { sqlite_path, fvk } => {
+            penumbra_wallet_next::Storage::initialize(
+                sqlite_path,
+                FullViewingKey::from_str(fvk.as_ref())?,
+                ChainParams::default(),
+            )
+            .await?;
+            Ok(())
+        }
         Command::Start {
+            sqlite_path,
             host,
             wallet_port,
-            node,
-            pd_port,
         } => {
-            tracing::info!(?host, ?wallet_port, ?node, ?pd_port, "starting pwalletd");
+            tracing::info!(?sqlite_path, ?host, ?wallet_port, "starting pwalletd");
 
-            unimplemented!();
+            let storage = penumbra_wallet_next::Storage::load(sqlite_path).await?;
+            let client =
+                ObliviousQueryClient::connect(format!("http://{}:{}", host, wallet_port)).await?;
+            let service = WalletService::new(storage, client).await?;
+
+            tokio::task::Builder::new()
+                .name("wallet_grpc_server")
+                .spawn(
+                    Server::builder()
+                        .add_service(WalletProtocolServer::new(service))
+                        .serve(
+                            format!("{}:{}", host, wallet_port)
+                                .parse()
+                                .expect("this is a valid address"),
+                        ),
+                );
+
+            todo!()
         }
     }
 }
