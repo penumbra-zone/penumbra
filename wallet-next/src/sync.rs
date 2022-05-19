d8407c79386c1f309c09eba8425ddcac91e92564
Kenny Foner <kwf@very.science>
2022-05-19 15:24:21 -0400

    Adapt wallet-next to use TCT

--- wallet-next/src/sync.rs
+++ wallet-next/src/sync.rs
@@ -32,24 +32,19 @@ pub fn scan_block(
         encrypted_note,
     } in note_payloads
     {
-        // Unconditionally insert the note commitment into the merkle tree
-        tracing::debug!(?note_commitment, "appending to note commitment tree");
-        note_commitment_tree.append(&note_commitment);
-
         // Try to decrypt the encrypted note using the ephemeral key and persistent incoming
         // viewing key -- if it doesn't decrypt, it wasn't meant for us.
         if let Ok(note) = Note::decrypt(encrypted_note.as_ref(), fvk.incoming(), &ephemeral_key) {
             tracing::debug!(?note_commitment, ?note, "found note while scanning");
-            // Mark the most-recently-inserted note commitment (the one corresponding to this
-            // note) as worth keeping track of, because it's ours
-            note_commitment_tree.witness();
 
-            // Insert the note associated with its computed nullifier into the nullifier map
-            let (pos, _auth_path) = note_commitment_tree
-                .authentication_path(&note_commitment)
-                .expect("we just witnessed this commitment");
+            // Keep track of this commitment for later witnessing
+            note_commitment_tree.insert(tct::Witness::Keep, note_commitment);
 
-            let nullifier = fvk.derive_nullifier(pos, &note_commitment);
+            let position = note_commitment_tree
+                .position_of(note_commitment)
+                .expect("witnessed note commitment is present");
+
+            let nullifier = fvk.derive_nullifier(position, &note_commitment);
 
             let diversifier = &note.diversifier();
 
@@ -63,6 +58,9 @@ pub fn scan_block(
             };
 
             new_notes.push(record);
+        } else {
+            // Don't remember this commitment; it wasn't ours
+            note_commitment_tree.insert(tct::Witness::Forget, note_commitment);
         }
     }
 
