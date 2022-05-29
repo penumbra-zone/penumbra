71931eaa28c4cde4130d9c07cde68503657f4986
Henry de Valence <hdevalence@penumbra.zone>
2022-05-29 02:03:29 -0700

    view: make note queries dynamic
    The query is built at runtime, but previously we were using the statically
    typed `query!` macro that has to build the query string at compile time.  So
    the parameters that were supposed to be part of the query structure were
    instead being treated as strings.
    
    To fix this, use the query _function_, rather than macro.  But this is annoying
    because it returns a generic Row type, so implement `sqlx::Decode` for
    `NoteRecord`.  This is also annoying, because we have to manually convert every
    single failure case into a `sqlx::Error`.

--- view/src/note_record.rs
+++ view/src/note_record.rs
@@ -1,8 +1,14 @@
-use penumbra_crypto::{keys::DiversifierIndex, note, Note, Nullifier};
+use penumbra_crypto::{
+    asset,
+    ka::Public,
+    keys::{Diversifier, DiversifierIndex},
+    note, FieldExt, Fq, Note, Nullifier, Value,
+};
 use penumbra_proto::{view as pb, Protobuf};
 use penumbra_tct as tct;
 
 use serde::{Deserialize, Serialize};
+use sqlx::Row;
 
 /// Corresponds to the NoteRecord proto
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -55,6 +61,108 @@ impl TryFrom<pb::NoteRecord> for NoteRecord {
             height_created: v.height_created,
             height_spent: v.height_spent,
             position: v.position.into(),
+        })
+    }
+}
+
+impl<'r> sqlx::FromRow<'r, sqlx::sqlite::SqliteRow> for NoteRecord {
+    fn from_row(row: &'r sqlx::sqlite::SqliteRow) -> Result<Self, sqlx::Error> {
+        // This is not a fun time.
+        // Mostly on account of sqlx::Error.
+
+        let diversifier =
+            Diversifier::try_from(row.get::<'r, &[u8], _>("diversifier")).map_err(|e| {
+                sqlx::Error::ColumnDecode {
+                    index: "diversifier".to_string(),
+                    source: e.into(),
+                }
+            })?;
+
+        let diversifier_index = DiversifierIndex::try_from(
+            row.get::<'r, &[u8], _>("diversifier_index"),
+        )
+        .map_err(|e| sqlx::Error::ColumnDecode {
+            index: "diversifier_index".to_string(),
+            source: e.into(),
+        })?;
+
+        let transmission_key = Public(
+            <[u8; 32]>::try_from(row.get::<'r, &[u8], _>("transmission_key")).map_err(|e| {
+                sqlx::Error::ColumnDecode {
+                    index: "transmission_key".to_string(),
+                    source: e.into(),
+                }
+            })?,
+        );
+
+        let amount = row.get::<'r, i64, _>("amount") as u64;
+
+        let asset_id = asset::Id(
+            Fq::from_bytes(
+                <[u8; 32]>::try_from(row.get::<'r, &[u8], _>("asset_id")).map_err(|e| {
+                    sqlx::Error::ColumnDecode {
+                        index: "asset_id".to_string(),
+                        source: e.into(),
+                    }
+                })?,
+            )
+            .map_err(|e| sqlx::Error::ColumnDecode {
+                index: "asset_id".to_string(),
+                source: e.into(),
+            })?,
+        );
+
+        let note_blinding = Fq::from_bytes(
+            <[u8; 32]>::try_from(row.get::<'r, &[u8], _>("blinding_factor")).map_err(|e| {
+                sqlx::Error::ColumnDecode {
+                    index: "blinding_factor".to_string(),
+                    source: e.into(),
+                }
+            })?,
+        )
+        .map_err(|e| sqlx::Error::ColumnDecode {
+            index: "blinding_factor".to_string(),
+            source: e.into(),
+        })?;
+
+        let note_commitment = note::Commitment::try_from(
+            row.get::<'r, &[u8], _>("note_commitment"),
+        )
+        .map_err(|e| sqlx::Error::ColumnDecode {
+            index: "note_commitment".to_string(),
+            source: e.into(),
+        })?;
+
+        let nullifier = Nullifier::try_from(row.get::<'r, &[u8], _>("nullifier")).map_err(|e| {
+            sqlx::Error::ColumnDecode {
+                index: "nullifier".to_string(),
+                source: e.into(),
+            }
+        })?;
+
+        let height_created = row.get::<'r, i64, _>("height_created") as u64;
+        let height_spent = row
+            .get::<'r, Option<i64>, _>("height_spent")
+            .map(|v| v as u64);
+        let position = (row.get::<'r, i64, _>("position") as u64).into();
+
+        let value = Value { amount, asset_id };
+        let note =
+            Note::from_parts(diversifier, transmission_key, value, note_blinding).map_err(|e| {
+                sqlx::Error::ColumnDecode {
+                    index: "note".to_string(),
+                    source: e.into(),
+                }
+            })?;
+
+        Ok(NoteRecord {
+            note_commitment,
+            note,
+            diversifier_index,
+            nullifier,
+            position,
+            height_created,
+            height_spent,
         })
     }
 }
