1636dafc747c2303aa6ccc7f0254c9c086f521d4
Kenny Foner <kwf@very.science>
2022-06-03 12:30:00 -0400

    Add state keys for quarantine related parts of state

--- component/src/shielded_pool/state_key.rs
+++ component/src/shielded_pool/state_key.rs
@@ -1,5 +1,5 @@
 use jmt::KeyHash;
-use penumbra_crypto::{asset, note, Nullifier};
+use penumbra_crypto::{asset, note, IdentityKey, Nullifier};
 use penumbra_tct::Root;
 
 pub fn token_supply(asset_id: &asset::Id) -> KeyHash {
@@ -36,4 +36,40 @@ pub fn spent_nullifier_lookup(nullifier: &Nullifier) -> KeyHash {
 
 pub fn commission_amounts(height: u64) -> KeyHash {
     format!("staking/commission_amounts/{}", height).into()
+}
+
+pub fn quarantined_notes_to_apply_at_height(height: u64) -> KeyHash {
+    format!(
+        "shielded_pool/quarantined_notes_to_apply_at_height/{}",
+        height
+    )
+    .into()
+}
+
+pub fn quarantined_nullifiers_to_apply_at(height: u64) -> KeyHash {
+    format!(
+        "shielded_pool/quarantined_nullifiers_to_apply_at_height/{}",
+        height
+    )
+    .into()
+}
+
+pub fn quarantined_notes_connected_to_validator(validator_identity: IdentityKey) -> KeyHash {
+    format!(
+        "shielded_pool/quarantined_notes_connected_to_validator/{}",
+        validator_identity
+    )
+    .into()
+}
+
+pub fn quarantined_nullifiers_connected_to_validator(validator_identity: IdentityKey) -> KeyHash {
+    format!(
+        "shielded_pool/quarantined_nullifiers_connected_to_validator/{}",
+        validator_identity
+    )
+    .into()
+}
+
+pub fn quarantined_nullifier_lookup(nullifier: &Nullifier) -> KeyHash {
+    format!("shielded_pool/quarantined_spent_nullifiers/{}", nullifier).into()
 }
