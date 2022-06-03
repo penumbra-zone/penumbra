e5583b3ef48f85bdb71a3b504fd8c8ea88d9c7f5
Ava Howell <ava@penumbra.zone>
2022-06-03 01:26:28 -0700

    add missing IbcAction import to client test

--- ibc/src/component/client.rs
+++ ibc/src/component/client.rs
@@ -670,6 +670,7 @@ mod tests {
     use ibc_proto::ibc::core::client::v1::MsgCreateClient as RawMsgCreateClient;
     use ibc_proto::ibc::core::client::v1::MsgUpdateClient as RawMsgUpdateClient;
     use penumbra_proto::ibc::ibc_action::Action as IbcActionInner;
+    use penumbra_proto::ibc::IbcAction;
     use penumbra_proto::Message;
     use penumbra_storage::Storage;
     use penumbra_tct as tct;
