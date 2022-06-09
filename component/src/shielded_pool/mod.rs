9ade384bbdc5aa096eec045059cf105c962296ab
Kenny Foner <kwf@very.science>
2022-06-09 19:22:39 -0400

    Define domain type for Quarantined proto type

--- component/src/shielded_pool/mod.rs
+++ component/src/shielded_pool/mod.rs
@@ -3,8 +3,10 @@ mod component;
 pub(crate) mod event;
 mod metrics;
 
+pub mod quarantined;
 pub mod state_key;
 
 pub use self::metrics::register_metrics;
 pub use commission::{CommissionAmount, CommissionAmounts};
 pub use component::{ShieldedPool, View};
+pub use quarantined::Quarantined;
