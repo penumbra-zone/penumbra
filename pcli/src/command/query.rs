2d5a0acc1c044080bae9e3d6d2cb669bd6c1edba
Kenny Foner <kwf@very.science>
2022-06-10 12:32:01 -0400

    Fixup: take correct argument type for quarantined_to_apply

--- pcli/src/command/query.rs
+++ pcli/src/command/query.rs
@@ -79,7 +79,7 @@ impl ShieldedPool {
         use penumbra_component::shielded_pool::state_key;
         match self {
             ShieldedPool::Anchor { height } => state_key::anchor_by_height(height),
-            ShieldedPool::QuarantinedToApply { epoch } => state_key::quarantined_to_apply(epoch),
+            ShieldedPool::QuarantinedToApply { epoch } => state_key::quarantined_to_apply(*epoch),
         }
     }
 
