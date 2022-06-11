3eb268dfbf66936ae8eebc8bec08a785863a7d7c
Eliza Weisman <eliza@buoyant.io>
2022-06-11 11:31:29 -0700

    pcli: make `BoxGrpcService` `Clone + Sync`
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- pcli/src/box_grpc_svc.rs
+++ pcli/src/box_grpc_svc.rs
@@ -5,11 +5,11 @@ use tonic::{
     codegen::http as grpc,
     transport::{self, Endpoint},
 };
-use tower::{util::UnsyncBoxService, Service, ServiceBuilder};
+use tower::{util::BoxCloneService, Service, ServiceBuilder};
 
 /// A type-erased gRPC service.
 pub(crate) type BoxGrpcService =
-    UnsyncBoxService<grpc::Request<ReqBody>, grpc::Response<RspBody>, BoxError>;
+    BoxCloneService<grpc::Request<ReqBody>, grpc::Response<RspBody>, BoxError>;
 
 pub(crate) type BoxError = Box<dyn std::error::Error + Send + Sync + 'static>;
 
@@ -23,23 +23,25 @@ pub(crate) async fn connect(ep: Endpoint) -> anyhow::Result<BoxGrpcService> {
         .map_response(|rsp: grpc::Response<transport::Body>| rsp.map(box_rsp_body))
         .map_err(BoxError::from)
         .service(conn);
-    Ok(UnsyncBoxService::new(svc))
+    Ok(BoxCloneService::new(svc))
 }
 
 /// Constructs a [`BoxGrpcService`] by erasing the type of an `S`-typed local
 /// (in-process) service instance.
 pub(crate) fn local<S, B>(svc: S) -> BoxGrpcService
 where
-    S: Service<grpc::Request<ReqBody>, Response = grpc::Response<B>> + 'static,
-    BoxError: From<S::Error> + From<B::Error>,
+    S: Service<grpc::Request<ReqBody>, Response = grpc::Response<B>>,
+    S: Clone + Send + Sync + 'static,
     S::Error: 'static,
+    S::Future: Send,
+    BoxError: From<S::Error> + From<B::Error>,
     B: Body<Data = Bytes> + Send + 'static,
 {
     let svc = ServiceBuilder::new()
         .map_response(|rsp: grpc::Response<B>| rsp.map(box_rsp_body))
         .map_err(BoxError::from)
         .service(svc);
-    UnsyncBoxService::new(svc)
+    BoxCloneService::new(svc)
 }
 
 /// Erases a response body's `Error` type, returning a `RspBody`.
