8ba7127019bb853cc91aced3f1541f68338b6d08
aubrey <aubrey@penumbra.zone>
2022-06-16 12:45:01 -0700

    Refactor of quarantine view data model

--- view/migrations/20220415094212_init.sql
+++ view/migrations/20220415094212_init.sql
@@ -5,6 +5,7 @@ CREATE TABLE sync_height (height BIGINT NOT NULL);
 CREATE TABLE note_commitment_tree (bytes BLOB NOT NULL);
 
 -- Minimal data required for balance tracking
+-- Meant to represent notes which have been accepted into the note set
 CREATE TABLE notes (
     note_commitment         BLOB PRIMARY KEY NOT NULL,
     height_spent            BIGINT, --null if unspent, otherwise spent at height_spent 
@@ -39,4 +40,38 @@ CREATE INDEX nullifier_idx on notes ( nullifier );
 CREATE TABLE assets (
     asset_id BLOB PRIMARY KEY NOT NULL,
     denom    TEXT NOT NULL
+);
+
+CREATE TABLE quarantined_notes (
+    note_commitment         BLOB PRIMARY KEY NOT NULL,
+    height_created          BIGINT NOT NULL,
+    -- note contents themselves:
+    diversifier             BLOB NOT NULL,
+    amount                  BIGINT NOT NULL,
+    asset_id                BLOB NOT NULL,
+    transmission_key        BLOB NOT NULL,
+    blinding_factor         BLOB NOT NULL,
+    -- precomputed decryption of the diversifier
+    diversifier_index       BLOB NOT NULL,
+    -- the quarantine status of the note
+    unbonding_epoch         BIGINT NOT NULL,
+    identity_key            BLOB NOT NULL
+);
+
+CREATE INDEX quarantined_notes_idx ON quarantined_notes (
+    identity_key,       -- first by identity key
+    unbonding_epoch,    -- then by unbonding epoch
+    diversifier_index,  -- then filter by account
+    amount,             -- then by amount
+    height_created      -- we don't really care about this, except informationally
+);
+
+-- Nullifiers and identity keys added here in the event of a provisional spend (height_spent updated in notes table, then provisionality of that spend is recorded here)
+CREATE TABLE quarantined_nullifiers (
+    nullifier               BLOB PRIMARY KEY NOT NULL,
+    identity_key            BLOB NOT NULL
+);
+
+CREATE INDEX identity_key_idx ON quarantined_nullifiers (
+    identity_key
 );
