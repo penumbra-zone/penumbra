a461685fa90645e553725b412433edd4a8801338
Ava Howell <ava@penumbra.zone>
2022-06-17 14:00:21 -0700

    add documentation

--- component/src/ibc/ibc_handler.rs
+++ component/src/ibc/ibc_handler.rs
@@ -1,3 +1,11 @@
+/// IBC "app handlers" for the IBC component.
+///
+/// An app handler defines an interface for any IBC application to consume verified IBC events from
+/// the core IBC component. IBC applications listen to channel events that occur on the Port ID
+/// that they have subscribed to, and apply application-specific state transition logic.
+///
+/// The primary IBC application is the ICS20 transfer application, which allows for interchain
+/// token transfers.
 use anyhow::Result;
 use async_trait::async_trait;
 use ibc::core::ics04_channel::msgs::acknowledgement::MsgAcknowledgement;
@@ -12,8 +20,9 @@ use ibc::core::ics04_channel::msgs::timeout::MsgTimeout;
 use ibc::core::ics24_host::identifier::PortId;
 use std::collections::HashMap;
 
-// defines an asynchronous handler for core IBC events, to be implemented by IBC apps (such as an
-// ICS20 transfer implementation, or interchain accounts implementation).
+/// AppHandlerCheck defines the interface for an IBC application to consume IBC channel and packet
+/// events, and apply their validation logic. This validation logic is used for stateful validation
+/// only.
 #[async_trait]
 pub trait AppHandlerCheck: Send + Sync {
     async fn chan_open_init_check(&self, msg: &MsgChannelOpenInit) -> Result<()>;
@@ -28,6 +37,9 @@ pub trait AppHandlerCheck: Send + Sync {
     async fn acknowledge_packet_check(&self, msg: &MsgAcknowledgement) -> Result<()>;
 }
 
+// AppHandlerExecute defines the interface for an IBC application to consume IBC channel and packet
+// events and apply their state transition logic. The IBC component will only call these methods
+// once the transaction has been validated using the AppHandlerCheck interface.
 #[async_trait]
 pub trait AppHandlerExecute: Send + Sync {
     async fn chan_open_init_execute(&mut self, msg: &MsgChannelOpenInit);
@@ -44,6 +56,8 @@ pub trait AppHandlerExecute: Send + Sync {
 
 pub trait AppHandler: AppHandlerCheck + AppHandlerExecute {}
 
+/// an AppRouter is an implementation of AppHandler that is the root router for all IBC
+/// applications. Applications can register themselves on an IBC port by calling AppRouter.bind().
 pub struct AppRouter {
     handlers: HashMap<PortId, Box<dyn AppHandler>>,
 }
@@ -54,6 +68,9 @@ impl AppRouter {
             handlers: HashMap::new(),
         }
     }
+
+    /// Bind an IBC application, given by `handler`, to the given port ID. This will panic if there
+    /// is already an application bound to the given port ID.
     pub fn bind(&mut self, port_id: PortId, handler: Box<dyn AppHandler>) {
         if self.handlers.contains_key(&port_id) {
             panic!("AppRouter: handler already bound for port {}", port_id);
