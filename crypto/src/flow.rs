fb5e1225868cbed47cb8f77f8c16c1de04e31e34
Chris Czub <chris@penumbra.zone>
2022-06-21 16:49:57 -0400

    Add MockFlowCiphertext

---
+++ crypto/src/flow.rs
@@ -0,0 +1,32 @@
+use penumbra_proto::dex as pb;
+
+#[derive(Serialize, Deserialize)]
+#[serde(try_from = "pb::MockFlowCiphertext", into = "pb::MockFlowCiphertext")]
+// TODO: should not be a raw u64, needs to be constant-length
+struct MockFlowCiphertext(u64);
+
+// Fake implementation for now, TODO: replace w/ additively homomorphic encryption impl
+impl MockFlowCiphertext {
+    pub fn mock_decrypt(&self) -> u64 {
+        self.0
+    }
+
+    pub fn add(&mut self, amount: u64) {
+        self.0 += amount
+    }
+}
+
+impl Protobuf<pb::MockFlowCiphertext> for MockFlowCiphertext {}
+
+impl From<MockFlowCiphertext> for pb::MockFlowCiphertext {
+    fn from(ik: MockFlowCiphertext) -> Self {
+        pb::MockFlowCiphertext { value: ik.0 }
+    }
+}
+
+impl TryFrom<pb::MockFlowCiphertext> for MockFlowCiphertext {
+    type Error = anyhow::Error;
+    fn try_from(ct: pb::MockFlowCiphertext) -> Result<Self, Self::Error> {
+        Ok(Self(ct.value))
+    }
+}
