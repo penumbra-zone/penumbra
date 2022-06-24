507b3452e03e57a0072cd87912d6f546591b4795
Kenny Foner <kwf@very.science>
2022-06-24 14:28:16 -0400

    Add missing docs

--- tct/src/internal/build/iresult.rs
+++ tct/src/internal/build/iresult.rs
@@ -10,6 +10,8 @@ pub enum IResult<C: Build> {
 }
 
 impl<C: Build> IResult<C> {
+    /// Change the type that is constructed by an [`IResult`], by providing a way to map over the
+    /// complete and incomplete types.
     pub fn map<D: Build>(
         self,
         on_incomplete: impl FnOnce(C) -> D,
@@ -37,12 +39,20 @@ impl<C: Build> IResult<C> {
 #[error("traversal incomplete, awaiting more instructions")]
 pub struct Incomplete;
 
+/// An instruction for constructing the tree was given which was not valid.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Error)]
 pub enum Unexpected {
+    /// The instruction said to construct a node, but only a leaf could be constructed.
     #[error("unexpected `Node` instruction; expected `Leaf`")]
     Node,
+    /// The instruction said to construct a leaf, but only a node could be constructed.
     #[error("unexpected `Leaf` instruction; expected `Node`")]
     Leaf,
+    /// A sequence of instructions tried to construct an internal node which didn't have any children.
+    ///
+    /// One structural invariant of the tree is that all internal nodes have at least one child,
+    /// which preserves the fact that the tree is _maximally pruned_: no dangling internal nodes
+    /// exist without witnessed children beneath them.
     #[error("unexpected: at least one child of internal node must be witnessed")]
     Unwitnessed,
 }
