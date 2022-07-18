796881f177cc1f05aaf0cb2f28afa52d14ee9e1e
aaronwinter <erwan.ounn.84@gmail.com>
2022-07-18 18:08:00 +0300

    refactor(component/stake): factor explicit key formatting to `stake::state_key`

--- component/src/stake/state_key.rs
+++ component/src/stake/state_key.rs
@@ -1,5 +1,43 @@
 use jmt::KeyHash;
+use penumbra_crypto::IdentityKey;
+use tendermint::{PublicKey, block};
+
+pub fn validators(id: &IdentityKey) -> KeyHash {
+    format!("staking/validators/{}", id).into()
+}
+
+pub fn state_by_validator(id: &IdentityKey) -> KeyHash {
+    format!("staking/validators/{}/state", id).into()
+}
+
+pub fn current_rate_by_validator(id: &IdentityKey) -> KeyHash {
+    format!("staking/validators/{}/rate/current", id).into()
+}
+
+pub fn next_rate_by_validator(id: &IdentityKey) -> KeyHash {
+    format!("staking/validators/{}/rate/next", id).into()
+}
+
+pub fn power_by_validator(id: &IdentityKey) -> KeyHash {
+    format!("staking/validators/{}/power", id).into()
+}
+
+pub fn bonding_state_by_validator(id: &IdentityKey) -> KeyHash {
+    format!("staking/validators/{}/bonding_state", id).into()
+}
+
+pub fn uptime_by_validator(id: &IdentityKey) -> KeyHash {
+    format!("staking/validator_uptime/{}", id).into()
+}
 
 pub fn slashed_validators(height: u64) -> KeyHash {
     format!("staking/slashed_validators/{}", height).into()
+}
+
+pub fn consensus_key(pk: &PublicKey) -> KeyHash {
+    format!("staking/consensus_key/{}", pk.to_hex()).into()
+}
+
+pub fn delegation_changes_by_height(height: u64) -> KeyHash {
+    format!("staking/delegation_changes/{}", height).into()
 }
