4c4a6a3d3302feb546fd7b6969705348a98d013f
Henry de Valence <hdevalence@penumbra.zone>
2022-08-06 17:42:13 -0700

    dex: add lp actions to Transaction
    Currently, these are all immediately rejected by the dex component, because the
    LP position actions can mint tokens, and this is only safe if the chain is
    checking against the reserves, which it doesn't yet do.

--- component/src/dex/component.rs
+++ component/src/dex/component.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use async_trait::async_trait;
 use penumbra_chain::genesis;
 use penumbra_storage::State;
-use penumbra_transaction::Transaction;
+use penumbra_transaction::{Action, Transaction};
 use tendermint::abci;
 use tracing::instrument;
 
@@ -26,8 +26,23 @@ impl Component for Dex {
     #[instrument(name = "dex", skip(self, _ctx, _begin_block))]
     async fn begin_block(&mut self, _ctx: Context, _begin_block: &abci::request::BeginBlock) {}
 
-    #[instrument(name = "dex", skip(_ctx, _tx))]
-    fn check_tx_stateless(_ctx: Context, _tx: &Transaction) -> Result<()> {
+    #[instrument(name = "dex", skip(_ctx, tx))]
+    fn check_tx_stateless(_ctx: Context, tx: &Transaction) -> Result<()> {
+        // It's important to reject all LP actions for now, to prevent
+        // inflation / minting bugs until we implement all required checks
+        // (e.g., minting tokens by withdrawing reserves we don't check)
+        for action in tx.transaction_body.actions.iter() {
+            match action {
+                Action::PositionOpen { .. }
+                | Action::PositionClose { .. }
+                | Action::PositionWithdraw { .. }
+                | Action::PositionRewardClaim { .. } => {
+                    return Err(anyhow::anyhow!("lp actions not supported yet"));
+                }
+                _ => {}
+            }
+        }
+
         // TODO: implement for Swap/SwapClaim
         Ok(())
     }
