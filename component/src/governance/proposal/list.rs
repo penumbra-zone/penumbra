d519a7242d9cb70bd38da685a7bfed5b42ae9497
Kenny Foner <kwf@very.science>
2022-08-16 13:18:38 -0400

    Finish filling in proposal submit implementation

--- component/src/governance/proposal/list.rs
+++ component/src/governance/proposal/list.rs
@@ -1,7 +1,30 @@
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeSet;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+use penumbra_proto::{governance as pb, Protobuf};
+
+/// A protobuf-represented duplicate-free set of proposal ids.
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct ProposalList {
-    proposals: BTreeSet<u64>,
+    pub proposals: BTreeSet<u64>,
+}
+
+impl Protobuf<pb::ProposalList> for ProposalList {}
+
+impl From<ProposalList> for pb::ProposalList {
+    fn from(list: ProposalList) -> Self {
+        pb::ProposalList {
+            proposals: list.proposals.into_iter().collect(),
+        }
+    }
+}
+
+impl TryFrom<pb::ProposalList> for ProposalList {
+    type Error = anyhow::Error;
+
+    fn try_from(msg: pb::ProposalList) -> Result<Self, Self::Error> {
+        Ok(ProposalList {
+            proposals: msg.proposals.into_iter().collect(),
+        })
+    }
 }
