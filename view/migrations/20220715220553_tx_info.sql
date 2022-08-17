84937ebfaaa9004abde983fe189ef35ce8be273e
aubrey <aubrey@penumbra.zone>
2022-08-17 16:27:24 -0700

    Add or modify view tables to hold extended transaction data

--- view/migrations/20220715220553_tx_info.sql
+++ view/migrations/20220715220553_tx_info.sql
@@ -7,5 +7,19 @@ CREATE TABLE tx_by_nullifier (
 
 CREATE TABLE tx (
     tx_hash                 BLOB PRIMARY KEY NOT NULL,
-    tx_bytes                BLOB NOT NULL
+    binding_sig             BLOB NOT NULL,
+    anchor                  BLOB NOT NULL,
+    expiry_height           INTEGER NOT NULL,
+    chain_id                TEXT NOT NULL,
+    fee                     INTEGER NOT NULL
+);
+
+CREATE TABLE tx_actions (
+    tx_hash                 BLOB PRIMARY KEY NOT NULL,
+    action_type             TEXT NOT NULL
+);
+
+CREATE TABLE tx_clues (
+    tx_hash                 BLOB PRIMARY KEY NOT NULL,
+    clue                    BLOB NOT NULL
 );
