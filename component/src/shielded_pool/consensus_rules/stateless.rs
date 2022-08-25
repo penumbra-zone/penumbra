45901da7729b6f868b8ed0b2d4855126eb2dd82d
redshiftzero <jen@penumbra.zone>
2022-08-25 17:43:13 -0400

    consensus rule: clues must be made with correct precision

--- component/src/shielded_pool/consensus_rules/stateless.rs
+++ component/src/shielded_pool/consensus_rules/stateless.rs
@@ -2,9 +2,7 @@ use penumbra_crypto::NotePayload;
 use penumbra_transaction::Transaction;
 
 pub fn num_clues_equal_to_num_outputs(tx: &Transaction) -> anyhow::Result<()> {
-    if tx.transaction_body().fmd_clues.len()
-        != tx.note_payloads().collect::<Vec<&NotePayload>>().len()
-    {
+    if tx.transaction_body().fmd_clues.len() != tx.note_payloads().count() {
         return Err(anyhow::anyhow!(
             "consensus rule violated: must have equal number of outputs and FMD clues"
         ));
