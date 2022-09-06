f315ea2a421bd7c980c64df22517b3f42cef9af0
Kenny Foner <kwf@very.science>
2022-09-06 13:48:22 -0400

    Endpoint for modifying a tree using mutable methods

--- tct/src/live.rs
+++ tct/src/live.rs
@@ -1,17 +1,24 @@
 //! A web service to view the live state of the TCT.
 
+use std::sync::Arc;
+
 use axum::{
     extract::{Path, Query},
     headers::ContentType,
     http::StatusCode,
-    routing::get,
+    routing::{get, post},
     Json, Router, TypedHeader,
 };
 
+use parking_lot::Mutex;
+use rand::{seq::SliceRandom, Rng};
 use serde_json::json;
 use tokio::sync::watch;
 
-use crate::{Commitment, Forgotten, Position, Tree};
+use crate::{
+    builder::{block, epoch},
+    Commitment, Forgotten, Position, Tree, Witness,
+};
 
 mod earliest;
 use earliest::Earliest;
@@ -192,7 +199,7 @@ pub async fn query(tree: watch::Receiver<Tree>) -> Router {
             })
         })
         .route("/commitments-ordered", {
-            let tree = tree.clone();
+            let tree = tree;
             get(|| async move {
                 Json(
                     tree.borrow()
@@ -208,6 +215,175 @@ pub async fn query(tree: watch::Receiver<Tree>) -> Router {
                         })
                         .collect::<Vec<_>>(),
                 )
+            })
+        })
+}
+
+/// An [`axum`] [`Router`] that serves a `POST` endpoint for updating the [`Tree`].
+///
+/// Queries taking arguments pass arguments via URL parameters. Results are returned in JSON format,
+/// with [`StatusCode::BAD_REQUEST`] being returned if the operation failed (i.e. an `Err` variant
+/// was returned).
+pub async fn control<R: Rng + Send + 'static>(rng: R, tree: Arc<watch::Sender<Tree>>) -> Router {
+    let rng = Arc::new(Mutex::new(rng));
+
+    Router::new()
+        .route("/new", {
+            let tree = tree.clone();
+            post(move || async move {
+                tree.send_modify(|tree| {
+                    *tree = Tree::new();
+                })
+            })
+        })
+        .route("/insert", {
+            #[derive(Deserialize)]
+            struct Insert {
+                witness: Witness,
+                commitment: Option<Commitment>,
+            }
+
+            let tree = tree.clone();
+            let rng = rng.clone();
+            post(
+                move |Query(Insert {
+                          witness,
+                          commitment,
+                      }): Query<Insert>| async move {
+                    let mut result = None;
+                    tree.send_modify(|tree| {
+                        result = Some(tree.insert(
+                            witness,
+                            // If no commitment is specified, generate a random one
+                            commitment.unwrap_or_else(|| Commitment::random(&mut *rng.lock())),
+                        ));
+                    });
+                    match result.take().unwrap() {
+                        Ok(position) => Ok(Json(json!({
+                            "epoch": position.epoch(),
+                            "block": position.block(),
+                            "commitment": position.commitment(),
+                        }))),
+                        Err(e) => Err((
+                            StatusCode::BAD_REQUEST,
+                            Json(json!({ "error": e.to_string() })),
+                        )),
+                    }
+                },
+            )
+        })
+        .route("/forget", {
+            #[derive(Deserialize)]
+            struct Forget {
+                commitment: Option<Commitment>,
+            }
+
+            let tree = tree.clone();
+            let rng = rng.clone();
+            post(
+                move |Query(Forget { commitment }): Query<Forget>| async move {
+                    let mut result = None;
+                    tree.send_modify(|tree| {
+                        if let Some(commitment) = commitment.or_else(|| {
+                            // If no commitment is specified, forget a random one that is present in
+                            // the tree
+                            tree.commitments()
+                                .map(|(c, _)| c)
+                                .collect::<Vec<_>>()
+                                .choose(&mut *rng.lock())
+                                .copied()
+                        }) {
+                            result = Some(tree.forget(commitment));
+                        } else {
+                            // If no commitment is specified and the tree contains no commitments to
+                            // forget, return that no commitments were forgotten
+                            result = Some(false);
+                        }
+                    });
+                    {
+                        Json(json!(result.take().unwrap()))
+                    }
+                },
+            )
+        })
+        .route("/insert-block-root", {
+            #[derive(Deserialize)]
+            struct InsertBlockRoot {
+                block_root: Option<block::Root>,
+            }
+
+            let tree = tree.clone();
+            let rng = rng.clone();
+            post(
+                move |Query(InsertBlockRoot { block_root }): Query<InsertBlockRoot>| async move {
+                    let mut result = None;
+                    tree.send_modify(|tree| {
+                        result = Some(tree.insert_block(
+                            // If no block root is specified, generate a random one
+                            block_root.unwrap_or_else(|| block::Root::random(&mut *rng.lock())),
+                        ));
+                    });
+                    match result.take().unwrap() {
+                        Ok(block_root) => Ok(block_root.to_string()),
+                        Err(e) => Err((
+                            StatusCode::BAD_REQUEST,
+                            Json(json!({ "error": e.to_string() })),
+                        )),
+                    }
+                },
+            )
+        })
+        .route("/end-block", {
+            let tree = tree.clone();
+            post(move || async move {
+                let mut result = None;
+                tree.send_modify(|tree| {
+                    result = Some(tree.end_block());
+                });
+                match result.take().unwrap() {
+                    Ok(block_root) => Ok(Json(block_root.to_string())),
+                    Err(e) => Err((
+                        StatusCode::BAD_REQUEST,
+                        Json(json!({ "error": e.to_string() })),
+                    )),
+                }
+            })
+        })
+        .route("/insert-epoch-root", {
+            #[derive(Deserialize)]
+            struct InsertEpochRoot {
+                epoch_root: Option<epoch::Root>,
+            }
+
+            let tree = tree.clone();
+            post(
+                move |Query(InsertEpochRoot { epoch_root }): Query<InsertEpochRoot>| async move {
+                    let mut result = None;
+                    tree.send_modify(|tree| {
+                        result = Some(tree.insert_epoch(
+                            // If no epoch root is specified, generate a random one
+                            epoch_root.unwrap_or_else(|| epoch::Root::random(&mut *rng.lock())),
+                        ));
+                    });
+                    match result.take() {
+                        Some(Ok(epoch_root)) => Ok(Json(epoch_root.to_string())),
+                        Some(Err(e)) => Err((StatusCode::BAD_REQUEST, e.to_string())),
+                        None => Err((StatusCode::INTERNAL_SERVER_ERROR, "".to_string())),
+                    }
+                },
+            )
+        })
+        .route("/end-epoch", {
+            post(move || async move {
+                let mut result = None;
+                tree.send_modify(|tree| {
+                    result = Some(tree.end_epoch());
+                });
+                match result.take() {
+                    Some(Ok(epoch_root)) => Ok(Json(epoch_root.to_string())),
+                    Some(Err(e)) => Err((StatusCode::BAD_REQUEST, e.to_string())),
+                    None => Err((StatusCode::INTERNAL_SERVER_ERROR, "".to_string())),
+                }
             })
         })
 }
---
+++ tct/src/random.rs
@@ -0,0 +1,91 @@
+//! Generating random values of various types.
+
+use ark_ed_on_bls12_377::Fq;
+use decaf377::FieldExt;
+use rand::{distributions::Distribution, Rng};
+
+use super::Commitment;
+use crate::{
+    builder::{block, epoch},
+    structure::Hash,
+    Root,
+};
+
+struct UniformFq;
+
+impl Distribution<Fq> for UniformFq {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Fq {
+        let mut bytes = [0u8; 32];
+        loop {
+            rng.fill_bytes(&mut bytes);
+            if let Ok(fq) = Fq::from_bytes(bytes) {
+                return fq;
+            }
+        }
+    }
+}
+
+impl Distribution<Commitment> for UniformFq {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Commitment {
+        Commitment(self.sample(rng))
+    }
+}
+
+impl Commitment {
+    /// Generate a random [`Commitment`].
+    pub fn random(mut rng: impl Rng) -> Self {
+        rng.sample(UniformFq)
+    }
+}
+
+impl Distribution<Hash> for UniformFq {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Hash {
+        Hash::new(self.sample(rng))
+    }
+}
+
+impl Hash {
+    /// Generate a random [`Hash`].
+    pub fn random(mut rng: impl Rng) -> Self {
+        rng.sample(UniformFq)
+    }
+}
+
+impl Distribution<Root> for UniformFq {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Root {
+        Root(self.sample(rng))
+    }
+}
+
+impl Root {
+    /// Generate a random [`Root`].
+    pub fn random(mut rng: impl Rng) -> Self {
+        rng.sample(UniformFq)
+    }
+}
+
+impl Distribution<epoch::Root> for UniformFq {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> epoch::Root {
+        epoch::Root(self.sample(rng))
+    }
+}
+
+impl epoch::Root {
+    /// Generate a random [`epoch::Root`].
+    pub fn random(mut rng: impl Rng) -> Self {
+        rng.sample(UniformFq)
+    }
+}
+
+impl Distribution<block::Root> for UniformFq {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> block::Root {
+        block::Root(self.sample(rng))
+    }
+}
+
+impl block::Root {
+    /// Generate a random [`block::Root`].
+    pub fn random(mut rng: impl Rng) -> Self {
+        rng.sample(UniformFq)
+    }
+}
