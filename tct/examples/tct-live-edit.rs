80422e2aeeed162d873e24cdd56bb8cc6e2eb63d
Kenny Foner <kwf@very.science>
2022-09-08 17:56:46 -0400

    Display help text in the browser

--- tct/examples/tct-live-edit.rs
+++ tct/examples/tct-live-edit.rs
@@ -59,29 +59,6 @@ async fn main() {
 
 fn help_text(address: &std::net::SocketAddr) {
     println!("Serving at http://{address}/view ...");
-    println!();
-    println!("Keyboard commands available in the browser:");
-    println!();
-    println!("  - 'n': reset the tree to new");
-    println!("  - 'c': insert a random commitment without remembering it");
-    println!("  - 'C': insert a random commitment and remember it");
-    println!("  - 'b': end the current block");
-    println!("  - 'B': insert a random block root");
-    println!("  - 'e': end the current epoch");
-    println!("  - 'E': insert a random epoch root");
-    println!("  - 'f': forget a random commitment");
-    println!("  - 'r': evaluate the root of the tree");
-    println!();
-    println!("Prefix a command key with a number to repeat it, vim-style.");
-    println!("For example, '3f' will forget three commitments randomly.");
-    println!();
-    println!("Mouse over an epoch, block, commitment, or hash to see more info.");
-    println!();
-    println!("Scroll and drag to zoom and pan.");
-    println!();
-    println!("Press Ctrl+C in this terminal to stop the server.");
-    println!();
-    println!("Have fun!");
 }
 
 // Serve the static file "key-control.js", which reads keystrokes and translates them into POST
