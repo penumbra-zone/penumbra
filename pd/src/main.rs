4b36eb32a9d52f022db9d07d1e3e68761a228d5f
Henry de Valence <hdevalence@penumbra.zone>
2022-09-08 22:33:55 -0700

    pd: experimentally enable grpc-web
    Using gRPC from a web context is difficult because of its reliance on HTTP/2
    features not exposed by browser APIs.  Instead, grpc-web allows a proxy to
    translate an HTTP/1 based protocol into normal gRPC.  Normally, this is done
    with Envoy or some other proxy service, but Rust and Tonic make this much more
    convenient, by making it easy to bundle the proxy into the application itself.
    
    I haven't tested this code from a web context, because that requires compiling
    our protos to JS and loading them into a page, but I have checked that this
    works just fine with normal gRPC (i.e., it does not degrade any functionality),
    so we could merge it experimentally and see if it unblocks proof of concept
    work towards #1373.

--- pd/src/main.rs
+++ pd/src/main.rs
@@ -197,8 +197,11 @@ async fn main() -> anyhow::Result<()> {
                         }
                         None => tracing::error_span!("grpc"),
                     })
-                    .add_service(ObliviousQueryServer::new(info.clone()))
-                    .add_service(SpecificQueryServer::new(info.clone()))
+                    // Allow HTTP/1, which will be used by grpc-web connections.
+                    .accept_http1(true)
+                    // Wrap each of the gRPC services in a tonic-web proxy:
+                    .add_service(tonic_web::enable(ObliviousQueryServer::new(info.clone())))
+                    .add_service(tonic_web::enable(SpecificQueryServer::new(info.clone())))
                     .serve(
                         format!("{}:{}", host, grpc_port)
                             .parse()
