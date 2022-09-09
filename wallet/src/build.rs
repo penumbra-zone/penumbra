9318417fa3db69c677ba0074c262fc5b5ca31878
redshiftzero <jen@penumbra.zone>
2022-09-09 12:17:15 -0400

    only request witness data for spends of non-zero amounts

--- wallet/src/build.rs
+++ wallet/src/build.rs
@@ -26,14 +26,17 @@ where
         })
         .await?;
 
-    // Get the witness data from the view service...
+    // Get the witness data from the view service only for non-zero amounts of value,
+    // since dummy spends will have a zero amount.
+    let note_commitments = plan
+        .spend_plans()
+        .filter(|plan| plan.note.amount() != 0)
+        .map(|spend| spend.note.commit().into())
+        .collect();
     let witness_data = view
         .witness(WitnessRequest {
             account_id: Some(fvk.hash().into()),
-            note_commitments: plan
-                .spend_plans()
-                .map(|spend| spend.note.commit().into())
-                .collect(),
+            note_commitments,
         })
         .await?;
 
