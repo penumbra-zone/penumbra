fbc136030c73917ff42e5b87cfe43bb4f65e3ca4
Henry de Valence <hdevalence@penumbra.zone>
2022-09-11 01:43:39 -0700

    shielded-pool: temporarily disable FMD outputs rule

--- component/src/shielded_pool/consensus_rules/stateless.rs
+++ component/src/shielded_pool/consensus_rules/stateless.rs
@@ -1,6 +1,10 @@
 use penumbra_transaction::Transaction;
 
-pub fn num_clues_equal_to_num_outputs(tx: &Transaction) -> anyhow::Result<()> {
+pub fn num_clues_equal_to_num_outputs(_tx: &Transaction) -> anyhow::Result<()> {
+    // TODO: re-enable after getting a better idea of what counts
+    // as an "output" (this broke after including the swap and swapclaim notepayloads)
+    // commenting out INTERNALLY to this fn prevents a dead code warning
+    /*
     if tx.transaction_body().fmd_clues.len() != tx.note_payloads().count() {
         Err(anyhow::anyhow!(
             "consensus rule violated: must have equal number of outputs and FMD clues"
@@ -8,4 +12,6 @@ pub fn num_clues_equal_to_num_outputs(tx: &Transaction) -> anyhow::Result<()> {
     } else {
         Ok(())
     }
+         */
+    Ok(())
 }
