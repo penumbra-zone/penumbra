b1a1bcb21080a6e8acf214a27736f356cd71b872
redshiftzero <jen@penumbra.zone>
2022-09-12 10:43:14 -0400

    consensus rule: memo is absent if no outputs, is present if outputs

--- component/src/shielded_pool/consensus_rules/stateless.rs
+++ component/src/shielded_pool/consensus_rules/stateless.rs
@@ -8,6 +8,21 @@ pub fn num_clues_equal_to_num_outputs(tx: &Transaction) -> anyhow::Result<()> {
     } else {
         Ok(())
     }
-         */
-    Ok(())
+}
+
+pub fn check_memo_exists_if_outputs_absent_if_not(tx: &Transaction) -> anyhow::Result<()> {
+    let num_outputs = tx.outputs().count();
+    if num_outputs > 0 && tx.transaction_body().memo.is_none() {
+        Err(anyhow::anyhow!(
+            "consensus rule violated: must have memo if outputs present"
+        ))
+    } else if num_outputs > 0 && tx.transaction_body().memo.is_some() {
+        Ok(())
+    } else if num_outputs == 0 && tx.transaction_body().memo.is_none() {
+        Ok(())
+    } else {
+        Err(anyhow::anyhow!(
+            "consensus rule violated: cannot have memo if no outputs present"
+        ))
+    }
 }
