6d3bd527bb2be1a5ab559e833ed0d9efdd2836a2
redshiftzero <jen@penumbra.zone>
2022-09-12 10:42:55 -0400

    consensus rule: re-enable num clues == num outputs check using new method

--- component/src/shielded_pool/consensus_rules/stateless.rs
+++ component/src/shielded_pool/consensus_rules/stateless.rs
@@ -1,11 +1,7 @@
 use penumbra_transaction::Transaction;
 
-pub fn num_clues_equal_to_num_outputs(_tx: &Transaction) -> anyhow::Result<()> {
-    // TODO: re-enable after getting a better idea of what counts
-    // as an "output" (this broke after including the swap and swapclaim notepayloads)
-    // commenting out INTERNALLY to this fn prevents a dead code warning
-    /*
-    if tx.transaction_body().fmd_clues.len() != tx.note_payloads().count() {
+pub fn num_clues_equal_to_num_outputs(tx: &Transaction) -> anyhow::Result<()> {
+    if tx.transaction_body().fmd_clues.len() != tx.outputs().count() {
         Err(anyhow::anyhow!(
             "consensus rule violated: must have equal number of outputs and FMD clues"
         ))
