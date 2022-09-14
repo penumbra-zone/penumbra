5da8b5cb6539e54640790a860bce2871ae6f5c97
plaidfinch <finch@plaidfinch.net>
2022-09-14 11:45:10 +0200

    Server-side repetition to reduce number of server requests

--- tct/src/live/control.rs
+++ tct/src/live/control.rs
@@ -10,7 +10,7 @@ use axum::{
 use parking_lot::Mutex;
 use rand::{seq::SliceRandom, Rng};
 use serde_json::json;
-use tokio::sync::watch;
+use tokio::{sync::watch, task::spawn_blocking};
 
 use crate::{
     builder::{block, epoch},
@@ -77,45 +77,60 @@ fn insert<R: Rng + Send + 'static>(
     struct Insert {
         witness: Witness,
         commitment: Option<Commitment>,
+        #[serde(default = "one")]
+        repeat: u16,
     }
 
     post(
         move |Query(Insert {
                   witness,
                   commitment,
+                  repeat,
               }): Query<Insert>| async move {
-            let result = marking_change(tree, |tree| {
-                if witness == Witness::Keep {
-                    // If we're at quota for number of commitments, forget until we're strictly below
-                    // quota again, so we can insert something
-                    if let Some(max_witnesses) = max_witnesses {
-                        let required_forgessions =
-                            (1 + tree.witnessed_count()).saturating_sub(max_witnesses);
-                        for commitment in
-                            random_commitments(&mut *rng.lock(), tree, required_forgessions)
-                        {
-                            tree.forget(commitment);
+            spawn_blocking(move || {
+                let mut result = None;
+
+                for _ in 0..repeat {
+                    result = Some(marking_change(tree.clone(), |tree| {
+                        if witness == Witness::Keep {
+                            // If we're at quota for number of commitments, forget until we're strictly below
+                            // quota again, so we can insert something
+                            if let Some(max_witnesses) = max_witnesses {
+                                let required_forgessions =
+                                    (1 + tree.witnessed_count()).saturating_sub(max_witnesses);
+                                for commitment in
+                                    random_commitments(&mut *rng.lock(), tree, required_forgessions)
+                                {
+                                    tree.forget(commitment);
+                                }
+                            }
                         }
-                    }
+                        // Now actually insert the commitment we wanted to insert
+                        tree.insert(
+                            witness,
+                            // If no commitment is specified, generate a random one
+                            commitment.unwrap_or_else(|| Commitment::random(&mut *rng.lock())),
+                        )
+                    }));
                 }
-                // Now actually insert the commitment we wanted to insert
-                tree.insert(
-                    witness,
-                    // If no commitment is specified, generate a random one
-                    commitment.unwrap_or_else(|| Commitment::random(&mut *rng.lock())),
-                )
-            });
-            match result {
-                Ok(position) => Ok(Json(json!({
-                    "epoch": position.epoch(),
-                    "block": position.block(),
-                    "commitment": position.commitment(),
-                }))),
-                Err(e) => Err((
-                    StatusCode::BAD_REQUEST,
-                    Json(json!({ "error": e.to_string() })),
-                )),
-            }
+                match result {
+                    Some(Ok(position)) => Ok(Json(json!({
+                        "epoch": position.epoch(),
+                        "block": position.block(),
+                        "commitment": position.commitment(),
+                    }))),
+                    Some(Err(e)) => Err((
+                        StatusCode::BAD_REQUEST,
+                        Json(json!({ "error": e.to_string() })),
+                    )),
+                    None => Err((
+                        StatusCode::BAD_REQUEST,
+                        Json(json!({ "error": "repeat must be greater than zero" })),
+                    )),
+                }
+            })
+            .await
+            .unwrap()
         },
     )
 }
@@ -136,24 +151,41 @@ fn forget<R: Rng + Send + 'static>(
     #[derive(Deserialize)]
     struct Forget {
         commitment: Option<Commitment>,
+        #[serde(default = "one")]
+        repeat: u16,
     }
 
-    post(|Query(Forget { commitment }): Query<Forget>| async move {
-        let result = marking_change(tree, |tree| {
-            if let Some(commitment) = commitment.or_else(|| {
-                // If no commitment is specified, forget a random extant one
-                random_commitments(&mut *rng.lock(), tree, 1).pop()
-            }) {
-                tree.forget(commitment)
-            } else {
-                // If no commitment is specified and the tree contains no commitments to
-                // forget, return that no commitments were forgotten
-                false
-            }
-        });
+    post(
+        |Query(Forget { commitment, repeat }): Query<Forget>| async move {
+            spawn_blocking(move || {
+                let mut result = None;
+                for _ in 0..repeat {
+                    result = Some(marking_change(tree.clone(), |tree| {
+                        if let Some(commitment) = commitment.or_else(|| {
+                            // If no commitment is specified, forget a random extant one
+                            random_commitments(&mut *rng.lock(), tree, 1).pop()
+                        }) {
+                            tree.forget(commitment)
+                        } else {
+                            // If no commitment is specified and the tree contains no commitments to
+                            // forget, return that no commitments were forgotten
+                            false
+                        }
+                    }));
+                }
 
-        Json(json!(result))
-    })
+                match result {
+                    Some(result) => Ok(Json(json!(result))),
+                    None => Err((
+                        StatusCode::BAD_REQUEST,
+                        Json(json!({ "error": "repeat must be greater than zero" })),
+                    )),
+                }
+            })
+            .await
+            .unwrap()
+        },
+    )
 }
 
 fn insert_block_root<R: Rng + Send + 'static>(
@@ -163,37 +195,67 @@ fn insert_block_root<R: Rng + Send + 'static>(
     #[derive(Deserialize)]
     struct InsertBlockRoot {
         block_root: Option<block::Root>,
+        #[serde(default = "one")]
+        repeat: u16,
     }
 
     post(
-        |Query(InsertBlockRoot { block_root }): Query<InsertBlockRoot>| async move {
-            let result = marking_change(tree, |tree| {
-                tree.insert_block(
-                    // If no block root is specified, generate a random one
-                    block_root.unwrap_or_else(|| block::Root::random(&mut *rng.lock())),
-                )
-            });
-            match result {
-                Ok(block_root) => Ok(block_root.to_string()),
-                Err(e) => Err((
-                    StatusCode::BAD_REQUEST,
-                    Json(json!({ "error": e.to_string() })),
-                )),
-            }
+        |Query(InsertBlockRoot { block_root, repeat }): Query<InsertBlockRoot>| async move {
+            spawn_blocking(move || {
+                let mut result = None;
+                for _ in 0..repeat {
+                    result = Some(marking_change(tree.clone(), |tree| {
+                        tree.insert_block(
+                            // If no block root is specified, generate a random one
+                            block_root.unwrap_or_else(|| block::Root::random(&mut *rng.lock())),
+                        )
+                    }));
+                }
+                match result {
+                    Some(Ok(block_root)) => Ok(block_root.to_string()),
+                    Some(Err(e)) => Err((
+                        StatusCode::BAD_REQUEST,
+                        Json(json!({ "error": e.to_string() })),
+                    )),
+                    None => Err((
+                        StatusCode::BAD_REQUEST,
+                        Json(json!({ "error": "repeat must be greater than zero" })),
+                    )),
+                }
+            })
+            .await
+            .unwrap()
         },
     )
 }
 
 fn end_block(tree: Arc<watch::Sender<Tree>>) -> MethodRouter {
-    post(|| async move {
-        let result = marking_change(tree, |tree| tree.end_block());
-        match result {
-            Ok(block_root) => Ok(Json(block_root.to_string())),
-            Err(e) => Err((
-                StatusCode::BAD_REQUEST,
-                Json(json!({ "error": e.to_string() })),
-            )),
-        }
+    #[derive(Deserialize)]
+    struct EndBlock {
+        #[serde(default = "one")]
+        repeat: u16,
+    }
+
+    post(|Query(EndBlock { repeat }): Query<EndBlock>| async move {
+        spawn_blocking(move || {
+            let mut result = None;
+            for _ in 0..repeat {
+                result = Some(marking_change(tree.clone(), |tree| tree.end_block()));
+            }
+            match result {
+                Some(Ok(block_root)) => Ok(Json(block_root.to_string())),
+                Some(Err(e)) => Err((
+                    StatusCode::BAD_REQUEST,
+                    Json(json!({ "error": e.to_string() })),
+                )),
+                None => Err((
+                    StatusCode::BAD_REQUEST,
+                    Json(json!({ "error": "repeat must be greater than zero" })),
+                )),
+            }
+        })
+        .await
+        .unwrap()
     })
 }
 
@@ -204,30 +266,70 @@ fn insert_epoch_root<R: Rng + Send + 'static>(
     #[derive(Deserialize)]
     struct InsertEpochRoot {
         epoch_root: Option<epoch::Root>,
+        #[serde(default = "one")]
+        repeat: u16,
     }
 
     post(
-        |Query(InsertEpochRoot { epoch_root }): Query<InsertEpochRoot>| async move {
-            let result = marking_change(tree, |tree| {
-                tree.insert_epoch(
-                    // If no epoch root is specified, generate a random one
-                    epoch_root.unwrap_or_else(|| epoch::Root::random(&mut *rng.lock())),
-                )
-            });
-            match result {
-                Ok(epoch_root) => Ok(Json(epoch_root.to_string())),
-                Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
-            }
+        |Query(InsertEpochRoot { epoch_root, repeat }): Query<InsertEpochRoot>| async move {
+            spawn_blocking(move || {
+                let mut result = None;
+                for _ in 0..repeat {
+                    result = Some(marking_change(tree.clone(), |tree| {
+                        tree.insert_epoch(
+                            // If no epoch root is specified, generate a random one
+                            epoch_root.unwrap_or_else(|| epoch::Root::random(&mut *rng.lock())),
+                        )
+                    }));
+                }
+                match result {
+                    Some(Ok(epoch_root)) => Ok(Json(epoch_root.to_string())),
+                    Some(Err(e)) => Err((
+                        StatusCode::BAD_REQUEST,
+                        Json(json!({ "error": e.to_string() })),
+                    )),
+                    None => Err((
+                        StatusCode::BAD_REQUEST,
+                        Json(json!({ "error": "repeat must be greater than zero" })),
+                    )),
+                }
+            })
+            .await
+            .unwrap()
         },
     )
 }
 
 fn end_epoch(tree: Arc<watch::Sender<Tree>>) -> MethodRouter {
-    post(|| async move {
-        let result = marking_change(tree, |tree| tree.end_epoch());
-        match result {
-            Ok(epoch_root) => Ok(Json(epoch_root.to_string())),
-            Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
-        }
+    #[derive(Deserialize)]
+    struct EndEpoch {
+        #[serde(default = "one")]
+        repeat: u16,
+    }
+
+    post(|Query(EndEpoch { repeat }): Query<EndEpoch>| async move {
+        spawn_blocking(move || {
+            let mut result = None;
+            for _ in 0..repeat {
+                result = Some(marking_change(tree.clone(), |tree| tree.end_epoch()));
+            }
+            match result {
+                Some(Ok(epoch_root)) => Ok(Json(epoch_root.to_string())),
+                Some(Err(e)) => Err((
+                    StatusCode::BAD_REQUEST,
+                    Json(json!({"error": e.to_string()})),
+                )),
+                None => Err((
+                    StatusCode::BAD_REQUEST,
+                    Json(json!({ "error": "repeat must be greater than zero" })),
+                )),
+            }
+        })
+        .await
+        .unwrap()
     })
+}
+
+fn one() -> u16 {
+    1
 }
