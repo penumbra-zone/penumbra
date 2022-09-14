17808ff8a8f47aa0cd1d70ef31f5403257373d4f
danb <dan@bryanlabs.net>
2022-09-14 16:42:36 -0400

    work sync with andrew

--- deployments/networks/testnet/kube/node-cluster/ci.sh
+++ deployments/networks/testnet/kube/node-cluster/ci.sh
@@ -35,7 +35,14 @@ $IMAGE:$VERSION \
 testnet generate \
 --validators-input-file "$CONTAINERHOME/vals.json"
 
+for i in $(seq $NVALS)
+do
+    CONFIGPATH=./pdcli/.penumbra/testnet_data/node$((i-1))/tendermint/config/
+    pushd $CONFIGPATH || exit
+    jq -r '.priv_key.value' node_key.json | base64 --decode | tail -c 32 | sha256sum  | cut -c -40  > "$WORKDIR"/p2p_id.txt
+    popd ... || exit
+done
 
-helm install testnet . --set count=$NVALS
+# helm install testnet . --set count=$NVALS
 
 # kubectl create configmap node-config --from-file=config/ -o yaml --dry-run=client | kubectl apply -f -
