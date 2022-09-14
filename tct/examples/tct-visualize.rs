d6ce7ff013ee8b66b0b331adf5e849734dc09f8e
plaidfinch <finch@plaidfinch.net>
2022-09-14 23:36:26 +0200

    Take advantage of tct acceleration for simulation

--- tct/examples/tct-visualize.rs
+++ tct/examples/tct-visualize.rs
@@ -2,18 +2,22 @@
 
 use std::{
     collections::BTreeMap,
+    fmt::Display,
     fs::File,
     io::{self, Write},
     path::PathBuf,
     process::{Command, Stdio},
+    str::FromStr,
     thread,
 };
 
 use anyhow::Result;
 use clap::Parser;
-use rand::Rng;
+use rand::{seq::SliceRandom, Rng, RngCore, SeedableRng};
+use rand_distr::Binomial;
 
-use penumbra_tct::{Commitment, Tree, Witness};
+use penumbra_tct::{self as tct, Commitment, Tree, Witness};
+use tct::structure::Hash;
 
 /// Visualize the structure of the Tiered Commitment Tree.
 #[derive(Parser, Debug)]
@@ -24,6 +28,9 @@ struct Args {
     /// The probability that any commitment is remembered.
     #[clap(long, default_value = "0.1")]
     remember: f64,
+    /// The probability that any remebered commitment is forgotten by the end.
+    #[clap(long, default_value = "0.0")]
+    forget: f64,
     /// The mean number of commitments in each block.
     #[clap(long, default_value = "8")]
     block_mean: u16,
@@ -39,57 +46,139 @@ struct Args {
     /// Don't write DOT files.
     #[clap(long)]
     no_dot: bool,
-    /// Farm out SVG render jobs to this many parallel processes.
-    #[clap(long)]
-    parallel: Option<usize>,
     /// Only write the final tree, not the intermediate stages.
     #[clap(long)]
     only_final: bool,
     /// Force the root every iteration.
     #[clap(long)]
     strict: bool,
+    /// Use the given seed for random generation.
+    #[clap(long)]
+    seed: Option<Seed>,
+    /// Accelerate tree construction, skipping frames for epochs and blocks with no remembered commitments.
+    #[clap(long)]
+    fast: bool,
+}
+
+#[derive(Debug)]
+struct Seed([u8; 32]);
+
+impl FromStr for Seed {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(Seed(
+            hex::decode(s)?
+                .try_into()
+                .map_err(|_| anyhow::anyhow!("seed is not 32 bytes"))?,
+        ))
+    }
+}
+
+impl Display for Seed {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", hex::encode(&self.0))
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    let mut rng = rand::thread_rng();
-    let schedule = schedule(&args);
+    let mut rng = if let Some(Seed(seed)) = args.seed {
+        rand_chacha::ChaCha20Rng::from_seed(seed)
+    } else {
+        let mut seed = [0; 32];
+        rand::rngs::OsRng.fill_bytes(&mut seed);
+        println!("Using random seed: {}", Seed(seed));
+        rand_chacha::ChaCha20Rng::from_seed(seed)
+    };
 
+    println!("Simulating {} epochs of activity ...", args.epochs);
+    let schedule = schedule(&mut rng, &args);
+
+    println!("Generating tree states and writing files ...");
     let mut tree = Tree::new();
 
-    for ((epoch, block), count) in schedule {
-        // Finish all empty blocks and epochs leading up to this one
-        while (epoch, block)
-            > (
-                tree.position().unwrap().epoch(),
-                tree.position().unwrap().block(),
-            )
-        {
-            if !args.only_final {
-                write_to_file(&tree, &args)?;
+    for epoch in 0..args.epochs {
+        if let Some(epoch) = schedule.get(&epoch) {
+            // (count, kept)
+            let mut epoch_schedule = BTreeMap::<u16, (u16, u16)>::new();
+            let mut total_kept = 0;
+            let mut total_count = 0;
+            for (&block, &count) in epoch {
+                let kept = rng.sample(Binomial::new(count.into(), args.remember).unwrap());
+                total_kept += kept;
+                total_count += count;
+                epoch_schedule.insert(block, (count, kept.try_into().unwrap()));
             }
-            tree.end_block()?;
-            if tree.position().unwrap().block() == args.epoch_size {
-                tree.end_epoch()?;
-            }
-        }
-        assert_eq!(epoch, tree.position().unwrap().epoch());
-        assert_eq!(block, tree.position().unwrap().block());
-        assert!(count > 0);
 
-        for _ in 0..count {
-            if !args.only_final {
-                write_to_file(&tree, &args)?;
+            // Shortcut when going fast and the epoch won't contain anything we'll remember
+            if args.fast && total_kept == 0 {
+                if !args.only_final {
+                    write_to_file(&tree, &args)?;
+                }
+
+                if total_count == 0 {
+                    // If the total count in the epoch is zero, it's the empty epoch
+                    tree.end_epoch().unwrap();
+                } else {
+                    // Otherwise it's some random non-empty epoch
+                    tree.insert_epoch(tct::builder::epoch::Root(gen_hash(&mut rng)))
+                        .unwrap();
+                }
+
+                // Skip doing anything more for this epoch
+                continue;
             }
-            // Insert a random commitment into the tree with probability `args.remember` that it
-            // will be kept when inserted
-            let witness = if rng.gen_range(0.0..=1.0) < args.remember {
-                Witness::Keep
-            } else {
-                Witness::Forget
-            };
-            tree.insert(witness, gen_commitment())?;
+
+            // For each block, generate its contents
+            for block in 0..args.epoch_size {
+                let (count, kept) = *epoch_schedule.get(&block).unwrap_or(&(0, 0));
+
+                if count == 0 {
+                    // End the block immediately because the count is zero
+                    if !args.only_final {
+                        write_to_file(&tree, &args)?;
+                    }
+                    tree.end_block().unwrap();
+                } else if args.fast && kept == 0 {
+                    // Shortcut when going fast and the block won't contain anything we'll remember
+                    if !args.only_final {
+                        write_to_file(&tree, &args)?;
+                    }
+                    tree.insert_block(tct::builder::block::Root(gen_hash(&mut rng)))
+                        .unwrap();
+                } else {
+                    // Otherwise we should construct the block explicitly, keeping as many commitments
+                    // as previously randomly decided
+                    let mut witnesses = Vec::new();
+                    for _ in 0..kept {
+                        witnesses.push(Witness::Keep);
+                    }
+                    for _ in kept..count {
+                        witnesses.push(Witness::Forget);
+                    }
+                    witnesses.shuffle(&mut rng);
+                    for witness in witnesses {
+                        if !args.only_final {
+                            write_to_file(&tree, &args)?;
+                        }
+                        tree.insert(witness, gen_commitment(&mut rng)).unwrap();
+                    }
+
+                    // End the block now we're done with it
+                    if !args.only_final {
+                        write_to_file(&tree, &args)?;
+                    }
+                    tree.end_block().unwrap();
+                }
+            }
         }
+
+        // End the epoch now we're done with it
+        if !args.only_final {
+            write_to_file(&tree, &args)?;
+        }
+        tree.end_epoch().unwrap();
     }
 
     // Write the final tree
@@ -99,16 +188,15 @@ fn main() -> anyhow::Result<()> {
 }
 
 /// Generate a Poisson distribution across the blocks to be simulated, binned by block.
-fn schedule(args: &Args) -> BTreeMap<(u16, u16), u16> {
-    let mut rng = rand::thread_rng();
-    let mut schedule = BTreeMap::new();
+fn schedule<R: Rng>(rng: &mut R, args: &Args) -> BTreeMap<u16, BTreeMap<u16, u16>> {
+    let mut schedule: BTreeMap<u16, BTreeMap<u16, u16>> = BTreeMap::new();
 
     let total_commitments = args.epochs * args.epoch_size * args.block_mean;
 
     for _ in 0..total_commitments {
         let epoch = rng.gen_range(0u16..args.epochs);
         let block = rng.gen_range(0u16..args.epoch_size);
-        *schedule.entry((epoch, block)).or_insert(0) += 1;
+        *schedule.entry(epoch).or_default().entry(block).or_default() += 1;
     }
 
     schedule
@@ -132,8 +220,7 @@ fn write_to_file(tree: &Tree, args: &Args) -> Result<()> {
     let svg_path = base_path.with_extension("svg");
     let dot_path = base_path.with_extension("dot");
 
-    if let Some(jobs) = args.parallel {
-    } else if args.no_svg {
+    if args.no_svg {
         if !args.no_dot {
             // Serialize the dot representation directly to the dot file
             println!("Writing {} ...", dot_path.display());
@@ -207,14 +294,25 @@ fn write_svg_direct<W: Write>(tree: &Tree, writer: &mut W) -> Result<()> {
 }
 
 /// Generate a random valid commitment by rejection sampling
-fn gen_commitment() -> Commitment {
-    let mut rng = rand::thread_rng();
+fn gen_commitment<R: Rng>(rng: &mut R) -> Commitment {
     let mut bytes = [0u8; 32];
 
     loop {
         rng.fill(&mut bytes);
         if let Ok(commitment) = Commitment::try_from(bytes) {
             return commitment;
+        }
+    }
+}
+
+/// Generate a random valid hash by rejection sampling
+fn gen_hash<R: Rng>(rng: &mut R) -> Hash {
+    // Rejection sample until finding a valid hash value
+    loop {
+        let mut bytes = [0; 32];
+        rng.fill_bytes(&mut bytes);
+        if let Ok(hash) = Hash::from_bytes(bytes) {
+            break hash;
         }
     }
 }
