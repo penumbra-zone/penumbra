a0a57da1c2a246e3e8c71ae8c7f55485e5743569
redshiftzero <jen@penumbra.zone>
2022-09-23 11:32:46 -0400

    crypto(transparent gadgets): note commitment integrity

--- crypto/src/proofs/transparent_gadgets.rs
+++ crypto/src/proofs/transparent_gadgets.rs
@@ -1,7 +1,8 @@
 use anyhow::{anyhow, Result};
+use decaf377_fmd as fmd;
 use penumbra_tct as tct;
 
-use crate::{keys, note, Nullifier};
+use crate::{keys, note, Fq, Nullifier, Value};
 
 /// Check the integrity of the nullifier.
 pub(crate) fn nullifier_integrity(
@@ -12,6 +13,29 @@ pub(crate) fn nullifier_integrity(
 ) -> Result<()> {
     if public_nullifier != nk.derive_nullifier(position, &note_commitment) {
         return Err(anyhow!("bad nullifier"));
+    }
+    Ok(())
+}
+
+/// Check the integrity of the note commitment.
+pub(crate) fn note_commitment_integrity(
+    note_blinding: Fq,
+    note_value: Value,
+    note_diversified_generator: decaf377::Element,
+    note_s_component_transmission_key: Fq,
+    note_clue_key: fmd::ClueKey,
+    note_commitment: note::Commitment,
+) -> Result<()> {
+    let note_commitment_test = note::commitment(
+        note_blinding,
+        note_value,
+        note_diversified_generator,
+        note_s_component_transmission_key,
+        &note_clue_key,
+    );
+
+    if note_commitment != note_commitment_test {
+        return Err(anyhow!("note commitment mismatch"));
     }
     Ok(())
 }
