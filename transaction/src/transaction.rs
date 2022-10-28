204c0314524e5c1bd9fd5a84692e54e9b6dd6b1b
erwanor <erwan.ounn.84@gmail.com>
2022-10-28 14:38:48 +0200

    transaction: replace negated commitment with balance commitment

--- transaction/src/transaction.rs
+++ transaction/src/transaction.rs
@@ -319,7 +319,7 @@ impl Transaction {
         // Add fee into binding verification key computation.
         let fee_v_blinding = Fr::zero();
         let fee_value_commitment = self.transaction_body.fee.commit(fee_v_blinding);
-        balance_commitments -= fee_value_commitment.0;
+        balance_commitments += fee_value_commitment.0;
 
         let binding_verification_key_bytes: VerificationKeyBytes<Binding> =
             balance_commitments.vartime_compress().0.into();
