39404cfa8e1f09c0e3ca31c6c1d0e984e4eab5b7
Henry de Valence <hdevalence@penumbra.zone>
2022-10-27 20:13:18 -0700

    proto: remove Transaction from TransactionView
    The `TransactionView` is supposed to *be* the transaction, not contain it.

--- transaction/src/view.rs
+++ transaction/src/view.rs
@@ -6,10 +6,7 @@ use decaf377_fmd::Clue;
 use penumbra_crypto::transaction::Fee;
 pub use transaction_perspective::TransactionPerspective;
 
-use crate::Transaction;
-
 pub struct TransactionView {
-    pub tx: Transaction,
     pub actions: Vec<ActionView>,
     pub expiry_height: u64,
     pub chain_id: String,
