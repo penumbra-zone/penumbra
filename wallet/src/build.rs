82b16c0d2f4aae778e12da788066a984036478da
therealyingtong <yingtong@z.cash>
2022-10-29 13:41:08 -0700

    client: change ViewTransaction::witness() to take TransactionPlan

--- wallet/src/build.rs
+++ wallet/src/build.rs
@@ -1,8 +1,6 @@
 use anyhow::Result;
 use penumbra_crypto::FullViewingKey;
 use penumbra_custody::{AuthorizeRequest, CustodyClient};
-use penumbra_proto::view::v1alpha1::WitnessRequest;
-use penumbra_tct::Proof;
 use penumbra_transaction::{plan::TransactionPlan, Transaction};
 use penumbra_view::ViewClient;
 use rand_core::{CryptoRng, RngCore};
@@ -27,33 +25,8 @@ where
         })
         .await?;
 
-    // Get the witness data from the view service only for non-zero amounts of value,
-    // since dummy spends will have a zero amount.
-    let note_commitments = plan
-        .spend_plans()
-        .filter(|plan| plan.note.amount() != 0u64.into())
-        .map(|spend| spend.note.commit().into())
-        .chain(
-            plan.swap_claim_plans()
-                .map(|swap_claim| swap_claim.swap_nft_note.commit().into()),
-        )
-        .collect();
-    let mut witness_data = view
-        .witness(WitnessRequest {
-            account_id: Some(fvk.hash().into()),
-            note_commitments,
-        })
-        .await?;
-
-    // Now we need to augment the witness data with dummy proofs such that
-    // note commitments corresponding to dummy spends also have proofs.
-    for nc in plan
-        .spend_plans()
-        .filter(|plan| plan.note.amount() == 0u64.into())
-        .map(|plan| plan.note.commit())
-    {
-        witness_data.add_proof(nc, Proof::dummy(&mut rng, nc));
-    }
+    // Send a witness request to the view service to get witness data
+    let witness_data = view.witness(fvk.hash(), &mut rng, &plan).await?;
 
     // ... and then build the transaction:
     plan.build(&mut rng, fvk, auth_data, witness_data)
