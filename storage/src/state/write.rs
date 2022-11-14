7601897fd5a2176cf9c67907a742842d876e7a14
erwanor <erwan.ounn.84@gmail.com>
2022-11-14 12:57:43 +0100

    chore(storage): remove `penumbra-crypto`, `penumbra-tct`, `penumbra-proto` deps

--- storage/src/state/write.rs
+++ storage/src/state/write.rs
@@ -1,35 +1,11 @@
-use std::{any::Any, fmt::Debug};
-use tendermint::abci;
-
-use penumbra_proto::{Message, Protobuf};
-
 use crate::StateRead;
+use std::any::Any;
+use tendermint::abci;
 
 /// Write access to chain state.
 pub trait StateWrite: StateRead + Send + Sync {
     /// Puts raw bytes into the verifiable key-value store with the given key.
     fn put_raw(&mut self, key: String, value: Vec<u8>);
-
-    /// Puts a domain type into the verifiable key-value store with the given key.
-    fn put<D, P>(&mut self, key: String, value: D)
-    where
-        D: Protobuf<P>,
-        // TODO: does this get less awful if P is an associated type of D?
-        P: Message + Default,
-        P: From<D>,
-        D: TryFrom<P> + Clone + Debug,
-        <D as TryFrom<P>>::Error: Into<anyhow::Error>,
-    {
-        self.put_proto(key, P::from(value));
-    }
-
-    /// Puts a proto type into the verifiable key-value store with the given key.
-    fn put_proto<P>(&mut self, key: String, value: P)
-    where
-        P: Message + Default + Debug,
-    {
-        self.put_raw(key, value.encode_to_vec());
-    }
 
     /// Delete a key from the verifiable key-value store.
     fn delete(&mut self, key: String);
