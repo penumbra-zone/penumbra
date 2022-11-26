6f087c24eb7931bcf0a304b5307635b81db9e548
Henry de Valence <hdevalence@penumbra.zone>
2022-11-25 17:36:27 -0800

    pd: implement auto-https using rustls-acme

--- pd/src/lib.rs
+++ pd/src/lib.rs
@@ -1,12 +1,14 @@
 //! Source code for the Penumbra node software.
 #![allow(clippy::clone_on_copy)]
 #![recursion_limit = "512"]
+
 mod consensus;
 mod info;
 mod mempool;
 mod metrics;
 mod snapshot;
 
+pub mod auto_https;
 pub mod testnet;
 
 pub use crate::metrics::register_metrics;
