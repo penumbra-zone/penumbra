74475755c4b8af9eeb2d5c68a8573395fa2d08de
Henry de Valence <hdevalence@penumbra.zone>
2022-11-28 22:25:12 -0800

    chain: add allocations to `genesis::AppState::default()`
    This commit moves the hardcoded test address from the pcli integration tests
    into the `chain` crate's `Default` impl for `genesis::AppState`, so that unit
    tests written against a default genesis state can test spends, outputs, etc, or
    other functionality involving moving value around in the shielded pool.

--- chain/src/lib.rs
+++ chain/src/lib.rs
@@ -16,3 +16,15 @@ pub use known_assets::KnownAssets;
 pub use note_source::NoteSource;
 pub use sync::{AnnotatedNotePayload, CompactBlock};
 pub use view::{StateReadExt, StateWriteExt};
+
+/// Hardcoded test data.
+pub mod test {
+    /// This address is for test purposes, allocations were added beginning with
+    /// the 016-Pandia testnet.
+    pub const TEST_SEED_PHRASE: &str = "benefit cherry cannon tooth exhibit law avocado spare tooth that amount pumpkin scene foil tape mobile shine apology add crouch situate sun business explain";
+
+    /// These addresses both correspond to the test wallet above.
+    pub const TEST_ADDRESS_0: &str = "penumbrav2t13vh0fkf3qkqjacpm59g23ufea9n5us45e4p5h6hty8vg73r2t8g5l3kynad87u0n9eragf3hhkgkhqe5vhngq2cw493k48c9qg9ms4epllcmndd6ly4v4dw2jcnxaxzjqnlvnw";
+    /// These addresses both correspond to the test wallet above.
+    pub const TEST_ADDRESS_1: &str = "penumbrav2t1gl609fq6xzjcqn3hz3crysw2s0nkt330lyhaq403ztmrm3yygsgdklt9uxfs0gedwp6sypp5k5ln9t62lvs9t0a990q832wnxak8r939g5u6uz5aessd8saxvv7ewlz4hhqnws";
+}
