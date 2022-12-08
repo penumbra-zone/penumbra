82766e3d043d65ddb1634e4a64c2f6537f24a845
Henry de Valence <hdevalence@penumbra.zone>
2022-12-07 21:49:08 -0800

    wip: first pass UndelegateClaim protos

--- proto/src/gen/penumbra.core.stake.v1alpha1.rs
+++ proto/src/gen/penumbra.core.stake.v1alpha1.rs
@@ -218,17 +218,71 @@ pub struct Undelegate {
     pub validator_identity: ::core::option::Option<super::super::crypto::v1alpha1::IdentityKey>,
     /// The index of the epoch in which this undelegation was performed.
     #[prost(uint64, tag="2")]
-    pub epoch_index: u64,
-    /// The amount to undelegate, in units of unbonded stake.
-    #[prost(message, optional, tag="3")]
+    pub start_epoch_index: u64,
+    /// The index of the epoch in which unbonding should complete.
+    #[prost(uint64, tag="3")]
+    pub end_epoch_index: u64,
+    /// The amount to undelegate, in units of unbonding tokens.
+    #[prost(message, optional, tag="4")]
     pub unbonded_amount: ::core::option::Option<super::super::crypto::v1alpha1::Amount>,
     /// The amount of delegation tokens consumed by this action.
     ///
     /// This is implied by the validator's exchange rate in the specified epoch
     /// (and should be checked in transaction validation!), but including it allows
     /// stateless verification that the transaction is internally consistent.
-    #[prost(message, optional, tag="4")]
+    #[prost(message, optional, tag="5")]
     pub delegation_amount: ::core::option::Option<super::super::crypto::v1alpha1::Amount>,
+}
+/// A transaction action finishing an undelegation, converting (slashable)
+/// "unbonding tokens" to (unslashable) staking tokens.
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct UndelegateClaim {
+    #[prost(message, optional, tag="1")]
+    pub body: ::core::option::Option<UndelegateClaimBody>,
+    #[prost(bytes="vec", tag="2")]
+    pub proof: ::prost::alloc::vec::Vec<u8>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct UndelegateClaimBody {
+    /// The identity key of the validator to finish undelegating from.
+    #[prost(message, optional, tag="1")]
+    pub validator_identity: ::core::option::Option<super::super::crypto::v1alpha1::IdentityKey>,
+    /// The epoch in which unbonding began, used to verify the penalty.
+    #[prost(uint64, tag="2")]
+    pub start_epoch_index: u64,
+    /// The epoch in which unbonding finished, used to verify the penalty.
+    #[prost(uint64, tag="3")]
+    pub end_epoch_index: u64,
+    /// The penalty applied to undelegation, in bps^2 (10e-8).
+    /// In the happy path (no slashing), this is 0.
+    #[prost(uint64, tag="4")]
+    pub penalty: u64,
+    /// The action's contribution to the transaction's value balance.
+    #[prost(message, optional, tag="5")]
+    pub balance_commitment: ::core::option::Option<super::super::crypto::v1alpha1::BalanceCommitment>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct UndelegateClaimPlan {
+    /// The identity key of the validator to finish undelegating from.
+    #[prost(message, optional, tag="1")]
+    pub validator_identity: ::core::option::Option<super::super::crypto::v1alpha1::IdentityKey>,
+    /// The epoch in which unbonding began, used to verify the penalty.
+    #[prost(uint64, tag="2")]
+    pub start_epoch_index: u64,
+    /// The epoch in which unbonding finished, used to verify the penalty.
+    #[prost(uint64, tag="3")]
+    pub end_epoch_index: u64,
+    /// The penalty applied to undelegation, in bps^2 (10e-8).
+    /// In the happy path (no slashing), this is 0.
+    #[prost(uint64, tag="4")]
+    pub penalty: u64,
+    /// The amount of unbonding tokens to claim.
+    /// This is a bare number because its denom is determined by the preceding data.
+    #[prost(message, optional, tag="5")]
+    pub unbonding_amount: ::core::option::Option<super::super::crypto::v1alpha1::Amount>,
+    /// The blinding factor to use for the value commitment.
+    #[prost(bytes="vec", tag="6")]
+    pub value_blinding: ::prost::alloc::vec::Vec<u8>,
 }
 /// A list of pending delegations and undelegations.
 #[derive(::serde::Deserialize, ::serde::Serialize)]
