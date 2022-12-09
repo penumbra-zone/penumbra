b9d325098a1acd260c5efaa094e74e0271161f56
Henry de Valence <hdevalence@penumbra.zone>
2022-12-09 10:19:04 -0800

    view: refactor notes table to just do note commitment lookups
    This change allows the notes table to act as an advice mechanism for
    out-of-band note commitments during scanning: we can add notes to the notes
    table in anticipation of their appearance, and then crossreference out-of-band
    notes against known notes in the table.
    
    Co-Authored-By: Aubrey Dyer <aubrey@penumbra.zone>

--- view/migrations/20220908223928_notes.sql
+++ view/migrations/20220908223928_notes.sql
@@ -1,40 +1,44 @@
+-- This table just records the mapping from note commitments to note plaintexts.
+-- This is also used as a way to give advice about out-of-band notes during scanning,
+-- by allowing the user to add notes to the database before they are scanned.
 CREATE TABLE notes (
     note_commitment         BLOB PRIMARY KEY NOT NULL,
     address                 BLOB NOT NULL,
     amount                  BIGINT NOT NULL,
     asset_id                BLOB NOT NULL,
-    blinding_factor         BLOB NOT NULL,
-    height_created          BIGINT NOT NULL,
-    -- precomputed decryption of the diversifier
-    address_index           BLOB NOT NULL,
-    -- the source of the note (a tx hash or structured data jammed into one)
-    source                  BLOB NOT NULL
+    blinding_factor         BLOB NOT NULL
 );
 
 -- general purpose note queries
 CREATE INDEX notes_idx ON notes (
     address,            
     asset_id,           
-    amount,
-    height_created,
-    address_index,
-    source
+    amount
 );
 
 -- Minimal data required for balance tracking
 -- Meant to represent notes which have been accepted into the note set
 CREATE TABLE spendable_notes (
     note_commitment         BLOB PRIMARY KEY NOT NULL,
-    --null if unspent, otherwise spent at height_spent 
-    height_spent            BIGINT, 
     -- the nullifier for this note, used to detect when it is spent
     nullifier               BLOB NOT NULL,
     -- the position of the note in the note commitment tree
-    position                BIGINT NOT NULL
+    position                BIGINT NOT NULL,
+    -- the height at which the note was created
+    height_created          BIGINT NOT NULL,
+    -- precomputed decryption of the diversifier
+    address_index           BLOB NOT NULL,
+    -- the source of the note (a tx hash or structured data jammed into one)
+    source                  BLOB NOT NULL,
+    --null if unspent, otherwise spent at height_spent 
+    height_spent            BIGINT 
 );
 
 -- general purpose note queries
 CREATE INDEX spendable_notes_idx ON spendable_notes (
-    height_spent,       -- null if unspent, so spent/unspent is first
-    nullifier
+    address_index,
+    source,
+    height_created,
+    nullifier,
+    height_spent       -- null if unspent, so spent/unspent is first
 );
