e7ec178bc01741776624650203ee8301c68d426f
Ava Howell <ava@penumbra.zone>
2022-12-14 17:34:23 -0800

    remove go-proto

--- proto/build_go_protos.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-protoc --proto_path=./proto --proto_path=./ibc-go-vendor --go_opt=Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types --go_out=go-proto --go_opt=paths=source_relative ./proto/*.proto
