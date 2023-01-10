a7cb7d0501793406cf112f9ab5b37618685a7626
redshiftzero <jen@penumbra.zone>
2023-01-10 12:21:53 -0500

    crypto: implement `Rseed` methods

--- crypto/src/rseed.rs
+++ crypto/src/rseed.rs
@@ -1,4 +1,7 @@
-use crate::{ka, Fq};
+use ark_ff::PrimeField;
+use rand::{CryptoRng, RngCore};
+
+use crate::{ka, prf, Fq, Fr};
 
 /// The rseed is a uniformly random 32-byte sequence included in the note plaintext.
 #[derive(Clone, Copy, Debug)]
@@ -6,17 +9,21 @@ pub struct Rseed(pub [u8; 32]);
 
 impl Rseed {
     /// Generate a new rseed from a random source.
-    pub fn generate(&self) -> Self {
-        todo!()
+    pub fn generate<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
+        let mut bytes = [0u8; 32];
+        rng.fill_bytes(&mut bytes);
+        Self(bytes)
     }
 
     /// Derive the ephemeral secret key from the rseed.
     pub fn derive_esk(&self) -> ka::Secret {
-        todo!()
+        let hash_result = prf::expand(b"Penumbra_DeriEsk", &self.0, &[4u8]);
+        ka::Secret::new_from_field(Fr::from_le_bytes_mod_order(hash_result.as_bytes()))
     }
 
     /// Derive note commitment randomness from the rseed.
     pub fn derive_note_blinding(&self) -> Fq {
-        todo!()
+        let hash_result = prf::expand(b"Penumbra_DeriRcm", &self.0, &[5u8]);
+        Fq::from_le_bytes_mod_order(hash_result.as_bytes())
     }
 }
