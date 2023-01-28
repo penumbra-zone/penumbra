9deacf33d0980c16265282474aa4bac7fdf69528
Henry de Valence <hdevalence@penumbra.zone>
2023-01-28 14:05:44 -0800

    proto: add TODO for final serialization cleanup

--- proto/src/serializers.rs
+++ proto/src/serializers.rs
@@ -1,7 +1,5 @@
-//! Serializers for adjusting the Serde implementations derived from the Rust
-//! proto types.
-//!
-//! This approach is inspired by the tendermint-rs implementation, and some of
-//! the serializers are adapted from that code.
+//! TODO: Extract the Bech32 prefixes into a module at the root of the crate,
+//! and then remove this module entirely.  We don't want to be custom-shaping
+//! serde formats, we want to have those match protobuf JSON exactly.
 
 pub mod bech32str;
