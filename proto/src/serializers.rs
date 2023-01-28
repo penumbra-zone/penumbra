6096e4f08169e536a5c9018a0e3980ad1f156e46
Henry de Valence <hdevalence@penumbra.zone>
2023-01-28 13:27:52 -0800

    proto: delete unused serializers

--- proto/src/serializers.rs
+++ proto/src/serializers.rs
@@ -4,13 +4,6 @@
 //! This approach is inspired by the tendermint-rs implementation, and some of
 //! the serializers are adapted from that code.
 
-pub mod hexstr;
-pub mod hexstr_bytes;
-
-pub mod base64str;
-pub mod base64str_bytes;
-
 pub mod bech32str;
 
-pub mod prost_any;
 pub mod vote;
