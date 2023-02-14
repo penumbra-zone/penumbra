0eebd834846e89970fbb6fae01c280eeeddd6ff7
redshiftzero <jen@penumbra.zone>
2023-02-14 18:46:38 -0500

    crypto(zk): disable expensive runtime checks when loading pk/vk
    `ark_serialize::{CanonicalSerialize, CanonicalDeserialize}` let us
    serialize and deserialize the proving and verification keys. However,
    their (upstream) implementations do a bunch of expensive runtime
    checks - in release mode it takes ~30s  to perform them all
    for the spend circuit. Since the proving and verification keys are
    from a trusted source (they are distributed in the binaries), doing
    these checks is unnecessary.
    
    The upstream traits also provide a `serialize_unchecked` and
    `deserialize_unchecked` that do not perform these checks. However,
    they must be used together, as they also disable compression. This
    results in the size of the proving keys doubling: from 3.2MB to 7MB
    for the output circuit, from 9.9M to 20M for the spend circuit.
    
    In the future we should reduce the size of the serialized proving
    and verification keys by providing serialization/deserialization
    methods that do not perform these expensive checks but also perform
    compression.

--- proof-params/src/lib.rs
+++ proof-params/src/lib.rs
@@ -19,24 +19,27 @@ pub static OUTPUT_PROOF_PROVING_KEY: Lazy<ProvingKey<Bls12_377>> =
 pub static OUTPUT_PROOF_VERIFICATION_KEY: Lazy<VerifyingKey<Bls12_377>> =
     Lazy::new(output_verification_parameters);
 
+// Note: Here we are using `CanonicalDeserialize::deserialize_unchecked` as the
+// parameters are being loaded from a trusted source (our source code).
+
 fn spend_proving_parameters() -> ProvingKey<Bls12_377> {
     let pk_params = include_bytes!("gen/spend_pk.bin");
-    ProvingKey::deserialize(&pk_params[..]).expect("can deserialize ProvingKey")
+    ProvingKey::deserialize_unchecked(&pk_params[..]).expect("can deserialize ProvingKey")
 }
 
 fn spend_verification_parameters() -> VerifyingKey<Bls12_377> {
     let vk_params = include_bytes!("gen/spend_vk.bin");
-    VerifyingKey::deserialize(&vk_params[..]).expect("can deserialize VerifyingKey")
+    VerifyingKey::deserialize_unchecked(&vk_params[..]).expect("can deserialize VerifyingKey")
 }
 
 fn output_proving_parameters() -> ProvingKey<Bls12_377> {
     let pk_params = include_bytes!("gen/output_pk.bin");
-    ProvingKey::deserialize(&pk_params[..]).expect("can deserialize ProvingKey")
+    ProvingKey::deserialize_unchecked(&pk_params[..]).expect("can deserialize ProvingKey")
 }
 
 fn output_verification_parameters() -> VerifyingKey<Bls12_377> {
     let vk_params = include_bytes!("gen/output_vk.bin");
-    VerifyingKey::deserialize(&vk_params[..]).expect("can deserialize VerifyingKey")
+    VerifyingKey::deserialize_unchecked(&vk_params[..]).expect("can deserialize VerifyingKey")
 }
 
 #[cfg(test)]
