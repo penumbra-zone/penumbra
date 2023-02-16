9e8b94a4060d1f4ca1f302eb43f3c3e43e801d36
finch <finch@plaidfinch.net>
2023-02-16 12:38:43 -0500

    Implement stateless checks for delegator voting

--- component/src/action_handler/actions/delegator_vote.rs
+++ component/src/action_handler/actions/delegator_vote.rs
@@ -1,15 +1,46 @@
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use async_trait::async_trait;
 use penumbra_storage::{StateRead, StateWrite};
-use penumbra_transaction::{action::DelegatorVote, Transaction};
+use penumbra_transaction::{
+    action::{DelegatorVote, DelegatorVoteBody},
+    Transaction,
+};
 
 use crate::ActionHandler;
 
 #[async_trait]
 impl ActionHandler for DelegatorVote {
-    async fn check_stateless(&self, _context: Arc<Transaction>) -> Result<()> {
+    async fn check_stateless(&self, context: Arc<Transaction>) -> Result<()> {
+        let DelegatorVote {
+            auth_sig,
+            proof,
+            body:
+                DelegatorVoteBody {
+                    start_height,
+                    value,
+                    nullifier,
+                    rk,
+                    // Unused in stateless checks:
+                    vote: _,            // Only used when executing the vote
+                    proposal: _,        // Checked against the current open proposals statefully
+                    unbonded_amount: _, // Checked against the proposal's snapshot exchange rate statefully
+                },
+        } = self;
+
+        let effect_hash = context.transaction_body().effect_hash();
+        let anchor = context.anchor;
+
+        // 1. Check spend auth signature using provided spend auth key.
+        rk.verify(effect_hash.as_ref(), auth_sig)
+            .context("delegator vote auth signature failed to verify")?;
+
+        // 2. Check that the proof verifies.
+        proof
+            .verify(anchor, *start_height, *value, *nullifier, *rk)
+            .context("a delegator vote proof did not verify")?;
+
         Ok(())
     }
 
