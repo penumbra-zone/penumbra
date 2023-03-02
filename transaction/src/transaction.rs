e7d6bbbb48b38c53683658fb209351f6fd367216
finch <finch@plaidfinch.net>
2023-03-02 18:54:25 -0500

    Iterate through filtered DaoSpend, DaoDeposit, DaoOutput

--- transaction/src/transaction.rs
+++ transaction/src/transaction.rs
@@ -23,8 +23,8 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     action::{
-        Delegate, DelegatorVote, Output, ProposalSubmit, ProposalWithdraw, Swap, Undelegate,
-        ValidatorVote,
+        DaoDeposit, DaoOutput, DaoSpend, Delegate, DelegatorVote, Output, ProposalSubmit,
+        ProposalWithdraw, Swap, Undelegate, ValidatorVote,
     },
     view::action_view::OutputView,
     Action, ActionView, IsAction, TransactionPerspective, TransactionView,
@@ -294,35 +294,35 @@ impl Transaction {
         })
     }
 
-    // pub fn dao_deposits(&self) -> impl Iterator<Item = &DaoDeposit> {
-    //     self.actions().filter_map(|action| {
-    //         if let Action::DaoDeposit(d) = action {
-    //             Some(d)
-    //         } else {
-    //             None
-    //         }
-    //     })
-    // }
+    pub fn dao_deposits(&self) -> impl Iterator<Item = &DaoDeposit> {
+        self.actions().filter_map(|action| {
+            if let Action::DaoDeposit(d) = action {
+                Some(d)
+            } else {
+                None
+            }
+        })
+    }
 
-    // pub fn dao_spends(&self) -> impl Iterator<Item = &DaoSpend> {
-    //     self.actions().filter_map(|action| {
-    //         if let Action::DaoSpend(s) = action {
-    //             Some(s)
-    //         } else {
-    //             None
-    //         }
-    //     })
-    // }
+    pub fn dao_spends(&self) -> impl Iterator<Item = &DaoSpend> {
+        self.actions().filter_map(|action| {
+            if let Action::DaoSpend(s) = action {
+                Some(s)
+            } else {
+                None
+            }
+        })
+    }
 
-    // pub fn dao_outputs(&self) -> impl Iterator<Item = &DaoOutput> {
-    //     self.actions().filter_map(|action| {
-    //         if let Action::DaoOutput(o) = action {
-    //             Some(o)
-    //         } else {
-    //             None
-    //         }
-    //     })
-    // }
+    pub fn dao_outputs(&self) -> impl Iterator<Item = &DaoOutput> {
+        self.actions().filter_map(|action| {
+            if let Action::DaoOutput(o) = action {
+                Some(o)
+            } else {
+                None
+            }
+        })
+    }
 
     pub fn transaction_body(&self) -> TransactionBody {
         self.transaction_body.clone()
