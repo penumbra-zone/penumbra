09e8efc95a19328b3ae11c1e424a701310514476
Henry de Valence <hdevalence@penumbra.zone>
2023-03-05 20:51:55 -0700

    dex: implement ActionHandler for PositionClose

--- component/src/action_handler/actions/position/close.rs
+++ component/src/action_handler/actions/position/close.rs
@@ -6,28 +6,23 @@ use penumbra_storage::{StateRead, StateWrite};
 use penumbra_transaction::{action::PositionClose, Transaction};
 
 use crate::action_handler::ActionHandler;
+use crate::dex::PositionManager;
 
 #[async_trait]
 /// Debits an opened position NFT and credits a closed position NFT.
 impl ActionHandler for PositionClose {
     async fn check_stateless(&self, _context: Arc<Transaction>) -> Result<()> {
-        // It's important to reject all LP actions for now, to prevent
-        // inflation / minting bugs until we implement all required checks
-        // (e.g., minting tokens by withdrawing reserves we don't check)
-        Err(anyhow::anyhow!("lp actions not supported yet"))
+        // Nothing to do: the only validation is of the state change,
+        // and that's done by the value balance mechanism.
+        Ok(())
     }
 
     async fn check_stateful<S: StateRead + 'static>(&self, _state: Arc<S>) -> Result<()> {
-        // It's important to reject all LP actions for now, to prevent
-        // inflation / minting bugs until we implement all required checks
-        // (e.g., minting tokens by withdrawing reserves we don't check)
-        Err(anyhow::anyhow!("lp actions not supported yet"))
+        Ok(())
     }
 
-    async fn execute<S: StateWrite>(&self, _state: S) -> Result<()> {
-        // It's important to reject all LP actions for now, to prevent
-        // inflation / minting bugs until we implement all required checks
-        // (e.g., minting tokens by withdrawing reserves we don't check)
-        Err(anyhow::anyhow!("lp actions not supported yet"))
+    async fn execute<S: StateWrite>(&self, mut state: S) -> Result<()> {
+        // TODO: how much logic should live in ActionHandlers vs PositionManager?
+        state.position_close(&self.position_id).await
     }
 }
