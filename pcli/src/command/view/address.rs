dec1d1d25ac7d1998a9d7f963f27fbec6c245a5f
aubrey <aubrey@penumbra.zone>
2023-03-09 17:28:46 -0800

    pcli: extend functionality of pcli view address  to utilize accounts

--- pcli/src/command/view/address.rs
+++ pcli/src/command/view/address.rs
@@ -2,12 +2,13 @@ use anyhow::Result;
 use base64::Engine;
 use rand_core::OsRng;
 
-use penumbra_crypto::FullViewingKey;
+use penumbra_crypto::{Address, FullViewingKey};
 
 #[derive(Debug, clap::Parser)]
 pub struct AddressCmd {
-    /// Show the address with a particular numerical index [default: 0].
-    index: u32,
+    /// The address to provide information about
+    #[clap(default_value = "0")]
+    address_or_index: String,
     /// Generate an ephemeral address instead of an indexed one.
     #[clap(short, long)]
     ephemeral: bool,
@@ -23,18 +24,50 @@ impl AddressCmd {
     }
 
     pub fn exec(&self, fvk: &FullViewingKey) -> Result<()> {
-        let (address, _dtk) = match self.ephemeral {
-            false => fvk.incoming().payment_address(self.index.into()),
-            true => fvk.incoming().ephemeral_address(OsRng, self.index.into()),
-        };
+        let index: Result<u32, _> = self.address_or_index.parse();
 
-        if self.base64 {
-            println!(
-                "{}",
-                base64::engine::general_purpose::STANDARD.encode(address.to_vec())
-            );
+        if let Ok(index) = index {
+            //index provided
+
+            print!("Account {}: ", index);
+
+            let (address, _dtk) = match self.ephemeral {
+                false => fvk.incoming().payment_address(index.into()),
+                true => fvk.incoming().ephemeral_address(OsRng, index.into()),
+            };
+
+            match self.ephemeral {
+                false => print!(""),
+                true => print!("(ephemeral) "),
+            };
+
+            if self.base64 {
+                println!(
+                    "{}",
+                    base64::engine::general_purpose::STANDARD.encode(address.to_vec()),
+                );
+            } else {
+                println!("{}", address);
+            };
         } else {
-            println!("{}", address);
+            //address or nothing provided
+
+            let address: Address = self
+                .address_or_index
+                .parse()
+                .map_err(|_| anyhow::anyhow!("Provided address is invalid."))?;
+
+            match fvk.address_index(&address) {
+                Some(address_index) => println!(
+                    "Address is viewable with this full viewing key. Account index is {0}. {1}",
+                    address_index.account,
+                    match address_index.randomizer != [0u8; 12] {
+                        true => "Address is ephemeral.",
+                        false => "",
+                    }
+                ),
+                None => println!("Address is not viewable with this full viewing key."),
+            }
         }
 
         Ok(())
