5df578bbbe7439953f1307528ef9e1848c163717
Henry de Valence <hdevalence@penumbra.zone>
2023-03-10 17:01:07 -0800

    pclientd: pull out utility methods, allow start without init
    Factoring out common `init_sqlite` and `load_or_init_sqlite` methods lets
    `pclientd` start when the config file is manually written, not just after
    `pclientd init` which autogenerates it.

--- pclientd/src/lib.rs
+++ pclientd/src/lib.rs
@@ -1,7 +1,8 @@
 use std::env;
+use std::path::Path;
 
-use anyhow::{Context, Result};
-use camino::{Utf8Path, Utf8PathBuf};
+use anyhow::Result;
+use camino::Utf8PathBuf;
 use clap::Parser;
 use penumbra_crypto::keys::{SeedPhrase, SpendKey};
 use penumbra_crypto::FullViewingKey;
@@ -13,9 +14,10 @@ use penumbra_proto::{
     custody::v1alpha1::custody_protocol_service_server::CustodyProtocolServiceServer,
     view::v1alpha1::view_protocol_service_server::ViewProtocolServiceServer,
 };
-use penumbra_view::ViewService;
+use penumbra_view::{Storage, ViewService};
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
+
 use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
@@ -29,6 +31,19 @@ pub struct ClientConfig {
     pub kms_config: Option<soft_kms::Config>,
     /// FVK for both view and custody modes
     pub fvk: FullViewingKey,
+}
+
+impl ClientConfig {
+    pub fn load<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let contents = std::fs::read_to_string(path)?;
+        Ok(toml::from_str(&contents)?)
+    }
+
+    pub fn save<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+        let contents = toml::to_string_pretty(&self)?;
+        std::fs::write(path, contents)?;
+        Ok(())
+    }
 }
 
 #[derive(Debug, Parser)]
@@ -79,40 +94,54 @@ pub enum Command {
 }
 
 impl Opt {
+    fn config_path(&self) -> Utf8PathBuf {
+        let mut path = self.home.clone();
+        path.push("config.toml");
+        path
+    }
+
+    fn sqlite_path(&self) -> Utf8PathBuf {
+        let mut path = self.home.clone();
+        path.push("pclientd-db.sqlite");
+        path
+    }
+
+    async fn init_sqlite(&self, fvk: &FullViewingKey) -> Result<Storage> {
+        // Initialize client and storage
+        let mut client =
+            ObliviousQueryServiceClient::connect(format!("http://{}:{}", self.node, self.pd_port))
+                .await?;
+
+        let params = client
+            .chain_parameters(tonic::Request::new(ChainParametersRequest {
+                chain_id: String::new(),
+            }))
+            .await?
+            .into_inner()
+            .try_into()?;
+
+        fs::create_dir_all(&self.home)?;
+
+        Ok(Storage::initialize(&self.sqlite_path(), fvk.clone(), params).await?)
+    }
+
+    async fn load_or_init_sqlite(&self, fvk: &FullViewingKey) -> Result<Storage> {
+        if self.sqlite_path().exists() {
+            Ok(Storage::load(self.sqlite_path()).await?)
+        } else {
+            self.init_sqlite(fvk).await
+        }
+    }
+
     pub async fn exec(self) -> Result<()> {
         let opt = self;
-        match opt.cmd {
+        match &opt.cmd {
             Command::Init {
                 full_viewing_key,
                 custody,
             } => {
-                // Initialize client and storage
-                let mut client = ObliviousQueryServiceClient::connect(format!(
-                    "http://{}:{}",
-                    opt.node, opt.pd_port
-                ))
-                .await?;
-
-                let params = client
-                    .chain_parameters(tonic::Request::new(ChainParametersRequest {
-                        chain_id: String::new(),
-                    }))
-                    .await?
-                    .into_inner()
-                    .try_into()?;
-
-                fs::create_dir_all(&opt.home)?;
-
-                let mut sqlite_path = opt.home.clone();
-                sqlite_path.push("pclientd-db.sqlite");
-
-                penumbra_view::Storage::initialize(
-                    <Utf8PathBuf as AsRef<Utf8Path>>::as_ref(&sqlite_path),
-                    FullViewingKey::from_str(full_viewing_key.as_ref())
-                        .context("The provided string is not a valid FullViewingKey")?,
-                    params,
-                )
-                .await?;
+                let fvk = full_viewing_key.parse()?;
+                opt.init_sqlite(&fvk).await?;
 
                 println!(
                     "Initializing storage and configuration at: {:?}",
@@ -193,35 +222,8 @@ impl Opt {
             Command::Start { host, view_port } => {
                 tracing::info!(?opt.home, ?host, ?view_port, ?opt.node, ?opt.pd_port, "starting pclientd");
 
-                let sqlite_path = &mut opt.home.clone();
-                sqlite_path.push("pclientd-db.sqlite");
-
-                println!(
-                    "Reading storage and configuration at: {:?}",
-                    fs::canonicalize(&opt.home)?
-                );
-
-                let storage = penumbra_view::Storage::load(
-                    <Utf8PathBuf as AsRef<Utf8Path>>::as_ref(&sqlite_path),
-                )
-                .await?;
-
-                let config_file_path = &mut opt.home.clone();
-                config_file_path.push("config.toml");
-
-                let config_contents = match fs::read_to_string(config_file_path) {
-                    // If successful return the files text as `contents`.
-                    // `c` is a local variable.
-                    Ok(c) => c,
-                    // Handle the `error` case.
-                    Err(_) => {
-                        // Write `msg` to `stderr`.
-                        eprintln!("Could not read file");
-                        "".to_string()
-                    }
-                };
-
-                let config: ClientConfig = toml::from_str(&config_contents)?;
+                let config = ClientConfig::load(opt.config_path())?;
+                let storage = opt.load_or_init_sqlite(&config.fvk).await?;
 
                 println!(
                     "Starting view service at node {:?} and port {:?}",
