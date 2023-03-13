f9180c4328b9082540cb3e16c9a068cc84f331a0
redshiftzero <jen@penumbra.zone>
2023-03-13 11:15:48 -0400

    use build method for build_dao_transaction

--- component/src/action_handler/actions/proposal/submit.rs
+++ component/src/action_handler/actions/proposal/submit.rs
@@ -300,21 +300,19 @@ const DAO_TRANSACTION_RNG_SEED: &[u8; 32] = b"Penumbra DAO's tx build rng seed";
 
 async fn build_dao_transaction(transaction_plan: TransactionPlan) -> Result<Transaction> {
     let effect_hash = transaction_plan.effect_hash(&DAO_FULL_VIEWING_KEY);
-    transaction_plan
-        .build_concurrent(
-            &mut ChaCha20Rng::from_seed(*DAO_TRANSACTION_RNG_SEED),
-            &DAO_FULL_VIEWING_KEY,
-            AuthorizationData {
-                effect_hash,
-                spend_auths: Default::default(),
-                delegator_vote_auths: Default::default(),
-            },
-            WitnessData {
-                anchor: penumbra_tct::Tree::new().root(),
-                state_commitment_proofs: Default::default(),
-            },
-        )
-        .await
+    transaction_plan.build(
+        &mut ChaCha20Rng::from_seed(*DAO_TRANSACTION_RNG_SEED),
+        &DAO_FULL_VIEWING_KEY,
+        AuthorizationData {
+            effect_hash,
+            spend_auths: Default::default(),
+            delegator_vote_auths: Default::default(),
+        },
+        WitnessData {
+            anchor: penumbra_tct::Tree::new().root(),
+            state_commitment_proofs: Default::default(),
+        },
+    )
 }
 
 #[cfg(test)]
