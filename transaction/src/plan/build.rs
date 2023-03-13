73c80ccbc49d52732a75ab95204faebf1b0cfe3f
redshiftzero <jen@penumbra.zone>
2023-03-13 10:24:05 -0400

    chore: switch to decaf377 0.3.0, rename fast-proofs to parallel

--- transaction/src/plan/build.rs
+++ transaction/src/plan/build.rs
@@ -189,7 +189,7 @@ impl TransactionPlan {
         })
     }
 
-    #[cfg(feature = "fast-proofs")]
+    #[cfg(feature = "parallel")]
     /// Build the transaction this plan describes while proving concurrently.
     /// This can be used in environments that support tokio tasks.
     pub async fn build_concurrent<R: CryptoRng + RngCore>(
