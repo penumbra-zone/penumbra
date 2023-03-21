0dc3558900063fb77418b3e48cd144133d6d44bc
Ava Howell <ava@avahowell.me>
2023-03-20 20:45:05 -0700

    remove unused compactblock state key

--- component/src/compactblock/state_key.rs
+++ component/src/compactblock/state_key.rs
@@ -1,8 +1,5 @@
 use std::string::String;
 
-pub fn stub_state_commitment_tree() -> &'static str {
-    "compactblock/stub/state_commitment_tree"
-}
 pub fn stub_compact_block() -> &'static str {
     "compactblock/stub/compact_block"
 }
