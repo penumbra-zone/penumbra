86887ab797c8873284825ade920a4dabdd72f5cf
Chris Czub <chris@penumbralabs.xyz>
2023-04-04 17:39:23 -0400

    Add stub narsil crate to workspace (#2260)

---
+++ narsil/src/bin/narsilcli.rs
@@ -0,0 +1,4 @@
+/// narsilcli is used as a command-line interface for the narsil server.
+fn main() {
+    println!("narsil is currently being forged, please contact the Dwarven-smith Telchar for more details")
+}
---
+++ narsil/src/bin/narsild.rs
@@ -0,0 +1,4 @@
+/// narsild spins up the narsil ledger implementation.
+fn main() {
+    println!("narsil is currently being forged, please contact the Dwarven-smith Telchar for more details")
+}
