3fb436e8e1e1c9327e0e231ab62f96fdd712d0aa
Chris Czub <chris@penumbralabs.xyz>
2023-04-05 12:58:10 -0400

    Add missing tower-trace crate

---
+++ tower-trace/src/lib.rs
@@ -0,0 +1,20 @@
+/// A vendored copy of the unpublished `tracing-tower` crate.
+pub mod trace;
+
+mod request_ext;
+pub use request_ext::RequestExt;
+
+use std::net::SocketAddr;
+
+// Extracted from tonic's remote_addr implementation; we'd like to instrument
+// spans with the remote addr at the server level rather than at the individual
+// request level, but the hook available to do that gives us an http::Request
+// rather than a tonic::Request, so the tonic::Request::remote_addr method isn't
+// available.
+pub fn remote_addr(req: &http::Request<()>) -> Option<SocketAddr> {
+    use tonic::transport::server::TcpConnectInfo;
+    // NOTE: needs to also check TlsConnectInfo if we use TLS
+    req.extensions()
+        .get::<TcpConnectInfo>()
+        .and_then(|i| i.remote_addr())
+}
