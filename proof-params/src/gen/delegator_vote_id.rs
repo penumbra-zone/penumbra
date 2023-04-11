2006aedca7597d2cc86a3a8bfa1236082ac5c790
redshiftzero <jen@penumbralabs.xyz>
2023-04-10 22:53:02 -0400

    crypto: parameter setup for delegator votes

---
+++ proof-params/src/gen/delegator_vote_id.rs
@@ -0,0 +1,3 @@
+
+pub const PROVING_KEY_ID: &'static str = "groth16pk1afhkjvahe3zss42d4rw6yxkp5gdq8m6pxx7zrenvu7msntqlpvasn3hrt9";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1dawnhu3zgcrpm0m0k2ttj6946khmua99vtnjz9qy8dsp6qyqsxqquqvx55";
