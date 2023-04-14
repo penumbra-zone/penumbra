8d2b8825d652003574a26b75504dd13e41c0df6b
finch <finch@plaidfinch.net>
2023-04-14 17:05:47 -0400

    Convert TreeStore to use rusqlite instead of sqlx

--- view/src/storage/sct.rs
+++ view/src/storage/sct.rs
@@ -1,49 +1,64 @@
+use std::ops::Range;
+
 use anyhow::Context as _;
-use async_trait::async_trait;
-use futures::{Stream, StreamExt};
-use sqlx::Either;
-use std::{ops::Range, pin::Pin};
+use rusqlite::Transaction;
 
 use penumbra_tct::{
-    storage::{AsyncRead, AsyncWrite, StoredPosition},
+    storage::{Read, StoredPosition, Write},
     structure::Hash,
     Commitment, Forgotten, Position,
 };
 
-pub struct TreeStore<'a, 'c: 'a>(pub &'a mut sqlx::Transaction<'c, sqlx::Sqlite>);
+pub struct TreeStore<'a, 'c: 'a>(pub &'a mut Transaction<'c>);
 
-#[async_trait]
-impl AsyncRead for TreeStore<'_, '_> {
+impl Read for TreeStore<'_, '_> {
     type Error = anyhow::Error;
 
-    async fn position(&mut self) -> Result<StoredPosition, Self::Error> {
-        Ok(sqlx::query!("SELECT position FROM sct_position LIMIT 1")
-            .fetch_one(&mut *self.0)
-            .await?
-            .position
-            .map(|p| Position::from(p as u64))
-            .into())
+    type HashesIter<'a> = Box<dyn Iterator<Item = Result<(Position, u8, Hash), Self::Error>> + 'a>
+    where
+        Self: 'a;
+
+    type CommitmentsIter<'a> = Box<dyn Iterator<Item = Result<(Position, Commitment), Self::Error>> + 'a>
+    where
+        Self: 'a;
+
+    fn position(&mut self) -> Result<StoredPosition, Self::Error> {
+        let stmt = self
+            .0
+            .prepare_cached("SELECT position FROM sct_position LIMIT 1")
+            .context("failed to prepare position query")?;
+        let position = stmt
+            .query_row::<Option<u64>, _, _>([], |row| row.get(0))
+            .context("failed to query position")?
+            .map(Position::from)
+            .into();
+        Ok(position)
     }
 
-    async fn forgotten(&mut self) -> Result<Forgotten, Self::Error> {
-        Ok((sqlx::query!("SELECT forgotten FROM sct_forgotten LIMIT 1")
-            .fetch_one(&mut *self.0)
-            .await?
-            .forgotten as u64)
-            .into())
+    fn forgotten(&mut self) -> Result<Forgotten, Self::Error> {
+        let stmt = self
+            .0
+            .prepare_cached("SELECT forgotten FROM sct_forgotten LIMIT 1")
+            .context("failed to prepare forgotten query")?;
+        let forgotten = stmt
+            .query_row::<u64, _, _>([], |row| row.get(0))
+            .context("failed to query forgotten")?
+            .into();
+        Ok(forgotten)
     }
 
-    async fn hash(&mut self, position: Position, height: u8) -> Result<Option<Hash>, Self::Error> {
+    fn hash(&mut self, position: Position, height: u8) -> Result<Option<Hash>, Self::Error> {
         let position = u64::from(position) as i64;
 
-        let bytes = sqlx::query!(
-            "SELECT hash FROM sct_hashes WHERE position = ? AND height = ? LIMIT 1",
-            position,
-            height
-        )
-        .fetch_optional(&mut *self.0)
-        .await?
-        .map(|r| r.hash);
+        let stmt = self
+            .0
+            .prepare_cached(
+                "SELECT hash FROM sct_hashes WHERE position = ?1 AND height = ?2 LIMIT 1",
+            )
+            .context("failed to prepare hash query")?;
+        let bytes = stmt
+            .query_row::<Option<Vec<u8>>, _, _>((&position, &height), |row| row.get(0))
+            .context("failed to query hash")?;
 
         Ok(bytes
             .map(|bytes| {
@@ -54,43 +69,55 @@ impl AsyncRead for TreeStore<'_, '_> {
             .transpose()?)
     }
 
-    fn hashes(
-        &mut self,
-    ) -> Pin<Box<dyn Stream<Item = Result<(Position, u8, Hash), Self::Error>> + Send + '_>> {
-        Box::pin(
-            sqlx::query!("SELECT position, height, hash FROM sct_hashes")
-                .fetch_many(&mut *self.0)
-                .map(|row| {
-                    let row = row?;
-                    if let Either::Right(row) = row {
-                        Ok::<_, Self::Error>(Some((
-                            Position::from(row.position as u64),
-                            row.height as u8,
-                            Hash::from_bytes(
-                                row.hash
-                                    .try_into()
-                                    .map_err(|_| anyhow::anyhow!("hash was of incorrect length"))?,
-                            )?,
-                        )))
-                        .context("could not decode hash from local database")
-                    } else {
-                        Ok(None)
-                    }
-                })
-                .filter_map(|item| async move { item.transpose() }),
-        )
+    fn hashes(&mut self) -> Self::HashesIter<'_> {
+        let mut stmt = match self
+            .0
+            .prepare_cached("SELECT position, height, hash FROM sct_hashes")
+            .context("failed to prepare hashes query")
+        {
+            Ok(stmt) => stmt,
+            // If an error happens while preparing the statement, shove it inside the first returned
+            // item of the iterator, because we can't return an outer error:
+            Err(e) => return Box::new(std::iter::once(Err(e))),
+        };
+
+        let rows = match stmt
+            .query_map([], |row| {
+                let position: i64 = row.get(0)?;
+                let height: u8 = row.get(1)?;
+                let hash: Vec<u8> = row.get(2)?;
+                Ok((position, height, hash))
+            })
+            .context("couldn't query database")
+        {
+            Ok(rows) => rows,
+            // If an error happens while querying the database, shove it inside the first
+            // returned item of the iterator, because we can't return an outer error:
+            Err(e) => return Box::new(std::iter::once(Err(e))),
+        };
+
+        Box::new(rows.map(|result| {
+            let result = result.context("couldn't get position, height, or hash");
+            result.and_then(|(position, height, hash)| {
+                let hash = <[u8; 32]>::try_from(hash)
+                    .map_err(|_| anyhow::anyhow!("hash was of incorrect length"))
+                    .and_then(|array| Hash::from_bytes(array).map_err(Into::into))?;
+                Ok::<_, anyhow::Error>((Position::from(position as u64), height, hash))
+            })
+        }))
     }
 
-    async fn commitment(&mut self, position: Position) -> Result<Option<Commitment>, Self::Error> {
+    fn commitment(&mut self, position: Position) -> Result<Option<Commitment>, Self::Error> {
         let position = u64::from(position) as i64;
 
-        let bytes: Option<Vec<u8>> = sqlx::query!(
-            "SELECT commitment FROM sct_commitments WHERE position = ? LIMIT 1",
-            position,
-        )
-        .fetch_optional(&mut *self.0)
-        .await?
-        .map(|r| r.commitment);
+        let stmt = self
+            .0
+            .prepare_cached("SELECT commitment FROM sct_commitments WHERE position = ?1 LIMIT 1")
+            .context("failed to prepare commitment query")?;
+
+        let bytes = stmt
+            .query_row::<Option<Vec<u8>>, _, _>((&position,), |row| row.get(0))
+            .context("failed to query commitment")?;
 
         Ok(bytes
             .map(|bytes| {
@@ -101,50 +128,68 @@ impl AsyncRead for TreeStore<'_, '_> {
             .transpose()?)
     }
 
-    fn commitments(
-        &mut self,
-    ) -> Pin<Box<dyn Stream<Item = Result<(Position, Commitment), Self::Error>> + Send + '_>> {
-        Box::pin(
-            sqlx::query!("SELECT position, commitment FROM sct_commitments")
-                .fetch_many(&mut *self.0)
-                .map(|row| {
-                    let row = row?;
-                    if let Either::Right(row) = row {
-                        Ok::<_, Self::Error>(Some((
-                            Position::from(row.position as u64),
-                            Commitment::try_from(<[u8; 32]>::try_from(row.commitment).map_err(
-                                |_| anyhow::anyhow!("commitment was of incorrect length"),
-                            )?)?,
-                        )))
-                        .context("could not decode note commitment from local database")
-                    } else {
-                        Ok(None)
-                    }
-                })
-                .filter_map(|item| async move { item.transpose() }),
-        )
+    fn commitments(&mut self) -> Self::CommitmentsIter<'_> {
+        let mut stmt = match self
+            .0
+            .prepare_cached("SELECT position, commitment FROM sct_commitments")
+            .context("failed to prepare commitments query")
+        {
+            Ok(stmt) => stmt,
+            // If an error happens while preparing the statement, shove it inside the first returned
+            // item of the iterator, because we can't return an outer error:
+            Err(e) => return Box::new(std::iter::once(Err(e))),
+        };
+
+        let rows = match stmt
+            .query_map([], |row| {
+                let position: i64 = row.get(0)?;
+                let commitment: Vec<u8> = row.get(1)?;
+                Ok((position, commitment))
+            })
+            .context("couldn't query database")
+        {
+            Ok(rows) => rows,
+            // If an error happens while querying the database, shove it inside the first
+            // returned item of the iterator, because we can't return an outer error:
+            Err(e) => return Box::new(std::iter::once(Err(e))),
+        };
+
+        Box::new(rows.map(|result| {
+            let result = result.context("couldn't get position or commitment");
+            result.and_then(|(position, commitment)| {
+                let commitment = <[u8; 32]>::try_from(commitment)
+                    .map_err(|_| anyhow::anyhow!("commitment was of incorrect length"))
+                    .and_then(|array| Commitment::try_from(array).map_err(Into::into))?;
+                Ok::<_, anyhow::Error>((Position::from(position as u64), commitment))
+            })
+        }))
     }
 }
 
-#[async_trait]
-impl AsyncWrite for TreeStore<'_, '_> {
-    async fn set_position(&mut self, position: StoredPosition) -> Result<(), Self::Error> {
+impl Write for TreeStore<'_, '_> {
+    fn set_position(&mut self, position: StoredPosition) -> Result<(), Self::Error> {
         let position = Option::from(position).map(|p: Position| u64::from(p) as i64);
-        sqlx::query!("UPDATE sct_position SET position = ?", position)
-            .execute(&mut *self.0)
-            .await?;
+
+        self.0
+            .prepare_cached("UPDATE sct_position SET position = ?1")
+            .context("failed to prepare position update")?
+            .execute(&[&position]);
+
         Ok(())
     }
 
-    async fn set_forgotten(&mut self, forgotten: Forgotten) -> Result<(), Self::Error> {
+    fn set_forgotten(&mut self, forgotten: Forgotten) -> Result<(), Self::Error> {
         let forgotten = u64::from(forgotten) as i64;
-        sqlx::query!("UPDATE sct_forgotten SET forgotten = ?", forgotten)
-            .execute(&mut *self.0)
-            .await?;
+
+        self.0
+            .prepare_cached("UPDATE sct_position SET forgotten = ?1")
+            .context("failed to prepare forgotten update")?
+            .execute(&[&forgotten]);
+
         Ok(())
     }
 
-    async fn add_hash(
+    fn add_hash(
         &mut self,
         position: Position,
         height: u8,
@@ -154,48 +199,48 @@ impl AsyncWrite for TreeStore<'_, '_> {
         let position = u64::from(position) as i64;
         let hash = hash.to_bytes().to_vec();
 
-        sqlx::query!(
-            "INSERT INTO sct_hashes (position, height, hash) VALUES (?, ?, ?) ON CONFLICT DO NOTHING",
-            position,
-            height,
-            hash
-        )
-        .execute(&mut *self.0).await?;
+        self.0.prepare_cached(
+            "INSERT INTO sct_hashes (position, height, hash) VALUES (?1, ?2, ?3) ON CONFLICT DO NOTHING"
+        ).context("failed to prepare hash insert")?
+            .execute((&position, &height, &hash))
+            .context("failed to insert hash")?;
+
         Ok(())
     }
 
-    async fn add_commitment(
+    fn add_commitment(
         &mut self,
         position: Position,
         commitment: Commitment,
     ) -> Result<(), Self::Error> {
         let position = u64::from(position) as i64;
         let commitment = <[u8; 32]>::from(commitment).to_vec();
-        sqlx::query!(
-            "INSERT INTO sct_commitments (position, commitment) VALUES (?, ?) ON CONFLICT DO NOTHING",
-            position,
-            commitment
-        )
-        .execute(&mut *self.0)
-        .await?;
+
+        self.0.prepare_cached(
+            "INSERT INTO sct_commitments (position, commitment) VALUES (?1, ?2) ON CONFLICT DO NOTHING"
+        ).context("failed to prepare commitment insert")?
+            .execute((&position, &commitment))
+            .context("failed to insert commitment")?;
+
         Ok(())
     }
 
-    async fn delete_range(
+    fn delete_range(
         &mut self,
         below_height: u8,
         positions: Range<Position>,
     ) -> Result<(), Self::Error> {
         let start = u64::from(positions.start) as i64;
         let end = u64::from(positions.end) as i64;
-        sqlx::query!(
-            "DELETE FROM sct_hashes WHERE position >= ? AND position < ? AND height < ?",
-            start,
-            end,
-            below_height
-        )
-        .execute(&mut *self.0)
-        .await?;
+
+        self.0
+            .prepare_cached(
+                "DELETE FROM sct_hashes WHERE position >= ?1 AND position < ?2 AND height < ?3",
+            )
+            .context("failed to prepare hash delete")?
+            .execute((&start, &end, &below_height))
+            .context("failed to delete hashes")?;
+
         Ok(())
     }
 }
