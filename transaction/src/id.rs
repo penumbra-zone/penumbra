1d1b2eec850d5e08bc1d58c49af572cd5e485afe
Henry de Valence <hdevalence@penumbralabs.xyz>
2023-04-26 18:21:42 -0700

    fix pcli

--- transaction/src/id.rs
+++ transaction/src/id.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use penumbra_proto::{penumbra::core::transaction::v1alpha1 as pb, DomainType};
 use serde::{Deserialize, Serialize};
 
@@ -5,6 +7,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 #[serde(try_from = "pb::Id", into = "pb::Id")]
 pub struct Id(pub [u8; 32]);
+
+impl AsRef<[u8]> for Id {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
 
 impl std::fmt::Debug for Id {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -15,6 +23,20 @@ impl std::fmt::Debug for Id {
 impl std::fmt::Display for Id {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", hex::encode(&self.0))
+    }
+}
+
+impl FromStr for Id {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = hex::decode(s)?;
+        if bytes.len() != 32 {
+            return Err(anyhow::anyhow!("invalid transaction ID length"));
+        }
+        let mut id = [0u8; 32];
+        id.copy_from_slice(&bytes);
+        Ok(Id(id))
     }
 }
 
