84923f935806ddfd38e71cac3234f7d0acd79b6c
Conor Schaefer <conor@penumbralabs.xyz>
2023-04-27 11:34:40 -0700

    pclientd: fix envfilter setting, unbreaking bin
    The envfilter setting pulled in via #2327 inadvertently broke the `pclientd`
    binary, by preventing it from running if the env var RUST_LOG was not set (the
    default behavior). The other bins like pd and narsild handled this error
    appropriately, via `or_else` and setting a default of "info". Porting that
    logic to pclientd resolves.

--- pclientd/src/main.rs
+++ pclientd/src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<()> {
     let fmt_layer = tracing_subscriber::fmt::layer()
         .with_ansi(atty::is(atty::Stream::Stdout))
         .with_target(false);
-    let filter_layer = EnvFilter::try_from_default_env()?;
+    let filter_layer = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
     let registry = tracing_subscriber::registry()
         .with(filter_layer)
         .with(fmt_layer);
