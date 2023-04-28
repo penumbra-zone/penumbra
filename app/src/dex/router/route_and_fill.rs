349afda676fdfb666a7bafc09e9a2a33cb0433d5
Chris Czub <chris@penumbralabs.xyz>
2023-04-28 18:19:23 -0400

    Start splitting apart logic in route and fill

--- app/src/dex/router/route_and_fill.rs
+++ app/src/dex/router/route_and_fill.rs
@@ -37,12 +37,55 @@ pub trait RouteAndFill: StateWrite + Sized {
         // Depending on the contents of the batch swap inputs, we might need to path search in either direction.
         if delta_1.value() > 0 {
             // There is input for asset 1, so we need to route for asset 1 -> asset 2
+            self.route_and_fill_inner(
+                trading_pair.asset_1(),
+                trading_pair.asset_2(),
+                delta_1,
+                delta_2,
+            )
+            .await?;
         }
 
         if delta_2.value() > 0 {
             // There is input for asset 2, so we need to route for asset 2 -> asset 1
+            self.route_and_fill_inner(
+                trading_pair.asset_2(),
+                trading_pair.asset_1(),
+                delta_2,
+                delta_1,
+            )
+            .await?;
         }
 
+        let output_data = BatchSwapOutputData {
+            height: block_height,
+            trading_pair,
+            delta_1,
+            delta_2,
+            lambda_1_1,
+            lambda_2_2,
+            lambda_1_2,
+            lambda_2_1,
+        };
+        tracing::debug!(?output_data);
+        Arc::get_mut(self)
+            .expect("expected state to have no other refs")
+            .set_output_data(output_data);
+        Ok(())
+    }
+
+    // TODO: this is publically exposed rn, but should be private
+    async fn route_and_fill_inner(
+        self: &mut Arc<Self>,
+        trading_pair: TradingPair,
+        batch_data: SwapFlow,
+        // TODO: use price_limit to clamp spill price or set to 1 for arb
+        price_limit: Amount,
+        block_height: u64,
+    ) -> Result<()>
+    where
+        Self: 'static,
+    {
         // Find the best route between the two assets in the trading pair.
         let (path, spill_price) = self
             // TODO: max hops should not be hardcoded
@@ -91,21 +134,6 @@ pub trait RouteAndFill: StateWrite + Sized {
         } else {
             (delta_1, delta_2, 0u64.into(), 0u64.into())
         };
-
-        let output_data = BatchSwapOutputData {
-            height: block_height,
-            trading_pair,
-            delta_1,
-            delta_2,
-            lambda_1_1,
-            lambda_2_2,
-            lambda_1_2,
-            lambda_2_1,
-        };
-        tracing::debug!(?output_data);
-        Arc::get_mut(self)
-            .expect("expected state to have no other refs")
-            .set_output_data(output_data);
         Ok(())
     }
 }
