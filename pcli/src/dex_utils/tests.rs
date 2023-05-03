63e9074f41e1f5059d1b9710e86dc3549c7b928c
erwanor <erwan.ounn.84@gmail.com>
2023-05-03 13:57:46 -0400

    pcli(dex_utils): test solving against known vector

---
+++ pcli/src/dex_utils/tests.rs
@@ -0,0 +1,32 @@
+mod tests {
+    use crate::dex_utils::approximate::xyk;
+    #[test]
+    /// Tests that the approximation work with a known solution obtained using LAPACK.
+    fn test_xyk_solver() -> anyhow::Result<()> {
+        let alphas = vec![2.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0];
+        let k = 2.0;
+        let n = alphas.len();
+
+        let solution_vector = xyk::solve(&alphas, k, n)?;
+        let known_solution = [
+            1.1010205144336442,
+            0.18619156818115412,
+            0.13340819077963983,
+            0.07839642626021837,
+            0.05320822764119848,
+            0.0391713189347902,
+            0.030397692310478064,
+            0.3782060614588767,
+        ];
+
+        assert_eq!(solution_vector.len(), known_solution.len());
+        solution_vector
+            .iter()
+            .zip(known_solution)
+            .for_each(|(solution_found, solution_known)| {
+                assert!(approx::abs_diff_eq!(*solution_found, solution_known))
+            });
+
+        Ok(())
+    }
+}
