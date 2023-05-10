99b6448a8acbd5f1fdbc9234a406b756b4394efa
erwanor <erwan.ounn.84@gmail.com>
2023-05-10 11:40:42 -0400

    pcli(dex_utils): render one-sided positions

--- pcli/src/command/utils.rs
+++ pcli/src/command/utils.rs
@@ -1,5 +1,9 @@
 use comfy_table::{presets, Table};
-use penumbra_crypto::{asset, dex::lp::position::Position};
+use penumbra_crypto::{
+    asset,
+    dex::{lp::position::Position, DirectedUnitPair},
+    Amount,
+};
 
 pub(crate) fn render_positions(asset_cache: &asset::Cache, positions: &[Position]) -> String {
     let mut table = Table::new();
@@ -45,7 +49,7 @@ pub(crate) fn render_positions(asset_cache: &asset::Cache, positions: &[Position
     format!("{table}")
 }
 
-pub(crate) fn render_approximated_positions(asset_cache: &asset::Cache, positions: &[Position]) -> String {
+pub(crate) fn render_xyk_approximation(pair: DirectedUnitPair, positions: &[Position]) -> String {
     let mut table = Table::new();
     table.load_preset(presets::NOTHING);
     table.set_header(vec![
@@ -58,31 +62,40 @@ pub(crate) fn render_approximated_positions(asset_cache: &asset::Cache, position
 
     for position in positions {
         let trading_pair = position.phi.pair;
-        let denom_1 = asset_cache
-            .get(&trading_pair.asset_1())
-            .expect("asset should be known to view service");
-        let denom_2 = asset_cache
-            .get(&trading_pair.asset_2())
-            .expect("asset should be known to view service");
 
-        let display_denom_1 = denom_1.default_unit();
-        let display_denom_2 = denom_2.default_unit();
+        let well_directed_phi = position.phi.orient_end(pair.end.id()).unwrap();
+        let price_a_to_b = well_directed_phi.effective_price();
+        let price_b_to_a = well_directed_phi.effective_price_inv();
+
+        let price_and_pair_entry =
+            if position.reserves_for(pair.start.id()).unwrap() == Amount::zero() {
+                format!(
+                    "{}: {:.5}, fee: {}bps",
+                    pair.flip().to_string(),
+                    price_b_to_a,
+                    position.phi.component.fee
+                )
+            } else {
+                format!(
+                    "{}: {:.5}, fee: {}bps",
+                    pair.to_string(),
+                    price_a_to_b,
+                    position.phi.component.fee
+                )
+            };
 
         table.add_row(vec![
             format!("{}", position.id()),
-            format!("({}, {})", display_denom_1, display_denom_2),
+            format!("{}", pair.to_string()),
             position.state.to_string(),
             format!(
                 "({}, {})",
-                display_denom_1.format_value(position.reserves.r1),
-                display_denom_2.format_value(position.reserves.r2)
+                pair.start
+                    .format_value(position.reserves_for(pair.start.id()).unwrap()),
+                pair.end
+                    .format_value(position.reserves_for(pair.end.id()).unwrap()),
             ),
-            format!(
-                "fee: {}bps, p/q: {:.6}, q/p: {:.18}",
-                position.phi.component.fee,
-                f64::from(position.phi.component.p) / f64::from(position.phi.component.q),
-                f64::from(position.phi.component.q) / f64::from(position.phi.component.p),
-            ),
+            price_and_pair_entry,
         ]);
     }
 
