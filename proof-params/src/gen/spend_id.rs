8eb0713d25b477d28d6a9298dc52fd70bd2d4f61
redshiftzero <jen@penumbralabs.xyz>
2023-05-10 18:48:15 -0400

    crypto(zk): update parameter setup for proofs involving position

--- proof-params/src/gen/spend_id.rs
+++ proof-params/src/gen/spend_id.rs
@@ -1,3 +1,3 @@
-pub const PROVING_KEY_ID: &'static str =
-    "groth16pk1vtxptxc88jjkcklwumdd7a223m278r64dzxjptpuyy9j6hvfjg7syts3e7";
-pub const VERIFICATION_KEY_ID: &'static str = "DUMMY_VALUE";
+
+pub const PROVING_KEY_ID: &'static str = "groth16pk1kwcm75dh774fz7kyua6jwzkx7ntp9vgqjrwg7mddewczn6vwcjassj2dmd";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1dt5lyj63cjuzx9ejv6g8l5ahs35gagjj94vquac5zzt7a36x9v4suxa58m";
