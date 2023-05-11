3c8944a897e95f5b6b073eff45e527fc09988aa1
Henry de Valence <hdevalence@penumbralabs.xyz>
2023-05-10 23:00:02 -0700

    dex: close positions only at the end of the block, not during it
    We don't want to actually close the position in the ActionHandler, because
    otherwise the economic effects could depend on intra-block ordering, and we'd
    lose the ability to do block-scoped JIT liquidity, where a single transaction
    opens and closes a position, keeping liquidity live only during that block's
    batch swap execution.

--- app/src/action_handler/actions/position/close.rs
+++ app/src/action_handler/actions/position/close.rs
@@ -2,12 +2,11 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use async_trait::async_trait;
-use penumbra_crypto::dex::lp::position;
 use penumbra_storage::{StateRead, StateWrite};
 use penumbra_transaction::action::PositionClose;
 
 use crate::action_handler::ActionHandler;
-use crate::dex::{PositionManager, PositionRead};
+use crate::dex::PositionManager;
 
 #[async_trait]
 /// Debits an opened position NFT and credits a closed position NFT.
@@ -24,23 +23,12 @@ impl ActionHandler for PositionClose {
     }
 
     async fn execute<S: StateWrite>(&self, mut state: S) -> Result<()> {
-        let mut metadata = state
-            .position_by_id(&self.position_id)
-            .await?
-            .ok_or_else(|| {
-                anyhow::anyhow!("could not find position with id {}", self.position_id)
-            })?;
-
-        if metadata.state != position::State::Opened {
-            return Err(anyhow::anyhow!(
-                "attempted to close position {} with state {}, expected Opened",
-                self.position_id,
-                metadata.state
-            ));
-        }
-
-        metadata.state = position::State::Closed;
-        state.put_position(metadata);
+        // We don't want to actually close the position here, because otherwise
+        // the economic effects could depend on intra-block ordering, and we'd
+        // lose the ability to do block-scoped JIT liquidity, where a single
+        // transaction opens and closes a position, keeping liquidity live only
+        // during that block's batch swap execution.
+        state.queue_close_position(self.position_id);
 
         Ok(())
     }
