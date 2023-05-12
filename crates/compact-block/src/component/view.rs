c379714811a981c7308c25aa4b799f7241121bd3
finch <finch@plaidfinch.net>
2023-05-12 16:42:09 -0400

    Finish disentanglement, build compact block exactly once

--- crates/compact-block/src/component/view.rs
+++ crates/compact-block/src/component/view.rs
@@ -12,21 +12,12 @@ pub trait StateReadExt: StateRead {
     async fn compact_block(&self, height: u64) -> Result<Option<CompactBlock>> {
         self.get(&state_key::compact_block(height)).await
     }
-
-    fn stub_compact_block(&self) -> CompactBlock {
-        self.object_get(state_key::stub_compact_block())
-            .unwrap_or_default()
-    }
 }
 
 impl<T: StateRead + ?Sized> StateReadExt for T {}
 
 #[async_trait]
 pub trait StateWriteExt: StateWrite {
-    fn stub_put_compact_block(&mut self, compact_block: CompactBlock) {
-        self.object_put(state_key::stub_compact_block(), compact_block);
-    }
-
     fn set_compact_block(&mut self, compact_block: CompactBlock) {
         let height = compact_block.height;
         self.put(state_key::compact_block(height), compact_block);
