699f3cf37ae0c6211e3a549b36ffadf03eb647ee
Chris Czub <chris@penumbralabs.xyz>
2023-05-11 23:40:41 -0400

    Update allocations to include test_usd for everyone as well as a less obscene amount of allocations to osiris

--- testnets/new-testnet.sh
+++ testnets/new-testnet.sh
@@ -17,7 +17,7 @@ echo "Setting up allocations for new testnet..."
 cut -d , -f 6 < discord_history.csv \
     | tail -n +2 | sort | uniq \
     | grep ^penumbrav \
-    | sed -e 's/^/"1_000__000_000","upenumbra","/; s/$/"/' \
+    | gawk '{ printf "\"1_000__000_000\",\"upenumbra\",\""; printf $1; print "\""; printf "\"1_000\",\"test_usd\",\""; printf $1; print "\""; }' \
     | cat base_allocations.csv - > "$NEW_TESTNET_DIRECTORY/allocations.csv"
 
 echo "All done!"
