1ec4ca43d290e72da627e9cbcec82964310fd4d9
erwanor <erwan.ounn.84@gmail.com>
2023-05-14 16:17:33 -0400

    testnet: use string fmt specifier in printf

--- testnets/new-testnet.sh
+++ testnets/new-testnet.sh
@@ -22,7 +22,7 @@ echo "Setting up allocations for new testnet..."
 cut -d , -f 6 < discord_history.csv \
     | tail -n +2 | sort | uniq \
     | grep ^penumbrav \
-    | xargs -I{} printf '"1_000__000_000","upenumbra","{}"\n"1_000","test_usd","{}"\n' \
+    | xargs -I{} printf '"1_000__000_000","upenumbra","%s"\n"1_000","test_usd","%s"\n' {} {} \
     | cat base_allocations.csv - > "$NEW_TESTNET_DIRECTORY/allocations.csv"
 
 echo "All done!"
