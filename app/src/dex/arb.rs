3f81593bd5de68cbc11b5f03e1122e30a352f26a
Henry de Valence <hdevalence@penumbralabs.xyz>
2023-05-15 10:56:36 -0700

    dex: correct accounting for flash loan in arb

--- app/src/dex/arb.rs
+++ app/src/dex/arb.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use anyhow::Result;
 use async_trait::async_trait;
 use penumbra_chain::component::StateReadExt;
-use penumbra_crypto::{asset, dex::execution::SwapExecution, Value};
+use penumbra_crypto::{asset, dex::execution::SwapExecution, Value, STAKING_TOKEN_ASSET_ID};
 use penumbra_storage::{StateDelta, StateWrite};
 use tracing::instrument;
 
@@ -39,7 +39,7 @@ pub trait Arbitrage: StateWrite + Sized {
             fixed_candidates: Arc::new(fixed_candidates),
         };
 
-        // flash-loan 2^64 of the arb token to ourselves.
+        // Create a flash-loan 2^64 of the arb token to ourselves.
         let flash_loan = Value {
             asset_id: arb_token,
             amount: u64::MAX.into(),
@@ -49,17 +49,38 @@ pub trait Arbitrage: StateWrite + Sized {
             .route_and_fill(arb_token, arb_token, flash_loan.amount, params)
             .await?;
 
-        let Some(arb_profit) = (flash_loan.amount + output).checked_sub(&unfilled_input) else {
-            tracing::debug!("found unprofitable arb, discarding");
+        // Because we're trading the arb token to itself, the total output is the
+        // output from the route-and-fill, plus the unfilled input.
+        let total_output = output + unfilled_input;
+
+        // Now "repay" the flash loan by subtracting it from the total output.
+        let Some(arb_profit) = total_output.checked_sub(&flash_loan.amount) else {
+            // This shouldn't happen, but because route-and-fill prioritizes
+            // guarantees about forward progress over precise application of
+            // price limits, it technically could occur.
+            tracing::debug!("mis-estimation in route-and-fill led to unprofitable arb, discarding");
             return Ok(());
         };
 
         if arb_profit == 0u64.into() {
+            // If we didn't make any profit, we don't need to do anything,
+            // and we can just discard the state delta entirely.
             tracing::debug!("found 0-profit arb, discarding");
             return Ok(());
         }
 
-        tracing::debug!(?arb_profit, "successfully arbitraged positions");
+        // TODO: hack to avoid needing an asset cache for nice debug output
+        let formatted_profit = if arb_token == *STAKING_TOKEN_ASSET_ID {
+            let unit = asset::REGISTRY.parse_unit("penumbra");
+            format!("{}{}", unit.format_value(arb_profit), unit)
+        } else {
+            format!("{}", arb_profit.value())
+        };
+        tracing::debug!(
+            arb_profit = %formatted_profit,
+            raw_profit = ?arb_profit,
+            "successfully arbitraged positions, burning profit"
+        );
 
         // TODO: this is a bit nasty, can it be simplified?
         // should this even be done "inside" the method, or all the way at the top?
