8a79b9803eaa4c7c7cad3d67de90e7cb46f84eca
Henry de Valence <hdevalence@penumbralabs.xyz>
2023-05-18 14:51:26 -0700

    pcli: better formatting for positions

--- pcli/src/command/query/dex.rs
+++ pcli/src/command/query/dex.rs
@@ -1,11 +1,9 @@
-use crate::command::utils;
 use anyhow::{Context, Result};
 use comfy_table::{presets, Table};
 use futures::{Stream, StreamExt, TryStreamExt};
 use std::pin::Pin;
 
 use penumbra_crypto::{
-    asset,
     dex::{
         execution::SwapExecution,
         lp::position::{self, Position},
@@ -21,7 +19,10 @@ use penumbra_proto::client::v1alpha1::{
 use penumbra_view::ViewClient;
 use tonic::transport::Channel;
 
-use crate::App;
+use crate::{
+    command::utils::{self, render_positions},
+    App,
+};
 
 #[derive(Debug, clap::Subcommand)]
 pub enum DexCmd {
@@ -349,48 +350,4 @@ impl DexCmd {
 
         Ok(())
     }
-}
-
-fn render_positions(asset_cache: &asset::Cache, positions: &[Position]) -> String {
-    let mut table = Table::new();
-    table.load_preset(presets::NOTHING);
-    table.set_header(vec![
-        "ID",
-        "Trading Pair",
-        "State",
-        "Reserves",
-        "Trading Function",
-    ]);
-
-    for position in positions {
-        let trading_pair = position.phi.pair;
-        let denom_1 = asset_cache
-            .get(&trading_pair.asset_1())
-            .expect("asset should be known to view service");
-        let denom_2 = asset_cache
-            .get(&trading_pair.asset_2())
-            .expect("asset should be known to view service");
-
-        let display_denom_1 = denom_1.default_unit();
-        let display_denom_2 = denom_2.default_unit();
-
-        table.add_row(vec![
-            format!("{}", position.id()),
-            format!("({}, {})", display_denom_1, display_denom_2),
-            position.state.to_string(),
-            format!(
-                "({}, {})",
-                display_denom_1.format_value(position.reserves.r1),
-                display_denom_2.format_value(position.reserves.r2)
-            ),
-            format!(
-                "fee: {}bps, p/q: {:.6}, q/p: {:.6}",
-                position.phi.component.fee,
-                f64::from(position.phi.component.p) / f64::from(position.phi.component.q),
-                f64::from(position.phi.component.q) / f64::from(position.phi.component.p),
-            ),
-        ]);
-    }
-
-    format!("{table}")
 }
