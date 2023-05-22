e02adc1a0efb7c9a7f704dc52ffee593cb07c495
redshiftzero <jen@penumbralabs.xyz>
2023-05-22 15:47:29 -0400

    crypto(zk): parameter setup for swapclaim

---
+++ crates/crypto/proof-params/src/gen/swapclaim_id.rs
@@ -0,0 +1,3 @@
+
+pub const PROVING_KEY_ID: &'static str = "groth16pk1ljus4k87c2tzufvltzs5qv59ncrl6t20eq0qdpyvqp3f24kf3rkq465dq0";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1fxl9t23x8ahcrqp4a6n9tkhhghu2cum7q09fgwwmcenkxvnxsj7qx6kr3l";
