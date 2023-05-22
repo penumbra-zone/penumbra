0d1f87e4b41b9f8a0eda6cb5c47e8b108a9f5641
redshiftzero <jen@penumbralabs.xyz>
2023-05-22 18:09:56 -0400

    test(zk): check parameters are updated for swap claim proof

--- crates/bin/pcli/tests/proof.rs
+++ crates/bin/pcli/tests/proof.rs
@@ -13,13 +13,17 @@ use penumbra_crypto::{
     stake::{IdentityKey, Penalty, UnbondingToken},
     Amount, Balance, Fee, Note, Value,
 };
-use penumbra_dex::{swap::proof::SwapProof, swap::SwapPlaintext, TradingPair};
+use penumbra_dex::{
+    swap::proof::SwapProof, swap::SwapPlaintext, swap_claim::proof::SwapClaimProof,
+    BatchSwapOutputData, TradingPair,
+};
 use penumbra_proof_params::{
     DELEGATOR_VOTE_PROOF_PROVING_KEY, DELEGATOR_VOTE_PROOF_VERIFICATION_KEY,
     NULLIFIER_DERIVATION_PROOF_PROVING_KEY, NULLIFIER_DERIVATION_PROOF_VERIFICATION_KEY,
     OUTPUT_PROOF_PROVING_KEY, OUTPUT_PROOF_VERIFICATION_KEY, SPEND_PROOF_PROVING_KEY,
-    SPEND_PROOF_VERIFICATION_KEY, SWAP_PROOF_PROVING_KEY, SWAP_PROOF_VERIFICATION_KEY,
-    UNDELEGATECLAIM_PROOF_PROVING_KEY, UNDELEGATECLAIM_PROOF_VERIFICATION_KEY,
+    SPEND_PROOF_VERIFICATION_KEY, SWAPCLAIM_PROOF_PROVING_KEY, SWAPCLAIM_PROOF_VERIFICATION_KEY,
+    SWAP_PROOF_PROVING_KEY, SWAP_PROOF_VERIFICATION_KEY, UNDELEGATECLAIM_PROOF_PROVING_KEY,
+    UNDELEGATECLAIM_PROOF_VERIFICATION_KEY,
 };
 use penumbra_tct as tct;
 use rand_core::OsRng;
@@ -190,6 +194,99 @@ fn swap_proof_parameters_vs_current_swap_circuit() {
     .expect("can create proof");
 
     let proof_result = proof.verify(vk, balance_commitment, swap_commitment, fee_commitment);
+
+    assert!(proof_result.is_ok());
+}
+
+#[test]
+fn swap_claim_proof_happy_path() {
+    let pk = &*SWAPCLAIM_PROOF_PROVING_KEY;
+    let vk = &*SWAPCLAIM_PROOF_VERIFICATION_KEY;
+
+    let mut rng = OsRng;
+
+    let seed_phrase = SeedPhrase::generate(OsRng);
+    let sk_recipient = SpendKey::from_seed_phrase(seed_phrase, 0);
+    let fvk_recipient = sk_recipient.full_viewing_key();
+    let ivk_recipient = fvk_recipient.incoming();
+    let (claim_address, _dtk_d) = ivk_recipient.payment_address(0u32.into());
+    let nk = *sk_recipient.nullifier_key();
+
+    let gm = asset::REGISTRY.parse_unit("gm");
+    let gn = asset::REGISTRY.parse_unit("gn");
+    let trading_pair = TradingPair::new(gm.id(), gn.id());
+
+    let delta_1_i = Amount::from(2u64);
+    let delta_2_i = Amount::from(0u64);
+    let fee = Fee::default();
+
+    let swap_plaintext = SwapPlaintext::new(
+        &mut rng,
+        trading_pair,
+        delta_1_i,
+        delta_2_i,
+        fee,
+        claim_address,
+    );
+    let fee = swap_plaintext.clone().claim_fee;
+    let mut sct = tct::Tree::new();
+    let swap_commitment = swap_plaintext.swap_commitment();
+    sct.insert(tct::Witness::Keep, swap_commitment).unwrap();
+    let anchor = sct.root();
+    let state_commitment_proof = sct.witness(swap_commitment).unwrap();
+    let position = state_commitment_proof.position();
+    let nullifier = nk.derive_nullifier(position, &swap_commitment);
+    let epoch_duration = 20;
+    let height = epoch_duration * position.epoch() + position.block();
+
+    let output_data = BatchSwapOutputData {
+        delta_1: Amount::from(100u64),
+        delta_2: Amount::from(100u64),
+        lambda_1: Amount::from(50u64),
+        lambda_2: Amount::from(25u64),
+        unfilled_1: Amount::from(23u64),
+        unfilled_2: Amount::from(50u64),
+        height: height.into(),
+        trading_pair: swap_plaintext.trading_pair,
+        epoch_height: position.epoch().into(),
+    };
+    let (lambda_1, lambda_2) = output_data
+        .pro_rata_outputs((delta_1_i.try_into().unwrap(), delta_2_i.try_into().unwrap()));
+
+    let (output_rseed_1, output_rseed_2) = swap_plaintext.output_rseeds();
+    let note_blinding_1 = output_rseed_1.derive_note_blinding();
+    let note_blinding_2 = output_rseed_2.derive_note_blinding();
+    let (output_1_note, output_2_note) = swap_plaintext.output_notes(&output_data);
+    let note_commitment_1 = output_1_note.commit();
+    let note_commitment_2 = output_2_note.commit();
+
+    let proof = SwapClaimProof::prove(
+        &mut rng,
+        &pk,
+        swap_plaintext,
+        state_commitment_proof,
+        nk,
+        anchor,
+        nullifier,
+        lambda_1,
+        lambda_2,
+        note_blinding_1,
+        note_blinding_2,
+        note_commitment_1,
+        note_commitment_2,
+        output_data,
+    )
+    .expect("can create proof");
+
+    let proof_result = proof.verify(
+        &vk,
+        anchor,
+        nullifier,
+        fee,
+        output_data,
+        note_commitment_1,
+        note_commitment_2,
+    );
 
     assert!(proof_result.is_ok());
 }
