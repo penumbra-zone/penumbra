14ebc080db0c9ab3a1e78eacb0e3f6ea01b8d4b2
redshiftzero <jen@penumbralabs.xyz>
2023-05-26 17:37:46 -0400

    bench(zk): swap claim proof

---
+++ crates/crypto/proof-params/benches/swap_claim.rs
@@ -0,0 +1,159 @@
+use ark_relations::r1cs::{
+    ConstraintSynthesizer, ConstraintSystem, OptimizationGoal, SynthesisMode,
+};
+use decaf377::Fq;
+use penumbra_crypto::{
+    asset,
+    keys::{NullifierKey, SeedPhrase, SpendKey},
+    Amount, Fee, Nullifier,
+};
+use penumbra_dex::{
+    swap::SwapPlaintext,
+    swap_claim::{SwapClaimCircuit, SwapClaimProof},
+    BatchSwapOutputData, TradingPair,
+};
+use penumbra_proof_params::SWAPCLAIM_PROOF_PROVING_KEY;
+use penumbra_tct as tct;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand_core::OsRng;
+
+#[allow(clippy::too_many_arguments)]
+fn prove(
+    swap_plaintext: SwapPlaintext,
+    state_commitment_proof: tct::Proof,
+    nk: NullifierKey,
+    anchor: tct::Root,
+    nullifier: Nullifier,
+    lambda_1: Amount,
+    lambda_2: Amount,
+    note_blinding_1: Fq,
+    note_blinding_2: Fq,
+    note_commitment_1: tct::Commitment,
+    note_commitment_2: tct::Commitment,
+    output_data: BatchSwapOutputData,
+) {
+    let _proof = SwapClaimProof::prove(
+        &mut OsRng,
+        &SWAPCLAIM_PROOF_PROVING_KEY,
+        swap_plaintext,
+        state_commitment_proof,
+        nk,
+        anchor,
+        nullifier,
+        lambda_1,
+        lambda_2,
+        note_blinding_1,
+        note_blinding_2,
+        note_commitment_1,
+        note_commitment_2,
+        output_data,
+    )
+    .expect("can create proof");
+}
+
+fn swap_claim_proving_time(c: &mut Criterion) {
+    let seed_phrase = SeedPhrase::generate(OsRng);
+    let sk_recipient = SpendKey::from_seed_phrase(seed_phrase, 0);
+    let fvk_recipient = sk_recipient.full_viewing_key();
+    let ivk_recipient = fvk_recipient.incoming();
+    let (claim_address, _dtk_d) = ivk_recipient.payment_address(0u32.into());
+    let nk = *sk_recipient.nullifier_key();
+
+    let gm = asset::REGISTRY.parse_unit("gm");
+    let gn = asset::REGISTRY.parse_unit("gn");
+    let trading_pair = TradingPair::new(gm.id(), gn.id());
+
+    let delta_1_i = Amount::from(2u64);
+    let delta_2_i = Amount::from(0u64);
+    let fee = Fee::default();
+
+    let swap_plaintext = SwapPlaintext::new(
+        &mut OsRng,
+        trading_pair,
+        delta_1_i,
+        delta_2_i,
+        fee,
+        claim_address,
+    );
+    let fee = swap_plaintext.clone().claim_fee;
+    let mut sct = tct::Tree::new();
+    let swap_commitment = swap_plaintext.swap_commitment();
+    sct.insert(tct::Witness::Keep, swap_commitment).unwrap();
+    let anchor = sct.root();
+    let state_commitment_proof = sct.witness(swap_commitment).unwrap();
+    let position = state_commitment_proof.position();
+    let nullifier = nk.derive_nullifier(position, &swap_commitment);
+    let epoch_duration = 20;
+    let height = epoch_duration * position.epoch() + position.block();
+
+    let output_data = BatchSwapOutputData {
+        delta_1: Amount::from(1000u64),
+        delta_2: Amount::from(1000u64),
+        lambda_1: Amount::from(50u64),
+        lambda_2: Amount::from(25u64),
+        unfilled_1: Amount::from(23u64),
+        unfilled_2: Amount::from(50u64),
+        height: height.into(),
+        trading_pair: swap_plaintext.trading_pair,
+        epoch_height: position.epoch().into(),
+    };
+    let (lambda_1, lambda_2) = output_data.pro_rata_outputs((delta_1_i, delta_2_i));
+
+    let (output_rseed_1, output_rseed_2) = swap_plaintext.output_rseeds();
+    let note_blinding_1 = output_rseed_1.derive_note_blinding();
+    let note_blinding_2 = output_rseed_2.derive_note_blinding();
+    let (output_1_note, output_2_note) = swap_plaintext.output_notes(&output_data);
+    let note_commitment_1 = output_1_note.commit();
+    let note_commitment_2 = output_2_note.commit();
+
+    c.bench_function("swap claim proving", |b| {
+        b.iter(|| {
+            prove(
+                swap_plaintext.clone(),
+                state_commitment_proof.clone(),
+                nk,
+                anchor,
+                nullifier,
+                lambda_1,
+                lambda_2,
+                note_blinding_1,
+                note_blinding_2,
+                note_commitment_1,
+                note_commitment_2,
+                output_data,
+            )
+        })
+    });
+
+    // Also print out the number of constraints.
+    let circuit = SwapClaimCircuit::new(
+        swap_plaintext,
+        state_commitment_proof,
+        nk,
+        lambda_1,
+        lambda_2,
+        note_blinding_1,
+        note_blinding_2,
+        anchor,
+        nullifier,
+        fee,
+        output_data,
+        note_commitment_1,
+        note_commitment_2,
+    );
+
+    let cs = ConstraintSystem::new_ref();
+    cs.set_optimization_goal(OptimizationGoal::Constraints);
+    cs.set_mode(SynthesisMode::Setup);
+
+    circuit
+        .generate_constraints(cs.clone())
+        .expect("can generate constraints");
+    cs.finalize();
+    let num_constraints = cs.num_constraints();
+    println!("Number of constraints: {}", num_constraints);
+}
+
+criterion_group!(benches, swap_claim_proving_time);
+criterion_main!(benches);
