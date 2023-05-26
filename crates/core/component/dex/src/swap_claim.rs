7e92ad6de4b818f41dde8c974953e6f61d3f517b
redshiftzero <jen@penumbralabs.xyz>
2023-05-26 17:37:46 -0400

    bench(zk): swap claim proof

--- crates/core/component/dex/src/swap_claim.rs
+++ crates/core/component/dex/src/swap_claim.rs
@@ -6,5 +6,5 @@ pub mod proof;
 
 pub use action::{Body, SwapClaim};
 pub use plan::SwapClaimPlan;
-pub use proof::SwapClaimProof;
+pub use proof::{SwapClaimCircuit, SwapClaimProof};
 pub use view::SwapClaimView;
