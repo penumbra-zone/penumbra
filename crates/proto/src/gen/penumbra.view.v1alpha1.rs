c6917ffffb76630eb9a62b1c80129ac7dd0c2185
aubrey <aubrey@penumbra.zone>
2023-06-01 09:48:39 -0700

    view: add RateData field to TransactionPlannerRequest.Delegate

--- crates/proto/src/gen/penumbra.view.v1alpha1.rs
+++ crates/proto/src/gen/penumbra.view.v1alpha1.rs
@@ -94,6 +94,10 @@ pub mod transaction_planner_request {
         pub rate_data: ::core::option::Option<
             super::super::super::core::stake::v1alpha1::RateData,
         >,
+        #[prost(message, optional, tag = "3")]
+        pub rate_data: ::core::option::Option<
+            super::super::super::core::stake::v1alpha1::RateData,
+        >,
     }
     #[allow(clippy::derive_partial_eq_without_eq)]
     #[derive(Clone, PartialEq, ::prost::Message)]
