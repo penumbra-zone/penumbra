fb5b9d458c440b86a46a8341210cf20016cfbb13
aubrey <aubrey@penumbra.zone>
2023-06-06 14:52:23 -0700

    Add functionality to replace the asset registry with the asset cache in cases of known assets

--- crates/core/crypto/src/asset/cache.rs
+++ crates/core/crypto/src/asset/cache.rs
@@ -1,6 +1,6 @@
-use std::{collections::BTreeMap, ops::Deref};
+use std::{collections::BTreeMap, ops::Deref, sync::Arc};
 
-use super::{DenomMetadata, Id, REGISTRY};
+use super::{denom_metadata, DenomMetadata, Id, REGISTRY};
 use crate::asset::denom_metadata::Unit;
 
 /// On-chain data structures only record a fixed-size [`Id`], so this type
@@ -23,6 +23,147 @@ impl Cache {
 
     fn _get_unit(&self, raw_denom: &str) -> Option<Unit> {
         self.units.get(raw_denom).cloned()
+    }
+
+    fn _try_populate(&mut self, _raw_denom: &str) -> anyhow::Result<Option<DenomMetadata>> {
+        // First try to parse the raw denom string as a specific denom unit of some kind, to see if already present in the cache
+
+        // Ok(if let Some(unit) = self._get_unit(raw_denom) {
+        //     // If the raw denom returns an associated unit, the denom metadata should already be present in the cache as well, so retrieve that.
+        //     self._get_by_id(unit.id())
+        // } else {
+        //     // If the raw denom isn't present in the cache, what should we do here in the typical case? simply return None to indicate population failed?
+        //     // In some cases, there will be countless possible denoms of a specific type/in a specific denom family (i.e. all the possible DelegationTokens),
+        //     // so we can't reasonably populate the cache with all of them or necessarily provide all metadata for them, but we can & should still parse the raw denom
+        //     // to determine whether the newly-witnessed denom is of a type we are familiar with.
+
+        //     // TODO: Before returning None, attempt to parse the raw denom to determine whether it is of a known type.
+        //     // If so, return some kind of "empty" or default denom metadata for that type, which can be used to populate the cache with the raw denom as the base denom
+        //     // and with whatever useful information we may otherwise be able to interpolate.
+
+        //     // Types:
+        //     // - DelegationToken
+        //     // - IbcToken
+        //     // - UnbondingToken
+        //     // - whatever else?
+
+        //     None
+        // })
+
+        unimplemented!("try_populate")
+    }
+
+    pub fn with_known_assets() -> Self {
+        let mut cache = Cache::default();
+
+        let known_assets = vec![
+            DenomMetadata {
+                inner: Arc::new(denom_metadata::Inner::new(
+                    "upenumbra".to_string(),
+                    vec![
+                        denom_metadata::BareDenomUnit {
+                            exponent: 6,
+                            denom: "penumbra".to_string(),
+                        },
+                        denom_metadata::BareDenomUnit {
+                            exponent: 3,
+                            denom: "mpenumbra".to_string(),
+                        },
+                    ],
+                )),
+            },
+            DenomMetadata {
+                inner: Arc::new(denom_metadata::Inner::new(
+                    "ugn".to_string(),
+                    vec![
+                        denom_metadata::BareDenomUnit {
+                            exponent: 6,
+                            denom: "gn".to_string(),
+                        },
+                        denom_metadata::BareDenomUnit {
+                            exponent: 3,
+                            denom: "mgn".to_string(),
+                        },
+                    ],
+                )),
+            },
+            DenomMetadata {
+                inner: Arc::new(denom_metadata::Inner::new(
+                    "ugm".to_string(),
+                    vec![
+                        denom_metadata::BareDenomUnit {
+                            exponent: 6,
+                            denom: "gm".to_string(),
+                        },
+                        denom_metadata::BareDenomUnit {
+                            exponent: 3,
+                            denom: "mgm".to_string(),
+                        },
+                    ],
+                )),
+            },
+            DenomMetadata {
+                inner: Arc::new(denom_metadata::Inner::new(
+                    "wtest_usd".to_string(),
+                    vec![denom_metadata::BareDenomUnit {
+                        exponent: 6,
+                        denom: "test_usd".to_string(),
+                    }],
+                )),
+            },
+            DenomMetadata {
+                inner: Arc::new(denom_metadata::Inner::new(
+                    "test_sat".to_string(),
+                    vec![denom_metadata::BareDenomUnit {
+                        exponent: 8,
+                        denom: "test_btc".to_string(),
+                    }],
+                )),
+            },
+            DenomMetadata {
+                inner: Arc::new(denom_metadata::Inner::new(
+                    "utest_atom".to_string(),
+                    vec![
+                        denom_metadata::BareDenomUnit {
+                            exponent: 6,
+                            denom: "test_atom".to_string(),
+                        },
+                        denom_metadata::BareDenomUnit {
+                            exponent: 3,
+                            denom: "mtest_atom".to_string(),
+                        },
+                    ],
+                )),
+            },
+            DenomMetadata {
+                inner: Arc::new(denom_metadata::Inner::new(
+                    "utest_osmo".to_string(),
+                    vec![
+                        denom_metadata::BareDenomUnit {
+                            exponent: 6,
+                            denom: "test_osmo".to_string(),
+                        },
+                        denom_metadata::BareDenomUnit {
+                            exponent: 3,
+                            denom: "mtest_osmo".to_string(),
+                        },
+                    ],
+                )),
+            },
+            DenomMetadata {
+                inner: Arc::new(denom_metadata::Inner::new(
+                    "uubtc".to_string(),
+                    vec![denom_metadata::BareDenomUnit {
+                        exponent: 6,
+                        denom: "ubtc".to_string(),
+                    }],
+                )),
+            },
+        ];
+
+        cache.extend(known_assets);
+
+        cache
     }
 }
 
