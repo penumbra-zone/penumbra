de75177ff6bdc89ed9cd141e083780811409b39d
redshiftzero <jen@penumbralabs.xyz>
2023-06-09 14:39:36 -0400

    crypto(zk): constrain divisor to be non-zero in `U128x128Var`

--- crates/crypto/proof-params/src/gen/swapclaim_id.rs
+++ crates/crypto/proof-params/src/gen/swapclaim_id.rs
@@ -1,3 +1,3 @@
 
-pub const PROVING_KEY_ID: &'static str = "groth16pk1qe8hdwxw7ya3zep99ncjvs4zrru5k5vmgcy0jtadfwdnqmcnysuq48s9hm";
-pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1rklaz0wj78r6khzx4uz0cg2q9ftgccmrvpvfq9spcqld9qg5gwmsxp6wyc";
+pub const PROVING_KEY_ID: &'static str = "groth16pk1dfcpdxkvwgxgktn8hw2ervlg8kvm0x6wqgg8xgnk42uqeehwqmsqtgmxha";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1tmvvk3vdfw7ynpj7kh8k6anfrrs0lzz8u2sect2c0ju2uf9yvzzsjz28jf";
