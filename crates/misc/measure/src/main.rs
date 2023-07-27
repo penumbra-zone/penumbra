0f94861aab57dec5805184fe4de6d0cf393704c5
Conor Schaefer <conor@penumbralabs.xyz>
2023-07-27 16:45:30 -0700

    fix(measure): use tendermint proxy
    A while back we ported most of the bins to use the
    TendermintProxyClient, but neglected to update the measure tool.
    This resulted in some perplexing results until I realized I was
    talking to my local devnet's pd, but expecting to find as many blocks as
    on the public testnet.

--- crates/misc/measure/src/main.rs
+++ crates/misc/measure/src/main.rs
@@ -9,11 +9,14 @@ use penumbra_chain::params::ChainParameters;
 use penumbra_compact_block::CompactBlock;
 use penumbra_proto::{
     client::v1alpha1::{
-        oblivious_query_service_client::ObliviousQueryServiceClient, ChainParametersRequest,
-        CompactBlockRangeRequest,
+        oblivious_query_service_client::ObliviousQueryServiceClient,
+        tendermint_proxy_service_client::TendermintProxyServiceClient, ChainParametersRequest,
+        CompactBlockRangeRequest, GetStatusRequest,
     },
     Message,
 };
+
+use tonic::transport::{Channel, ClientTlsConfig};
 use url::Url;
 
 #[derive(Debug, Parser)]
@@ -32,14 +35,6 @@ pub struct Opt {
         parse(try_from_str = url::Url::parse)
     )]
     node: Url,
-    // TODO: use TendermintProxyService instead
-    /// The URL for the Tendermint RPC endpoint of the remote node.
-    #[clap(
-        long,
-        default_value = "https://rpc.testnet.penumbra.zone",
-        env = "PENUMBRA_NODE_TM_URL"
-    )]
-    tendermint_url: Url,
     #[clap(subcommand)]
     pub cmd: Command,
     /// The filter for log messages.
@@ -207,50 +202,35 @@ impl Opt {
         Ok(())
     }
 
-    // This code is ripped from the view service code, and could be split out into something common.
     #[instrument(skip(self))]
     pub async fn latest_known_block_height(&self) -> Result<(u64, bool), anyhow::Error> {
-        let client = reqwest::Client::new();
-
-        let rsp: serde_json::Value = client
-            .get(format!("{}/status", self.tendermint_url.clone()))
-            .send()
-            .await?
-            .json()
-            .await?;
-
-        tracing::debug!(?rsp, "retrieved tendermint status");
-
+        let mut client = get_tendermint_proxy_client(self.node.clone()).await?;
+        let rsp = client.get_status(GetStatusRequest {}).await?.into_inner();
         let sync_info = rsp
-            .get("result")
-            .and_then(|r| r.get("sync_info"))
-            .ok_or_else(|| anyhow::anyhow!("could not parse sync_info in JSON response"))?;
+            .sync_info
+            .ok_or_else(|| anyhow::anyhow!("could not parse sync_info in gRPC response"))?;
 
-        let latest_block_height = sync_info
-            .get("latest_block_height")
-            .and_then(|c| c.as_str())
-            .ok_or_else(|| anyhow::anyhow!("could not parse latest_block_height in JSON response"))?
-            .parse()?;
-
-        let node_catching_up = sync_info
-            .get("catching_up")
-            .and_then(|c| c.as_bool())
-            .ok_or_else(|| anyhow::anyhow!("could not parse catching_up in JSON response"))?;
-
-        // There is a `max_peer_block_height` available in TM 0.35, however it should not be used
-        // as it does not seem to reflect the consensus height. Since clients use `latest_known_block_height`
-        // to determine the height to attempt syncing to, a validator reporting a non-consensus height
-        // can cause a DoS to clients attempting to sync if `max_peer_block_height` is used.
-        let latest_known_block_height = latest_block_height;
-
-        tracing::debug!(
-            ?latest_block_height,
-            ?node_catching_up,
-            ?latest_known_block_height
-        );
-
-        Ok((latest_known_block_height, node_catching_up))
+        let latest_block_height = sync_info.latest_block_height;
+        let node_catching_up = sync_info.catching_up;
+        Ok((latest_block_height, node_catching_up))
     }
+}
+
+// This code is ripped from the pcli code, and could be split out into something common.
+async fn get_tendermint_proxy_client(
+    pd_url: Url,
+) -> Result<TendermintProxyServiceClient<Channel>, anyhow::Error> {
+    let pd_channel: Channel = match pd_url.scheme() {
+        "http" => Channel::from_shared(pd_url.to_string())?.connect().await?,
+        "https" => {
+            Channel::from_shared(pd_url.to_string())?
+                .tls_config(ClientTlsConfig::new())?
+                .connect()
+                .await?
+        }
+        other => anyhow::bail!(format!("unknown url scheme {other}")),
+    };
+    Ok(TendermintProxyServiceClient::new(pd_channel))
 }
 
 #[tokio::main]
