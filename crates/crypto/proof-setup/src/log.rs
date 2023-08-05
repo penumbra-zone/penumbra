da06bc87894194b9203dd78bd6fbd967ecf7d04c
Lúcás C. Meier <lucas@cronokirby.com>
2023-08-05 13:39:05 +0200

    Setup: allow resuming during log verification

--- crates/crypto/proof-setup/src/log.rs
+++ crates/crypto/proof-setup/src/log.rs
@@ -23,7 +23,7 @@ pub trait Phase {
     /// with respect to other contributions.
     type CRSElements: Hashable;
     /// The type of a contribution before any kind of validation.
-    type RawContribution;
+    type RawContribution: Hashable;
     /// A contribution after having been validated.
     type Contribution: Hashable;
 
@@ -68,15 +68,35 @@ impl<P: Phase> ContributionLog<P> {
     /// This requires the root elements which these contributions all build upon.
     ///
     /// This might fail to find any good contributions, in which case None will be returned.
-    pub fn last_good_contribution(&self, root: &P::CRSElements) -> Option<P::Contribution> {
+    pub fn last_good_contribution(
+        &self,
+        root: &P::CRSElements,
+        checkpoint: Option<ContributionHash>,
+    ) -> Option<P::Contribution> {
         // We start by assuming that the root is correct, and the first good elements
         // the log should start building on. From there, we keep considering new contributions,
         // each of which can become the most recent good contribution if all conditions are met.
         // By the end, we'll have the last good contribution for the log as a whole.
         let root_hash = root.hash();
+        // By default, we start with 0, and no last known good contribution
+        let mut start = 0;
         let mut last_good: Option<P::Contribution> = None;
+        // If we have a checkpoint, we might want to start at a different place, however:
+        if let Some(checkpoint) = checkpoint {
+            if let Some((i, matching)) = self
+                .contributions
+                .iter()
+                .enumerate()
+                .find(|(_, c)| c.hash() == checkpoint)
+            {
+                if let Some(valid) = P::validate(root, matching) {
+                    start = i + 1;
+                    last_good = Some(valid);
+                }
+            }
+        }
 
-        for contribution in &self.contributions {
+        for contribution in &self.contributions[start..] {
             // 1. Check if the parent we're building off of is the last good contribution.
             let expected_parent_hash = last_good.as_ref().map(|c| c.hash()).unwrap_or(root_hash);
             if P::parent_hash(contribution) != expected_parent_hash {
@@ -190,7 +210,7 @@ mod test {
     fn test_empty_log_search_returns_none() {
         let log: ContributionLog<DummyPhase> = ContributionLog::new(vec![]);
         assert!(log
-            .last_good_contribution(&DummyElements { id: 0 })
+            .last_good_contribution(&DummyElements { id: 0 }, None)
             .is_none());
     }
 
@@ -199,7 +219,7 @@ mod test {
         let log: ContributionLog<DummyPhase> =
             ContributionLog::new(vec![contribution!(0, 1, true, true)]);
         assert_eq!(
-            log.last_good_contribution(&DummyElements { id: 0 }),
+            log.last_good_contribution(&DummyElements { id: 0 }, None),
             Some(log.contributions[0])
         );
     }
@@ -211,7 +231,7 @@ mod test {
             contribution!(1, 2, true, true),
         ]);
         assert_eq!(
-            log.last_good_contribution(&DummyElements { id: 0 }),
+            log.last_good_contribution(&DummyElements { id: 0 }, None),
             Some(log.contributions[1])
         );
     }
@@ -222,7 +242,10 @@ mod test {
             contribution!(0, 1, true, true),
             contribution!(1, 2, true, true),
         ]);
-        assert_eq!(log.last_good_contribution(&DummyElements { id: 2 }), None);
+        assert_eq!(
+            log.last_good_contribution(&DummyElements { id: 2 }, None),
+            None
+        );
     }
 
     #[test]
@@ -243,8 +266,59 @@ mod test {
             contribution!(5, 7, true, true),
         ]);
         assert_eq!(
-            log.last_good_contribution(&DummyElements { id: 0 }),
+            log.last_good_contribution(&DummyElements { id: 0 }, None),
             Some(log.contributions[6])
+        );
+    }
+
+    #[test]
+    fn test_multi_log_with_skips_and_resuming_search() {
+        let log: ContributionLog<DummyPhase> = ContributionLog::new(vec![
+            contribution!(0, 1, true, true),
+            // Invalid
+            contribution!(1, 2, false, true),
+            // Valid
+            contribution!(1, 3, true, true),
+            // Valid, but wrong parent
+            contribution!(1, 4, true, true),
+            // Valid
+            contribution!(3, 5, true, true),
+            // Bad linking
+            contribution!(5, 6, true, false),
+            // Valid
+            contribution!(5, 7, true, true),
+        ]);
+        assert_eq!(
+            log.last_good_contribution(&DummyElements { id: 0 }, Some(hash_from_u8(5))),
+            Some(log.contributions[6]),
+        );
+    }
+
+    #[test]
+    fn test_multi_log_with_skips_and_bad_checkpoint() {
+        let log: ContributionLog<DummyPhase> = ContributionLog::new(vec![
+            contribution!(0, 1, true, true),
+            // Invalid
+            contribution!(1, 2, false, true),
+            // Valid
+            contribution!(1, 3, true, true),
+            // Valid, but wrong parent
+            contribution!(1, 4, true, true),
+            // Valid
+            contribution!(3, 5, true, true),
+            // Bad linking
+            contribution!(5, 6, true, false),
+            // Valid
+            contribution!(5, 7, true, true),
+        ]);
+        assert_eq!(
+            log.last_good_contribution(&DummyElements { id: 0 }, Some(hash_from_u8(100))),
+            Some(log.contributions[6]),
+        );
+        // Should work because we validate internal consistency
+        assert_eq!(
+            log.last_good_contribution(&DummyElements { id: 0 }, Some(hash_from_u8(2))),
+            Some(log.contributions[6]),
         );
     }
 }
