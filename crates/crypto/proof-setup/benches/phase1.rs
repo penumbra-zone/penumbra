6503b640bd266e2eb967c3e9907647e0fcf2ee0c
redshiftzero <jen@penumbralabs.xyz>
2023-08-11 17:50:17 -0400

    bench: phase 1 proving time for circuit of size 37,655 (swap)

---
+++ crates/crypto/proof-setup/benches/phase1.rs
@@ -0,0 +1,30 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand_core::OsRng;
+
+use penumbra_proof_setup::{
+    log::{ContributionHash, Hashable},
+    phase1::{CRSElements, Contribution},
+};
+
+fn run_phase1_prove(parent: ContributionHash, old: &CRSElements) -> Contribution {
+    Contribution::make(&mut OsRng, parent, old)
+}
+
+fn run_phase1_verify() {}
+
+fn phase1_benchmarks(c: &mut Criterion) {
+    // Generate contribution for degree = 37,655
+    // (size of largest proof)
+    let d = 37_655;
+    let root = CRSElements::root(d);
+    let root_hash = root.hash();
+
+    c.bench_function("phase 1 prove", |b| {
+        b.iter(|| run_phase1_prove(root_hash, &root))
+    });
+
+    // c.bench_function("phase 1 verify", |b| b.iter(|| run_phase1_verify()));
+}
+
+criterion_group!(benches, phase1_benchmarks);
+criterion_main!(benches);
