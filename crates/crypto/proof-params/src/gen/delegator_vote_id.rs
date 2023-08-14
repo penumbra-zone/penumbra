6a00c1fd6c71e8c4e45a2e40e0b329d2c9b04e8f
redshiftzero <jen@penumbralabs.xyz>
2023-08-14 10:41:45 -0400

    chore: redo ZKP parameter setup

--- crates/crypto/proof-params/src/gen/delegator_vote_id.rs
+++ crates/crypto/proof-params/src/gen/delegator_vote_id.rs
@@ -1,3 +1,3 @@
 
-pub const PROVING_KEY_ID: &'static str = "groth16pk12j3zfsg5cl3gpg09zxzk4dkzzysxpsyvr08xqqkphn3ncfs6tasqxv9jrq";
-pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1672n5ydxcnmq4t2qmeq97jjf3rlktsrzmuv3j067rz09grsf8pssyazj4h";
+pub const PROVING_KEY_ID: &'static str = "groth16pk1h3y7ezzulg5v3vxnkzegtkuzvgw7z8gpql6hyadf3mq97jn92a3sa9w0l8";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1jffc7gpnqchvrzyc3lz58gax7ct6272f92sen46kn8gwsa2qjevsukf9y5";
--- crates/crypto/proof-params/src/gen/swap_id.rs
+++ crates/crypto/proof-params/src/gen/swap_id.rs
@@ -1,3 +1,3 @@
 
-pub const PROVING_KEY_ID: &'static str = "groth16pk1tvrwyfnv4tz6kv4hjx53qh072pncfdjhxfrghk4tap9w0twwq6aqxq6chy";
-pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1zp7n4sg6pdjpcengqjaze8kkmgzdz6e3gldn5kdgxa5vaaq9zzmqa6ynhc";
+pub const PROVING_KEY_ID: &'static str = "groth16pk1l5fhnhuqfgyw8qf2yt9hrl0m6ywpz4xuydrqh40yg2wjzvz9kdqqjl0ndn";
+pub const VERIFICATION_KEY_ID: &'static str = "groth16vk1ecjxhxd5tfk8wzjc3cxh9lvztldh987z8nahrlrvygqmql4mrsnq96w3my";
