ce2d319bd5534fd28600227b28506e32b8504493
Erwan Or <erwan.ounn.84@gmail.com>
2023-08-23 20:32:41 -0400

    pd: use a range query for client synchronization (#2881)
    * compact-block: implement `compact_block_stream` in extension trait
    
    * compact-block: use combinators to process stream
    
    * pd(oblivious): stream blocks up to the specified `end_height`
    
    * pd(oblivious): add a comment about range query
    
    * compact_block: use `key_component` in range
    
    * compact_block: use range query in `stream_compact_block`
    
    * pd(oblivious): send `compact_block`
    
    * component(compact_block): adjust `StateRead` extension trait
    
    * compact_block(state_key): concatenate prefix and height keys
    
    * pd: properly handle stream consumption

--- crates/core/component/compact-block/src/state_key.rs
+++ crates/core/component/compact-block/src/state_key.rs
@@ -1,3 +1,14 @@
 pub fn compact_block(height: u64) -> String {
-    format!("compactblock/{height:020}")
+    format!(
+        "{}{}",
+        crate::state_key::prefix(),
+        crate::state_key::height(height)
+    )
+}
+pub fn prefix() -> &'static str {
+    "compactblock/"
+}
+
+pub fn height(height: u64) -> String {
+    format!("{height:020}")
 }
