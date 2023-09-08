04986567989196ab58d722a2d1de6b82a067311d
Lucas Meier <lucas@cronokirby.com>
2023-09-07 22:20:18 -0700

    Setup: Fix phase1 benchmark
    Also adjusts the size to the rounded up power of 2, reflecting what
    actually happens.

--- crates/crypto/proof-setup/benches/phase1.rs
+++ crates/crypto/proof-setup/benches/phase1.rs
@@ -3,14 +3,14 @@ use rand_core::OsRng;
 
 use penumbra_proof_setup::{
     log::{ContributionHash, Hashable},
-    phase1::{CRSElements, Contribution, RawContribution},
+    {Phase1CRSElements, Phase1Contribution, Phase1RawContribution},
 };
 
-fn run_phase1_prove(parent: ContributionHash, old: &CRSElements) -> Contribution {
-    Contribution::make(&mut OsRng, parent, old)
+fn run_phase1_prove(parent: ContributionHash, old: &Phase1CRSElements) -> Phase1Contribution {
+    Phase1Contribution::make(&mut OsRng, parent, old)
 }
 
-fn run_phase1_verify(contribution: RawContribution, parent: &CRSElements) {
+fn run_phase1_verify(contribution: Phase1RawContribution, parent: &Phase1CRSElements) {
     let validated_contribution = contribution
         .validate(&mut OsRng)
         .expect("this is a valid contribution");
@@ -20,15 +20,15 @@ fn run_phase1_verify(contribution: RawContribution, parent: &CRSElements) {
 fn phase1_benchmarks(c: &mut Criterion) {
     // Generate contribution for degree = 37,061
     // (size of largest proof)
-    let d = 37_655;
-    let root = CRSElements::root(d);
+    let d = 1 << 16;
+    let root = Phase1CRSElements::root(d);
     let root_hash = root.hash();
 
     c.bench_function("phase 1 prove", |b| {
         b.iter(|| run_phase1_prove(root_hash, &root))
     });
 
-    let new_contribution = Contribution::make(&mut OsRng, root_hash, &root);
+    let new_contribution = Phase1Contribution::make(&mut OsRng, root_hash, &root);
     c.bench_function("phase 1 verify", |b| {
         b.iter(|| run_phase1_verify(new_contribution.clone().into(), &root))
     });
