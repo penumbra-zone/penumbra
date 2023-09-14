303f5da826fe2296c56580a4de87e2622f33e761
Chris Czub <chris@penumbralabs.xyz>
2023-09-14 16:02:24 -0400

    Remove usage of "unwrap" in the narsil crate

--- crates/narsil/narsil/src/bin/narsilcli.rs
+++ crates/narsil/narsil/src/bin/narsilcli.rs
@@ -1,3 +1,4 @@
+#![deny(clippy::unwrap_used)]
 /// narsilcli is used as a command-line interface for the narsil server.
 fn main() {
     println!("narsil is currently being forged, please contact the Dwarven-smith Telchar for more details")
