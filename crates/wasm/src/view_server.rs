8dc50e6af00c31ecf663bd4721fc93ebe6a3fd91
Valentine <walik19988@gmail.com>
2023-10-06 15:33:09 +0300

    #3091 separate block scanning and receiving updates for wasm (#3146)
    * modify scan_block() function
    
    * modify get_updates() function
    
    * fmt
    
    * fix
    
    * fix docs
    
    * flush_update and _inner refactoring
    
    * clears state in flush_updates
    
    * fmt
    
    * fix docs

--- crates/wasm/src/view_server.rs
+++ crates/wasm/src/view_server.rs
@@ -58,11 +58,12 @@ pub struct ViewServer {
     epoch_duration: u64,
     fvk: FullViewingKey,
     notes: BTreeMap<note::StateCommitment, SpendableNoteRecord>,
-    notes_by_nullifier: BTreeMap<Nullifier, SpendableNoteRecord>,
     swaps: BTreeMap<tct::StateCommitment, SwapRecord>,
     denoms: BTreeMap<Id, DenomMetadata>,
     nct: Tree,
     storage: IndexedDBStorage,
+    last_position: Option<StoredPosition>,
+    last_forgotten: Option<Forgotten>,
 }
 
 #[wasm_bindgen]
@@ -91,112 +92,30 @@ impl ViewServer {
             fvk,
             epoch_duration,
             notes: Default::default(),
-            notes_by_nullifier: Default::default(),
             denoms: Default::default(),
             nct: tree,
             swaps: Default::default(),
             storage: IndexedDBStorage::new(constants).await?,
+            last_position: None,
+            last_forgotten: None,
         };
         Ok(view_server)
     }
 
     /// Scans block for notes, swaps
-    /// This method does not return SCT updates (nct_updates).
-    /// Although we can make it do if we want to save SCT updates after each blob is processed
-    /// Arguments:
+    /// Returns true if the block contains new notes, swaps or false if the block is empty for us
     ///     compact_block: `v1alpha1::CompactBlock`
-    /// Returns: `ScanBlockResult`
+    /// Scan results are saved in-memory rather than returned
+    /// Use `flush_updates()` to get the scan results
+    /// Returns: `bool`
     #[wasm_bindgen]
-    pub async fn scan_block(&mut self, compact_block: JsValue) -> Result<JsValue, Error> {
-        let result = self.scan_block_inner(compact_block).await?;
-        serde_wasm_bindgen::to_value(&result)
-    }
-
-    /// get SCT state updates
-    /// This method is necessary because we save SCT updates to indexedDB once every 1000 blocks
-    /// rather than after each block
-    /// Arguments:
-    ///     last_position: `Option<StoredPosition>`
-    ///     last_forgotten: `Option<Forgotten>`
-    /// Returns: `ScanBlockResult`
-    #[wasm_bindgen]
-    pub fn get_updates(
-        &mut self,
-        last_position: JsValue,
-        last_forgotten: JsValue,
-    ) -> Result<JsValue, Error> {
-        let result = self.get_updates_inner(last_position, last_forgotten)?;
-        serde_wasm_bindgen::to_value(&result)
-    }
-
-    /// get SCT root
-    /// SCT root can be compared with the root obtained by GRPC and verify that there is no divergence
-    /// Returns: `Root`
-    #[wasm_bindgen]
-    pub fn get_nct_root(&mut self) -> Result<JsValue, Error> {
-        let root = self.nct.root();
-        serde_wasm_bindgen::to_value(&root)
-    }
-
-    /// get LP NFT asset
-    /// Arguments:
-    ///     position_value: `lp::position::Position`
-    ///     position_state_value: `lp::position::State`
-    /// Returns: `DenomMetadata`
-    #[wasm_bindgen]
-    pub fn get_lpnft_asset(
-        &mut self,
-        position_value: JsValue,
-        position_state_value: JsValue,
-    ) -> Result<JsValue, Error> {
-        let position: Position = serde_wasm_bindgen::from_value(position_value)?;
-        let position_state = serde_wasm_bindgen::from_value(position_state_value)?;
-        let lp_nft = LpNft::new(position.id(), position_state);
-        let denom = lp_nft.denom();
-        serde_wasm_bindgen::to_value(&denom)
-    }
-}
-
-impl ViewServer {
-    pub fn get_updates_inner(
-        &mut self,
-        last_position: JsValue,
-        last_forgotten: JsValue,
-    ) -> WasmResult<ScanBlockResult> {
-        let stored_position: Option<StoredPosition> =
-            serde_wasm_bindgen::from_value(last_position)?;
-        let stored_forgotten: Option<Forgotten> = serde_wasm_bindgen::from_value(last_forgotten)?;
-
-        let nct_updates: Updates = self
-            .nct
-            .updates(
-                stored_position.unwrap_or_default(),
-                stored_forgotten.unwrap_or_default(),
-            )
-            .collect::<Updates>();
-
-        let result = ScanBlockResult {
-            height: self.latest_height,
-            nct_updates,
-            new_notes: self.notes.clone().into_values().collect(),
-            new_swaps: self.swaps.clone().into_values().collect(),
-        };
-        Ok(result)
-    }
-
-    pub async fn scan_block_inner(
-        &mut self,
-        compact_block: JsValue,
-    ) -> WasmResult<ScanBlockResult> {
+    pub async fn scan_block(&mut self, compact_block: JsValue) -> WasmResult<bool> {
         let block_proto: penumbra_proto::core::component::compact_block::v1alpha1::CompactBlock =
             serde_wasm_bindgen::from_value(compact_block)?;
 
         let block: CompactBlock = block_proto.try_into()?;
 
-        // Newly detected spendable notes.
-        let mut new_notes = Vec::new();
-        // Newly detected claimable swaps.
-        let mut new_swaps: Vec<SwapRecord> = Vec::new();
+        let mut found_new_data: bool = false;
 
         for state_payload in block.state_payloads {
             let clone_payload = state_payload.clone();
@@ -228,11 +147,9 @@ impl ViewServer {
                                 position: note_position,
                                 source,
                             };
-                            new_notes.push(note_record.clone());
                             self.notes
                                 .insert(payload.note_commitment, note_record.clone());
-                            self.notes_by_nullifier
-                                .insert(nullifier, note_record.clone());
+                            found_new_data = true;
                         }
                         None => {
                             self.nct.insert(Forget, payload.note_commitment)?;
@@ -264,7 +181,6 @@ impl ViewServer {
                                 output_data: *batch_data,
                                 height_claimed: None,
                             };
-                            new_swaps.push(swap_record.clone());
                             self.swaps.insert(payload.commitment, swap_record);
 
                             let batch_data =
@@ -276,6 +192,7 @@ impl ViewServer {
 
                             self.storage.store_advice(output_1).await?;
                             self.storage.store_advice(output_2).await?;
+                            found_new_data = true;
                         }
                         None => {
                             self.nct.insert(Forget, payload.commitment)?;
@@ -283,49 +200,41 @@ impl ViewServer {
                     }
                 }
                 StatePayload::RolledUp(commitment) => {
-                    if let std::collections::btree_map::Entry::Occupied(mut e) =
-                        self.notes.entry(commitment)
-                    {
-                        // This is a note we anticipated, so retain its auth path.
+                    // This is a note we anticipated, so retain its auth path.
 
-                        let advice_result = self.storage.read_advice(commitment).await?;
+                    let advice_result = self.storage.read_advice(commitment).await?;
 
-                        match advice_result {
-                            None => {}
-                            Some(note) => {
-                                let position = self.nct.insert(Keep, commitment)?;
-
-                                let address_index_1 = self
-                                    .fvk
-                                    .incoming()
-                                    .index_for_diversifier(note.diversifier());
-
-                                let nullifier = Nullifier::derive(
-                                    self.fvk.nullifier_key(),
-                                    position,
-                                    &commitment,
-                                );
-
-                                let source = clone_payload.source().cloned().unwrap_or_default();
-
-                                let spendable_note = SpendableNoteRecord {
-                                    note_commitment: note.commit(),
-                                    height_spent: Some(u64::MAX),
-                                    height_created: block.height,
-                                    note: note.clone(),
-                                    address_index: address_index_1,
-                                    nullifier,
-                                    position,
-                                    source,
-                                };
-
-                                e.insert(spendable_note.clone());
-                                new_notes.push(spendable_note.clone());
-                            }
+                    match advice_result {
+                        None => {
+                            self.nct.insert(Forget, commitment)?;
                         }
-                    } else {
-                        // This is someone else's note.
-                        self.nct.insert(Forget, commitment)?;
+                        Some(note) => {
+                            let position = self.nct.insert(Keep, commitment)?;
+
+                            let address_index_1 = self
+                                .fvk
+                                .incoming()
+                                .index_for_diversifier(note.diversifier());
+
+                            let nullifier =
+                                Nullifier::derive(self.fvk.nullifier_key(), position, &commitment);
+
+                            let source = clone_payload.source().cloned().unwrap_or_default();
+
+                            let spendable_note = SpendableNoteRecord {
+                                note_commitment: note.commit(),
+                                height_spent: None,
+                                height_created: block.height,
+                                note: note.clone(),
+                                address_index: address_index_1,
+                                nullifier,
+                                position,
+                                source,
+                            };
+                            self.notes
+                                .insert(spendable_note.note_commitment, spendable_note.clone());
+                            found_new_data = true;
+                        }
                     }
                 }
             }
@@ -338,13 +247,64 @@ impl ViewServer {
 
         self.latest_height = block.height;
 
-        let result = ScanBlockResult {
+        Ok(found_new_data)
+    }
+
+    /// Get new notes, swaps, SCT state updates
+    /// Function also clears state
+    /// Returns: `ScanBlockResult`
+    #[wasm_bindgen]
+    pub fn flush_updates(&mut self) -> WasmResult<JsValue> {
+        let nct_updates: Updates = self
+            .nct
+            .updates(
+                self.last_position.unwrap_or_default(),
+                self.last_forgotten.unwrap_or_default(),
+            )
+            .collect::<Updates>();
+
+        let updates = ScanBlockResult {
             height: self.latest_height,
-            nct_updates: Default::default(),
-            new_notes,
-            new_swaps,
+            nct_updates: nct_updates.clone(),
+            new_notes: self.notes.clone().into_values().collect(),
+            new_swaps: self.swaps.clone().into_values().collect(),
         };
+
+        self.notes = Default::default();
+        self.swaps = Default::default();
+
+        self.last_position = nct_updates.set_position;
+        self.last_forgotten = nct_updates.set_forgotten;
+
+        let result = serde_wasm_bindgen::to_value(&updates)?;
         Ok(result)
+    }
+
+    /// get SCT root
+    /// SCT root can be compared with the root obtained by GRPC and verify that there is no divergence
+    /// Returns: `Root`
+    #[wasm_bindgen]
+    pub fn get_nct_root(&mut self) -> Result<JsValue, Error> {
+        let root = self.nct.root();
+        serde_wasm_bindgen::to_value(&root)
+    }
+
+    /// get LP NFT asset
+    /// Arguments:
+    ///     position_value: `lp::position::Position`
+    ///     position_state_value: `lp::position::State`
+    /// Returns: `DenomMetadata`
+    #[wasm_bindgen]
+    pub fn get_lpnft_asset(
+        &mut self,
+        position_value: JsValue,
+        position_state_value: JsValue,
+    ) -> Result<JsValue, Error> {
+        let position: Position = serde_wasm_bindgen::from_value(position_value)?;
+        let position_state = serde_wasm_bindgen::from_value(position_state_value)?;
+        let lp_nft = LpNft::new(position.id(), position_state);
+        let denom = lp_nft.denom();
+        serde_wasm_bindgen::to_value(&denom)
     }
 }
 
