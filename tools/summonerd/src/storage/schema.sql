d60b93be95206317c3176a8d6279f69cf774568e
redshiftzero <jen@penumbralabs.xyz>
2023-10-11 12:56:09 -0400

    summonerd: add phase 1 contributions table

--- tools/summonerd/src/storage/schema.sql
+++ tools/summonerd/src/storage/schema.sql
@@ -1,3 +1,14 @@
+-- used for storing phase 1 contribution information
+CREATE TABLE phase1_contributions (
+    slot    INTEGER PRIMARY KEY,
+    -- 1 if this is a root
+    is_root INTEGER NOT NULL,
+    -- If this is the root, will be just the elements, and not a full contribution
+    contribution_or_crs BLOB NOT NULL,
+    -- NULL in the specific case that this is the root 
+    address BLOB
+);
+
 -- used for storing phase 2 contribution information
 CREATE TABLE phase2_contributions (
     slot    INTEGER PRIMARY KEY,
