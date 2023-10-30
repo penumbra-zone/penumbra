d0a3ba0c4f40275c0d752e9fa623c9bebe7c6a9d
redshiftzero <jen@penumbralabs.xyz>
2023-10-30 10:59:09 -0400

    replace address prefix with mainnet address prefix

--- crates/core/component/chain/src/lib.rs
+++ crates/core/component/chain/src/lib.rs
@@ -35,9 +35,9 @@ pub mod test_keys {
     pub const SEED_PHRASE: &str = "comfort ten front cycle churn burger oak absent rice ice urge result art couple benefit cabbage frequent obscure hurry trick segment cool job debate";
 
     /// These addresses both correspond to the test wallet above.
-    pub const ADDRESS_0_STR: &str = "penumbrav2t147mfall0zr6am5r45qkwht7xqqrdsp50czde7empv7yq2nk3z8yyfh9k9520ddgswkmzar22vhz9dwtuem7uxw0qytfpv7lk3q9dp8ccaw2fn5c838rfackazmgf3ahhvhypxd";
+    pub const ADDRESS_0_STR: &str = "penumbra147mfall0zr6am5r45qkwht7xqqrdsp50czde7empv7yq2nk3z8yyfh9k9520ddgswkmzar22vhz9dwtuem7uxw0qytfpv7lk3q9dp8ccaw2fn5c838rfackazmgf3ahh09cxmz";
     /// These addresses both correspond to the test wallet above.
-    pub const ADDRESS_1_STR: &str = "penumbrav2t1vmmz304hjlkjq6xv4al5dqumvgk3ek82rneagj07vdqkudjvl6y7zxzr5k6qq24yc7yyyekpu9qm7ef3acg2u8p950hs6hu3e73guq5pfmmvm63qudfx4qmg8h7fdweydr93ku";
+    pub const ADDRESS_1_STR: &str = "penumbra1vmmz304hjlkjq6xv4al5dqumvgk3ek82rneagj07vdqkudjvl6y7zxzr5k6qq24yc7yyyekpu9qm7ef3acg2u8p950hs6hu3e73guq5pfmmvm63qudfx4qmg8h7fdweyw3ektn";
 
     pub static ADDRESS_0: Lazy<Address> = Lazy::new(|| {
         ADDRESS_0_STR
