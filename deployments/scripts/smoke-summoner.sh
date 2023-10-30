f88cf5f9fd056cd282eb36d7d23473662ddaddb3
redshiftzero <jen@penumbra.zone>
2023-10-30 14:33:24 -0400

    test: update syntax for pcli init command in summonerd smoke test (#3242)
    * test: update syntax for pcli init command in summonerd smoke test
    
    * test: get fvk from config file
    
    * test: print addresses for debugging purposes

--- deployments/scripts/smoke-summoner.sh
+++ deployments/scripts/smoke-summoner.sh
@@ -20,7 +20,7 @@ if ! hash cometbft > /dev/null 2>&1 ; then
     exit 1
 fi
 
-export RUST_LOG="summonerd=info,pcli=info,pd=info,penumbra=info"
+export RUST_LOG="summonerd=info,pd=info,penumbra=info"
 
 # Duration that the network will run before we start the ceremony.
 TESTNET_BOOTTIME="${TESTNET_BOOTTIME:-20}"
@@ -57,9 +57,10 @@ cargo run --quiet --release --bin summonerd -- generate-phase1 --output phase1.b
 
 echo "Setting up storage directory..."
 mkdir /tmp/summonerd
-cargo run --quiet --release --bin pcli -- --home /tmp/summonerd --node http://127.0.0.1:8080 keys generate
-export SUMMONER_ADDRESS=$(PCLI_UNLEASH_DANGER="yes" cargo run --quiet --release --bin pcli -- --home /tmp/summonerd --node http://127.0.0.1:8080 view address 0 2>&1)
-export SUMMONER_FVK=$(PCLI_UNLEASH_DANGER="yes" cargo run --quiet --release --bin pcli -- --home /tmp/summonerd --node http://127.0.0.1:8080 keys export full-viewing-key 2>&1)
+cargo run --quiet --release --bin pcli -- --home /tmp/summonerd init --grpc-url http://127.0.0.1:8080 soft-kms generate
+export SUMMONER_ADDRESS=$(PCLI_UNLEASH_DANGER="yes" cargo run --quiet --release --bin pcli -- --home /tmp/summonerd view address 0 2>&1)
+echo $SUMMONER_ADDRESS
+export SUMMONER_FVK=$(grep "full_viewing_key" /tmp/summonerd/config.toml | cut -d= -f2 | tr -d ' "')
 cargo run --quiet --release --bin summonerd -- init --storage-dir /tmp/summonerd --phase1-root phase1.bin
 
 echo "Starting phase 1 run..."
@@ -73,11 +74,12 @@ echo "Setting up test accounts..."
 # the home directory, and so if there is already a backup wallet, we refuse to overwrite it,
 # and will exit non-zero. We don't care about the backup wallet for this test, so we ignore the
 # exit code.
-echo $SEED_PHRASE | cargo run --quiet --release --bin pcli -- --home /tmp/account1 keys import phrase || true
-export ACCOUNT1_ADDRESS=$(PCLI_UNLEASH_DANGER="yes" cargo run --quiet --release --bin pcli -- --home /tmp/account1 --node http://127.0.0.1:8080 view address 0 2>&1)
+echo $SEED_PHRASE | cargo run --quiet --release --bin pcli -- --home /tmp/account1 init --grpc-url http://127.0.0.1:8080 soft-kms import-phrase || true
+export ACCOUNT1_ADDRESS=$(PCLI_UNLEASH_DANGER="yes" cargo run --quiet --release --bin pcli -- --home /tmp/account1 view address 0 2>&1)
+echo $ACCOUNT1_ADDRESS
 
 echo "Phase 1 contributions..."
-cargo run --quiet --release --bin pcli -- --node http://127.0.0.1:8080 --home /tmp/account1 ceremony contribute --coordinator-url http://127.0.0.1:8081 --coordinator-address $SUMMONER_ADDRESS --phase 1 --bid 10penumbra
+cargo run --quiet --release --bin pcli -- --home /tmp/account1 ceremony contribute --coordinator-url http://127.0.0.1:8081 --coordinator-address $SUMMONER_ADDRESS --phase 1 --bid 10penumbra
 
 echo "Stopping phase 1 run..."
 if ! kill -0 "$phase1_pid" ; then
@@ -98,7 +100,7 @@ phase2_pid="$!"
 trap 'kill -9 "$phase2_pid"' EXIT
 
 echo "Phase 2 contributions..."
-cargo run --quiet --release --bin pcli -- --node http://127.0.0.1:8080 --home /tmp/account1 ceremony contribute --coordinator-url http://127.0.0.1:8081 --coordinator-address $SUMMONER_ADDRESS --phase 2 --bid 10penumbra
+cargo run --quiet --release --bin pcli -- --home /tmp/account1 ceremony contribute --coordinator-url http://127.0.0.1:8081 --coordinator-address $SUMMONER_ADDRESS --phase 2 --bid 10penumbra
 
 echo "Exporting keys..."
 cargo run --quiet --release --bin summonerd -- export --storage-dir /tmp/summonerd --target-dir ./crates/crypto/proof-params/src/gen
