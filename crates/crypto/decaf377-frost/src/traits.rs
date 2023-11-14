840c401f7d8840ad334d568ae0be4e35dc4b4108
Lucas Meier <lucas@cronokirby.com>
2023-11-13 23:57:31 -0800

    Add decaf377-frost crate
    It's easier to vendor this here, because we can then use our existing
    protobuf tooling to generate serialization methods.

---
+++ crates/crypto/decaf377-frost/src/traits.rs
@@ -0,0 +1,123 @@
+use ark_ff::{Field as _, One, UniformRand, Zero};
+use decaf377::{Element, FieldExt, Fr};
+use decaf377_rdsa::SpendAuth;
+pub use frost_core::{frost, Ciphersuite, Field, FieldError, Group, GroupError};
+use rand_core;
+
+use crate::hash::Hasher;
+
+#[derive(Copy, Clone)]
+pub struct Decaf377ScalarField;
+
+impl Field for Decaf377ScalarField {
+    type Scalar = Fr;
+
+    type Serialization = [u8; 32];
+
+    fn zero() -> Self::Scalar {
+        Fr::zero()
+    }
+
+    fn one() -> Self::Scalar {
+        Fr::one()
+    }
+
+    fn invert(scalar: &Self::Scalar) -> Result<Self::Scalar, FieldError> {
+        scalar.inverse().ok_or(FieldError::InvalidZeroScalar)
+    }
+
+    fn random<R: rand_core::RngCore + rand_core::CryptoRng>(rng: &mut R) -> Self::Scalar {
+        Fr::rand(rng)
+    }
+
+    fn serialize(scalar: &Self::Scalar) -> Self::Serialization {
+        scalar.to_bytes()
+    }
+
+    fn little_endian_serialize(scalar: &Self::Scalar) -> Self::Serialization {
+        scalar.to_bytes()
+    }
+
+    fn deserialize(buf: &Self::Serialization) -> Result<Self::Scalar, FieldError> {
+        Fr::from_bytes(*buf).map_err(|_| FieldError::MalformedScalar)
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct Decaf377Group;
+
+impl Group for Decaf377Group {
+    type Field = Decaf377ScalarField;
+
+    type Element = Element;
+
+    type Serialization = [u8; 32];
+
+    fn cofactor() -> <Self::Field as Field>::Scalar {
+        Fr::one()
+    }
+
+    fn identity() -> Self::Element {
+        Element::default()
+    }
+
+    fn generator() -> Self::Element {
+        decaf377::basepoint()
+    }
+
+    fn serialize(element: &Self::Element) -> Self::Serialization {
+        element.vartime_compress().0
+    }
+
+    fn deserialize(buf: &Self::Serialization) -> Result<Self::Element, GroupError> {
+        decaf377::Encoding(*buf)
+            .vartime_decompress()
+            .map_err(|_| GroupError::MalformedElement)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub struct Decaf377Rdsa;
+
+#[allow(non_snake_case)]
+impl Ciphersuite for Decaf377Rdsa {
+    const ID: &'static str = "decaf377-rdsa";
+
+    type Group = Decaf377Group;
+
+    type HashOutput = [u8; 32];
+
+    type SignatureSerialization = crate::Signature<SpendAuth>;
+
+    fn H1(m: &[u8]) -> <<Self::Group as Group>::Field as Field>::Scalar {
+        Hasher::default().update(b"rho").update(m).finalize_scalar()
+    }
+
+    fn H2(m: &[u8]) -> <<Self::Group as Group>::Field as Field>::Scalar {
+        Hasher::default().update(m).finalize_scalar()
+    }
+
+    fn H3(m: &[u8]) -> <<Self::Group as Group>::Field as Field>::Scalar {
+        Hasher::default()
+            .update(b"nonce")
+            .update(m)
+            .finalize_scalar()
+    }
+
+    fn H4(m: &[u8]) -> Self::HashOutput {
+        Hasher::default().update(b"msg").update(m).finalize()
+    }
+
+    fn H5(m: &[u8]) -> Self::HashOutput {
+        Hasher::default().update(b"com").update(m).finalize()
+    }
+
+    fn HDKG(m: &[u8]) -> Option<<<Self::Group as Group>::Field as Field>::Scalar> {
+        Some(Hasher::default().update(b"dkg").update(m).finalize_scalar())
+    }
+
+    fn HID(m: &[u8]) -> Option<<<Self::Group as Group>::Field as Field>::Scalar> {
+        Some(Hasher::default().update(b"id").update(m).finalize_scalar())
+    }
+
+}
