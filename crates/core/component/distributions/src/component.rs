9925f3454039201434d5a777fa0a27186785cf79
Erwan <erwanor@penumbralabs.xyz>
2023-11-20 10:34:02 -0500

    distributions: compute and distribute inflation per epoch

--- crates/core/component/distributions/src/component.rs
+++ crates/core/component/distributions/src/component.rs
@@ -4,7 +4,7 @@ mod view;
 
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use async_trait::async_trait;
 use penumbra_component::Component;
 // use penumbra_dex::{component::StateReadExt as _, component::StateWriteExt as _};
@@ -40,7 +40,7 @@ impl Component for Distributions {
                 // TODO(erwan): it's not yet totally clear if it is necessary, or even desirable, for the
                 // distributions component to track the total issuance. The shielded pool component
                 // already does that. We do it anyway for now so that we can write the rest of the scaffolding.
-                state.set_total_issued(genesis_issuance);
+                state.set_staking_token_issuance_for_epoch(genesis_issuance);
             }
         };
     }
@@ -59,33 +59,48 @@ impl Component for Distributions {
     ) {
     }
 
-    #[instrument(name = "distributions", skip(_state))]
-    async fn end_epoch<S: StateWrite + 'static>(_state: &mut Arc<S>) -> Result<()> {
-        Ok(())
+    #[instrument(name = "distributions", skip(state))]
+    async fn end_epoch<S: StateWrite + 'static>(state: &mut Arc<S>) -> Result<()> {
+        let state = Arc::get_mut(state).context("state should be unique")?;
+        let new_issuance = state.compute_new_issuance().await?;
+        tracing::debug!(?new_issuance, "computed new issuance for epoch");
+        Ok(state.distribute(new_issuance).await)
     }
 }
 
 #[async_trait]
 trait DistributionManager: StateWriteExt {
     /// Compute the total new issuance of staking tokens for this epoch.
-    /// TODO(erwan): this is a stub implementation.
     async fn compute_new_issuance(&self) -> Result<Amount> {
-        let base_reward_rate: u64 = 0;
-        let total_issued = self
-            .total_issued()
+        use penumbra_chain::component::StateReadExt as _;
+        let current_block_height = self.get_block_height().await?;
+        let current_epoch = self.get_epoch_for_height(current_block_height).await?;
+        let num_blocks = current_block_height
+            .checked_sub(current_epoch.start_height)
+            .expect("epoch start height is less than or equal to current block height");
+
+        let staking_issuance_per_block = self
+            .get_distributions_params()
             .await?
-            .expect("total issuance has been initialized");
-        const BPS_SQUARED: u64 = 1_0000_0000; // reward rate is measured in basis points squared
-        let new_issuance = total_issued * base_reward_rate / BPS_SQUARED;
-        Ok(new_issuance.into())
+            .staking_issuance_per_block;
+
+        tracing::debug!(
+            number_of_blocks_in_epoch = num_blocks,
+            staking_issuance_per_block,
+            "calculating issuance per epoch"
+        );
+
+        let new_issuance_for_epoch = staking_issuance_per_block
+            .checked_mul(num_blocks)
+            .expect("infaillible unless issuance is pathological");
+
+        Ok(Amount::from(new_issuance_for_epoch))
     }
 
     /// Update the object store with the new issuance of staking tokens for this epoch.
-    /// TODO(erwan): this is a stub implementation.
-    async fn distribute(&mut self) -> Result<()> {
-        let new_issuance = self.compute_new_issuance().await?;
-        tracing::debug!(?new_issuance, "computed new issuance for epoch");
-        self.set_total_issued(new_issuance);
-        todo!()
+    async fn distribute(&mut self, new_issuance: Amount) {
+        self.set_staking_token_issuance_for_epoch(new_issuance)
     }
 }
+
+impl<T: StateWrite + ?Sized> DistributionManager for T {}
