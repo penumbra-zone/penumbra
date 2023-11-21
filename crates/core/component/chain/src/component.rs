5bbd69955a3a63c763f964b80fc5ad59e7a17443
Ava Howell <ava@avahowell.me>
2023-11-21 12:24:09 -0500

    fix docs, remove unnecessary clone, use is_empty

--- crates/core/component/chain/src/component.rs
+++ crates/core/component/chain/src/component.rs
@@ -5,14 +5,14 @@ pub mod rpc;
 use ibc_types::core::commitment::MerklePrefix;
 use once_cell::sync::Lazy;
 
-// the commitment prefix for the IBC state in the penumbra chain.
+/// the commitment prefix for the IBC state in the penumbra chain.
 pub static PENUMBRA_IBC_COMMITMENT_PREFIX: Lazy<MerklePrefix> = Lazy::new(|| MerklePrefix {
     key_prefix: "ibc/".as_bytes().to_vec(),
 });
 
-// the ICS23 proof spec for penumbra's IBC state; this can be used to verify proofs
-// for other substores in the penumbra state, provided that the data is indeed inside a substore
-// (as opposed to directly in the root store.)
+/// the ICS23 proof spec for penumbra's IBC state; this can be used to verify proofs
+/// for other substores in the penumbra state, provided that the data is indeed inside a substore
+/// (as opposed to directly in the root store.)
 pub static PENUMBRA_IBC_PROOF_SPECS: Lazy<Vec<ics23::ProofSpec>> = Lazy::new(|| {
     vec![
         penumbra_storage::ics23_spec(),
