2fef22192e625d1c961edb786e256337f491d3c0
Henry de Valence <hdevalence@penumbralabs.xyz>
2023-11-21 16:50:07 -0800

    ibc: vendor ICS23 proof spec
    As explained in the comment:
    The proof specs need to be available for use cases like relayers, so it should be
    outside of the feature-gated component implementation, and we only depend on
    penumbra-storage when actually implementing the component (since that pulls in all of rocksdb, etc).
    Vendoring isn't ideal, but this data is effectively "vendored" anyways since it needs to
    be replicated across relayers, chain configs, ....

--- crates/core/component/ibc/src/prefix.rs
+++ crates/core/component/ibc/src/prefix.rs
@@ -9,15 +9,47 @@ pub static IBC_COMMITMENT_PREFIX: Lazy<MerklePrefix> = Lazy::new(|| MerklePrefix
     key_prefix: IBC_SUBSTORE_PREFIX.as_bytes().to_vec(),
 });
 
+// Vendored from the jmt crate, rather than importing it from e.g., penumbra-storage
+// The proof specs need to be available for use cases like relayers, so it should be
+// outside of the feature-gated component implementation, and we only depend on
+// penumbra-storage when actually implementing the component (since that pulls in all of rocksdb, etc).
+// Vendoring isn't ideal, but this data is effectively "vendored" anyways since it needs to
+// be replicated across relayers, chain configs, ....
+mod vendored {
+    const LEAF_DOMAIN_SEPARATOR: &[u8] = b"JMT::LeafNode";
+    const INTERNAL_DOMAIN_SEPARATOR: &[u8] = b"JMT::IntrnalNode";
+
+    const SPARSE_MERKLE_PLACEHOLDER_HASH: [u8; 32] = *b"SPARSE_MERKLE_PLACEHOLDER_HASH__";
+
+    pub fn ics23_spec() -> ics23::ProofSpec {
+        ics23::ProofSpec {
+            leaf_spec: Some(ics23::LeafOp {
+                hash: ics23::HashOp::Sha256.into(),
+                prehash_key: ics23::HashOp::Sha256.into(),
+                prehash_value: ics23::HashOp::Sha256.into(),
+                length: ics23::LengthOp::NoPrefix.into(),
+                prefix: LEAF_DOMAIN_SEPARATOR.to_vec(),
+            }),
+            inner_spec: Some(ics23::InnerSpec {
+                hash: ics23::HashOp::Sha256.into(),
+                child_order: vec![0, 1],
+                min_prefix_length: INTERNAL_DOMAIN_SEPARATOR.len() as i32,
+                max_prefix_length: INTERNAL_DOMAIN_SEPARATOR.len() as i32,
+                child_size: 32,
+                empty_child: SPARSE_MERKLE_PLACEHOLDER_HASH.to_vec(),
+            }),
+            min_depth: 0,
+            max_depth: 64,
+            prehash_key_before_comparison: true,
+        }
+    }
+}
+
 /// the ICS23 proof spec for penumbra's IBC state; this can be used to verify proofs
 /// for other substores in the penumbra state, provided that the data is indeed inside a substore
 /// (as opposed to directly in the root store.)
-pub static IBC_PROOF_SPECS: Lazy<Vec<ics23::ProofSpec>> = Lazy::new(|| {
-    vec![
-        penumbra_storage::ics23_spec(),
-        penumbra_storage::ics23_spec(),
-    ]
-});
+pub static IBC_PROOF_SPECS: Lazy<Vec<ics23::ProofSpec>> =
+    Lazy::new(|| vec![vendored::ics23_spec(), vendored::ics23_spec()]);
 
 /// TODO: upstream into ibc-types
 pub trait MerklePrefixExt {
