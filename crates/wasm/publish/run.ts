39a1fe1c53ae1d1b5d76d670c2822bf588e32cd5
Tal Derei <talderei99@gmail.com>
2023-11-28 12:24:43 -0800

    reverting NPM package changes

--- crates/wasm/publish/run.ts
+++ crates/wasm/publish/run.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { execSync } from 'child_process';
-import { copyFileSync, existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 
 const TARGETS = ['web', 'nodejs', 'bundler'];
 
@@ -13,42 +13,9 @@ TARGETS.forEach(target => {
     },
   );
 
-  if (target === 'bundler') {
-    // Copy binary files to the package directory
-    const binaryDir = path.join(process.cwd(), '../../crypto/proof-params/src/gen/');
-    const targetPackageDir = path.join(process.cwd(), `${target}`);
-
-    // Ensure the target binary directory exists
-    if (existsSync(binaryDir)) {
-      const targetBinaryDir = path.join(targetPackageDir, 'bin');
-      if (!existsSync(targetBinaryDir)) {
-        mkdirSync(targetBinaryDir);
-      }
-
-      // Copy binary files to the package directory
-      const binaryFiles = [
-        'delegator_vote_pk.bin', 
-        'nullifier_derivation_pk.bin',
-        'output_pk.bin',
-        'spend_pk.bin',
-        'swap_pk.bin',
-        'swapclaim_pk.bin',
-        'undelegateclaim_pk.bin'
-      ]; 
-      binaryFiles.forEach(file => {
-        const sourcePath = path.join(binaryDir, file);
-        const targetPath = path.join(targetBinaryDir, file);
-        copyFileSync(sourcePath, targetPath);
-      });
-    }
-  }
-
   // Rename package to target-specific names
   const packageJsonPath = path.join(process.cwd(), `${target}/package.json`);
   const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
-  if (!packageJson.files.includes('bin')) {
-    packageJson.files.push('bin');
-  }
   packageJson.name = `@penumbra-zone/wasm-${target}`;
   writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8');
 
