8bbe2e7cba7c7dc5cac896e7e7434573f2df5add
Lucas Meier <lucas@cronokirby.com>
2023-12-01 11:42:15 -0800

    pcli: rename threshold follow to threshold sign

--- crates/bin/pcli/src/command/threshold.rs
+++ crates/bin/pcli/src/command/threshold.rs
@@ -4,14 +4,14 @@ use crate::{terminal::ActualTerminal, App};
 
 #[derive(Debug, clap::Subcommand)]
 pub enum ThresholdCmd {
-    /// Follow along with the threshold signing process
-    Follow,
+    /// Contribute to signing a transaction with threshold custody
+    Sign,
 }
 
 impl ThresholdCmd {
     pub fn offline(&self) -> bool {
         match self {
-            ThresholdCmd::Follow => true,
+            ThresholdCmd::Sign => true,
         }
     }
 
@@ -21,6 +21,10 @@ impl ThresholdCmd {
             crate::config::CustodyConfig::Threshold(config) => config,
             _ => anyhow::bail!("this command can only be used with the threshold custody backend"),
         };
-        penumbra_custody::threshold::follow(config, &ActualTerminal).await
+        match self {
+            ThresholdCmd::Sign => {
+                penumbra_custody::threshold::follow(config, &ActualTerminal).await
+            }
+        }
     }
 }
