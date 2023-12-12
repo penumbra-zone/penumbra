d77cd105da3a43e3893d7a625c196f8dcc534c09
Henry de Valence <hdevalence@penumbralabs.xyz>
2023-12-11 21:19:27 -0800

    cnidarium: update docs

--- crates/cnidarium-component/src/lib.rs
+++ crates/cnidarium-component/src/lib.rs
@@ -1,4 +1,4 @@
-//! Core trait definitions for components of the Penumbra application.
+//! Core trait definitions for components of an ABCI application using [`cnidarium`].
 //!
 //! This crate defines two traits for use by "component crates":
 //!
@@ -20,17 +20,22 @@
 //!   a public API, like the rest of the chain state);
 //! - a `crate::event` module defining any events emitted by the component;
 //!
-//! The `penumbra_transaction` crate depends on all of the component crates
-//! without the `component` feature, so it can assemble all of the actions for
-//! each component into a top-level transaction type.  This means all async code
-//! should be confined to the `component` module, so that the transaction
-//! definitions don't depend on `tokio`, `mio`, etc and can be used without
-//! problems in wasm or other non-async contexts.
+//! The structure of the feature-gated `component` submodule allows reusing data
+//! structures between client and server (fullnode) code.
 //!
-//! The `penumbra_app` crate depends on all of the component crates with the
-//! `component` feature enabled, so it can assemble all of the `ActionHandler`
-//! implementations into a top-level `ActionHandler` implementation and call
-//! each component's `Component` implementation at the appropriate times.
+//! For instance, the `penumbra_transaction` crate depends on all of the
+//! component crates without the `component` feature, so it can assemble all of
+//! the actions for each component into a top-level transaction type.  This
+//! means all async code should be confined to the `component` module, so that
+//! the transaction definitions don't depend on `tokio`, `mio`, etc and can be
+//! used without problems in wasm or other non-async contexts.
+//!
+//! On the other hand, the `penumbra_app` crate depends on all of the component
+//! crates with the `component` feature enabled, so it can assemble all of the
+//! `ActionHandler` implementations into a top-level `ActionHandler`
+//! implementation and call each component's `Component` implementation at the
+//! appropriate times.
+
 #![deny(clippy::unwrap_used)]
 
 mod action_handler;
