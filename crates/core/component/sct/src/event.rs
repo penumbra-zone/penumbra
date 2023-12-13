26137239f0f149a5161c44f1467a6d2c3225f830
Erwan <erwanor@penumbralabs.xyz>
2023-12-13 18:44:12 -0500

    sct: add global root anchor event

--- crates/core/component/sct/src/event.rs
+++ crates/core/component/sct/src/event.rs
@@ -1,33 +1,23 @@
-use penumbra_tct as tct;
-use tct::builder::{block, epoch};
-use tendermint::abci::{Event, EventAttributeIndexExt};
+use penumbra_proto::penumbra::core::component::sct::v1alpha1 as pb;
+use penumbra_tct::builder::{block, epoch};
 
-pub fn sct_anchor(height: u64, anchor: &tct::Root) -> Event {
-    Event::new(
-        "sct_anchor",
-        [
-            ("height", height.to_string()).index(),
-            ("anchor", anchor.to_string()).index(),
-        ],
-    )
+pub fn sct_anchor(height: u64, anchor: &tct::Root) -> pb::EventRootAnchor {
+    pb::EventRootAnchor {
+        height: Some(height.into()),
+        root_anchor: Some(anchor.into()),
+    }
 }
 
-pub fn sct_block_anchor(height: u64, anchor: &block::Root) -> Event {
-    Event::new(
-        "sct_block_anchor",
-        [
-            ("height", height.to_string()).index(),
-            ("anchor", anchor.to_string()).index(),
-        ],
-    )
+pub fn sct_epoch_anchor(index: u64, anchor: &epoch::Root) -> pb::EventEpochAnchor {
+    pb::EventEpochAnchor {
+        index: Some(index.into()),
+        epoch_anchor: Some(anchor.into()),
+    }
 }
 
-pub fn sct_epoch_anchor(index: u64, anchor: &epoch::Root) -> Event {
-    Event::new(
-        "sct_epoch_anchor",
-        [
-            ("index", index.to_string()).index(),
-            ("anchor", anchor.to_string()).index(),
-        ],
-    )
+pub fn sct_block_anchor(height: u64, anchor: &block::Root) -> pb::EventBlockAnchor {
+    pb::EventBlockAnchor {
+        height: Some(height.into()),
+        block_anchor: Some(anchor.into()),
+    }
 }
