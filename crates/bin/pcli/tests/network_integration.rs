f726c6ba74fb481514739f8972a7cb0878e5d085
Chris Czub <chris@penumbralabs.xyz>
2024-01-12 20:15:18 -0500

    slowly reintroduce tests

--- crates/bin/pcli/tests/network_integration.rs
+++ crates/bin/pcli/tests/network_integration.rs
@@ -685,102 +685,102 @@ fn swap() {
         .stdout(predicate::str::is_match(r"1\s*1000(\.[0-9]+)?penumbra").unwrap());
 }
 
-// // Note: As part of #2589, we changed the way DEX calculations are performed. In particular,
-// // we now perform the division before the multiplication, which means that the result is
-// // slightly lossy. Since we systematically round down non-integral amounts, this means that
-// // NFT trading is no longer possible under the current implementation. We should fix this in the
-// // future, but for now we disable the test.
-// // #[ignore]
-// // #[test]
-// // fn swap_nft() {
-// //     let tmpdir = load_wallet_into_tmpdir();
-// //
-// //     // Create a liquidity position selling 1cube for 1penumbra each.
-// //     let mut sell_cmd = Command::cargo_bin("pcli").unwrap();
-// //     sell_cmd
-// //         .args([
-// //             "--home",
-// //             tmpdir.path().to_str().unwrap(),
-// //             "tx",
-// //             "position",
-// //             "order",
-// //             "sell",
-// //             "1cube@1penumbra",
-// //         ])
-// //         .timeout(std::time::Duration::from_secs(TIMEOUT_COMMAND_SECONDS));
-// //     sell_cmd.assert().success();
-// //
-// //     let mut balance_cmd = Command::cargo_bin("pcli").unwrap();
-// //     balance_cmd
-// //         .args([
-// //             "--home",
-// //             tmpdir.path().to_str().unwrap(),
-// //             "view",
-// //             "balance",
-// //         ])
-// //         .timeout(std::time::Duration::from_secs(TIMEOUT_COMMAND_SECONDS));
-// //
-// //     balance_cmd
-// //         .assert()
-// //         // Address 0 has no `cube`.
-// //         .stdout(
-// //             predicate::str::is_match(format!(r"0\s*[0-9]+.*cube"))
-// //                 .unwrap()
-// //                 .not(),
-// //         )
-// //         // Address 1 should also have no cube.
-// //         .stdout(
-// //             predicate::str::is_match(format!(r"1\s*[0-9]+.*cube"))
-// //                 .unwrap()
-// //                 .not(),
-// //         )
-// //         // Address 1 has 1001penumbra.
-// //         .stdout(predicate::str::is_match(format!(r"1\s*1001penumbra")).unwrap())
-// //         // Address 0 should have some penumbra
-// //         .stdout(predicate::str::is_match(format!(r"0\s*[0-9]+.*penumbra")).unwrap());
-// //
-// //     // Swap 1penumbra for some cube from address 1.
-// //     let mut swap_cmd = Command::cargo_bin("pcli").unwrap();
-// //     swap_cmd
-// //         .args([
-// //             "--home",
-// //             tmpdir.path().to_str().unwrap(),
-// //             "tx",
-// //             "swap",
-// //             "1penumbra",
-// //             "--into",
-// //             "cube",
-// //             "--source",
-// //             "1",
-// //         ])
-// //         .timeout(std::time::Duration::from_secs(TIMEOUT_COMMAND_SECONDS));
-// //     swap_cmd.assert().success();
-// //
-// //     // Sleep to allow the outputs from the swap to be processed.
-// //     thread::sleep(*UNBONDING_DURATION);
-// //     let mut balance_cmd = Command::cargo_bin("pcli").unwrap();
-// //     balance_cmd
-// //         .args([
-// //             "--home",
-// //             tmpdir.path().to_str().unwrap(),
-// //             "view",
-// //             "balance",
-// //         ])
-// //         .timeout(std::time::Duration::from_secs(TIMEOUT_COMMAND_SECONDS));
-// //
-// //     balance_cmd
-// //         .assert()
-// //         // Address 1 has 1cube now
-// //         .stdout(predicate::str::is_match(format!(r"1\s*1cube")).unwrap())
-// //         // and address 0 has no cube.
-// //         .stdout(
-// //             predicate::str::is_match(format!(r"0\s*[0-9]+.*cube"))
-// //                 .unwrap()
-// //                 .not(),
-// //         )
-// //         // Address 1 spent 1penumbra.
-// //         .stdout(predicate::str::is_match(format!(r"1\s*1000penumbra")).unwrap());
-// // }
+// Note: As part of #2589, we changed the way DEX calculations are performed. In particular,
+// we now perform the division before the multiplication, which means that the result is
+// slightly lossy. Since we systematically round down non-integral amounts, this means that
+// NFT trading is no longer possible under the current implementation. We should fix this in the
+// future, but for now we disable the test.
+// #[ignore]
+// #[test]
+// fn swap_nft() {
+//     let tmpdir = load_wallet_into_tmpdir();
+//
+//     // Create a liquidity position selling 1cube for 1penumbra each.
+//     let mut sell_cmd = Command::cargo_bin("pcli").unwrap();
+//     sell_cmd
+//         .args([
+//             "--home",
+//             tmpdir.path().to_str().unwrap(),
+//             "tx",
+//             "position",
+//             "order",
+//             "sell",
+//             "1cube@1penumbra",
+//         ])
+//         .timeout(std::time::Duration::from_secs(TIMEOUT_COMMAND_SECONDS));
+//     sell_cmd.assert().success();
+//
+//     let mut balance_cmd = Command::cargo_bin("pcli").unwrap();
+//     balance_cmd
+//         .args([
+//             "--home",
+//             tmpdir.path().to_str().unwrap(),
+//             "view",
+//             "balance",
+//         ])
+//         .timeout(std::time::Duration::from_secs(TIMEOUT_COMMAND_SECONDS));
+//
+//     balance_cmd
+//         .assert()
+//         // Address 0 has no `cube`.
+//         .stdout(
+//             predicate::str::is_match(format!(r"0\s*[0-9]+.*cube"))
+//                 .unwrap()
+//                 .not(),
+//         )
+//         // Address 1 should also have no cube.
+//         .stdout(
+//             predicate::str::is_match(format!(r"1\s*[0-9]+.*cube"))
+//                 .unwrap()
+//                 .not(),
+//         )
+//         // Address 1 has 1001penumbra.
+//         .stdout(predicate::str::is_match(format!(r"1\s*1001penumbra")).unwrap())
+//         // Address 0 should have some penumbra
+//         .stdout(predicate::str::is_match(format!(r"0\s*[0-9]+.*penumbra")).unwrap());
+//
+//     // Swap 1penumbra for some cube from address 1.
+//     let mut swap_cmd = Command::cargo_bin("pcli").unwrap();
+//     swap_cmd
+//         .args([
+//             "--home",
+//             tmpdir.path().to_str().unwrap(),
+//             "tx",
+//             "swap",
+//             "1penumbra",
+//             "--into",
+//             "cube",
+//             "--source",
+//             "1",
+//         ])
+//         .timeout(std::time::Duration::from_secs(TIMEOUT_COMMAND_SECONDS));
+//     swap_cmd.assert().success();
+//
+//     // Sleep to allow the outputs from the swap to be processed.
+//     thread::sleep(*UNBONDING_DURATION);
+//     let mut balance_cmd = Command::cargo_bin("pcli").unwrap();
+//     balance_cmd
+//         .args([
+//             "--home",
+//             tmpdir.path().to_str().unwrap(),
+//             "view",
+//             "balance",
+//         ])
+//         .timeout(std::time::Duration::from_secs(TIMEOUT_COMMAND_SECONDS));
+//
+//     balance_cmd
+//         .assert()
+//         // Address 1 has 1cube now
+//         .stdout(predicate::str::is_match(format!(r"1\s*1cube")).unwrap())
+//         // and address 0 has no cube.
+//         .stdout(
+//             predicate::str::is_match(format!(r"0\s*[0-9]+.*cube"))
+//                 .unwrap()
+//                 .not(),
+//         )
+//         // Address 1 spent 1penumbra.
+//         .stdout(predicate::str::is_match(format!(r"1\s*1000penumbra")).unwrap());
+// }
 
 // #[ignore]
 // #[test]
