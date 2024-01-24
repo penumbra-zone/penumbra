f9ea27c0e46fe2e6c06031958b190f47f3d5ccaa
Erwan <erwanor@penumbralabs.xyz>
2024-01-24 15:02:22 -0500

    last run

--- crates/bin/pcli/tests/network_integration.rs
+++ crates/bin/pcli/tests/network_integration.rs
@@ -299,8 +299,10 @@ fn delegate_and_undelegate() {
     // need to pull the amount of delegation tokens we obtained so that we can later
     // try to execute an undelegation (`tx undelegate <AMOUNT><DELEGATION_TOKEN_DENOM>`).
     // To do this, we use a regex to extract the amount of delegation tokens we obtained:
-    let delegation_token_pattern = Regex::new(r"\(\d*\.?\d*delegation_[a-zA-Z0-9]*\)").unwrap();
-    let (delegation_token_str, []) = delegation_token_pattern
+    //
+    //
+    let delegation_token_pattern = Regex::new(r"(\d+\.?\d+[a-z]?delegation_[a-zA-Z0-9]*)").unwrap();
+    let (delegation_token_str, [_match]) = delegation_token_pattern
         .captures(&balance_output_string)
         .expect("can find delegation token in balance output")
         .extract();
