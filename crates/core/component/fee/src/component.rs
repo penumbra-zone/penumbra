1c24027aa4bc49ac13a764558f431c11ec2ee909
Erwan <erwanor@penumbralabs.xyz>
2024-01-26 12:48:12 -0500

    fee: rm superfluous `object_delete` + fix tracing label

--- crates/core/component/fee/src/component.rs
+++ crates/core/component/fee/src/component.rs
@@ -2,7 +2,7 @@ mod view;
 
 use std::sync::Arc;
 
-use crate::{genesis, state_key};
+use crate::genesis;
 use async_trait::async_trait;
 use cnidarium::StateWrite;
 use cnidarium_component::Component;
@@ -17,7 +17,7 @@ pub struct Fee {}
 impl Component for Fee {
     type AppState = genesis::Content;
 
-    #[instrument(name = "staking", skip(state, app_state))]
+    #[instrument(name = "fee", skip(state, app_state))]
     async fn init_chain<S: StateWrite>(mut state: S, app_state: Option<&Self::AppState>) {
         match app_state {
             Some(genesis) => {
@@ -28,18 +28,14 @@ impl Component for Fee {
         }
     }
 
-    #[instrument(name = "staking", skip(state, _begin_block))]
+    #[instrument(name = "fee", skip(_state, _begin_block))]
     async fn begin_block<S: StateWrite + 'static>(
-        state: &mut Arc<S>,
+        _state: &mut Arc<S>,
         _begin_block: &abci::request::BeginBlock,
     ) {
-        let state = Arc::get_mut(state).expect("state should be unique");
-
-        // Clear the gas prices changed marker for this block
-        state.object_delete(state_key::gas_prices_changed());
     }
 
-    #[instrument(name = "staking", skip(_state, _end_block))]
+    #[instrument(name = "fee", skip(_state, _end_block))]
     async fn end_block<S: StateWrite + 'static>(
         _state: &mut Arc<S>,
         _end_block: &abci::request::EndBlock,
@@ -47,7 +43,7 @@ impl Component for Fee {
         // TODO: update gas prices here eventually
     }
 
-    #[instrument(name = "staking", skip(_state))]
+    #[instrument(name = "fee", skip(_state))]
     async fn end_epoch<S: StateWrite + 'static>(_state: &mut Arc<S>) -> anyhow::Result<()> {
         Ok(())
     }
