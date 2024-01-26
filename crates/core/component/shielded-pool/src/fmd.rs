7de72282adcc00fca63f8fd7b21498470fc2b0d9
Erwan <erwanor@penumbralabs.xyz>
2024-01-26 17:59:39 -0500

    shielded-pool: define `FmdParameters`

---
+++ crates/core/component/shielded-pool/src/fmd.rs
@@ -0,0 +1,42 @@
+#[derive(Clone, Debug, Serialize, Deserialize)]
+// #[serde(try_from = "pb::FmdParameters", into = "pb::FmdParameters")]
+pub struct FmdParameters {
+    /// Bits of precision.
+    pub precision_bits: u8,
+    /// The block height at which these parameters became effective.
+    pub as_of_block_height: u64,
+}
+
+// TODO(erwan): re-enable on second pass
+// impl DomainType for FmdParameters {
+//     type Proto = pb::FmdParameters;
+// }
+//
+// impl TryFrom<pb::FmdParameters> for FmdParameters {
+//     type Error = anyhow::Error;
+//
+//     fn try_from(msg: pb::FmdParameters) -> Result<Self, Self::Error> {
+//         Ok(FmdParameters {
+//             precision_bits: msg.precision_bits.try_into()?,
+//             as_of_block_height: msg.as_of_block_height,
+//         })
+//     }
+// }
+//
+// impl From<FmdParameters> for pb::FmdParameters {
+//     fn from(params: FmdParameters) -> Self {
+//         pb::FmdParameters {
+//             precision_bits: u32::from(params.precision_bits),
+//             as_of_block_height: params.as_of_block_height,
+//         }
+//     }
+// }
+
+impl Default for FmdParameters {
+    fn default() -> Self {
+        Self {
+            precision_bits: 0,
+            as_of_block_height: 1,
+        }
+    }
+}
