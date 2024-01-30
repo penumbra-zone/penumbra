f883c4e1c73f802a0c7802443e6a2f4795ffbfc8
Erwan <erwanor@penumbralabs.xyz>
2024-01-30 14:56:10 -0500

    app(state_key): namespace state keys by domain

--- crates/core/app/src/app/state_key.rs
+++ crates/core/app/src/app/state_key.rs
@@ -1,7 +1,22 @@
-pub fn app_state() -> &'static str {
-    "genesis/app_state"
+pub mod genesis {
+    pub fn app_state() -> &'static str {
+        "application/genesis/app_state"
+    }
 }
 
-pub fn transactions_by_height(block_height: u64) -> String {
-    format!("cometbft-data/transactions_by_height/{block_height:020}")
+pub mod cometbft_data {
+    use crate::COMETBFT_SUBSTORE_PREFIX;
+
+    pub fn transactions_by_height(block_height: u64) -> String {
+        format!(
+            "{}/transactions_by_height/{block_height:020}",
+            COMETBFT_SUBSTORE_PREFIX
+        )
+    }
+}
+
+pub mod counters {
+    pub fn halt_count() -> &'static str {
+        "application/counters/halt_count"
+    }
 }
