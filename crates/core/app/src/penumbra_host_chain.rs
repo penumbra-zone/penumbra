c871d7824e55b449705679cb5f45ce9a8e82fdef
Erwan <erwanor@penumbralabs.xyz>
2024-01-30 19:28:42 -0500

    app(host_chain): reimplement the host chain interface

--- crates/core/app/src/penumbra_host_chain.rs
+++ crates/core/app/src/penumbra_host_chain.rs
@@ -1,7 +1,8 @@
-use anyhow::Context;
 use async_trait::async_trait;
 use penumbra_ibc::component::HostInterface;
-use tendermint::Time;
+use penumbra_sct::component::EpochRead;
+
+use crate::app::StateReadExt;
 
 /// The implementation of HostInterface for Penumbra. This encapsulates all of the chain-specific
 /// data that the ibc implementation needs.
@@ -11,20 +12,20 @@ pub struct PenumbraHost {}
 #[async_trait]
 impl HostInterface for PenumbraHost {
     async fn get_chain_id<S: cnidarium::StateRead>(state: S) -> anyhow::Result<String> {
-        todo!("MERGEBLOCK(erwan)")
+        state.get_chain_id().await
     }
 
     async fn get_revision_number<S: cnidarium::StateRead>(state: S) -> anyhow::Result<u64> {
-        todo!()
+        state.get_revision_number().await
     }
 
     async fn get_block_height<S: cnidarium::StateRead>(state: S) -> anyhow::Result<u64> {
-        todo!("MERGEBLOCK(erwan)")
+        state.get_block_height().await
     }
 
     async fn get_block_timestamp<S: cnidarium::StateRead>(
         state: S,
     ) -> anyhow::Result<tendermint::Time> {
-        todo!()
+        state.get_block_timestamp().await
     }
 }
