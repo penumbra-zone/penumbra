9a8739d147a0bfd713455148bb21be6f5261ee1c
Tal Derei <talderei99@gmail.com>
2024-02-02 17:28:06 -0600

    check R1CS matrices

--- crates/old-backend/src/lib.rs
+++ crates/old-backend/src/lib.rs
@@ -1,10 +1,31 @@
 #[cfg(test)]
 mod tests {
-    use decaf377::*;
-    use decaf377_outdated::*;
+    use penumbra_proof_params::{
+        DummyWitness, SPEND_PROOF_PROVING_KEY, generate_constraint_matrices, generate_test_parameters
+    };
+    use penumbra_shielded_pool::{SpendCircuit, OutputCircuit, ConvertCircuit, NullifierDerivationCircuit};
+    use penumbra_dex::{swap::proof::SwapCircuit, swap_claim::proof::SwapClaimCircuit};
+    use rand_core::OsRng;
+    use rand_core::CryptoRngCore;
+
+    #[test]
+    fn test_generate_parameters() {
+        generate_constraint_matrices::<SpendCircuit>();
+        generate_constraint_matrices::<OutputCircuit>();
+        generate_constraint_matrices::<SwapCircuit>();
+        generate_constraint_matrices::<SwapClaimCircuit>();
+        generate_constraint_matrices::<ConvertCircuit>();
+        generate_constraint_matrices::<NullifierDerivationCircuit>();
+    }
 
     #[test]
     fn test_serialization_comparison() {
-        
+        let mut rng = OsRng;
+        let (pk, vk) = generate_test_parameters::<SpendCircuit>(&mut rng);
+        let (pk, vk) = generate_test_parameters::<OutputCircuit>(&mut rng);
+        let (pk, vk) = generate_test_parameters::<SwapCircuit>(&mut rng);
+        let (pk, vk) = generate_test_parameters::<SwapClaimCircuit>(&mut rng);
+        let (pk, vk) = generate_test_parameters::<ConvertCircuit>(&mut rng);
+        let (pk, vk) = generate_test_parameters::<NullifierDerivationCircuit>(&mut rng);
     }
 }
