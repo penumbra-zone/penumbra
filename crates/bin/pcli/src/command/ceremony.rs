912d725fd77b282c8b86f9329efecf5d15ae7d46
Conor Schaefer <conor@penumbralabs.xyz>
2024-02-05 10:14:54 -0800

    feat(pcli): default coordinator address for ceremony
    During Phase 1 of the summoning ceremony, some contributors were
    confused about the `--coordinator-address` flag, and incorrectly
    overrode it to a wallet address they control. Let's set a sane default
    value for that arg, and remove its display from the web page.

--- crates/bin/pcli/src/command/ceremony.rs
+++ crates/bin/pcli/src/command/ceremony.rs
@@ -89,7 +89,10 @@ pub enum CeremonyCmd {
         coordinator_url: Url,
         /// The Penumbra wallet address of the coordination server. Bids will be sent to this
         /// address, so the coordinator can compute the contributor's place in the queue.
-        #[clap(long)]
+        #[clap(
+            long,
+            default_value = "penumbra1qvqr8cvqyf4pwrl6svw9kj8eypf3fuunrcs83m30zxh57y2ytk94gygmtq5k82cjdq9y3mlaa3fwctwpdjr6fxnwuzrsy4ezm0u2tqpzw0sed82shzcr42sju55en26mavjnw4"
+        )]
         coordinator_address: Address,
         /// Amount to spend during bid. Must be specified typed values, e.g. '50penumbra'.
         /// Only the 'penumbra' token is accepted for contributions. Bids are additive,
