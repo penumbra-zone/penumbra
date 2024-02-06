1c916faf11e931f0d03e8936ec0c5de580cb94cb
Erwan <erwanor@penumbralabs.xyz>
2024-02-06 10:16:31 -0500

    staking(validator_handler): track manager

--- crates/core/component/stake/src/component/validator_handler.rs
+++ crates/core/component/stake/src/component/validator_handler.rs
@@ -1,2 +1,6 @@
-mod validator_manager;
+pub(crate) mod validator_manager;
+pub(crate) use validator_manager::ValidatorManager;
+
 pub mod validator_store;
+pub use validator_store::ValidatorDataRead;
+pub use validator_store::ValidatorStore;
