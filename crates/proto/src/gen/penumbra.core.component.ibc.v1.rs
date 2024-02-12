46fe8ad893cbd25422bad29e918955c1e82c5439
Conor Schaefer <conor@penumbralabs.xyz>
2024-02-12 13:25:12 -0800

    ibc: populate ics20 withdrawal with memo field

--- crates/proto/src/gen/penumbra.core.component.ibc.v1.rs
+++ crates/proto/src/gen/penumbra.core.component.ibc.v1.rs
@@ -29,6 +29,9 @@ pub struct FungibleTokenPacketData {
     /// the recipient address on the destination chain
     #[prost(string, tag = "4")]
     pub receiver: ::prost::alloc::string::String,
+    /// optional memo
+    #[prost(string, tag = "5")]
+    pub memo: ::prost::alloc::string::String,
 }
 impl ::prost::Name for FungibleTokenPacketData {
     const NAME: &'static str = "FungibleTokenPacketData";
