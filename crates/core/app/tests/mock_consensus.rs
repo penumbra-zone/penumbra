e15809e802b1c08fc534e054d0404caca136d896
katelyn martin <katelyn@penumbralabs.xyz>
2024-02-14 09:57:55 -0500

    mock-consensus: 🚀 test node can send an empty block
    fixes #3792.
    
    see #3588.
    
    * `block` module defining interfaces to build a tendermint block,
      holding a unique reference to the `TestNode`.
    * `abci` module defining `TestNode` interfaces that will send consensus
      requests to the application. this includes:
      * BeginBlock
      * DeliverTx
      * EndBlock
      * Commit
    * `send_block` module defining a `TestNode` interface to send the
      requisite abci requests, given a `tendermint::Block`.
    * documentation is added for assorted public interfaces.
    
    this represents a huge, exciting step for work on the mock engine! we
    can now initialize and send an (empty) block to the consensus service.
    
    ✨ 🎊 ✨ 🎊 ✨
    
    what next?
    
    this isn't a _comprehensive_ set of interfaces.
    `penumbra_mock_consensus::block::Builder` will certainly grow more
    methods as it is iterated upon. we'll use work in porting tests
    (see #3788) to drive which other fields are needed.
    
    forthcoming work will build upon this to:
    
    * introduce more builder methods to set other `Header` fields (e.g.
      timestamp, see #3759)
    * use the reference to the test node to set other `Header` fields (e.g.
      height)
    
    todo comments are left to that effect.
    
    ---
    
    * #3588
    * #3792

--- crates/core/app/tests/mock_consensus.rs
+++ crates/core/app/tests/mock_consensus.rs
@@ -9,6 +9,7 @@ use cnidarium::TempStorage;
 use common::BuilderExt;
 use penumbra_app::server::consensus::Consensus;
 use penumbra_genesis::AppState;
+use tendermint::evidence::List;
 
 #[tokio::test]
 async fn mock_consensus_can_send_an_init_chain_request() -> anyhow::Result<()> {
@@ -29,6 +30,38 @@ async fn mock_consensus_can_send_an_init_chain_request() -> anyhow::Result<()> {
     };
 
     tracing::info!(hash = %engine.last_app_hash_hex(), "finished init chain");
+
+    // Free our temporary storage.
+    drop(storage);
+    drop(guard);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn mock_consensus_can_send_a_single_empty_block() -> anyhow::Result<()> {
+    // Install a test logger, and acquire some temporary storage.
+    let guard = common::set_tracing_subscriber();
+    let storage = TempStorage::new().await?;
+
+    // Instantiate the consensus service, and start the test node.
+    let mut engine = {
+        use penumbra_mock_consensus::TestNode;
+        let app_state = AppState::default();
+        let consensus = Consensus::new(storage.as_ref().clone());
+        TestNode::builder()
+            .single_validator()
+            .with_penumbra_auto_app_state(app_state)?
+            .init_chain(consensus)
+            .await?
+    };
+
+    let block = engine
+        .block()
+        .with_data(vec![])
+        .with_evidence(List::new(Vec::new()))
+        .finish()?;
+    engine.send_block(block).await?;
 
     // Free our temporary storage.
     drop(storage);
