b2b719295ec3d358d87921e566a9a0d6084c8957
katelyn martin <katelyn@penumbralabs.xyz>
2024-02-16 11:08:29 -0500

    shielded-pool: 🚰 tap proof_result

--- crates/core/component/shielded-pool/src/spend/proof.rs
+++ crates/core/component/shielded-pool/src/spend/proof.rs
@@ -31,6 +31,7 @@ use penumbra_keys::keys::{
 };
 use penumbra_proof_params::{DummyWitness, VerifyingKeyExt, GROTH16_PROOF_LENGTH_BYTES};
 use penumbra_sct::{Nullifier, NullifierVar};
+use tap::Tap;
 
 /// The public input for a [`SpendProof`].
 #[derive(Clone, Debug)]
@@ -354,16 +355,15 @@ impl SpendProof {
 
         tracing::trace!(?public_inputs);
         let start = std::time::Instant::now();
-        let proof_result = Groth16::<Bls12_377, LibsnarkReduction>::verify_with_processed_vk(
+        Groth16::<Bls12_377, LibsnarkReduction>::verify_with_processed_vk(
             vk,
             public_inputs.as_slice(),
             &proof,
         )
-        .map_err(VerificationError::SynthesisError)?;
-        tracing::debug!(?proof_result, elapsed = ?start.elapsed());
-        proof_result
-            .then_some(())
-            .ok_or(VerificationError::InvalidProof)
+        .map_err(VerificationError::SynthesisError)?
+        .tap(|proof_result| tracing::debug!(?proof_result, elapsed = ?start.elapsed()))
+        .then_some(())
+        .ok_or(VerificationError::InvalidProof)
     }
 }
 
