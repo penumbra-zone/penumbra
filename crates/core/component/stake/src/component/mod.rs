d52bd554b3d001a5f949ec68c5d7ecb4b5356c74
Erwan <erwanor@penumbralabs.xyz>
2024-02-26 14:45:30 -0500

    staking: remove `component::FP_SCALING_FACTOR` dup

--- crates/core/component/stake/src/component/mod.rs
+++ crates/core/component/stake/src/component/mod.rs
@@ -3,17 +3,13 @@ pub mod metrics;
 pub mod rpc;
 pub mod stake;
 pub mod validator_handler;
-
 mod epoch_handler;
 
-use once_cell::sync::Lazy;
 pub use stake::Staking;
+pub use self::metrics::register_metrics;
+
 // Max validator power is 1152921504606846975 (i64::MAX / 8)
 // https://github.com/tendermint/tendermint/blob/master/types/validator_set.go#L25
 pub const MAX_VOTING_POWER: u128 = 1152921504606846975;
-pub static FP_SCALING_FACTOR: Lazy<penumbra_num::fixpoint::U128x128> =
-    Lazy::new(|| 1_0000_0000u128.into());
-
-pub use self::metrics::register_metrics;
 pub use stake::{ConsensusIndexRead, SlashingData};
 pub use stake::{StateReadExt, StateWriteExt};
