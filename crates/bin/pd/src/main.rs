c10670c690c0547129dc38b490f036d240ac94f2
katelyn martin <katelyn@penumbralabs.xyz>
2024-03-11 12:40:11 -0400

    pd: pull grpc down into helper function

--- crates/bin/pd/src/main.rs
+++ crates/bin/pd/src/main.rs
@@ -128,13 +128,10 @@ async fn main() -> anyhow::Result<()> {
                 "starting pd"
             );
 
-            let tm_proxy = TendermintProxy::new(cometbft_addr);
             let abci_server = tokio::task::Builder::new()
                 .name("abci_server")
                 .spawn(penumbra_app::server::new(storage.clone()).listen_tcp(abci_bind))
                 .expect("failed to spawn abci server");
-
-            let ibc = penumbra_ibc::component::rpc::IbcQuery::<PenumbraHost>::new(storage.clone());
 
             // TODO: Once we migrate to Tonic 0.10.0, we'll be able to use the
             // `Routes` structure to have each component define a method that
@@ -170,68 +167,7 @@ async fn main() -> anyhow::Result<()> {
             use penumbra_shielded_pool::component::rpc::Server as ShieldedPoolServer;
             use penumbra_stake::component::rpc::Server as StakeServer;
 
-            let mut grpc_server = Server::builder()
-                .trace_fn(|req| match remote_addr(req) {
-                    Some(remote_addr) => {
-                        tracing::error_span!("grpc", ?remote_addr)
-                    }
-                    None => tracing::error_span!("grpc"),
-                })
-                // Allow HTTP/1, which will be used by grpc-web connections.
-                // This is particularly important when running locally, as gRPC
-                // typically uses HTTP/2, which requires HTTPS. Accepting HTTP/2
-                // allows local applications such as web browsers to talk to pd.
-                .accept_http1(true)
-                // As part of #2932, we are disabling all timeouts until we circle back to our
-                // performance story.
-                // Sets a timeout for all gRPC requests, but note that in the case of streaming
-                // requests, the timeout is only applied to the initial request. This means that
-                // this does not prevent long lived streams, for example to allow clients to obtain
-                // new blocks.
-                // .timeout(std::time::Duration::from_secs(7))
-                // Wrap each of the gRPC services in a tonic-web proxy:
-                .add_service(we(StorageQueryServiceServer::new(StorageServer::new(
-                    storage.clone(),
-                ))))
-                .add_service(we(AppQueryServiceServer::new(AppServer::new(
-                    storage.clone(),
-                ))))
-                .add_service(we(CompactBlockQueryServiceServer::new(
-                    CompactBlockServer::new(storage.clone()),
-                )))
-                .add_service(we(DexQueryServiceServer::new(DexServer::new(
-                    storage.clone(),
-                ))))
-                .add_service(we(FeeQueryServiceServer::new(FeeServer::new(
-                    storage.clone(),
-                ))))
-                .add_service(we(GovernanceQueryServiceServer::new(
-                    GovernanceServer::new(storage.clone()),
-                )))
-                .add_service(we(SctQueryServiceServer::new(SctServer::new(
-                    storage.clone(),
-                ))))
-                .add_service(we(ShieldedPoolQueryServiceServer::new(
-                    ShieldedPoolServer::new(storage.clone()),
-                )))
-                .add_service(we(StakeQueryServiceServer::new(StakeServer::new(
-                    storage.clone(),
-                ))))
-                .add_service(we(ClientQueryServer::new(ibc.clone())))
-                .add_service(we(ChannelQueryServer::new(ibc.clone())))
-                .add_service(we(ConnectionQueryServer::new(ibc.clone())))
-                .add_service(we(TendermintProxyServiceServer::new(tm_proxy.clone())))
-                .add_service(we(tonic_reflection::server::Builder::configure()
-                    .register_encoded_file_descriptor_set(penumbra_proto::FILE_DESCRIPTOR_SET)
-                    .build()
-                    .with_context(|| "could not configure grpc reflection service")?));
-
-            if enable_expensive_rpc {
-                grpc_server = grpc_server.add_service(we(SimulationServiceServer::new(
-                    DexServer::new(storage.clone()),
-                )));
-            }
-
+            let mut grpc_server = f(&storage, cometbft_addr, enable_expensive_rpc)?;
             // Create Axum routes for the frontend app.
             let frontend = pd::zipserve::router("/app/", pd::MINIFRONT_ARCHIVE_BYTES);
             let node_status = pd::zipserve::router("/", pd::NODE_STATUS_ARCHIVE_BYTES);
@@ -524,4 +460,101 @@ async fn main() -> anyhow::Result<()> {
         }
     }
     Ok(())
+}
+
+fn f(
+    storage: &cnidarium::Storage,
+    cometbft_addr: url::Url,
+    enable_expensive_rpc: bool,
+) -> anyhow::Result<tonic::transport::server::Router> {
+    use cnidarium::rpc::proto::v1::query_service_server::QueryServiceServer as StorageQueryServiceServer;
+    use penumbra_proto::core::{
+        app::v1::query_service_server::QueryServiceServer as AppQueryServiceServer,
+        component::{
+            compact_block::v1::query_service_server::QueryServiceServer as CompactBlockQueryServiceServer,
+            dex::v1::query_service_server::QueryServiceServer as DexQueryServiceServer,
+            fee::v1::query_service_server::QueryServiceServer as FeeQueryServiceServer,
+            governance::v1::query_service_server::QueryServiceServer as GovernanceQueryServiceServer,
+            sct::v1::query_service_server::QueryServiceServer as SctQueryServiceServer,
+            shielded_pool::v1::query_service_server::QueryServiceServer as ShieldedPoolQueryServiceServer,
+            stake::v1::query_service_server::QueryServiceServer as StakeQueryServiceServer,
+        },
+    };
+    use tonic_web::enable as we;
+
+    use cnidarium::rpc::Server as StorageServer;
+    use penumbra_app::rpc::Server as AppServer;
+    use penumbra_compact_block::component::rpc::Server as CompactBlockServer;
+    use penumbra_dex::component::rpc::Server as DexServer;
+    use penumbra_fee::component::rpc::Server as FeeServer;
+    use penumbra_governance::component::rpc::Server as GovernanceServer;
+    use penumbra_sct::component::rpc::Server as SctServer;
+    use penumbra_shielded_pool::component::rpc::Server as ShieldedPoolServer;
+    use penumbra_stake::component::rpc::Server as StakeServer;
+
+    let tm_proxy = TendermintProxy::new(cometbft_addr);
+    let ibc = penumbra_ibc::component::rpc::IbcQuery::<PenumbraHost>::new(storage.clone());
+    let mut grpc_server = Server::builder()
+        .trace_fn(|req| match remote_addr(req) {
+            Some(remote_addr) => {
+                tracing::error_span!("grpc", ?remote_addr)
+            }
+            None => tracing::error_span!("grpc"),
+        })
+        // Allow HTTP/1, which will be used by grpc-web connections.
+        // This is particularly important when running locally, as gRPC
+        // typically uses HTTP/2, which requires HTTPS. Accepting HTTP/2
+        // allows local applications such as web browsers to talk to pd.
+        .accept_http1(true)
+        // As part of #2932, we are disabling all timeouts until we circle back to our
+        // performance story.
+        // Sets a timeout for all gRPC requests, but note that in the case of streaming
+        // requests, the timeout is only applied to the initial request. This means that
+        // this does not prevent long lived streams, for example to allow clients to obtain
+        // new blocks.
+        // .timeout(std::time::Duration::from_secs(7))
+        // Wrap each of the gRPC services in a tonic-web proxy:
+        .add_service(we(StorageQueryServiceServer::new(StorageServer::new(
+            storage.clone(),
+        ))))
+        .add_service(we(AppQueryServiceServer::new(AppServer::new(
+            storage.clone(),
+        ))))
+        .add_service(we(CompactBlockQueryServiceServer::new(
+            CompactBlockServer::new(storage.clone()),
+        )))
+        .add_service(we(DexQueryServiceServer::new(DexServer::new(
+            storage.clone(),
+        ))))
+        .add_service(we(FeeQueryServiceServer::new(FeeServer::new(
+            storage.clone(),
+        ))))
+        .add_service(we(GovernanceQueryServiceServer::new(
+            GovernanceServer::new(storage.clone()),
+        )))
+        .add_service(we(SctQueryServiceServer::new(SctServer::new(
+            storage.clone(),
+        ))))
+        .add_service(we(ShieldedPoolQueryServiceServer::new(
+            ShieldedPoolServer::new(storage.clone()),
+        )))
+        .add_service(we(StakeQueryServiceServer::new(StakeServer::new(
+            storage.clone(),
+        ))))
+        .add_service(we(ClientQueryServer::new(ibc.clone())))
+        .add_service(we(ChannelQueryServer::new(ibc.clone())))
+        .add_service(we(ConnectionQueryServer::new(ibc.clone())))
+        .add_service(we(TendermintProxyServiceServer::new(tm_proxy.clone())))
+        .add_service(we(tonic_reflection::server::Builder::configure()
+            .register_encoded_file_descriptor_set(penumbra_proto::FILE_DESCRIPTOR_SET)
+            .build()
+            .with_context(|| "could not configure grpc reflection service")?));
+
+    if enable_expensive_rpc {
+        grpc_server = grpc_server.add_service(we(SimulationServiceServer::new(DexServer::new(
+            storage.clone(),
+        ))));
+    }
+
+    Ok(grpc_server)
 }
