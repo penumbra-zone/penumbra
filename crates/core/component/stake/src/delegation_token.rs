0e0859e7206f30d2277d584ea7e7f28d1d5ed4f5
katelyn martin <katelyn@penumbralabs.xyz>
2024-03-14 11:24:30 -0400

    stake: 📑 tweak delegation token rust docs
    https://rust-lang.github.io/rfcs/1574-more-api-documentation-conventions.html#summary-sentence

--- crates/core/component/stake/src/delegation_token.rs
+++ crates/core/component/stake/src/delegation_token.rs
@@ -36,22 +36,22 @@ impl DelegationToken {
         }
     }
 
-    /// Get the base denomination for this delegation token.
+    /// Returns the base denomination for this delegation token.
     pub fn denom(&self) -> asset::Metadata {
         self.base_denom.clone()
     }
 
-    /// Get the default display denomination for this delegation token.
+    /// Returns the default display denomination for this delegation token.
     pub fn default_unit(&self) -> asset::Unit {
         self.base_denom.default_unit()
     }
 
-    /// Get the asset ID for this delegation token.
+    /// Returns the asset ID for this delegation token.
     pub fn id(&self) -> asset::Id {
         self.base_denom.id()
     }
 
-    /// Get the identity key of the validator this delegation token is associated with.
+    /// Returns the identity key of the validator this delegation token is associated with.
     pub fn validator(&self) -> IdentityKey {
         self.validator_identity.clone()
     }
