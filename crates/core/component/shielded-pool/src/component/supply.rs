4ee642b80566d6209b8a71148a7e1a72d67c0147
Erwan Or <erwan.ounn.84@gmail.com>
2024-03-14 18:09:10 -0400

    shielded-pool: chop down `SupplyWrite/Read`

--- crates/core/component/shielded-pool/src/component/supply.rs
+++ crates/core/component/shielded-pool/src/component/supply.rs
@@ -2,7 +2,6 @@ use anyhow::Result;
 use async_trait::async_trait;
 use cnidarium::{StateRead, StateWrite};
 use penumbra_asset::asset::{self, Metadata};
-use penumbra_num::Amount;
 use penumbra_proto::{StateReadProto, StateWriteProto};
 
 use tracing::instrument;
@@ -11,10 +10,6 @@ use crate::state_key;
 
 #[async_trait]
 pub trait SupplyRead: StateRead {
-    async fn token_supply(&self, asset_id: &asset::Id) -> Result<Option<Amount>> {
-        self.get(&state_key::token_supply(asset_id)).await
-    }
-
     async fn denom_by_asset(&self, asset_id: &asset::Id) -> Result<Option<Metadata>> {
         self.get(&state_key::denom_by_asset(asset_id)).await
     }
@@ -37,60 +32,6 @@ pub trait SupplyWrite: StateWrite {
             self.put(state_key::denom_by_asset(&id), denom.clone());
             Ok(())
         }
-    }
-
-    async fn increase_token_supply(
-        &mut self,
-        asset_id: &asset::Id,
-        amount_to_add: Amount,
-    ) -> Result<()> {
-        let key = state_key::token_supply(asset_id);
-        let current_supply: Amount = self.get(&key).await?.unwrap_or(0u128.into());
-
-        tracing::debug!(
-            ?current_supply,
-            ?amount_to_add,
-            ?asset_id,
-            "increasing token supply"
-        );
-        let new_supply = current_supply.checked_add(&amount_to_add).ok_or_else(|| {
-            anyhow::anyhow!(
-                "overflow updating token {} supply {} with delta {}",
-                asset_id,
-                current_supply,
-                amount_to_add
-            )
-        })?;
-
-        self.put(key, new_supply);
-        Ok(())
-    }
-
-    async fn decrease_token_supply(
-        &mut self,
-        asset_id: &asset::Id,
-        amount_to_sub: Amount,
-    ) -> Result<()> {
-        let key = state_key::token_supply(asset_id);
-        let current_supply: Amount = self.get(&key).await?.unwrap_or(0u128.into());
-
-        tracing::debug!(
-            ?current_supply,
-            ?amount_to_sub,
-            ?asset_id,
-            "decreasing token supply"
-        );
-        let new_supply = current_supply.checked_sub(&amount_to_sub).ok_or_else(|| {
-            anyhow::anyhow!(
-                "overflow updating token {} supply {} with delta {}",
-                asset_id,
-                current_supply,
-                amount_to_sub
-            )
-        })?;
-
-        self.put(key, new_supply);
-        Ok(())
     }
 }
 
