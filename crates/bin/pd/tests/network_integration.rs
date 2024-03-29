837b6a33e6b557aad3e3a1aa33f10699b7ba9007
Jesse Pinho <jesse@jessepinho.com>
2024-03-29 11:06:26 -0700

    fix(pd): minifront asset directory subpath
    Follow-up to #4132, in which we updated the minifront assets
    but mistakenly with the wrong subdir nesting. This broke serving
    minifront from pd's gRPC endpoint, but our tests didn't catch that,
    so we only noticed when it hit preview.
    
    Added a test to the pd integration suite as a sanity check.
    Made sure to confirm that the test fails on current main, and passes
    with the asset fix included in this commit.
    
    Squashed and committed by @conorsch.

--- crates/bin/pd/tests/network_integration.rs
+++ crates/bin/pd/tests/network_integration.rs
@@ -4,6 +4,8 @@
 //! headers in all contexts. Does NOT evaluate application logic; see the
 //! integration tests for pcli/pclientd for that.
 
+use http::StatusCode;
+
 #[ignore]
 #[tokio::test]
 /// Confirm that permissive CORS headers are returned in HTTP responses
@@ -19,5 +21,21 @@ async fn check_cors_headers() -> anyhow::Result<()> {
         r.headers().get("access-control-expose-headers").unwrap(),
         "*"
     );
+    Ok(())
+}
+
+#[ignore]
+#[tokio::test]
+/// Confirm that the a naive GET on the gRPC route returns a 200,
+/// as a sanity check that we haven't badly broken the minifront static asset bundling.
+/// This check does *not* confirm that page works correctly, but it does confirm
+/// it's at least loading, which guards against path regressions in the asset building.
+/// See GH4139 for context.
+async fn check_minifront_http_ok() -> anyhow::Result<()> {
+    let client = reqwest::Client::new();
+    let pd_url =
+        std::env::var("PENUMBRA_NODE_PD_URL").unwrap_or("http://localhost:8080".to_string());
+    let r = client.get(pd_url).send().await?;
+    assert_eq!(r.status(), StatusCode::OK);
     Ok(())
 }
