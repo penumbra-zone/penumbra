ca16ab138a907f5de021861a2c99a641c904e5b4
katelyn martin <katelyn@penumbralabs.xyz>
2024-04-01 09:47:49 -0400

    tests(app): ✨ add integration test coverage for undelegation (#4110)
    this introduces additional test coverage for the staking component,
    examining the mock client's notes during delegation, the unbonding
    period, and after claiming an undelegation.
    
    * #4115
    * #3995

--- crates/core/component/stake/src/component/action_handler/undelegate.rs
+++ crates/core/component/stake/src/component/action_handler/undelegate.rs
@@ -1,4 +1,4 @@
-use anyhow::{ensure, Result};
+use anyhow::Result;
 use async_trait::async_trait;
 use cnidarium::StateWrite;
 use penumbra_sct::component::clock::EpochRead;
@@ -27,12 +27,19 @@ impl ActionHandler for Undelegate {
         // This let us provide a more helpful error message if an epoch boundary was crossed.
         // And it ensures that the unbonding delay is enforced correctly.
         let current_epoch = state.get_current_epoch().await?;
-        ensure!(
-            u.from_epoch == current_epoch,
-            "undelegation was prepared for epoch {} but the current epoch is {}",
-            u.from_epoch.index,
-            current_epoch.index
-        );
+        let prepared_for_current_epoch = u.from_epoch == current_epoch;
+        if !prepared_for_current_epoch {
+            tracing::error!(
+                from = ?u.from_epoch,
+                current = ?current_epoch,
+                "undelegation was prepared for a different epoch",
+            );
+            anyhow::bail!(
+                "undelegation was prepared for epoch {} but the current epoch is {}",
+                u.from_epoch.index,
+                current_epoch.index
+            );
+        }
 
         // For undelegations, we enforce correct computation (with rounding)
         // of the *unbonded amount based on the delegation amount*, because
@@ -56,12 +63,18 @@ impl ActionHandler for Undelegate {
             })?;
         let expected_unbonded_amount = rate_data.unbonded_amount(u.delegation_amount);
 
-        ensure!(
-            u.unbonded_amount == expected_unbonded_amount,
-            "undelegation amount {} does not match expected amount {}",
-            u.unbonded_amount,
-            expected_unbonded_amount,
-        );
+        if u.unbonded_amount != expected_unbonded_amount {
+            tracing::error!(
+                actual = %u.unbonded_amount,
+                expected = %expected_unbonded_amount,
+                "undelegation amount does not match expected amount",
+            );
+            anyhow::bail!(
+                "undelegation amount {} does not match expected amount {}",
+                u.unbonded_amount,
+                expected_unbonded_amount,
+            );
+        }
 
         /* ----- execution ------ */
 
