a7b75fb755da9cb509c5db1bd3a665aeac4f45aa
finch <finch@starlingcyber.net>
2024-04-01 19:23:02 -0400

    Basic nix packaging using Crane (bad caching!)

--- flake.nix
+++ flake.nix
@@ -4,35 +4,70 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    cargo2nix {
+      url = "github:cargo2nix/cargo2nix/release-0.11.0";
+      inputs = {
+          nixpkgs.follows = "nixpkgs";
+          flake-utils.follows = "flake-utils";
+        };
+      }
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
+          overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
-            inherit system;
+            inherit system overlays;
           };
           nativeBuildInputs = with pkgs; [
             pkg-config
           ];
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           buildInputs = with pkgs; [
             clang
             openssl
-            rustup
+            rustToolchain
           ];
+          code = pkgs.callPackage ./. { inherit pkgs crane rustToolchain buildInputs; };
         in
         with pkgs;
-        {
-          devShells.default = mkShell {
+        rec {
+          devShells.default = mkShell rec {
             inherit buildInputs nativeBuildInputs;
             shellHook = ''
               export CC="${pkgs.clang}/bin/clang"
               export CXX="${pkgs.clang}/bin/clang++"
               export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
             '';
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib/libclang.so";
           };
+
+          packages = {
+            pd = code.pd;
+            pcli = code.pcli;
+            pclientd = code.pclientd;
+            all = pkgs.symlinkJoin {
+              name = "all";
+              paths = with code; [ pd pcli pclientd ];
+            };
+          };
+
+          defaultPackage = self.packages.${system}.all;
         }
       );
 }
