f57c3565129edfea3467376d0fc225ecd6ba2306
katelyn martin <katelyn@penumbralabs.xyz>
2024-04-09 16:46:28 -0400

    tests(app): 🏪 outline `TestNodeExt` extension

--- crates/core/app/tests/common/mod.rs
+++ crates/core/app/tests/common/mod.rs
@@ -3,7 +3,9 @@
 // NB: Allow dead code, and unused imports. these are shared and consumed by files in `tests/`.
 #![allow(dead_code, unused_imports)]
 
-pub use self::{temp_storage_ext::TempStorageExt, test_node_builder_ext::BuilderExt};
+pub use self::{
+    temp_storage_ext::TempStorageExt, test_node_builder_ext::BuilderExt, test_node_ext::TestNodeExt,
+};
 
 use {
     async_trait::async_trait,
@@ -24,6 +26,11 @@ mod test_node_builder_ext;
 
 /// Extensions to [`TempStorage`][cnidarium::TempStorage].
 mod temp_storage_ext;
+
+/// Penumbra-specific extensions to the mock consensus test node.
+///
+/// See [`TestNodeExt`].
+mod test_node_ext;
 
 // Installs a tracing subscriber to log events until the returned guard is dropped.
 pub fn set_tracing_subscriber() -> tracing::subscriber::DefaultGuard {
@@ -52,46 +59,3 @@ pub fn set_tracing_subscriber() -> tracing::subscriber::DefaultGuard {
 
 /// A [`TestNode`] coupled with Penumbra's [`Consensus`] service.
 pub type PenumbraTestNode = TestNode<ConsensusService>;
-
-#[async_trait]
-pub trait TestNodeExt: Sized {
-    async fn fast_forward_to_next_epoch(
-        &mut self,
-        storage: &TempStorage,
-    ) -> anyhow::Result<penumbra_sct::epoch::Epoch>;
-}
-
-#[async_trait]
-impl<C> TestNodeExt for TestNode<C>
-where
-    C: tower::Service<
-            tendermint::v0_37::abci::ConsensusRequest,
-            Response = tendermint::v0_37::abci::ConsensusResponse,
-            Error = tower::BoxError,
-        > + Send
-        + Clone
-        + 'static,
-    C::Future: Send + 'static,
-    C::Error: Sized,
-{
-    async fn fast_forward_to_next_epoch(
-        &mut self,
-        storage: &TempStorage,
-    ) -> Result<penumbra_sct::epoch::Epoch, anyhow::Error> {
-        use {penumbra_sct::component::clock::EpochRead, tap::Tap};
-
-        let get_epoch = || async { storage.latest_snapshot().get_current_epoch().await };
-        let start = get_epoch()
-            .await?
-            .tap(|start| tracing::info!(?start, "fast forwarding to next epoch"));
-
-        loop {
-            self.block().execute().await?;
-            let current = get_epoch().await?;
-            if current != start {
-                tracing::debug!(end = ?current, ?start, "reached next epoch");
-                return Ok(current);
-            }
-        }
-    }
-}
