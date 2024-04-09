300e7b4ba7cc22138cb8bdc472aee7c146db7a65
katelyn martin <katelyn@penumbralabs.xyz>
2024-04-09 16:39:06 -0400

    tests(app): 🍱 outline `TempStorageExt` extension

--- crates/core/app/tests/common/mod.rs
+++ crates/core/app/tests/common/mod.rs
@@ -3,7 +3,7 @@
 // NB: Allow dead code, and unused imports. these are shared and consumed by files in `tests/`.
 #![allow(dead_code, unused_imports)]
 
-pub use self::test_node_builder_ext::BuilderExt;
+pub use self::{temp_storage_ext::TempStorageExt, test_node_builder_ext::BuilderExt};
 
 use {
     async_trait::async_trait,
@@ -21,6 +21,9 @@ use {
 ///
 /// See [`BuilderExt`].
 mod test_node_builder_ext;
+
+/// Extensions to [`TempStorage`][cnidarium::TempStorage].
+mod temp_storage_ext;
 
 // Installs a tracing subscriber to log events until the returned guard is dropped.
 pub fn set_tracing_subscriber() -> tracing::subscriber::DefaultGuard {
@@ -49,33 +52,6 @@ pub fn set_tracing_subscriber() -> tracing::subscriber::DefaultGuard {
 
 /// A [`TestNode`] coupled with Penumbra's [`Consensus`] service.
 pub type PenumbraTestNode = TestNode<ConsensusService>;
-
-#[async_trait]
-pub trait TempStorageExt: Sized {
-    async fn apply_genesis(self, genesis: AppState) -> anyhow::Result<Self>;
-    async fn apply_default_genesis(self) -> anyhow::Result<Self>;
-}
-
-#[async_trait]
-impl TempStorageExt for TempStorage {
-    async fn apply_genesis(self, genesis: AppState) -> anyhow::Result<Self> {
-        // Check that we haven't already applied a genesis state:
-        if self.latest_version() != u64::MAX {
-            anyhow::bail!("database already initialized");
-        }
-
-        // Apply the genesis state to the storage
-        let mut app = App::new(self.latest_snapshot()).await?;
-        app.init_chain(&genesis).await;
-        app.commit(self.deref().clone()).await;
-
-        Ok(self)
-    }
-
-    async fn apply_default_genesis(self) -> anyhow::Result<Self> {
-        self.apply_genesis(Default::default()).await
-    }
-}
 
 #[async_trait]
 pub trait TestNodeExt: Sized {
