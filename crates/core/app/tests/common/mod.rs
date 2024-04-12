de66328c70b239e2d51016db769667040871a3f2
katelyn martin <katelyn@penumbralabs.xyz>
2024-04-12 13:23:42 -0400

    tests: 🤝 hoist test subscriber into a standalone library (#4200)
    in #4193, @zbuc is working on some changes to the dex component. in the
    `penumbra-app` crate, we have a helpful utility to define a tracing
    subscriber that correctly interacts with `libtest`'s output capturing,
    and can be configured via `RUST_LOG` when running tests.
    
    this is a tremendously helpful aid in debugging issues during feature
    development, and is not specific to the `penumbra-app` crate.
    
    this commit hoists that test utility into a standalone library in
    `crates/test/`, so that other components like the dex can also create a
    guard to capture traces in tests.
    
    now we can share this, by doing this:
    
    ```rust
     #[tokio::test]
     async fn example_test_case() -> anyhow::Result<()> {
         // Install a test logger...
         let guard = set_tracing_subscriber();
    
         // Test logic here...
    
         drop(guard);
         Ok(())
     }
    ```
    
    #### checklist before requesting a review
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
      > only changes test code

--- crates/core/app/tests/common/mod.rs
+++ crates/core/app/tests/common/mod.rs
@@ -2,9 +2,12 @@
 
 // NB: these reëxports are shared and consumed by files in `tests/`.
 #[allow(unused_imports)]
-pub use self::{
-    temp_storage_ext::TempStorageExt, test_node_builder_ext::BuilderExt,
-    test_node_ext::TestNodeExt, tracing_subscriber::set_tracing_subscriber,
+pub use {
+    self::{
+        temp_storage_ext::TempStorageExt, test_node_builder_ext::BuilderExt,
+        test_node_ext::TestNodeExt,
+    },
+    penumbra_test_subscriber::set_tracing_subscriber,
 };
 
 /// Penumbra-specific extensions to the mock consensus builder.
@@ -19,9 +22,3 @@ mod temp_storage_ext;
 ///
 /// See [`TestNodeExt`].
 mod test_node_ext;
-
-/// A pretty [`tracing`] subscriber for use in test cases.
-///
-/// NB: this subscriber makes use of a test writer, that is compatible with `cargo test`'s output
-/// capturing.
-mod tracing_subscriber;
