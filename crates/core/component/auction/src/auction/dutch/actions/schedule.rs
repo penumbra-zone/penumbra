04f63d2624544319c37846bbeb479d324cdcc648
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-15 15:47:29 -0400

    auction(dutch): impl pb conversion for schedule

--- crates/core/component/auction/src/auction/dutch/actions/schedule.rs
+++ crates/core/component/auction/src/auction/dutch/actions/schedule.rs
@@ -1,4 +1,5 @@
 use crate::auction::dutch::DutchAuctionDescription;
+use anyhow::anyhow;
 use penumbra_proto::{core::component::auction::v1alpha1 as pb, DomainType};
 use serde::{Deserialize, Serialize};
 
@@ -9,4 +10,32 @@ use serde::{Deserialize, Serialize};
 )]
 pub struct ActionDutchAuctionSchedule {
     pub description: DutchAuctionDescription,
+}
+
+/* Protobuf impls */
+impl DomainType for ActionDutchAuctionSchedule {
+    type Proto = pb::ActionDutchAuctionSchedule;
+}
+
+impl From<ActionDutchAuctionSchedule> for pb::ActionDutchAuctionSchedule {
+    fn from(domain: ActionDutchAuctionSchedule) -> Self {
+        pb::ActionDutchAuctionSchedule {
+            description: Some(domain.description.into()),
+        }
+    }
+}
+
+impl TryFrom<pb::ActionDutchAuctionSchedule> for ActionDutchAuctionSchedule {
+    type Error = anyhow::Error;
+
+    fn try_from(msg: pb::ActionDutchAuctionSchedule) -> Result<Self, Self::Error> {
+        Ok(ActionDutchAuctionSchedule {
+            description: msg
+                .description
+                .ok_or_else(|| {
+                    anyhow!("ActionDutchAuctionSchedule message is missing a description")
+                })?
+                .try_into()?,
+        })
+    }
 }
