f3b8ba7840a89aecbd54b08153005d0956062a23
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-16 08:08:42 -0400

    auction(nft): augment `AuctionNft` with denom metadata

--- crates/core/component/auction/src/auction/nft.rs
+++ crates/core/component/auction/src/auction/nft.rs
@@ -1,5 +1,6 @@
 use crate::auction::id::AuctionId;
 use anyhow::{anyhow, Result};
+use penumbra_asset::asset::{self};
 use penumbra_proto::{core::component::auction::v1alpha1 as pb, DomainType};
 
 /// An non-fungible token (NFT) tracking the state and ownership of an auction.
@@ -10,6 +11,17 @@ pub struct AuctionNft {
     /// The state of an auction, its specific semantics depend on the
     /// type of auction the NFT resolves to.
     pub seq: u64,
+    /// The metadata corresponding to the Nft's denomination.
+    pub metadata: asset::Metadata,
+}
+
+impl AuctionNft {
+    pub fn new(id: AuctionId, seq: u64) -> AuctionNft {
+        let metadata = asset::REGISTRY
+            .parse_denom(&format!("auctionnft_{seq}_{id}"))
+            .expect("auction nft denom is valid");
+        AuctionNft { id, seq, metadata }
+    }
 }
 
 /* Protobuf impls ;*/
@@ -30,12 +42,11 @@ impl TryFrom<pb::AuctionNft> for AuctionNft {
     type Error = anyhow::Error;
 
     fn try_from(msg: pb::AuctionNft) -> Result<Self, Self::Error> {
-        Ok(Self {
-            id: msg
-                .id
-                .ok_or_else(|| anyhow!("AuctionNft message is missing an auction id"))?
-                .try_into()?,
-            seq: msg.seq,
-        })
+        let id: AuctionId = msg
+            .id
+            .ok_or_else(|| anyhow!("AuctionNft message is missing an auction id"))?
+            .try_into()?;
+        let seq = msg.seq;
+        Ok(AuctionNft::new(id, seq))
     }
 }
