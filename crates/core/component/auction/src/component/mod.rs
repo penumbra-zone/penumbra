85ee4a9340500239bbdda77e53a75b16f3d539b8
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-17 09:25:52 -0400

    auction(component): define dutch auction manager mod

--- crates/core/component/auction/src/component/mod.rs
+++ crates/core/component/auction/src/component/mod.rs
@@ -1,8 +1,9 @@
 pub mod action_handler;
 mod auction;
+mod auction_store;
+mod dutch_auction;
 pub mod metrics;
 pub mod rpc;
 
-pub use self::auction::{StateReadExt, StateWriteExt};
-mod auction_store;
+pub use auction::{StateReadExt, StateWriteExt};
 pub(crate) use auction_store::AuctionStoreRead;
