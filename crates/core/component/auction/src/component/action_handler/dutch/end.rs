1d7ced445a61f986a15c6567cd9f439fb8de6a65
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-17 09:25:51 -0400

    auction(ah): add validation rules for end DA

--- crates/core/component/auction/src/component/action_handler/dutch/end.rs
+++ crates/core/component/auction/src/component/action_handler/dutch/end.rs
@@ -1,9 +1,11 @@
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use async_trait::async_trait;
 use cnidarium::StateWrite;
 use cnidarium_component::ActionHandler;
 
-use crate::auction::dutch::actions::ActionDutchAuctionEnd;
+use crate::auction::dutch::ActionDutchAuctionEnd;
+use crate::component::AuctionStoreRead;
+use anyhow::{bail, Context};
 
 #[async_trait]
 impl ActionHandler for ActionDutchAuctionEnd {
@@ -12,7 +14,24 @@ impl ActionHandler for ActionDutchAuctionEnd {
         Ok(())
     }
 
-    async fn check_and_execute<S: StateWrite>(&self, mut _state: S) -> Result<()> {
+    async fn check_and_execute<S: StateWrite>(&self, state: S) -> Result<()> {
+        let auction_id = self.auction_id;
+
+        let auction_state = state
+            .get_dutch_auction_by_id(auction_id)
+            .await
+            .context("the auction associated with this id is not a dutch auction")?;
+
+        let Some(auction) = auction_state else {
+            bail!("no auction found for id {auction_id}")
+        };
+
+        // Check that the sequence number for the auction state is 0 (opened) or 1 (closed).
+        ensure!(
+            auction.state.sequence <= 1,
+            "auction MUST have a sequence number set to opened (0) or closed (1) (got: {})",
+            auction.state.sequence
+        );
         Ok(())
     }
 }
