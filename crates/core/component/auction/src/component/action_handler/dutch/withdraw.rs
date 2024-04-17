51aeb7e3a8b3b4787f6b9d580de0c6ec02dd0a54
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-17 09:25:52 -0400

    auction(ah): sequence continuity check in withdraw DA

--- crates/core/component/auction/src/component/action_handler/dutch/withdraw.rs
+++ crates/core/component/auction/src/component/action_handler/dutch/withdraw.rs
@@ -18,12 +18,18 @@ impl ActionHandler for ActionDutchAuctionWithdraw {
             self.seq
         );
 
+        ensure!(
+            self.seq < u64::MAX,
+            "the sequence number maximum is `u64::MAX`"
+        );
+
         Ok(())
     }
 
     async fn check_and_execute<S: StateWrite>(&self, state: S) -> Result<()> {
         let auction_id = self.auction_id;
 
+        // Check that the auction exists and is a Dutch auction.
         let auction_state = state
             .get_dutch_auction_by_id(auction_id)
             .await
@@ -33,6 +39,15 @@ impl ActionHandler for ActionDutchAuctionWithdraw {
             bail!("no auction found for id {auction_id}")
         };
 
+        // Check that sequence number is incremented by one.
+        ensure!(
+            self.seq == auction_state.state.sequence.saturating_add(1),
+            "the action sequence number MUST be incremented by one (previous: {}, action: {})",
+            self.seq,
+            auction_state.state.sequence
+        );
+
+        // Check that the reported balance commitment, match the recorded reserves.
         let auction_input_reserves = Value {
             amount: auction_state.state.input_reserves,
             asset_id: auction_state.description.input.asset_id,
