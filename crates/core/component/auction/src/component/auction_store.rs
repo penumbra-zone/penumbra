61c591a75ba1566f0e6962e838ed053ef5c4087f
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-17 09:25:51 -0400

    auction: define `AuctionStoreRead`

---
+++ crates/core/component/auction/src/component/auction_store.rs
@@ -0,0 +1,62 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use cnidarium::{StateRead, StateWrite};
+use cnidarium_component::Component;
+use penumbra_proto::core::component::auction::v1alpha1 as pb;
+use penumbra_proto::DomainType;
+use penumbra_proto::Name;
+use penumbra_proto::StateReadProto;
+use prost_types::Any;
+use std::sync::Arc;
+use tendermint::v0_37::abci;
+use tracing::instrument;
+
+use crate::{
+    auction::{dutch::DutchAuction, id::AuctionId},
+    state_key,
+};
+
+/// Extension trait providing read access to auction data.
+#[async_trait]
+pub trait AuctionStoreRead: StateRead {
+    /// Returns whether the supplied `auction_id` exists in the chain state.
+    async fn auction_id_exists(&self, auction_id: AuctionId) -> bool {
+        self.get_raw_auction(auction_id).await.is_some()
+    }
+
+    /// Fetch a [`DutchAuction`] from storage, returning `None` if none
+    /// were found with the provided identifier.
+    ///
+    /// # Errors
+    /// This method returns an error if the auction state associated with the
+    /// specified `auction_id` is *not* of type `DutchAuction`.
+    async fn get_dutch_auction_by_id(&self, auction_id: AuctionId) -> Result<Option<DutchAuction>> {
+        let Some(any_auction) = self.get_raw_auction(auction_id).await else {
+            return Ok(None);
+        };
+
+        let dutch_auction_type_str = pb::DutchAuction::full_name();
+
+        anyhow::ensure!(
+            any_auction.type_url == dutch_auction_type_str,
+            "error deserializing auction state, expected type to be {}, but got: {}",
+            dutch_auction_type_str,
+            any_auction.type_url
+        );
+
+        Ok(Some(DutchAuction::decode(any_auction.value.as_ref())?))
+    }
+
+    /// Returns raw auction data if found under the specified `auction_id`,
+    /// and `None` otherwise
+    async fn get_raw_auction(&self, auction_id: AuctionId) -> Option<Any> {
+        self.get_proto(&state_key::auction_store::by_id(auction_id))
+            .await
+            .expect("no storage errors")
+    }
+}
+
+impl<T: StateRead + ?Sized> AuctionStoreRead for T {}
+
+#[cfg(tests)]
+mod tests {}
