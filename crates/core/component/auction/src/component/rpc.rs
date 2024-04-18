de5d57d663f0af6b11e4c3ec1a9d48ea5c140e41
Tal Derei <talderei99@gmail.com>
2024-04-17 18:49:33 -0700

    service request / response

--- crates/core/component/auction/src/component/rpc.rs
+++ crates/core/component/auction/src/component/rpc.rs
@@ -3,18 +3,20 @@
 use penumbra_proto::{
     core::component::auction::v1alpha1::{
         query_service_server::QueryService, AuctionStateByIdRequest, AuctionStateByIdResponse,
+        DutchAuctionState,
     },
     DomainType,
 };
 
 use async_stream::try_stream;
-use cnidarium::Storage;
 use futures::{StreamExt, TryStreamExt};
+use penumbra_proto::Message;
 use std::pin::Pin;
 use tonic::Status;
 use tracing::instrument;
 
 use super::AuctionStoreRead;
+use cnidarium::Storage;
 
 pub struct Server {
     storage: Storage,
@@ -34,15 +36,33 @@ impl QueryService for Server {
         request: tonic::Request<AuctionStateByIdRequest>,
     ) -> Result<tonic::Response<AuctionStateByIdResponse>, Status> {
         let state = self.storage.latest_snapshot();
+        let request = request.into_inner();
 
-        let auction_data = state.get_raw_auction().await.map_err(|e| {
-            tonic::Status::unknown(format!(
-                "could not get stateful auction data for specified auction id: {e}"
-            ))
-        })?;
+        let id = request
+            .id
+            .ok_or_else(|| Status::invalid_argument("missing auction id"))?
+            .try_into()
+            .map_err(|_| Status::invalid_argument("invalid auction id"))?;
+
+        let auction_data = state
+            .get_raw_auction(id)
+            .await
+            .ok_or_else(|| tonic::Status::not_found("auction data not found for specified id"))?;
+
+        if auction_data.type_url
+            != format!("penumbra.core.component.auction.v1alpha1.DutchAuctionState")
+        {
+            return Err(Status::invalid_argument(
+                "Auction data type does not contain a `DutchAuctionState`",
+            ));
+        }
+
+        // Attempt to deserialize value into a `DutchAuctionState` message.
+        let auction_state_proto = DutchAuctionState::decode(auction_data.value.as_ref())
+            .map_err(|_| Status::invalid_argument("Failed to decode DutchAuctionState"))?;
 
         Ok(tonic::Response::new(AuctionStateByIdResponse {
-            auction: todo!(),
+            auction: Some(auction_state_proto),
             positions: todo!(),
         }))
     }
