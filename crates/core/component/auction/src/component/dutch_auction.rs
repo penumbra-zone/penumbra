730ac7635b51f25eead5741c5c5c20c551e659f9
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-22 12:56:39 -0400

    auction: impl `execute_dutch_auction`

--- crates/core/component/auction/src/component/dutch_auction.rs
+++ crates/core/component/auction/src/component/dutch_auction.rs
@@ -1,13 +1,22 @@
+use std::pin::Pin;
+
 use crate::auction::dutch::{DutchAuction, DutchAuctionDescription, DutchAuctionState};
 use crate::auction::AuctionId;
+use crate::component::trigger_data::TriggerData;
 use crate::component::AuctionStoreRead;
 use crate::state_key;
 use anyhow::Result;
 use async_trait::async_trait;
-use cnidarium::StateWrite;
+use cnidarium::{StateRead, StateWrite};
+use futures::StreamExt;
 use penumbra_asset::{Balance, Value};
+use penumbra_dex::component::{PositionManager, PositionRead};
+use penumbra_dex::lp::position::Position;
+use penumbra_dex::lp::Reserves;
+use penumbra_dex::DirectedTradingPair;
 use penumbra_num::Amount;
 use penumbra_proto::core::component::auction::v1alpha1 as pb;
+use penumbra_proto::StateWriteProto;
 use penumbra_sct::component::clock::EpochRead;
 use prost::{Message, Name};
 
@@ -28,17 +37,21 @@ pub(crate) trait DutchAuctionManager: StateWrite {
             nonce: _,
         } = description;
 
-        let next_trigger = compute_next_trigger(TriggerData {
+        let auction_trigger = TriggerData {
             start_height,
             end_height,
             step_count,
-            current_height: self
-                .get_block_height()
-                .await
-                .expect("block height is not missing"),
-        })
-        .expect("infaillible because of action validation")
-        .expect("action validation guarantees the auction is not expired");
+        };
+
+        let current_height = self
+            .get_block_height()
+            .await
+            .expect("block height is not missing");
+
+        let next_trigger = auction_trigger
+            .compute_next_trigger_height(current_height)
+            .expect("infaillible because of action validation")
+            .expect("action validation guarantees the auction is not expired");
 
         let state = DutchAuctionState {
             sequence: 0,
@@ -51,9 +64,150 @@ pub(crate) trait DutchAuctionManager: StateWrite {
         let dutch_auction = DutchAuction { description, state };
 
         // Set the triggger
-        self.set_trigger_for_id(auction_id, next_trigger);
+        self.set_trigger_for_dutch_id(auction_id, next_trigger);
         // Write position to state
         self.write_dutch_auction_state(dutch_auction);
+    }
+
+    /// Execute the [`DutchAuction`] associated with [`AuctionId`], ticking its
+    /// internal state using its immutable description.
+    ///
+    /// For a given auction, this translates into withdrawing a PCL liquidity position,
+    /// credit and zero-out its reserves, and finally, examine the auction's termination
+    /// condition.
+    async fn execute_dutch_auction(
+        &mut self,
+        auction_id: AuctionId,
+        trigger_height: u64,
+    ) -> Result<()> {
+        let old_dutch_auction = self
+            .get_dutch_auction_by_id(auction_id)
+            .await
+            .expect("no deserialization errors")
+            .expect("the auction exists");
+
+        let DutchAuctionDescription {
+            input,
+            output_id,
+            max_output: _,
+            min_output: _,
+            start_height,
+            end_height,
+            step_count,
+            nonce: _,
+        } = old_dutch_auction.description;
+
+        let DutchAuctionState {
+            sequence: _,
+            current_position,
+            next_trigger: _,
+            input_reserves: _,
+            output_reserves: _,
+        } = old_dutch_auction.state;
+
+        let auction_input_id = input.asset_id;
+        let auction_output_id = output_id;
+
+        let auction_trigger = TriggerData {
+            start_height,
+            end_height,
+            step_count,
+        };
+
+        // Recover the LP's balances, if it exists.
+        let lp_reserves = if let Some(auction_lp_id) = current_position {
+            self.close_position_by_id(&auction_lp_id)
+                .await
+                .map_err(|e| {
+                    tracing::error!(
+                        ?e,
+                        ?auction_lp_id,
+                        ?auction_id,
+                        "failed to close dutch auction LP"
+                    )
+                })
+                .expect("position should exist and be opened or closed");
+            self.withdraw_position(auction_lp_id, 0u64)
+                .await
+                .map_err(|e| {
+                    tracing::error!(
+                        ?e,
+                        ?auction_lp_id,
+                        ?auction_id,
+                        "failed to close dutch auction LP"
+                    )
+                })
+                .expect("no state incoherence")
+        } else {
+            Balance::zero()
+        };
+
+        // We remove the execution trigger that we are currently processing:
+        self.unset_trigger_for_dutch_id(auction_id, trigger_height);
+
+        // Prepare a new auction, based on the previous one.
+        let mut new_dutch_auction = DutchAuction {
+            description: old_dutch_auction.description,
+            state: old_dutch_auction.state,
+        };
+
+        let balance_input_asset = lp_reserves
+            .provided()
+            .filter(|v| v.asset_id == input.asset_id)
+            .map(|v| v.amount)
+            .sum::<Amount>();
+        let balance_output_asset = lp_reserves
+            .provided()
+            .filter(|v| v.asset_id == output_id)
+            .map(|v| v.amount)
+            .sum::<Amount>();
+
+        new_dutch_auction.state.current_position = None;
+        // We overwrite the input amount
+        new_dutch_auction.state.input_reserves = balance_input_asset;
+        new_dutch_auction.state.output_reserves += balance_output_asset;
+        new_dutch_auction.state.next_trigger = 0;
+
+        // Compute the current step index, between 0 and `step_count`.
+        let step_index = auction_trigger
+            .compute_step_index(trigger_height)
+            .expect("trigger data is validated");
+
+        if step_index >= step_count {
+            // If the termination condition has been reached, we set the auction
+            // sequence to 1 (Closed).
+            new_dutch_auction.state.sequence = 1;
+        } else {
+            // Otherwise, we compute the next trigger height and generate a liquidity
+            // position for the new auction round.
+            let next_trigger = auction_trigger
+                .compute_next_trigger_height(trigger_height)
+                .expect("trigger data is validated")
+                .expect("the step count has not been reached");
+
+            // We compute the price parameters for the LP:
+            let (p, q) = compute_pq_at_step(&new_dutch_auction.description, step_index);
+            let pair = DirectedTradingPair::new(auction_input_id, auction_output_id);
+            let nonce = new_dutch_auction.description.nonce.clone();
+            let lp_reserves = Reserves {
+                r1: new_dutch_auction.state.input_reserves,
+                r2: Amount::zero(),
+            };
+            // Finally, we create a new position and set it to be auto-closing.
+            let mut lp = Position::new_with_nonce(nonce, pair, 0u32, p, q, lp_reserves);
+            lp.close_on_fill = true;
+            let id = lp.id();
+            new_dutch_auction.state.current_position = Some(id);
+
+            // We open the position, and, on success, record an execution trigger:
+            let _ = self.open_position(lp).await?;
+            self.set_trigger_for_dutch_id(auction_id, next_trigger);
+        };
+
+        // We write-back the updated auction state, and complete execution.
+        self.write_dutch_auction_state(new_dutch_auction);
+
+        Ok(())
     }
 
     /// Terminate the Dutch auction associated with the specified [`AuctionId`].
