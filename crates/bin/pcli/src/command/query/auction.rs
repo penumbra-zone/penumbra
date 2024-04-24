161d02f35c77745b01530c07ef917dc107e2329d
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-24 13:48:20 -0400

    pcli: exercise rpc to query DA seq

--- crates/bin/pcli/src/command/query/auction.rs
+++ crates/bin/pcli/src/command/query/auction.rs
@@ -1,11 +1,16 @@
 use crate::App;
 use anyhow::Context;
 use clap::Subcommand;
+use comfy_table::{presets, Table};
 use penumbra_asset::{asset, Value};
+use penumbra_auction::auction::dutch::DutchAuction;
 use penumbra_auction::auction::AuctionId;
 use penumbra_keys::keys::AddressIndex;
 use penumbra_num::Amount;
+use penumbra_proto::core::component::auction::v1alpha1::query_service_client::QueryServiceClient;
+use penumbra_proto::core::component::auction::v1alpha1::AuctionStateByIdRequest;
 use penumbra_proto::view::v1::GasPricesRequest;
+use penumbra_proto::Name;
 use penumbra_view::ViewClient;
 use penumbra_wallet::plan::Planner;
 use rand_core::OsRng;
@@ -20,31 +25,49 @@ pub enum AuctionCmd {
 /// Commands related to querying Dutch auctions.
 #[derive(Debug, Subcommand)]
 pub enum DutchCmd {
-    /// Withdraws the reserves of the Dutch auction.
-    #[clap(display_order = 100, name = "id")]
-    DutchAuctionQueryId {
-        /// The auction to withdraw funds from.
+    #[clap(display_order = 100, name = "state")]
+    State {
         #[clap(long, display_order = 100)]
-        auction_id: String,
+        auction_id: AuctionId,
     },
 }
 
 impl DutchCmd {
-    /// Process the command by performing the appropriate action.
     pub async fn exec(&self, app: &mut App) -> anyhow::Result<()> {
         match self {
-            DutchCmd::DutchAuctionQueryId { auction_id } => {
-                let auction_id = auction_id.parse::<AuctionId>()?;
+            DutchCmd::State { auction_id } => {
+                let auction_id = auction_id.clone();
+                let mut client = QueryServiceClient::new(app.pd_channel().await?);
+                let rsp = client
+                    .auction_state_by_id(AuctionStateByIdRequest {
+                        id: Some(auction_id.into()),
+                    })
+                    .await?
+                    .into_inner();
 
-                // Query stateful auction information from view server
-                let client = app.view().auctions_by_id(auction_id).await?;
+                let pb_auction_state = rsp
+                    .auction
+                    .ok_or_else(|| anyhow::anyhow!("auction state is missing!"))?;
+                use penumbra_proto::core::component::auction::v1alpha1 as pb_auction;
+                use penumbra_proto::DomainType;
 
-                let dutch_auction = &client[0];
+                if pb_auction_state.type_url == pb_auction::DutchAuction::type_url() {
+                    let auction_state = DutchAuction::decode(pb_auction_state.value)?;
+                    println!("dutch auction with id {auction_id:?}");
 
-                println!(
-                    "Dutch auction description and state: {:?} and {:?}",
-                    dutch_auction.description, dutch_auction.state
-                );
+                    let mut table = Table::new();
+                    table.load_preset(presets::NOTHING);
+                    table
+                        .set_header(vec!["", ""])
+                        .add_row(vec!["Auction Id", &auction_id.to_string()])
+                        .add_row(vec![
+                            "State",
+                            &format!("Opened ({})", auction_state.state.sequence),
+                        ]); // TODO: render state string
+                    println!("{table}");
+                } else {
+                    unimplemented!("only supporting dutch auctions at the moment, come back later");
+                }
             }
         }
         Ok(())
