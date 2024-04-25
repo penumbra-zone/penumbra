e06e2588b6eabce1333c92152dd31ba77df8f4c9
Erwan Or <erwanor@penumbralabs.xyz>
2024-04-25 11:10:24 -0400

    pcli: add `AuctionCmd`

---
+++ crates/bin/pcli/src/command/view/auction.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use penumbra_keys::FullViewingKey;
+use penumbra_view::ViewClient;
+
+#[derive(Debug, clap::Args)]
+pub struct AuctionCmd {
+    #[clap(long)]
+    /// If set, includes the inactive auctions as well.
+    pub include_inactive: bool,
+}
+
+impl AuctionCmd {
+    pub fn offline(&self) -> bool {
+        false
+    }
+
+    pub async fn exec(
+        &self,
+        view_client: &mut impl ViewClient,
+        _fvk: &FullViewingKey,
+    ) -> Result<()> {
+        let auctions = view_client.auctions().await?;
+
+        auctions.iter().for_each(|(id, snr)|{
+            println!("{id:?} {}",snr.note.amount());
+        });
+        todo!()
+    }
+}
