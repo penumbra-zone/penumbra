6a0d4a7ebd24c7eda8dcb5d50c133d564358a08f
finch <finch@starlingcyber.net>
2024-04-27 04:04:34 -0400

    Add new `pcli query validator uptime` command

--- crates/bin/pcli/src/command/query/validator.rs
+++ crates/bin/pcli/src/command/query/validator.rs
@@ -1,16 +1,21 @@
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, ops::RangeInclusive, time::Duration};
 
 use anyhow::{Context, Result};
 use colored::Colorize;
 use comfy_table::{presets, Table};
 use futures::TryStreamExt;
+use penumbra_app::params::AppParameters;
 use penumbra_num::Amount;
+use penumbra_proto::core::app::v1::{
+    query_service_client::QueryServiceClient as AppQueryServiceClient, AppParametersRequest,
+};
 use penumbra_proto::core::component::stake::v1::{
     query_service_client::QueryServiceClient as StakeQueryServiceClient, ValidatorInfoRequest,
+    ValidatorStatusRequest, ValidatorUptimeRequest,
 };
 use penumbra_stake::{
     validator::{self, ValidatorToml},
-    IdentityKey,
+    IdentityKey, Uptime,
 };
 
 use crate::App;
@@ -32,6 +37,11 @@ pub enum ValidatorCmd {
         /// The JSON file to write the definition to [default: stdout].
         #[clap(long)]
         file: Option<String>,
+        /// The identity key of the validator to fetch.
+        identity_key: String,
+    },
+    /// Get the uptime of the validator.
+    Uptime {
         /// The identity key of the validator to fetch.
         identity_key: String,
     },
@@ -208,6 +218,110 @@ impl ValidatorCmd {
                 } else {
                     println!("{}", toml::to_string_pretty(&validator)?);
                 }
+            }
+            ValidatorCmd::Uptime { identity_key } => {
+                let identity_key = identity_key.parse::<IdentityKey>()?;
+
+                let mut client = StakeQueryServiceClient::new(app.pd_channel().await?);
+
+                // What's the uptime?
+                let uptime: Uptime = client
+                    .validator_uptime(ValidatorUptimeRequest {
+                        identity_key: Some(identity_key.into()),
+                    })
+                    .await?
+                    .into_inner()
+                    .uptime
+                    .ok_or_else(|| anyhow::anyhow!("uptime must be present in response"))?
+                    .try_into()?;
+
+                // Is the validator active?
+                let status: validator::Status = client
+                    .validator_status(ValidatorStatusRequest {
+                        identity_key: Some(identity_key.into()),
+                    })
+                    .await?
+                    .into_inner()
+                    .status
+                    .ok_or_else(|| anyhow::anyhow!("status must be present in response"))?
+                    .try_into()?;
+                let state = status.state;
+                let active = matches!(state, validator::State::Active);
+
+                // Get the chain parameters
+                let mut client = AppQueryServiceClient::new(app.pd_channel().await?);
+                let params: AppParameters = client
+                    .app_parameters(tonic::Request::new(AppParametersRequest {}))
+                    .await?
+                    .into_inner()
+                    .app_parameters
+                    .ok_or_else(|| anyhow::anyhow!("empty AppParametersResponse message"))?
+                    .try_into()?;
+
+                let as_of_height = uptime.as_of_height();
+                let missed_blocks = uptime.num_missed_blocks();
+                let window_len = uptime.missed_blocks_window();
+
+                let mut downtime_ranges: Vec<RangeInclusive<u64>> = Vec::new();
+                for missed_block in uptime.missed_blocks() {
+                    if let Some(range) = downtime_ranges.last_mut() {
+                        if range.end() + 1 == missed_block {
+                            *range = *range.start()..=missed_block;
+                        } else {
+                            downtime_ranges.push(missed_block..=missed_block);
+                        }
+                    } else {
+                        downtime_ranges.push(missed_block..=missed_block);
+                    }
+                }
+
+                let percent_uptime =
+                    100.0 * (window_len as f64 - missed_blocks as f64) / window_len as f64;
+                let min_uptime_blocks =
+                    window_len as u64 - params.stake_params.missed_blocks_maximum;
+                let percent_min_uptime = 100.0 * min_uptime_blocks as f64 / window_len as f64;
+                let percent_downtime = 100.0 * missed_blocks as f64 / window_len as f64;
+                let percent_downtime_penalty =
+                    // Converting from basis points squared to percentage
+                    100.0 * params.stake_params.slashing_penalty_downtime as f64 * 100.0;
+                let min_remaining_downtime_blocks = (window_len as u64)
+                    .saturating_sub(missed_blocks as u64)
+                    .saturating_sub(min_uptime_blocks);
+                let min_remaining_downtime = humantime::Duration::from(Duration::from_secs(
+                    (min_remaining_downtime_blocks * 5) as u64,
+                ));
+
+                println!("Current uptime: {percent_uptime}% as of height {as_of_height}");
+                let state_note = if active {
+                    " (delegated funds are at stake)"
+                } else {
+                    ""
+                };
+                println!("Current status: {state}{state_note}");
+                if active {
+                    println!("Minimum required uptime: {min_uptime_blocks}/{window_len} blocks ({percent_min_uptime}%)");
+                }
+                if !downtime_ranges.is_empty() {
+                    let s = if missed_blocks == 1 { "" } else { "s" };
+                    println!(
+                        "Validator missed signing {missed_blocks}/{window_len} block{s} ({percent_downtime}%):"
+                    );
+                }
+                for range in downtime_ranges.iter() {
+                    let blocks = range.end() - range.start() + 1;
+                    let s = if blocks == 1 { "" } else { "s" };
+                    println!("  • {range:?} ({blocks} block{s})");
+                }
+                if active {
+                    println!(
+                        "Downtime penalty: {percent_downtime_penalty}% penalty will be applied if uptime falls below minimum"
+                    );
+                    if !downtime_ranges.is_empty() {
+                        println!(
+                            "Minimum remaining consecutive downtime before penalty: {min_remaining_downtime_blocks} blocks (approximately {min_remaining_downtime})"
+                        );
+                    }
+                };
             }
         }
 
