62b9d01d06ffddb68dabfefe27818961f56ed846
Erwan Or <erwanor@penumbralabs.xyz>
2024-05-18 17:41:38 -0700

    penumbra-test(subscriber): allow specifying custom `EnvFilter`s

--- crates/test/tracing-subscriber/src/lib.rs
+++ crates/test/tracing-subscriber/src/lib.rs
@@ -15,14 +15,21 @@ pub fn set_tracing_subscriber() -> DefaultGuard {
     let filter = "info,penumbra_app=trace,penumbra_mock_consensus=trace";
     let filter = EnvFilter::try_from_default_env()
         .or_else(|_| EnvFilter::try_new(filter))
-        .expect("should have a valid filter directive")
-        // Without explicitly disabling the `r1cs` target, the ZK proof implementations
-        // will spend an enormous amount of CPU and memory building useless tracing output.
-        .add_directive(
-            "r1cs=off"
-                .parse()
-                .expect("rics=off is a valid filter directive"),
-        );
+        .expect("should have a valid filter directive");
+    set_tracing_subscriber_with_env_filter(filter)
+}
+
+/// Install a tracing subscriber with a custom filter.
+//  NB: this is marked as "dead code" but it is used by integration tests.
+#[allow(dead_code)]
+pub fn set_tracing_subscriber_with_env_filter(filter: EnvFilter) -> DefaultGuard {
+    // Without explicitly disabling the `r1cs` target, the ZK proof implementations
+    // will spend an enormous amount of CPU and memory building useless tracing output.
+    let filter = filter.add_directive(
+        "r1cs=off"
+            .parse()
+            .expect("rics=off is a valid filter directive"),
+    );
 
     let subscriber = fmt()
         .with_env_filter(filter)
