c94ef22d35cd209f4db864db2ed80882abfc4a0d
Chris Czub <chris@penumbralabs.xyz>
2024-05-21 16:44:15 -0400

    Return streaming total supply

--- crates/core/component/shielded-pool/src/component/rpc.rs
+++ crates/core/component/shielded-pool/src/component/rpc.rs
@@ -1,14 +1,25 @@
-use std::pin::Pin;
+use std::{collections::BTreeMap, pin::Pin};
 
+use anyhow::Context as _;
+use async_stream::try_stream;
 use cnidarium::Storage;
-use penumbra_asset::asset;
-use penumbra_proto::core::component::shielded_pool::v1::{
-    query_service_server::QueryService, AssetMetadataByIdRequest, AssetMetadataByIdResponse,
-    AssetMetadataByIdsRequest, AssetMetadataByIdsResponse, TotalSupplyRequest, TotalSupplyResponse,
+use futures::{StreamExt as _, TryStreamExt as _};
+use penumbra_asset::asset::{self, Metadata};
+use penumbra_ibc::component::state_key as ibc_state_key;
+use penumbra_num::Amount;
+use penumbra_proto::{
+    core::component::shielded_pool::v1::{
+        query_service_server::QueryService, AssetMetadataByIdRequest, AssetMetadataByIdResponse,
+        AssetMetadataByIdsRequest, AssetMetadataByIdsResponse, TotalSupplyRequest,
+        TotalSupplyResponse,
+    },
+    StateReadProto as _,
 };
 
 use tonic::Status;
 use tracing::instrument;
+
+use crate::state_key;
 
 use super::AssetRegistryRead;
 
@@ -33,12 +44,109 @@ impl QueryService for Server {
     type TotalSupplyStream =
         Pin<Box<dyn futures::Stream<Item = Result<TotalSupplyResponse, tonic::Status>> + Send>>;
 
-    #[instrument(skip(self, request))]
+    /// Returns the total supply for all IBC assets.
+    /// Internally-minted assets (Penumbra tokens, LP tokens, delegation tokens, etc.)
+    /// are also included but the supplies are hardcoded at 0 for now.
+    ///
+    /// TODO: Implement a way to fetch the total supply for these assets.
+    #[instrument(skip(self, _request))]
     async fn total_supply(
         &self,
-        request: tonic::Request<TotalSupplyRequest>,
+        _request: tonic::Request<TotalSupplyRequest>,
     ) -> Result<tonic::Response<Self::TotalSupplyStream>, tonic::Status> {
-        todo!("need way to fetch total asset supply from shielded pool")
+        let snapshot = self.storage.latest_snapshot();
+
+        // Find every non-IBC known asset
+        let s = snapshot.prefix(state_key::denom_metadata_by_asset::prefix());
+        let mut total_supply = s
+            .filter_map(move |i: anyhow::Result<(String, Metadata)>| async move {
+                if i.is_err() {
+                    return Some(Err(i.context("bad denom in state").err().unwrap()));
+                }
+                let (_key, denom_metadata) = i.expect("should not be an error");
+
+                // Return a hardcoded 0 supply for now
+                Some(Ok((denom_metadata, Amount::from(0u32))))
+            })
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .collect::<anyhow::Result<Vec<_>>>()
+            .map_err(|e| tonic::Status::internal(e.to_string()))?
+            .into_iter()
+            .collect::<BTreeMap<_, _>>();
+
+        let s = snapshot.prefix(ibc_state_key::ics20_value_balance::prefix());
+        let ibc_amounts = s
+            .filter_map(move |i: anyhow::Result<(String, Amount)>| async move {
+                if i.is_err() {
+                    return Some(Err(i.context("bad amount in state").err().unwrap()));
+                }
+                let (key, amount) = i.expect("should not be an error");
+
+                // Extract the asset ID from the key
+                let asset_id = key.split('/').last();
+                if asset_id.is_none() {
+                    return Some(Err(asset_id
+                        .context("bad IBC ics20 value balance key in state")
+                        .err()
+                        .unwrap()));
+                }
+                let asset_id = asset_id.expect("should not be an error");
+
+                // Parse the asset ID
+                let asset_id = asset_id.parse::<asset::Id>();
+                if asset_id.is_err() {
+                    return Some(Err(asset_id
+                        .context("invalid IBC ics20 value balance asset ID in state")
+                        .err()
+                        .unwrap()));
+                }
+                let asset_id = asset_id.expect("should not be an error");
+
+                Some(Ok((asset_id, amount)))
+            })
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .collect::<anyhow::Result<Vec<_>>>()
+            .map_err(|e| tonic::Status::internal(e.to_string()))?;
+
+        // Fetch the denoms associated with the IBC asset IDs
+        for (asset_id, amount) in ibc_amounts {
+            let denom_metadata = snapshot.denom_metadata_by_asset(&asset_id).await;
+            if denom_metadata.is_none() {
+                return Err(tonic::Status::internal(
+                    "bad IBC ics20 value balance key in state".to_string(),
+                ));
+            }
+            let denom_metadata = denom_metadata.expect("should not be an error");
+
+            total_supply.insert(denom_metadata, amount);
+        }
+
+        // Kind of a boneheaded streaming approach due to the various Collect calls above
+        // but works for now.
+        let stream = try_stream! {
+            for (denom_metadata, amount) in total_supply {
+                yield TotalSupplyResponse {
+                    denom_metadata: Some(denom_metadata.into()),
+                    amount: Some(amount.into()),
+                };
+            }
+        };
+
+        Ok(tonic::Response::new(
+            stream
+                .map_err(|e: anyhow::Error| {
+                    tonic::Status::unavailable(format!(
+                        "error getting position value from storage: {e}"
+                    ))
+                })
+                // TODO: how do we instrument a Stream
+                //.instrument(Span::current())
+                .boxed(),
+        ))
     }
 
     #[instrument(skip(self, request))]
