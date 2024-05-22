ba3ec0ce8e71f03728cfa4b8b0ba7c3c7e81453f
katelyn martin <katelyn@penumbralabs.xyz>
2024-05-22 00:00:00 +0000

    tendermint-proxy: 📕 add brief crate-level documentation
    add a link to the RPC this implements, and what it does. add a link to
    the proxy type.

--- crates/util/tendermint-proxy/src/lib.rs
+++ crates/util/tendermint-proxy/src/lib.rs
@@ -1,4 +1,12 @@
 #![deny(clippy::unwrap_used)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
+//! Facilities for proxying gRPC requests to an upstream Tendermint/CometBFT RPC.
+//!
+//! Most importantly, this crate provides [`TendermintProxy`], which implements Penumbra's
+//! [`tendermint_proxy`][proxy-proto] RPC.
+//!
+//! [proxy-proto]: https://buf.build/penumbra-zone/penumbra/docs/main:penumbra.util.tendermint_proxy.v1
+
 mod tendermint_proxy;
 pub use tendermint_proxy::TendermintProxy;
