da3ef0a312c193d74c55bfd0282dad2affdb93d0
Erwan Or <erwan.ounn.84@gmail.com>
2024-06-10 09:50:31 -0400

    pcli(gda): apply web fixes to pcli recipes (#4580)
    ## Describe your changes
    
    This aligns the pcli gda sampling method on the one used by minifront.
    First, the current parameters are incorrect for 48h, and second, this
    means we don't have two codepaths to maintain in sync.
    
    ## Issue ticket number and link
    fix mistakes in recipe parameters by erwanor · Pull Request #1207 ·
    penumbra-zon…
    
    ## Checklist before requesting a review
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
      > Client changes

--- crates/bin/pcli/src/command/tx/auction/dutch/gda.rs
+++ crates/bin/pcli/src/command/tx/auction/dutch/gda.rs
@@ -1,9 +1,9 @@
 use clap::ArgEnum;
 use penumbra_asset::Value;
 use penumbra_auction::auction::dutch::DutchAuctionDescription;
+use rand::Rng;
 use rand::RngCore;
 use rand_core::OsRng;
-use rand_distr::Distribution;
 use serde::Serialize;
 
 #[derive(ArgEnum, Clone, Debug, Serialize)]
@@ -40,21 +40,17 @@ impl GdaRecipe {
         }
     }
 
-    pub fn poisson_intensity_per_block(&self) -> f64 {
-        match &self {
-            GdaRecipe::TenMinutes => 0.064614,
-            GdaRecipe::ThirtyMinutes => 0.050577,
-            GdaRecipe::OneHour => 0.042122,
-            GdaRecipe::TwoHours => 0.022629,
-            GdaRecipe::SixHours => 0.010741,
-            GdaRecipe::TwelveHours => 0.00537,
-            GdaRecipe::OneDay => 0.03469,
-            GdaRecipe::TwoDays => 0.00735,
-        }
-    }
-
     pub fn poisson_intensity(&self) -> f64 {
-        self.poisson_intensity_per_block() * self.as_blocks() as f64
+        match &self {
+            GdaRecipe::TenMinutes => 0.0645833333333,
+            GdaRecipe::ThirtyMinutes => 0.05058333333,
+            GdaRecipe::OneHour => 0.02525,
+            GdaRecipe::TwoHours => 0.02266666666,
+            GdaRecipe::SixHours => 0.01075,
+            GdaRecipe::TwelveHours => 0.0053333333,
+            GdaRecipe::OneDay => 0.035,
+            GdaRecipe::TwoDays => 0.00175,
+        }
     }
 
     pub fn num_auctions(&self) -> u64 {
@@ -115,7 +111,6 @@ impl GradualAuction {
     }
 
     pub fn generate_start_heights(&self) -> Vec<u64> {
-        use rand_distr::Exp;
         let lambda = self.recipe.poisson_intensity();
         let num_auctions = self.recipe.num_auctions();
         let start_height = self.start_height;
@@ -130,12 +125,14 @@ impl GradualAuction {
         );
 
         let mut rng = rand::thread_rng();
-        let exp_dist = Exp::new(1.0 / lambda).expect("lambda too small");
         let mut current_height = start_height as f64;
 
         let mut auction_starts = Vec::with_capacity(num_auctions as usize);
         for _ in 0..num_auctions {
-            let ff_clock = exp_dist.sample(&mut rng);
+            // See https://en.wikipedia.org/wiki/Inverse_transform_sampling
+            // aka. a smirnov transform that is a big workshopped with the abs, but it generates a
+            // nice calendar of auctions.
+            let ff_clock = (rng.gen::<f64>() / lambda).ln().abs();
             current_height += ff_clock;
             let height = current_height.ceil() as u64;
             tracing::debug!(height, arrival_time = ff_clock, "selected auction start");
