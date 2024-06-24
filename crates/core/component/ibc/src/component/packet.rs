02785e4d0e866ac22fff20ccbd5af3d8f5b41145
Chris Czub <chris@penumbralabs.xyz>
2024-06-24 15:37:34 -0400

    Update packet.rs
    Signed-off-by: Chris Czub <chris@penumbralabs.xyz>

--- crates/core/component/ibc/src/component/packet.rs
+++ crates/core/component/ibc/src/component/packet.rs
@@ -162,7 +162,7 @@ pub trait SendPacketRead: StateRead {
             anyhow::bail!(
                 "timeout timestamp {} is less than the latest timestamp on the counterparty {}",
                 packet.timeout_timestamp,
-                chain_ts,
+                chain_ts.nanoseconds(),
             );
         }
 
