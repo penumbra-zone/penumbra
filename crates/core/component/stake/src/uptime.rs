a6c36cef5364443920ae5eed9c5d0510be75a629
dynst <148708712+dynst@users.noreply.github.com>
2024-06-25 00:00:00 +0000

    fix comment in `Uptime::missed_blocks`
    Fix on #4279.

--- crates/core/component/stake/src/uptime.rs
+++ crates/core/component/stake/src/uptime.rs
@@ -80,7 +80,7 @@ impl Uptime {
 
     /// Enumerates the missed blocks over the window in terms of absolute block height.
     pub fn missed_blocks(&self) -> impl Iterator<Item = u64> + DoubleEndedIterator + '_ {
-        // The height of the next block to be recorded (not yet recorded):
+        // The height of the latest block that's been recorded:
         let current_height = self.as_of_block_height;
         // The length of the window of blocks being recorded:
         let window_len = self.signatures.len();
