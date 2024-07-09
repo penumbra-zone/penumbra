c3323a9176128ea4c6541e81bae1c09a9b6d92e6
Lucas Meier <lucas@cronokirby.com>
2024-07-09 01:55:18 -0700

    Reexport PgTransaction from cometindex
    Makes it much more convenient to implement AppView

--- crates/bin/pindexer/src/lib.rs
+++ crates/bin/pindexer/src/lib.rs
@@ -1,4 +1,4 @@
-pub use cometindex::{AppView, Indexer, ContextualizedEvent};
+pub use cometindex::{AppView, Indexer, ContextualizedEvent, PgTransaction};
 
 mod indexer_ext;
 pub use indexer_ext::IndexerExt;
