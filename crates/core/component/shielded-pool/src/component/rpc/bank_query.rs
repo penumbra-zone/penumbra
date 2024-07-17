f6a77db7a8ec49a31df19c34f6c56fec4693998a
dynst <148708712+dynst@users.noreply.github.com>
2024-07-17 00:00:00 +0000

    use `map` instead of `filter_map`

--- crates/core/component/shielded-pool/src/component/rpc/bank_query.rs
+++ crates/core/component/shielded-pool/src/component/rpc/bank_query.rs
@@ -44,14 +44,11 @@ impl BankQuery for Server {
         // Find every non-IBC known asset
         let s = snapshot.prefix(state_key::denom_metadata_by_asset::prefix());
         let mut total_supply = s
-            .filter_map(move |i: anyhow::Result<(String, Metadata)>| async move {
-                let (_key, denom_metadata) = match i {
-                    Ok(x) => x,
-                    Err(e) => return Some(Err(e).context("bad denom in state")),
-                };
+            .map(move |i: anyhow::Result<(String, Metadata)>| {
+                let (_key, denom_metadata) = i.context("bad denom in state")?;
 
                 // Return a hardcoded 0 supply for now
-                Some(Ok((denom_metadata, Amount::from(0u32))))
+                Ok((denom_metadata, Amount::from(0u32)))
             })
             .collect::<Vec<_>>()
             .await
@@ -63,31 +60,19 @@ impl BankQuery for Server {
 
         let s = snapshot.prefix(ibc_state_key::ics20_value_balance::prefix());
         let ibc_amounts = s
-            .filter_map(move |i: anyhow::Result<(String, Amount)>| async move {
-                let (key, amount) = match i {
-                    Ok(x) => x,
-                    Err(e) => return Some(Err(e).context("bad amount in state")),
-                };
+            .map(move |i: anyhow::Result<(String, Amount)>| {
+                let (key, amount) = i.context("bad amount in state")?;
 
                 // Extract the asset ID from the key
                 let asset_id = key.split('/').last();
-                let asset_id = match asset_id {
-                    Some(x) => x,
-                    None => return Some(None.context("bad IBC ics20 value balance key in state")),
-                };
+                let asset_id = asset_id.context("bad IBC ics20 value balance key in state")?;
 
                 // Parse the asset ID
                 let asset_id = asset_id.parse::<asset::Id>();
-                let asset_id = match asset_id {
-                    Ok(x) => x,
-                    Err(e) => {
-                        return Some(
-                            Err(e).context("invalid IBC ics20 value balance asset ID in state"),
-                        )
-                    }
-                };
+                let asset_id =
+                    asset_id.context("invalid IBC ics20 value balance asset ID in state")?;
 
-                Some(Ok((asset_id, amount)))
+                Ok((asset_id, amount))
             })
             .collect::<Vec<_>>()
             .await
