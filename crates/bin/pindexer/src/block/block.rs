e4846f1ac8ce52dc77c7b83fd2fc75ad21d02140
Atris <vacekj@outlook.com>
2024-07-18 14:58:12 +0200

    feat: correctly define and insert blockdetail during indexing

--- crates/bin/pindexer/src/block/block.rs
+++ crates/bin/pindexer/src/block/block.rs
@@ -1,23 +1,29 @@
 use cometindex::{async_trait, sqlx, AppView, ContextualizedEvent, PgTransaction};
+use penumbra_proto::{core::component::sct::v1 as pb, event::ProtoEvent};
 
 #[derive(Debug)]
 pub struct Block {}
 
 #[async_trait]
 impl AppView for Block {
-    async fn init_chain(&self, dbtx: &mut PgTransaction, _: &serde_json::Value) -> Result<(), anyhow::Error> {
+    async fn init_chain(
+        &self,
+        dbtx: &mut PgTransaction,
+        _: &serde_json::Value,
+    ) -> Result<(), anyhow::Error> {
         sqlx::query(
             // table name is module path + struct name
             "
 CREATE TABLE IF NOT EXISTS block_details (
     id SERIAL PRIMARY KEY,
+    root BYTEA NOT NULL,
     height BYTEA NOT NULL,
-    timestamp BYTEA NOT NULL
+    timestamp TIMESTAMPTZ NOT NULL
 );
 ",
         )
-            .execute(dbtx.as_mut())
-            .await?;
+        .execute(dbtx.as_mut())
+        .await?;
         Ok(())
     }
 
@@ -30,7 +36,18 @@ CREATE TABLE IF NOT EXISTS block_details (
         dbtx: &mut PgTransaction,
         event: &ContextualizedEvent,
     ) -> Result<(), anyhow::Error> {
-        dbg!(event);
+        let pe = pb::EventBlockRoot::from_event(event.as_ref())?;
+
+        sqlx::query(
+            "
+            INSERT INTO block_details (height, timestamp)
+            VALUES ($1, $2)
+            ",
+        )
+            .bind(&pe.height)
+            .bind(&pe.timestamp)
+            .execute(dbtx.as_mu())
+            .await?;
 
         Ok(())
     }
--- crates/bin/pindexer/src/main.rs
+++ crates/bin/pindexer/src/main.rs
@@ -1,10 +1,9 @@
 use anyhow::Result;
-use pindexer::{Indexer, IndexerExt as _};
 use pindexer::block::block::Block;
+use pindexer::{Indexer, IndexerExt as _};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    dbg!("hello");
     Indexer::new()
         .with_default_tracing()
         .with_default_penumbra_app_views()
