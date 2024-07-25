a80e1776e5ec558850777717197641047d7d0a04
ejmg <elias.jm.garcia@gmail.com>
2024-07-24 19:52:06 -0500

    Disable Block indexer due to timestamp errors. TODO: fix

--- crates/bin/pindexer/src/main.rs
+++ crates/bin/pindexer/src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use clap::Parser as _;
-use pindexer::{block::Block, block_events::BlockEvents};
+// TODO: Fix timestamp extraction. Currently throws a hard panic because one is never found
+// use pindexer::block::Block;
+use pindexer::block_events::BlockEvents;
 use pindexer::{Indexer, IndexerExt as _, Options};
 
 #[tokio::main]
@@ -8,7 +10,7 @@ async fn main() -> Result<()> {
     Indexer::new(Options::parse())
         .with_default_tracing()
         .with_default_penumbra_app_views()
-        .with_index(Block {})
+        // .with_index(Block {})
         .with_index(BlockEvents {})
         .run()
         .await?;
