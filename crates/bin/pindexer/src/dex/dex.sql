935a17c08e6d2eab3e33d1975b0586595f7f818a
Josef <vacekj@outlook.com>
2024-07-30 17:02:59 +0200

    feat: lp indexing (#4771)
    ## Describe your changes
    
    implements lp positions and execution indexing
    
    TODO from reviews:
    - [x] We want to store the trading pair likely as a new derived type
    (tuple) of (BYTEA, BYTEA)
    - [x] We definitely need a table storing the current state of any given
    position, including permanent information like the trading pair, price,
    fees.
    - [x] We need to include the reserve amounts in the lp_updates table, to
    have a history of the reserves over time.
    - [x] Price info, for both updates and current state
    
    ## Issue ticket number and link
    
    fixes #4739 
    
    ## Checklist before requesting a review
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
    indexer-only changes
    
    ---------
    
    Co-authored-by: Lucas Meier <lucas@cronokirby.com>

--- crates/bin/pindexer/src/dex/dex.sql
+++ crates/bin/pindexer/src/dex/dex.sql
@@ -10,15 +10,14 @@
 -- tables, rather than needing to do a join with another table.
 
 
-DROP TYPE IF EXISTS Value CASCADE;
 DROP DOMAIN IF EXISTS Amount;
-
 CREATE DOMAIN Amount AS NUMERIC(39, 0) NOT NULL;
+
+DROP TYPE IF EXISTS Value CASCADE;
 CREATE TYPE Value AS (
   amount Amount,
   asset BYTEA
 );
-
 
 -- Keeps track of changes to the dex's value circuit breaker.
 CREATE TABLE IF NOT EXISTS dex_value_circuit_breaker_change (
@@ -31,23 +30,86 @@ CREATE TABLE IF NOT EXISTS dex_value_circuit_breaker_change (
 );
 
 -- One step of an execution trace.
-CREATE TABLE IF NOT EXISTS trace_step (
+CREATE TABLE IF NOT EXISTS dex_trace_step (
   id SERIAL PRIMARY KEY,
   value Value
 );
 
 -- A single trace, showing what a small amount of an input asset was exchanged for.
-CREATE TABLE IF NOT EXISTS trace (
+CREATE TABLE IF NOT EXISTS dex_trace (
   id SERIAL PRIMARY KEY,
-  step_start INTEGER REFERENCES trace_step(id),
-  step_end INTEGER REFERENCES trace_step(id)
+  step_start INTEGER REFERENCES dex_trace_step(id),
+  step_end INTEGER REFERENCES dex_trace_step(id)
 );
 
 --- Represents instances where arb executions happened.
-CREATE TABLE IF NOT EXISTS arb (
+CREATE TABLE IF NOT EXISTS dex_arb (
   height BIGINT PRIMARY KEY,
   input Value,
   output Value,
-  trace_start INTEGER REFERENCES trace(id),
-  trace_end INTEGER REFERENCES trace(id)
+  trace_start INTEGER REFERENCES dex_trace(id),
+  trace_end INTEGER REFERENCES dex_trace(id)
+);
+
+DROP DOMAIN IF EXISTS Bps;
+CREATE DOMAIN Bps AS INTEGER CHECK(VALUE BETWEEN 0 and 10000);
+
+-- Holds the current state of a given liquidity position
+CREATE TABLE IF NOT EXISTS dex_lp (
+  id BYTEA PRIMARY KEY,
+  -- The enum for the current state of the position
+  state TEXT NOT NULL,
+  -- The first asset of the position
+  asset1 BYTEA NOT NULL,
+  -- The second asset of the position
+  asset2 BYTEA NOT NULL,
+  p Amount,
+  q Amount,
+  -- The fee, in basis points
+  fee_bps Bps,
+  -- How much of asset2 you get when swapping asset1.
+  price12 NUMERIC GENERATED ALWAYS AS (((1 - fee_bps::NUMERIC / 10000) * (p / q))) STORED,
+  -- How much of asset1 you get when swapping asset2.
+  price21 NUMERIC GENERATED ALWAYS AS (((1 - fee_bps::NUMERIC / 10000) * (q / p))) STORED,
+  -- Whether the position will be closed when all reserves are depleted
+  close_on_fill BOOLEAN NOT NULL,
+  -- The amount of reserves of asset 1.
+  reserves1 Amount,
+  -- The amount of reserves of asset 2.
+  reserves2 Amount
+);
+-- So that we can easily query positions by ascending price
+CREATE INDEX ON dex_lp(asset1, price12);
+CREATE INDEX ON dex_lp(asset2, price21);
+
+-- Holds update events to liquidity position
+CREATE TABLE IF NOT EXISTS dex_lp_update (
+  id SERIAL PRIMARY KEY,
+  -- The block height where the update occurred.
+  height BIGINT NOT NULL,
+  -- The identifier for the position
+  position_id BYTEA NOT NULL,
+  -- The new state of the position
+  state TEXT NOT NULL,
+  -- The new reserves of asset1 (potentially null)
+  reserves1 NUMERIC(39, 0),
+  -- The new reserves of asset2 (potentially null)
+  reserves2 NUMERIC(39, 0),
+  -- If present, a reference to the execution table, for execution events
+  execution_id INTEGER
+);
+CREATE INDEX ON dex_lp_update(position_id, height DESC, id DESC);
+CREATE INDEX ON dex_lp_update(execution_id);
+
+-- Holds data related to execution events
+CREATE TABLE IF NOT EXISTS dex_lp_execution (
+  id SERIAL PRIMARY KEY,
+  -- The amount of asset1 that was pushed into (or pulled out of, if negative) this position.
+  inflow1 Amount,
+  -- As above, with asset2.
+  inflow2 Amount,
+  -- The start asset for this execution.
+  context_start BYTEA NOT NULL,
+  -- The end asset for this execution.
+  context_end BYTEA NOT NULL
 );
--- crates/bin/pindexer/src/dex/mod.rs
+++ crates/bin/pindexer/src/dex/mod.rs
@@ -1,9 +1,11 @@
 use std::collections::HashSet;
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
 use cometindex::async_trait;
 use penumbra_asset::asset::Id as AssetId;
-use penumbra_dex::SwapExecution;
+use penumbra_dex::lp::position::{Id, Position};
+use penumbra_dex::lp::{self, TradingFunction};
+use penumbra_dex::{DirectedTradingPair, SwapExecution};
 use penumbra_num::Amount;
 use penumbra_proto::{event::ProtoEvent, penumbra::core::component::dex::v1 as pb};
 use sqlx::{PgPool, Postgres, Transaction};
@@ -31,13 +33,39 @@ enum Event {
         height: u64,
         execution: SwapExecution,
     },
+    /// A parsed version of [pb::EventPositionOpen]
+    PositionOpen { height: u64, position: Position },
+    /// A parsed version of [pb::EventPositionWithdraw]
+    PositionWithdraw {
+        height: u64,
+        position_id: Id,
+        reserves_1: Amount,
+        reserves_2: Amount,
+        sequence: u64,
+    },
+    /// A parsed version of [pb::EventPositionClose]
+    PositionClose { height: u64, position_id: Id },
+    /// A parsed version of [pb::EventPositionExecution]
+    PositionExecution {
+        height: u64,
+        position_id: Id,
+        reserves_1: Amount,
+        reserves_2: Amount,
+        prev_reserves_1: Amount,
+        prev_reserves_2: Amount,
+        context: DirectedTradingPair,
+    },
 }
 
 impl Event {
-    const NAMES: [&'static str; 3] = [
+    const NAMES: [&'static str; 7] = [
         "penumbra.core.component.dex.v1.EventValueCircuitBreakerCredit",
         "penumbra.core.component.dex.v1.EventValueCircuitBreakerDebit",
         "penumbra.core.component.dex.v1.EventArbExecution",
+        "penumbra.core.component.dex.v1.EventPositionWithdraw",
+        "penumbra.core.component.dex.v1.EventPositionOpen",
+        "penumbra.core.component.dex.v1.EventPositionClose",
+        "penumbra.core.component.dex.v1.EventPositionExecution",
     ];
 
     /// Index this event, using the handle to the postgres transaction.
@@ -95,7 +123,7 @@ impl Event {
                     let mut step_end = None;
                     for step in trace {
                         let (id,): (i32,) = sqlx::query_as(
-                            r#"INSERT INTO trace_step VALUES (DEFAULT, (CAST($1 AS Amount), $2)) RETURNING id;"#,
+                            r#"INSERT INTO dex_trace_step VALUES (DEFAULT, (CAST($1 AS Amount), $2)) RETURNING id;"#,
                         )
                         .bind(step.amount.to_string())
                         .bind(Sql::from(step.asset_id))
@@ -107,7 +135,7 @@ impl Event {
                         step_end = Some(id);
                     }
                     let (id,): (i32,) = sqlx::query_as(
-                        r#"INSERT INTO trace VALUES (DEFAULT, $1, $2) RETURNING id;"#,
+                        r#"INSERT INTO dex_trace VALUES (DEFAULT, $1, $2) RETURNING id;"#,
                     )
                     .bind(step_start)
                     .bind(step_end)
@@ -118,7 +146,7 @@ impl Event {
                     }
                     trace_end = Some(id);
                 }
-                sqlx::query(r#"INSERT INTO arb VALUES ($1, (CAST($2 AS Amount), $3), (CAST($4 AS AMOUNT), $5), $6, $7);"#)
+                sqlx::query(r#"INSERT INTO dex_arb VALUES ($1, (CAST($2 AS Amount), $3), (CAST($4 AS Amount), $5), $6, $7);"#)
                     .bind(i64::try_from(*height)?)
                     .bind(execution.input.amount.to_string())
                     .bind(Sql::from(execution.input.asset_id))
@@ -128,6 +156,176 @@ impl Event {
                     .bind(trace_end)
                     .execute(dbtx.as_mut())
                     .await?;
+                Ok(())
+            }
+            Event::PositionOpen { height, position } => {
+                let Position {
+                    state,
+                    phi: TradingFunction { pair, component },
+                    ..
+                } = position;
+                let id = position.id().0;
+                tracing::debug!(
+                    p = component.p.to_string(),
+                    q = component.q.to_string(),
+                    r1 = position.reserves_1().amount.to_string(),
+                    r2 = position.reserves_2().amount.to_string()
+                );
+                sqlx::query(
+                    "
+                INSERT INTO dex_lp (id, state, asset1, asset2, p, q, fee_bps, close_on_fill, reserves1, reserves2)
+                VALUES ($1, $2, $3, $4, CAST($5 as Amount), CAST($6 AS Amount), $7, $8, CAST($9 AS Amount), CAST($10 AS Amount));
+                ",
+                )
+                .bind(id)
+                .bind(state.to_string())
+                .bind(pair.asset_1().to_bytes())
+                .bind(pair.asset_2().to_bytes())
+                .bind(component.p.to_string())
+                .bind(component.q.to_string())
+                .bind(i32::try_from(component.fee)?)
+                .bind(position.close_on_fill)
+                .bind(position.reserves_1().amount.to_string())
+                .bind(position.reserves_2().amount.to_string())
+                .execute(dbtx.as_mut())
+                .await?;
+                sqlx::query(
+                    "
+                INSERT INTO dex_lp_update (height, position_id, state, reserves1, reserves2)
+                VALUES ($1, $2, $3, CAST($4 AS Amount), CAST($5 AS Amount));
+                ",
+                )
+                .bind(i64::try_from(*height)?)
+                .bind(id)
+                .bind(state.to_string())
+                .bind(position.reserves_1().amount.to_string())
+                .bind(position.reserves_2().amount.to_string())
+                .execute(dbtx.as_mut())
+                .await?;
+                Ok(())
+            }
+            Event::PositionClose {
+                height,
+                position_id,
+            } => {
+                let state = lp::position::State::Closed;
+                sqlx::query(
+                    "
+                INSERT INTO dex_lp_update (height, position_id, state)
+                VALUES ($1, $2, $3)
+                ",
+                )
+                .bind(i64::try_from(*height)?)
+                .bind(position_id.0)
+                .bind(state.to_string())
+                .execute(dbtx.as_mut())
+                .await?;
+                sqlx::query(
+                    "
+                UPDATE dex_lp
+                SET state = $2
+                WHERE id = $1;
+                ",
+                )
+                .bind(position_id.0)
+                .bind(state.to_string())
+                .execute(dbtx.as_mut())
+                .await?;
+                Ok(())
+            }
+            Event::PositionWithdraw {
+                height,
+                position_id,
+                reserves_1: reserves1,
+                reserves_2: reserves2,
+                sequence,
+            } => {
+                let state = lp::position::State::Withdrawn {
+                    sequence: *sequence,
+                };
+                let reserves1 = reserves1.to_string();
+                let reserves2 = reserves2.to_string();
+                sqlx::query(
+                    "
+                INSERT INTO dex_lp_update (height, position_id, state, reserves1, reserves2)
+                VALUES ($1, $2, $3, CAST($4 AS Amount), CAST($5 AS Amount))
+                ",
+                )
+                .bind(i64::try_from(*height)?)
+                .bind(position_id.0)
+                .bind(state.to_string())
+                .bind(&reserves1)
+                .bind(&reserves2)
+                .execute(dbtx.as_mut())
+                .await?;
+                sqlx::query(
+                    "
+                UPDATE dex_lp
+                SET state = $2, reserves1 = CAST($3 AS Amount), reserves2 = CAST($4 AS Amount)
+                WHERE id = $1;
+                ",
+                )
+                .bind(position_id.0)
+                .bind(state.to_string())
+                .bind(&reserves1)
+                .bind(&reserves2)
+                .execute(dbtx.as_mut())
+                .await?;
+                Ok(())
+            }
+            Event::PositionExecution {
+                height,
+                position_id,
+                reserves_1: reserves1,
+                reserves_2: reserves2,
+                context,
+                prev_reserves_1: prev_reserves1,
+                prev_reserves_2: prev_reserves2,
+            } => {
+                let state = lp::position::State::Opened;
+                let reserves1 = reserves1.to_string();
+                let reserves2 = reserves2.to_string();
+                let prev_reserves1 = prev_reserves1.to_string();
+                let prev_reserves2 = prev_reserves2.to_string();
+                let id: i32 = sqlx::query_scalar(
+                    "
+                INSERT INTO dex_lp_execution (inflow1, inflow2, context_start, context_end)
+                VALUES (CAST($1 AS Amount) - CAST($2 AS Amount), CAST($3 AS Amount) - CAST($4 AS Amount), $5, $6)
+                RETURNING id;
+            ",)
+                .bind(&reserves1)
+                .bind(prev_reserves1)
+                .bind(&reserves2)
+                .bind(prev_reserves2)
+                .bind(context.start.to_bytes())
+                .bind(context.end.to_bytes())
+                .fetch_one(dbtx.as_mut()).await?;
+                sqlx::query(
+                    "
+            INSERT INTO dex_lp_update (height, position_id, state, reserves1, reserves2, execution_id)
+            VALUES ($1, $2, $3, CAST($4 AS Amount), CAST($5 AS Amount), $6)
+            ",
+                )
+                .bind(i64::try_from(*height)?)
+                .bind(position_id.0)
+                .bind(state.to_string())
+                .bind(&reserves1)
+                .bind(&reserves2)
+                .bind(id)
+                .execute(dbtx.as_mut())
+                .await?;
+                sqlx::query(
+                    "
+                UPDATE dex_lp
+                SET reserves1 = CAST($2 AS Amount), reserves2 = CAST($3 AS Amount)
+                WHERE id = $1;
+                ",
+                )
+                .bind(position_id.0)
+                .bind(&reserves1)
+                .bind(&reserves2)
+                .execute(dbtx.as_mut())
+                .await?;
                 Ok(())
             }
         }
@@ -182,6 +380,92 @@ impl<'a> TryFrom<&'a ContextualizedEvent> for Event {
                     .ok_or(anyhow!("missing swap execution"))?
                     .try_into()?;
                 Ok(Self::ArbExecution { height, execution })
+            }
+            // LP Withdraw
+            x if x == Event::NAMES[3] => {
+                let pe = pb::EventPositionWithdraw::from_event(event.as_ref())?;
+                let height = event.block_height;
+                let position_id = pe
+                    .position_id
+                    .ok_or(anyhow!("missing position id"))?
+                    .try_into()?;
+                let reserves_1 = pe
+                    .reserves_1
+                    .ok_or(anyhow!("missing reserves_1"))?
+                    .try_into()?;
+                let reserves_2 = pe
+                    .reserves_2
+                    .ok_or(anyhow!("missing reserves_2"))?
+                    .try_into()?;
+                let sequence = pe.sequence;
+                Ok(Self::PositionWithdraw {
+                    height,
+                    position_id,
+                    reserves_1,
+                    reserves_2,
+                    sequence,
+                })
+            }
+            // LP Open
+            x if x == Event::NAMES[4] => {
+                let pe = pb::EventPositionOpen::from_event(event.as_ref())?;
+                let height = event.block_height;
+                let position = pe
+                    .position
+                    .ok_or(anyhow!("missing position"))
+                    .context("(make sure you're using pd >= 0.79.3)")?
+                    .try_into()?;
+                Ok(Self::PositionOpen { height, position })
+            }
+            // LP Close
+            x if x == Event::NAMES[5] => {
+                let pe = pb::EventPositionClose::from_event(event.as_ref())?;
+                let height = event.block_height;
+                let position_id = pe
+                    .position_id
+                    .ok_or(anyhow!("missing position id"))?
+                    .try_into()?;
+
+                Ok(Self::PositionClose {
+                    height,
+                    position_id,
+                })
+            }
+            // LP Execution
+            x if x == Event::NAMES[6] => {
+                let pe = pb::EventPositionExecution::from_event(event.as_ref())?;
+                let height = event.block_height;
+                let position_id = pe
+                    .position_id
+                    .ok_or(anyhow!("missing position id"))?
+                    .try_into()?;
+                let reserves_1 = pe
+                    .reserves_1
+                    .ok_or(anyhow!("missing reserves_1"))?
+                    .try_into()?;
+                let reserves_2 = pe
+                    .reserves_2
+                    .ok_or(anyhow!("missing reserves_2"))?
+                    .try_into()?;
+                let prev_reserves_1 = pe
+                    .prev_reserves_1
+                    .ok_or(anyhow!("missing reserves_1"))?
+                    .try_into()?;
+                let prev_reserves_2 = pe
+                    .prev_reserves_2
+                    .ok_or(anyhow!("missing reserves_2"))?
+                    .try_into()?;
+                let context: DirectedTradingPair =
+                    pe.context.ok_or(anyhow!("missing context"))?.try_into()?;
+                Ok(Self::PositionExecution {
+                    height,
+                    position_id,
+                    reserves_1,
+                    reserves_2,
+                    prev_reserves_1,
+                    prev_reserves_2,
+                    context,
+                })
             }
             x => Err(anyhow!(format!("unrecognized event kind: {x}"))),
         }
