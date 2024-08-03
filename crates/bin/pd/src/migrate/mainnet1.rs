00ce96e26a1c98b100ece2aa861bb92b9fee1844
Lucas Meier <lucas@cronokirby.com>
2024-08-02 17:34:17 -0700

    feat(pd)!: stateful migration for IBC noble fixes
    This adds a state change to the migration, in order to replace all of
    the packets which had error acknowledgements, so that, post-migration,
    the acks can be replayed with the correct logic for error acks, the
    packets having been replaced in the state.
    
    This also changes the IBC component visibility of a trait to reuse the
    logic for inserting packet commitments.
    
    (cherry picked from commit 09a822e68ba4c237d58859076daa872d2bd22923)

--- crates/bin/pd/src/migrate/mainnet1.rs
+++ crates/bin/pd/src/migrate/mainnet1.rs
@@ -4,19 +4,91 @@
 //!
 //! There's no state-altering logic in this migration: the migration simply
 //! flips the halt bit off, permitting the chain to restart post-upgrade.
-use anyhow::Context;
+use cnidarium::Snapshot;
 use cnidarium::{StateDelta, Storage};
+use ibc_types::core::channel::{Packet, PortId};
+use ibc_types::transfer::acknowledgement::TokenTransferAcknowledgement;
 use jmt::RootHash;
 use penumbra_app::app::StateReadExt as _;
 use penumbra_governance::StateWriteExt;
+use penumbra_ibc::{component::ChannelStateWriteExt as _, IbcRelay};
 use penumbra_sct::component::clock::EpochManager;
 use penumbra_sct::component::clock::EpochRead;
+use penumbra_transaction::{Action, Transaction};
 use std::path::PathBuf;
 use tracing::instrument;
 
 use crate::network::generate::NetworkConfig;
 
+/// The block where proposal #2 passed, enabling outbound ICS20 transfers.
+const ICS20_TRANSFER_START_HEIGHT: u64 = 411616;
+
+/// Find all of the lost transfers inside of a transaction.
+///
+/// In other words, look for relayed packet acknowledgements relating to ICS20 transfers containing an error.
+/// These packets were not correctly handled, being deleted when the ack had an error,
+/// as if the ack were successful.
+fn tx_lost_transfers(transaction: Transaction) -> impl Iterator<Item = Packet> {
+    transaction
+        .transaction_body()
+        .actions
+        .into_iter()
+        .filter_map(move |action| match action {
+            Action::IbcRelay(IbcRelay::Acknowledgement(m)) => {
+                // Make sure we're only looking at ICS20 related packets
+                if m.packet.port_on_b != PortId::transfer() {
+                    return None;
+                }
+                // This shouldn't fail to parse, because the transaction wouldn't have been
+                // included otherwise, but if for some reason it doesn't, ignore it.
+                let transfer: TokenTransferAcknowledgement =
+                    match serde_json::from_slice(m.acknowledgement.as_slice()) {
+                        Err(_) => return None,
+                        Ok(x) => x,
+                    };
+                // If the ack was successful, then that packet was correctly handled, so don't
+                // consider it.
+                match transfer {
+                    TokenTransferAcknowledgement::Success(_) => None,
+                    TokenTransferAcknowledgement::Error(_) => Some(m.packet),
+                }
+            }
+            _ => None,
+        })
+}
+
+/// Retrieve all the packets resulting in a locked transfer because of error acks.
+///
+/// This does so by looking at all transactions, looking for the relayed acknowledgements.
+async fn lost_transfers(state: &StateDelta<Snapshot>) -> anyhow::Result<Vec<Packet>> {
+    let mut out = Vec::new();
+    let end_height = state.get_block_height().await?;
+    // We only need to start from the height where transfers were enabled via governance.
+    for height in ICS20_TRANSFER_START_HEIGHT..=end_height {
+        let transactions = state.transactions_by_height(height).await?.transactions;
+        for tx in transactions.into_iter() {
+            for lost in tx_lost_transfers(tx.try_into()?) {
+                out.push(lost);
+            }
+        }
+    }
+    Ok(out)
+}
+
+/// Replace all the packets that were erroneously removed from the state.
+async fn replace_lost_packets(delta: &mut StateDelta<Snapshot>) -> anyhow::Result<()> {
+    let lost_packets = lost_transfers(delta).await?;
+    for packet in lost_packets {
+        // This will undo what happens in https://github.com/penumbra-zone/penumbra/blob/882a061bd69ce14b01711041bbc0c0ce209e2823/crates/core/component/ibc/src/component/msg_handler/acknowledgement.rs#L99.
+        delta.put_packet_commitment(&packet);
+    }
+    Ok(())
+}
+
 /// Run the full migration, emitting a new genesis event, representing historical state.
+///
+/// This will have the effect of reinserting packets which had acknowledgements containing
+/// errors, and erroneously removed from state, as if the acknowledgements had contained successes.
 #[instrument]
 pub async fn migrate(
     storage: Storage,
@@ -37,14 +109,26 @@ pub async fn migrate(
         .expect("chain state has a block height");
     let post_upgrade_height = pre_upgrade_height.wrapping_add(1);
 
-    // Set halt bit to 0, so chain can start again.
     let mut delta = StateDelta::new(initial_state);
-    delta.ready_to_start();
-    delta.put_block_height(0u64);
-    let _ = storage
-        .commit_in_place(delta)
-        .await
-        .context("failed to reset halt bit")?;
+    let (migration_duration, post_upgrade_root_hash) = {
+        let start_time = std::time::SystemTime::now();
+
+        // Reinsert all of the erroneously removed packets
+        replace_lost_packets(&mut delta).await?;
+
+        // Reset the application height and halt flag.
+        delta.ready_to_start();
+        delta.put_block_height(0u64);
+
+        // Finally, commit the changes to the chain state.
+        let post_upgrade_root_hash = storage.commit_in_place(delta).await?;
+        tracing::info!(?post_upgrade_root_hash, "post-migration root hash");
+
+        (
+            start_time.elapsed().expect("start is set"),
+            post_upgrade_root_hash,
+        )
+    };
     storage.release().await;
 
     // The migration is complete, now we need to generate a genesis file. To do this, we need
@@ -80,7 +164,10 @@ pub async fn migrate(
 
     tracing::info!(
         pre_upgrade_height,
+        post_upgrade_height,
         ?pre_upgrade_root_hash,
+        ?post_upgrade_root_hash,
+        duration = migration_duration.as_secs(),
         "successful migration!"
     );
 
