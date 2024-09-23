a27d839b247860fd4a07b96abca436b5d78d3877
Lucas Meier <lucas@cronokirby.com>
2024-09-23 10:06:32 -0700

    pindexer: supply: fix pindexer main

--- crates/bin/pindexer/src/main.rs
+++ crates/bin/pindexer/src/main.rs
@@ -1,12 +1,14 @@
 use anyhow::Result;
 use clap::Parser as _;
-use pindexer::{supply::Supply, Indexer, Options};
+use pindexer::block::Block;
+use pindexer::{Indexer, IndexerExt as _, Options};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     Indexer::new(Options::parse())
         .with_default_tracing()
-        .with_index(Supply::new())
+        .with_default_penumbra_app_views()
+        .with_index(Block {})
         .run()
         .await?;
 
