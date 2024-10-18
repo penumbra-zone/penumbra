fdf24129dfb4dea8ec9b513384350f559a6b8213
Lucas Meier <lucas@cronokirby.com>
2024-10-18 02:51:15 -0700

    Draw the rest of the owl

--- crates/core/component/funding/src/event.rs
+++ crates/core/component/funding/src/event.rs
@@ -1,14 +1,42 @@
+use anyhow::{anyhow, Context};
 use penumbra_num::Amount;
-use penumbra_proto::penumbra::core::component::funding::v1 as pb;
+use penumbra_proto::{penumbra::core::component::funding::v1 as pb, DomainType, Name as _};
 
-pub fn funding_stream_reward(
-    recipient: String,
-    epoch_index: u64,
-    reward_amount: Amount,
-) -> pb::EventFundingStreamReward {
-    pb::EventFundingStreamReward {
-        recipient,
-        epoch_index,
-        reward_amount: Some(reward_amount.into()),
+#[derive(Clone, Debug)]
+pub struct EventFundingStreamReward {
+    pub recipient: String,
+    pub epoch_index: u64,
+    pub reward_amount: Amount,
+}
+
+impl TryFrom<pb::EventFundingStreamReward> for EventFundingStreamReward {
+    type Error = anyhow::Error;
+
+    fn try_from(value: pb::EventFundingStreamReward) -> Result<Self, Self::Error> {
+        fn inner(value: pb::EventFundingStreamReward) -> anyhow::Result<EventFundingStreamReward> {
+            Ok(EventFundingStreamReward {
+                recipient: value.recipient,
+                epoch_index: value.epoch_index,
+                reward_amount: value
+                    .reward_amount
+                    .ok_or(anyhow!("missing `reward_amount`"))?
+                    .try_into()?,
+            })
+        }
+        inner(value).context(format!("parsing {}", pb::EventFundingStreamReward::NAME))
     }
+}
+
+impl From<EventFundingStreamReward> for pb::EventFundingStreamReward {
+    fn from(value: EventFundingStreamReward) -> Self {
+        Self {
+            recipient: value.recipient,
+            epoch_index: value.epoch_index,
+            reward_amount: Some(value.reward_amount.into()),
+        }
+    }
+}
+
+impl DomainType for EventFundingStreamReward {
+    type Proto = pb::EventFundingStreamReward;
 }
