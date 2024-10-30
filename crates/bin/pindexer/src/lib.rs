5ccdb35783e48fc9669bf1bf20f36b96ea0b0592
Lucas Meier <lucas@cronokirby.com>
2024-10-29 21:04:18 -0700

    pindexer: remove dex AppView

--- crates/bin/pindexer/src/lib.rs
+++ crates/bin/pindexer/src/lib.rs
@@ -3,13 +3,11 @@ pub use cometindex::{opt::Options, AppView, ContextualizedEvent, Indexer, PgPool
 mod indexer_ext;
 pub use indexer_ext::IndexerExt;
 pub mod block;
-pub mod dex;
 pub mod dex_ex;
 pub mod ibc;
 pub mod insights;
 mod parsing;
 pub mod shielded_pool;
-mod sql;
 pub mod stake;
 pub mod supply;
 
