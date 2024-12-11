e3446815f47853e00e886016cee4ebaf080a184c
redshiftzero <jen@penumbralabs.xyz>
2024-12-11 16:40:31 -0500

    ibc: mark `use_compat_address` field as deprecated

--- crates/proto/src/gen/penumbra.core.component.ibc.v1.rs
+++ crates/proto/src/gen/penumbra.core.component.ibc.v1.rs
@@ -71,6 +71,7 @@ pub struct Ics20Withdrawal {
     pub source_channel: ::prost::alloc::string::String,
     /// Whether to use a "compat" (bech32, non-m) address for the return address in the withdrawal,
     /// for compatability with chains that expect to be able to parse the return address as bech32.
+    #[deprecated]
     #[prost(bool, tag = "8")]
     pub use_compat_address: bool,
     /// Arbitrary string data to be included in the `memo` field
