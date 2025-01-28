d44c409f79e7bd869a6abad4e348b142a7d1b9f2
Lúcás Meier <lucas@cronokirby.com>
2025-01-28 13:18:07 -0800

    Add chain parameters for configuring LQT distributions (#5018)
    Closes #5012.
    
    Testing shouldn't be required at this stage.
    
    - [x] I have added guiding text to explain how a reviewer should test
    these changes.
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
    > This is consensus-breaking, because validators will disagree about,
    e.g. what people are voting on.

--- crates/proto/src/gen/penumbra.core.component.distributions.v1.serde.rs
+++ crates/proto/src/gen/penumbra.core.component.distributions.v1.serde.rs
@@ -9,11 +9,19 @@ impl serde::Serialize for DistributionsParameters {
         if self.staking_issuance_per_block != 0 {
             len += 1;
         }
+        if self.liquidity_tournament_incentive_per_block != 0 {
+            len += 1;
+        }
         let mut struct_ser = serializer.serialize_struct("penumbra.core.component.distributions.v1.DistributionsParameters", len)?;
         if self.staking_issuance_per_block != 0 {
             #[allow(clippy::needless_borrow)]
             #[allow(clippy::needless_borrows_for_generic_args)]
             struct_ser.serialize_field("stakingIssuancePerBlock", ToString::to_string(&self.staking_issuance_per_block).as_str())?;
+        }
+        if self.liquidity_tournament_incentive_per_block != 0 {
+            #[allow(clippy::needless_borrow)]
+            #[allow(clippy::needless_borrows_for_generic_args)]
+            struct_ser.serialize_field("liquidityTournamentIncentivePerBlock", ToString::to_string(&self.liquidity_tournament_incentive_per_block).as_str())?;
         }
         struct_ser.end()
     }
@@ -27,11 +35,14 @@ impl<'de> serde::Deserialize<'de> for DistributionsParameters {
         const FIELDS: &[&str] = &[
             "staking_issuance_per_block",
             "stakingIssuancePerBlock",
+            "liquidity_tournament_incentive_per_block",
+            "liquidityTournamentIncentivePerBlock",
         ];
 
         #[allow(clippy::enum_variant_names)]
         enum GeneratedField {
             StakingIssuancePerBlock,
+            LiquidityTournamentIncentivePerBlock,
             __SkipField__,
         }
         impl<'de> serde::Deserialize<'de> for GeneratedField {
@@ -55,6 +66,7 @@ impl<'de> serde::Deserialize<'de> for DistributionsParameters {
                     {
                         match value {
                             "stakingIssuancePerBlock" | "staking_issuance_per_block" => Ok(GeneratedField::StakingIssuancePerBlock),
+                            "liquidityTournamentIncentivePerBlock" | "liquidity_tournament_incentive_per_block" => Ok(GeneratedField::LiquidityTournamentIncentivePerBlock),
                             _ => Ok(GeneratedField::__SkipField__),
                         }
                     }
@@ -75,6 +87,7 @@ impl<'de> serde::Deserialize<'de> for DistributionsParameters {
                     V: serde::de::MapAccess<'de>,
             {
                 let mut staking_issuance_per_block__ = None;
+                let mut liquidity_tournament_incentive_per_block__ = None;
                 while let Some(k) = map_.next_key()? {
                     match k {
                         GeneratedField::StakingIssuancePerBlock => {
@@ -85,6 +98,14 @@ impl<'de> serde::Deserialize<'de> for DistributionsParameters {
                                 Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
                             ;
                         }
+                        GeneratedField::LiquidityTournamentIncentivePerBlock => {
+                            if liquidity_tournament_incentive_per_block__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("liquidityTournamentIncentivePerBlock"));
+                            }
+                            liquidity_tournament_incentive_per_block__ = 
+                                Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
                         GeneratedField::__SkipField__ => {
                             let _ = map_.next_value::<serde::de::IgnoredAny>()?;
                         }
@@ -92,6 +113,7 @@ impl<'de> serde::Deserialize<'de> for DistributionsParameters {
                 }
                 Ok(DistributionsParameters {
                     staking_issuance_per_block: staking_issuance_per_block__.unwrap_or_default(),
+                    liquidity_tournament_incentive_per_block: liquidity_tournament_incentive_per_block__.unwrap_or_default(),
                 })
             }
         }
