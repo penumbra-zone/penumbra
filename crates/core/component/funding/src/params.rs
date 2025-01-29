cd92572dd4a3ef9c7760ddc8bb7d06d41dd46790
Lúcás Meier <lucas@cronokirby.com>
2025-01-29 10:57:45 -0800

    funding: add component parameters for LQT (#5020)
    Closes #5013.
    
    Diverges slightly from the ADR draft in that it creates a nested proto
    sub-message specific to the LQT. My rationale here was that this is
    nicer than prefixing every relevant field with "liquidity_tournament",
    both in the proto definitions, but also in the Rust code, because that
    way we can have a default for that entire set of parameters, and then
    the code can explicitly mark the absence of that set as to be
    interpreted as this default. Also works nicer for the future, in which
    we might be using the funding component for other things, each of which
    can cleanly have its own set of parameters.
    
    I also added a little utility type for representing percentages. Not
    strictly necessary, but I think it's a good move towards type safety.
    The motivation here was all of the annoyance of dealing with keeping
    track of what u64s were bps or bps^2 or yadda yadda yadda in other parts
    of the codebase.
    
    I've added some relevant unit tests.
    
    - [x] I have added guiding text to explain how a reviewer should test
    these changes.
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
      > adds new parameters that can be voted on, so consensus-breaking.
    
    ---------
    
    Co-authored-by: Erwan Or <erwanor@penumbralabs.xyz>

--- crates/core/component/funding/src/params.rs
+++ crates/core/component/funding/src/params.rs
@@ -1,10 +1,60 @@
+use penumbra_sdk_num::Percentage;
 use penumbra_sdk_proto::core::component::funding::v1 as pb;
 use penumbra_sdk_proto::DomainType;
 use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct LiquidityTournamentParameters {
+    // The fraction of gauge votes that an asset must clear to receive rewards.
+    pub gauge_threshold: Percentage,
+    // The maximum number of liquidity positions that can receive rewards.
+    pub max_positions: u64,
+    // The maximum number of delegators that can receive rewards.
+    pub max_delegators: u64,
+    // The share of rewards that go to delegators, instead of positions.
+    pub delegator_share: Percentage,
+}
+
+impl Default for LiquidityTournamentParameters {
+    fn default() -> Self {
+        Self {
+            gauge_threshold: Percentage::from_percent(100),
+            max_positions: 0,
+            max_delegators: 0,
+            delegator_share: Percentage::zero(),
+        }
+    }
+}
+
+impl TryFrom<pb::funding_parameters::LiquidityTournament> for LiquidityTournamentParameters {
+    type Error = anyhow::Error;
+
+    fn try_from(proto: pb::funding_parameters::LiquidityTournament) -> Result<Self, Self::Error> {
+        Ok(Self {
+            gauge_threshold: Percentage::from_percent(proto.gauge_threshold_percent),
+            max_positions: proto.max_positions,
+            max_delegators: proto.max_delegators,
+            delegator_share: Percentage::from_percent(proto.delegator_share_percent),
+        })
+    }
+}
+
+impl From<LiquidityTournamentParameters> for pb::funding_parameters::LiquidityTournament {
+    fn from(value: LiquidityTournamentParameters) -> Self {
+        Self {
+            gauge_threshold_percent: value.gauge_threshold.to_percent(),
+            max_positions: value.max_positions,
+            max_delegators: value.max_delegators,
+            delegator_share_percent: value.delegator_share.to_percent(),
+        }
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(try_from = "pb::FundingParameters", into = "pb::FundingParameters")]
-pub struct FundingParameters {}
+pub struct FundingParameters {
+    pub liquidity_tournament: LiquidityTournamentParameters,
+}
 
 impl DomainType for FundingParameters {
     type Proto = pb::FundingParameters;
@@ -13,19 +63,30 @@ impl DomainType for FundingParameters {
 impl TryFrom<pb::FundingParameters> for FundingParameters {
     type Error = anyhow::Error;
 
-    fn try_from(_params: pb::FundingParameters) -> anyhow::Result<Self> {
-        Ok(FundingParameters {})
+    fn try_from(proto: pb::FundingParameters) -> anyhow::Result<Self> {
+        Ok(FundingParameters {
+            // Explicitly consider missing parameters to *be* the default parameters, for upgrades.
+            liquidity_tournament: proto
+                .liquidity_tournament
+                .map(LiquidityTournamentParameters::try_from)
+                .transpose()?
+                .unwrap_or_default(),
+        })
     }
 }
 
 impl From<FundingParameters> for pb::FundingParameters {
-    fn from(_params: FundingParameters) -> Self {
-        pb::FundingParameters {}
+    fn from(params: FundingParameters) -> Self {
+        pb::FundingParameters {
+            liquidity_tournament: Some(params.liquidity_tournament.into()),
+        }
     }
 }
 
 impl Default for FundingParameters {
     fn default() -> Self {
-        Self {}
+        Self {
+            liquidity_tournament: LiquidityTournamentParameters::default(),
+        }
     }
 }
