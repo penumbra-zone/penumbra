0ad102fa0ccf473a0bfb9a73bfc4d636a1c8ab52
Lúcás Meier <lucas@cronokirby.com>
2025-01-29 10:57:45 -0800

    funding: add component parameters for LQT (#5020)
    Closes #5013.
    
    Diverges slightly from the ADR draft in that it creates a nested proto
    sub-message specific to the LQT. My rationale here was that this is
    nicer than prefixing every relevant field with "liquidity_tournament",
    both in the proto definitions, but also in the Rust code, because that
    way we can have a default for that entire set of parameters, and then
    the code can explicitly mark the absence of that set as to be
    interpreted as this default. Also works nicer for the future, in which
    we might be using the funding component for other things, each of which
    can cleanly have its own set of parameters.
    
    I also added a little utility type for representing percentages. Not
    strictly necessary, but I think it's a good move towards type safety.
    The motivation here was all of the annoyance of dealing with keeping
    track of what u64s were bps or bps^2 or yadda yadda yadda in other parts
    of the codebase.
    
    I've added some relevant unit tests.
    
    - [x] I have added guiding text to explain how a reviewer should test
    these changes.
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
      > adds new parameters that can be voted on, so consensus-breaking.
    
    ---------
    
    Co-authored-by: Erwan Or <erwanor@penumbralabs.xyz>

--- crates/proto/src/gen/penumbra.core.component.funding.v1.serde.rs
+++ crates/proto/src/gen/penumbra.core.component.funding.v1.serde.rs
@@ -140,8 +140,14 @@ impl serde::Serialize for FundingParameters {
         S: serde::Serializer,
     {
         use serde::ser::SerializeStruct;
-        let len = 0;
-        let struct_ser = serializer.serialize_struct("penumbra.core.component.funding.v1.FundingParameters", len)?;
+        let mut len = 0;
+        if self.liquidity_tournament.is_some() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("penumbra.core.component.funding.v1.FundingParameters", len)?;
+        if let Some(v) = self.liquidity_tournament.as_ref() {
+            struct_ser.serialize_field("liquidityTournament", v)?;
+        }
         struct_ser.end()
     }
 }
@@ -152,10 +158,13 @@ impl<'de> serde::Deserialize<'de> for FundingParameters {
         D: serde::Deserializer<'de>,
     {
         const FIELDS: &[&str] = &[
+            "liquidity_tournament",
+            "liquidityTournament",
         ];
 
         #[allow(clippy::enum_variant_names)]
         enum GeneratedField {
+            LiquidityTournament,
             __SkipField__,
         }
         impl<'de> serde::Deserialize<'de> for GeneratedField {
@@ -177,7 +186,10 @@ impl<'de> serde::Deserialize<'de> for FundingParameters {
                     where
                         E: serde::de::Error,
                     {
-                            Ok(GeneratedField::__SkipField__)
+                        match value {
+                            "liquidityTournament" | "liquidity_tournament" => Ok(GeneratedField::LiquidityTournament),
+                            _ => Ok(GeneratedField::__SkipField__),
+                        }
                     }
                 }
                 deserializer.deserialize_identifier(GeneratedVisitor)
@@ -195,14 +207,192 @@ impl<'de> serde::Deserialize<'de> for FundingParameters {
                 where
                     V: serde::de::MapAccess<'de>,
             {
-                while map_.next_key::<GeneratedField>()?.is_some() {
-                    let _ = map_.next_value::<serde::de::IgnoredAny>()?;
+                let mut liquidity_tournament__ = None;
+                while let Some(k) = map_.next_key()? {
+                    match k {
+                        GeneratedField::LiquidityTournament => {
+                            if liquidity_tournament__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("liquidityTournament"));
+                            }
+                            liquidity_tournament__ = map_.next_value()?;
+                        }
+                        GeneratedField::__SkipField__ => {
+                            let _ = map_.next_value::<serde::de::IgnoredAny>()?;
+                        }
+                    }
                 }
                 Ok(FundingParameters {
+                    liquidity_tournament: liquidity_tournament__,
                 })
             }
         }
         deserializer.deserialize_struct("penumbra.core.component.funding.v1.FundingParameters", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for funding_parameters::LiquidityTournament {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if self.gauge_threshold_percent != 0 {
+            len += 1;
+        }
+        if self.max_positions != 0 {
+            len += 1;
+        }
+        if self.max_delegators != 0 {
+            len += 1;
+        }
+        if self.delegator_share_percent != 0 {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("penumbra.core.component.funding.v1.FundingParameters.LiquidityTournament", len)?;
+        if self.gauge_threshold_percent != 0 {
+            #[allow(clippy::needless_borrow)]
+            #[allow(clippy::needless_borrows_for_generic_args)]
+            struct_ser.serialize_field("gaugeThresholdPercent", ToString::to_string(&self.gauge_threshold_percent).as_str())?;
+        }
+        if self.max_positions != 0 {
+            #[allow(clippy::needless_borrow)]
+            #[allow(clippy::needless_borrows_for_generic_args)]
+            struct_ser.serialize_field("maxPositions", ToString::to_string(&self.max_positions).as_str())?;
+        }
+        if self.max_delegators != 0 {
+            #[allow(clippy::needless_borrow)]
+            #[allow(clippy::needless_borrows_for_generic_args)]
+            struct_ser.serialize_field("maxDelegators", ToString::to_string(&self.max_delegators).as_str())?;
+        }
+        if self.delegator_share_percent != 0 {
+            #[allow(clippy::needless_borrow)]
+            #[allow(clippy::needless_borrows_for_generic_args)]
+            struct_ser.serialize_field("delegatorSharePercent", ToString::to_string(&self.delegator_share_percent).as_str())?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for funding_parameters::LiquidityTournament {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "gauge_threshold_percent",
+            "gaugeThresholdPercent",
+            "max_positions",
+            "maxPositions",
+            "max_delegators",
+            "maxDelegators",
+            "delegator_share_percent",
+            "delegatorSharePercent",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            GaugeThresholdPercent,
+            MaxPositions,
+            MaxDelegators,
+            DelegatorSharePercent,
+            __SkipField__,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "gaugeThresholdPercent" | "gauge_threshold_percent" => Ok(GeneratedField::GaugeThresholdPercent),
+                            "maxPositions" | "max_positions" => Ok(GeneratedField::MaxPositions),
+                            "maxDelegators" | "max_delegators" => Ok(GeneratedField::MaxDelegators),
+                            "delegatorSharePercent" | "delegator_share_percent" => Ok(GeneratedField::DelegatorSharePercent),
+                            _ => Ok(GeneratedField::__SkipField__),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = funding_parameters::LiquidityTournament;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct penumbra.core.component.funding.v1.FundingParameters.LiquidityTournament")
+            }
+
+            fn visit_map<V>(self, mut map_: V) -> std::result::Result<funding_parameters::LiquidityTournament, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut gauge_threshold_percent__ = None;
+                let mut max_positions__ = None;
+                let mut max_delegators__ = None;
+                let mut delegator_share_percent__ = None;
+                while let Some(k) = map_.next_key()? {
+                    match k {
+                        GeneratedField::GaugeThresholdPercent => {
+                            if gauge_threshold_percent__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("gaugeThresholdPercent"));
+                            }
+                            gauge_threshold_percent__ = 
+                                Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::MaxPositions => {
+                            if max_positions__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("maxPositions"));
+                            }
+                            max_positions__ = 
+                                Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::MaxDelegators => {
+                            if max_delegators__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("maxDelegators"));
+                            }
+                            max_delegators__ = 
+                                Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::DelegatorSharePercent => {
+                            if delegator_share_percent__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("delegatorSharePercent"));
+                            }
+                            delegator_share_percent__ = 
+                                Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::__SkipField__ => {
+                            let _ = map_.next_value::<serde::de::IgnoredAny>()?;
+                        }
+                    }
+                }
+                Ok(funding_parameters::LiquidityTournament {
+                    gauge_threshold_percent: gauge_threshold_percent__.unwrap_or_default(),
+                    max_positions: max_positions__.unwrap_or_default(),
+                    max_delegators: max_delegators__.unwrap_or_default(),
+                    delegator_share_percent: delegator_share_percent__.unwrap_or_default(),
+                })
+            }
+        }
+        deserializer.deserialize_struct("penumbra.core.component.funding.v1.FundingParameters.LiquidityTournament", FIELDS, GeneratedVisitor)
     }
 }
 impl serde::Serialize for GenesisContent {
