40730f39cf6c923594da44d93c3c33bd5ed366d1
Tal Derei <70081547+TalDerei@users.noreply.github.com>
2025-01-29 16:20:14 -0800

    sct: extend commitment source with LQT variant (#5024)
    ## Describe your changes
    
    - new commitment source kind to track the providence of LQT reward notes
    
    ## Issue ticket number and link
    
    references https://github.com/penumbra-zone/penumbra/issues/5011
    
    ## Checklist before requesting a review
    
    - [x] I have added guiding text to explain how a reviewer should test
    these changes.
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:

--- crates/proto/src/gen/penumbra.core.component.sct.v1.serde.rs
+++ crates/proto/src/gen/penumbra.core.component.sct.v1.serde.rs
@@ -221,6 +221,9 @@ impl serde::Serialize for CommitmentSource {
                 commitment_source::Source::Genesis(v) => {
                     struct_ser.serialize_field("genesis", v)?;
                 }
+                commitment_source::Source::Lqt(v) => {
+                    struct_ser.serialize_field("lqt", v)?;
+                }
             }
         }
         struct_ser.end()
@@ -241,6 +244,7 @@ impl<'de> serde::Deserialize<'de> for CommitmentSource {
             "community_pool_output",
             "communityPoolOutput",
             "genesis",
+            "lqt",
         ];
 
         #[allow(clippy::enum_variant_names)]
@@ -250,6 +254,7 @@ impl<'de> serde::Deserialize<'de> for CommitmentSource {
             FundingStreamReward,
             CommunityPoolOutput,
             Genesis,
+            Lqt,
             __SkipField__,
         }
         impl<'de> serde::Deserialize<'de> for GeneratedField {
@@ -277,6 +282,7 @@ impl<'de> serde::Deserialize<'de> for CommitmentSource {
                             "fundingStreamReward" | "funding_stream_reward" => Ok(GeneratedField::FundingStreamReward),
                             "communityPoolOutput" | "community_pool_output" => Ok(GeneratedField::CommunityPoolOutput),
                             "genesis" => Ok(GeneratedField::Genesis),
+                            "lqt" => Ok(GeneratedField::Lqt),
                             _ => Ok(GeneratedField::__SkipField__),
                         }
                     }
@@ -332,6 +338,13 @@ impl<'de> serde::Deserialize<'de> for CommitmentSource {
                                 return Err(serde::de::Error::duplicate_field("genesis"));
                             }
                             source__ = map_.next_value::<::std::option::Option<_>>()?.map(commitment_source::Source::Genesis)
+;
+                        }
+                        GeneratedField::Lqt => {
+                            if source__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("lqt"));
+                            }
+                            source__ = map_.next_value::<::std::option::Option<_>>()?.map(commitment_source::Source::Lqt)
 ;
                         }
                         GeneratedField::__SkipField__ => {
@@ -724,6 +737,123 @@ impl<'de> serde::Deserialize<'de> for commitment_source::Ics20Transfer {
             }
         }
         deserializer.deserialize_struct("penumbra.core.component.sct.v1.CommitmentSource.Ics20Transfer", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for commitment_source::LiquidityTournamentReward {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if self.epoch != 0 {
+            len += 1;
+        }
+        if self.tx_hash.is_some() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("penumbra.core.component.sct.v1.CommitmentSource.LiquidityTournamentReward", len)?;
+        if self.epoch != 0 {
+            #[allow(clippy::needless_borrow)]
+            #[allow(clippy::needless_borrows_for_generic_args)]
+            struct_ser.serialize_field("epoch", ToString::to_string(&self.epoch).as_str())?;
+        }
+        if let Some(v) = self.tx_hash.as_ref() {
+            struct_ser.serialize_field("txHash", v)?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for commitment_source::LiquidityTournamentReward {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "epoch",
+            "tx_hash",
+            "txHash",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            Epoch,
+            TxHash,
+            __SkipField__,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "epoch" => Ok(GeneratedField::Epoch),
+                            "txHash" | "tx_hash" => Ok(GeneratedField::TxHash),
+                            _ => Ok(GeneratedField::__SkipField__),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = commitment_source::LiquidityTournamentReward;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct penumbra.core.component.sct.v1.CommitmentSource.LiquidityTournamentReward")
+            }
+
+            fn visit_map<V>(self, mut map_: V) -> std::result::Result<commitment_source::LiquidityTournamentReward, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut epoch__ = None;
+                let mut tx_hash__ = None;
+                while let Some(k) = map_.next_key()? {
+                    match k {
+                        GeneratedField::Epoch => {
+                            if epoch__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("epoch"));
+                            }
+                            epoch__ = 
+                                Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::TxHash => {
+                            if tx_hash__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("txHash"));
+                            }
+                            tx_hash__ = map_.next_value()?;
+                        }
+                        GeneratedField::__SkipField__ => {
+                            let _ = map_.next_value::<serde::de::IgnoredAny>()?;
+                        }
+                    }
+                }
+                Ok(commitment_source::LiquidityTournamentReward {
+                    epoch: epoch__.unwrap_or_default(),
+                    tx_hash: tx_hash__,
+                })
+            }
+        }
+        deserializer.deserialize_struct("penumbra.core.component.sct.v1.CommitmentSource.LiquidityTournamentReward", FIELDS, GeneratedVisitor)
     }
 }
 impl serde::Serialize for commitment_source::Transaction {
