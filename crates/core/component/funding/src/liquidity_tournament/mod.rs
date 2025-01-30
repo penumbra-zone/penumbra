9eaf28834c83ea5a76063d4453f81e3099f30c9a
Lúcás Meier <lucas@cronokirby.com>
2025-01-30 10:19:03 -0800

    Implement LQT Voting action, and stateless checks (#5027)
    Closes #5014.
    
    This implements the action for LQT voting, up to (and including)
    stateless checks. This is similar to delegator voting, but with some
    light simplification. This also reuses the strategy of UndelegateClaim
    in using an inner proof shared with a different circuit, which is, imo,
    cleaner code since the fact that the delegator vote circuit can be
    reused is a happy coincidence.
    
    Testing deferred.
    
    - [x] I have added guiding text to explain how a reviewer should test
    these changes.
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
      > you betcha
    
    ---------
    
    Signed-off-by: Lúcás Meier <cronokirby@gmail.com>
    Co-authored-by: Erwan Or <erwanor@penumbralabs.xyz>

---
+++ crates/core/component/funding/src/liquidity_tournament/mod.rs
@@ -0,0 +1,9 @@
+mod action;
+mod view;
+
+pub mod proof;
+pub use action::{ActionLiquidityTournamentVote, LiquidityTournamentVoteBody};
+pub use view::ActionLiquidityTournamentVoteView;
+
+/// The maximum number of allowable bytes in the denom string.
+pub const LIQUIDITY_TOURNAMENT_VOTE_DENOM_MAX_BYTES: usize = 256;
