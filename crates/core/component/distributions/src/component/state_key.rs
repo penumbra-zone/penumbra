b829a8bed1d40aac71dc1308cf5123e7c6ba4199
Tal Derei <70081547+TalDerei@users.noreply.github.com>
2025-02-01 13:57:42 -0800

    distributions: implement rpc services (#5040)
    - Implements rpc services for the distributions component in
    `DistributionService`
    - indexes distributions state key by epoch index and writes to NV
    storage
    
    @cronokirby had some suggestions (which can be discussed in this PR) for
    replacing the historical epoch RPC, so I'm punting on the impl for now.
    
    references https://github.com/penumbra-zone/penumbra/issues/5039
    
    - [x] I have added guiding text to explain how a reviewer should test
    these changes.
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
      > LQT branch

--- crates/core/component/distributions/src/component/state_key.rs
+++ crates/core/component/distributions/src/component/state_key.rs
@@ -3,9 +3,22 @@ pub fn staking_token_issuance_for_epoch() -> &'static str {
     "distributions/staking_token_issuance_for_epoch"
 }
 
-// The amount of LQT rewards issued for this epoch.
-pub fn lqt_reward_issuance_for_epoch() -> &'static str {
-    "distributions/lqt_reward_issuance_for_epoch"
+pub mod lqt {
+    pub mod v1 {
+        pub mod budget {
+            pub(crate) fn prefix(epoch_index: u64) -> String {
+                format!("distributions/lqt/v1/budget/{epoch_index:020}")
+            }
+
+            /// The amount of LQT rewards issued for this epoch.
+            pub fn for_epoch(epoch_index: u64) -> [u8; 48] {
+                let prefix_bytes = prefix(epoch_index);
+                let mut key = [0u8; 48];
+                key[0..48].copy_from_slice(prefix_bytes.as_bytes());
+                key
+            }
+        }
+    }
 }
 
 pub fn distributions_parameters() -> &'static str {
