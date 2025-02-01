b18a1d04fde9ef4771825dcfccb0c6077455d708
Lucas Meier <lucas@cronokirby.com>
2025-02-01 00:38:17 -0800

    LQT: implement double voting proptest for gauge

--- crates/core/component/funding/src/component/liquidity_tournament/rewards/gauge.rs
+++ crates/core/component/funding/src/component/liquidity_tournament/rewards/gauge.rs
@@ -129,3 +129,76 @@ impl<'s> FinalizedGauge<'s> {
         self.voters.iter().copied()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use penumbra_sdk_governance::VotingReceiptToken;
+    use proptest::prelude::*;
+    use rand_chacha::rand_core::SeedableRng as _;
+
+    fn finalize(gauge: Gauge) -> FinalizedGauge {
+        gauge.finalize(Percentage::from_percent(10), 10)
+    }
+
+    fn trace(gauge: Gauge) -> (BTreeMap<asset::Id, Share>, BTreeMap<Vec<u8>, Share>) {
+        let result = finalize(gauge);
+        (
+            result.asset_shares().map(|(s, a)| (a, s)).collect(),
+            result
+                .voter_shares()
+                .map(|(s, v)| (v.to_vec(), s))
+                .collect(),
+        )
+    }
+
+    // so that we can easily pass in references to slices.
+    const ALL_BYTES: [u8; 256] = {
+        let mut arr = [0u8; 256];
+        let mut i = 0;
+        while i < 256 {
+            arr[i] = i as u8;
+            i += 1;
+        }
+        arr
+    };
+
+    fn addr_for(byte: u8) -> &'static [u8] {
+        let i = usize::from(byte);
+        &ALL_BYTES[i..i + 1]
+    }
+
+    fn asset_for(byte: u8) -> asset::Id {
+        asset::Id(u64::from(byte).into())
+    }
+
+    fn voting_power_combines_inner(votes: Vec<(u8, u32, u8)>) {
+        // Calculate gauge without deduplication.
+        let gauge = {
+            let mut out = Gauge::empty();
+            for (asset, power, voter) in votes.iter().copied() {
+                out.tally(asset_for(asset), power.into(), addr_for(voter));
+            }
+            out
+        };
+        let gauge_dedup = {
+            let mut deduped = BTreeMap::new();
+            for (asset, power, voter) in votes.into_iter() {
+                *deduped.entry((asset, voter)).or_insert(0u64) += u64::from(power);
+            }
+            let mut out = Gauge::empty();
+            for ((asset, voter), power) in deduped.into_iter() {
+                out.tally(asset_for(asset), power, addr_for(voter));
+            }
+            out
+        };
+        assert_eq!(trace(gauge), trace(gauge_dedup));
+    }
+
+    proptest! {
+        #[test]
+        fn voting_power_combines(votes in proptest::collection::vec((0u8..10, any::<u32>(), any::<u8>()), 0..100)) {
+            voting_power_combines_inner(votes);
+        }
+    }
+}
