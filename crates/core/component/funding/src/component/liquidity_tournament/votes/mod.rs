d5eff9826a59ee59d94b1017e59d60af89d904ab
Lucas Meier <lucas@cronokirby.com>
2025-02-04 09:51:53 -0800

    LQT: Use StateRead/WriteProto consistently

--- crates/core/component/funding/src/component/liquidity_tournament/votes/mod.rs
+++ crates/core/component/funding/src/component/liquidity_tournament/votes/mod.rs
@@ -5,52 +5,43 @@ use cnidarium::{StateRead, StateWrite};
 use futures::{Stream, StreamExt as _, TryStreamExt as _};
 use penumbra_sdk_asset::asset;
 use penumbra_sdk_keys::Address;
+use penumbra_sdk_proto::{DomainType, StateReadProto as _, StateWriteProto as _};
 use penumbra_sdk_sct::component::source::SourceContext as _;
 use penumbra_sdk_txhash::TransactionId;
 
 use crate::component::state_key;
 
-async fn get_u64(state: impl StateRead, key: &[u8]) -> u64 {
+async fn get_total(state: impl StateRead, epoch_index: u64) -> u64 {
     state
-        .nonverifiable_get_raw(key)
+        .nonverifiable_get_proto(&state_key::lqt::v1::votes::total::key(epoch_index))
         .await
-        .ok()
-        .and_then(|x| Some(u64::from_le_bytes(TryInto::<[u8; 8]>::try_into(x?).ok()?)))
+        .expect("should be able to read u64")
         .unwrap_or_default()
 }
 
-async fn put_u64(mut state: impl StateWrite, key: Vec<u8>, value: u64) {
-    state.nonverifiable_put_raw(key, value.to_le_bytes().to_vec());
-}
-
-async fn get_total(state: impl StateRead, epoch_index: u64) -> u64 {
-    get_u64(state, &state_key::lqt::v1::votes::total::key(epoch_index)).await
-}
-
-async fn put_total(state: impl StateWrite, epoch_index: u64, total: u64) {
-    put_u64(
-        state,
+fn put_total(mut state: impl StateWrite, epoch_index: u64, total: u64) {
+    state.nonverifiable_put_proto(
         state_key::lqt::v1::votes::total::key(epoch_index).to_vec(),
         total,
     )
-    .await
 }
 
 async fn get_asset_total(state: impl StateRead, epoch_index: u64, asset: asset::Id) -> u64 {
-    get_u64(
-        state,
-        &state_key::lqt::v1::votes::by_asset::total::key(epoch_index, asset),
-    )
-    .await
+    state
+        .nonverifiable_get_proto(&state_key::lqt::v1::votes::by_asset::total::key(
+            epoch_index,
+            asset,
+        ))
+        .await
+        .expect("should be able to read u64")
+        .unwrap_or_default()
 }
 
-async fn put_asset_total(state: impl StateWrite, epoch_index: u64, asset: asset::Id, total: u64) {
-    put_u64(
-        state,
+fn put_asset_total(mut state: impl StateWrite, epoch_index: u64, asset: asset::Id, total: u64) {
+    state.nonverifiable_put_proto(
         state_key::lqt::v1::votes::by_asset::total::key(epoch_index, asset).to_vec(),
         total,
     )
-    .await
 }
 
 fn put_ranked_asset(mut state: impl StateWrite, epoch_index: u64, power: u64, asset: asset::Id) {
@@ -72,26 +63,28 @@ async fn get_voter_total(
     asset: asset::Id,
     voter: &Address,
 ) -> u64 {
-    get_u64(
-        state,
-        &state_key::lqt::v1::votes::by_voter::total::key(epoch_index, asset, voter),
-    )
-    .await
+    state
+        .nonverifiable_get_proto(&state_key::lqt::v1::votes::by_voter::total::key(
+            epoch_index,
+            asset,
+            voter,
+        ))
+        .await
+        .expect("should be able to read u64")
+        .unwrap_or_default()
 }
 
-async fn put_voter_total(
-    state: impl StateWrite,
+fn put_voter_total(
+    mut state: impl StateWrite,
     epoch_index: u64,
     asset: asset::Id,
     voter: &Address,
     total: u64,
 ) {
-    put_u64(
-        state,
+    state.nonverifiable_put_proto(
         state_key::lqt::v1::votes::by_voter::total::key(epoch_index, asset, voter).to_vec(),
         total,
     )
-    .await
 }
 
 fn put_ranked_voter(
@@ -102,9 +95,9 @@ fn put_ranked_voter(
     voter: &Address,
     tx: TransactionId,
 ) {
-    state.nonverifiable_put_raw(
+    state.nonverifiable_put_proto(
         state_key::lqt::v1::votes::by_voter::ranked::key(epoch_index, asset, power, voter).to_vec(),
-        tx.0.to_vec(),
+        tx.to_proto(),
     )
 }
 
@@ -145,14 +138,14 @@ pub trait StateReadExt: StateRead + Sized {
     ) -> Pin<Box<dyn Stream<Item = anyhow::Result<((u64, Address), TransactionId)>> + Send + 'static>>
     {
         self
-        .nonverifiable_prefix_raw(&state_key::lqt::v1::votes::by_voter::ranked::prefix(
+        .nonverifiable_prefix_proto(&state_key::lqt::v1::votes::by_voter::ranked::prefix(
             epoch,
             asset
         ))
-        .map_ok(|(k, v)| {
+        .map_ok(|(k, v) : (_, <TransactionId as DomainType>::Proto)| {
             let key = state_key::lqt::v1::votes::by_voter::ranked::parse_key(&k)
                 .expect("should be able to parse lqt::v1::votes::by_voter::ranked key");
-            let value = TransactionId(v.try_into().expect("should be able to parse transaction hash from lqt::v1::votes::by_voter::ranked_key"));
+            let value = v.try_into().expect("should be able to parse transaction hash from lqt::v1::votes::by_voter::ranked_key");
             (key, value)
         }).boxed()
     }
@@ -172,14 +165,14 @@ pub trait StateWriteExt: StateWrite + Sized {
         // Increment total.
         {
             let current = get_total(&self, epoch).await;
-            put_total(&mut *self, epoch, add_power(current, power)).await;
+            put_total(&mut *self, epoch, add_power(current, power))
         }
         // Adjust asset indices.
         {
             let current = get_asset_total(&self, epoch, asset).await;
             delete_ranked_asset(&mut *self, epoch, current, asset);
             let new = add_power(current, power);
-            put_asset_total(&mut *self, epoch, asset, new).await;
+            put_asset_total(&mut *self, epoch, asset, new);
             put_ranked_asset(&mut *self, epoch, new, asset);
         }
         // Adjust voter indices.
@@ -187,7 +180,7 @@ pub trait StateWriteExt: StateWrite + Sized {
             let current = get_voter_total(&self, epoch, asset, voter).await;
             delete_ranked_voter(&mut *self, epoch, asset, current, voter);
             let new = add_power(current, power);
-            put_voter_total(&mut *self, epoch, asset, voter, new).await;
+            put_voter_total(&mut *self, epoch, asset, voter, new);
             put_ranked_voter(&mut self, epoch, asset, new, voter, tx);
         }
     }
