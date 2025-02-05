acc77c3b02f1262339f19b43426a78ad6e90a244
Lucas Meier <lucas@cronokirby.com>
2025-02-05 00:10:29 -0800

    picturesque: correctly run and initialize postgres
    This restricts communication to just the unix socket,
    so it should be completely encapsulated.

--- tools/picturesque/src/main.rs
+++ tools/picturesque/src/main.rs
@@ -1,7 +1,27 @@
-use picturesque::run_devnet;
+use std::io::IsTerminal as _;
 
+use clap::Parser;
+use picturesque::{run_devnet, Options};
+use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _, EnvFilter};
+
+fn setup_tracing() -> anyhow::Result<()> {
+    // The `FmtLayer` is used to print to the console.
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(std::io::stdout().is_terminal())
+        .with_target(true);
+    // The `EnvFilter` layer is used to filter events based on `RUST_LOG`.
+    let filter_layer = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
+
+    // Register the tracing subscribers.
+    let registry = tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer);
+    registry.init();
+    Ok(())
+}
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    run_devnet().await?;
+    setup_tracing()?;
+    run_devnet(Options::parse()).await?;
     Ok(())
 }
