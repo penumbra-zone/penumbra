e8b3c4c468ccebc29377ca4aaa774857f10495a4
Lucas Meier <lucas@cronokirby.com>
2025-02-05 00:10:29 -0800

    picturesque: correctly run and initialize postgres
    This restricts communication to just the unix socket,
    so it should be completely encapsulated.

--- tools/picturesque/src/lib.rs
+++ tools/picturesque/src/lib.rs
@@ -1,3 +1,23 @@
-pub async fn run_devnet() -> anyhow::Result<()> {
+use clap::Parser;
+use std::path::PathBuf;
+
+mod postgres;
+
+/// This struct represents the command-line options
+#[derive(Clone, Debug, Parser)]
+#[clap(name = "picturesque", about = "a simple tool to run devnets", version)]
+pub struct Options {
+    /// The base of operations for the tool.
+    #[clap(short, long)]
+    pub directory: PathBuf,
+}
+
+#[tracing::instrument]
+pub async fn run_devnet(options: Options) -> anyhow::Result<()> {
+    tracing::info!("spawning postgres");
+    let postgres = tokio::spawn(postgres::run(options.directory.canonicalize()?));
+    tokio::select! {
+        x = postgres => x??
+    };
     Ok(())
 }
