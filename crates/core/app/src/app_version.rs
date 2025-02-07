689754a8514bfd075f6bf8c2e5b3a84c3ce90225
Conor Schaefer <conorsch@users.noreply.github.com>
2025-02-07 09:02:00 -0800

    chore: bump app version 9 -> 10 (#5073)
    Bumps the protocol version to denote the addition of LQT functionality.
    Also bumps the crate versions from 1.0.x -> 2.0.x, as an alpha. No tag
    has been created, to avoid triggering the formal release workflows,
    since we're still testing.
    
    I'm submitting this PR ahead of a planned testnet upgrade, so we'll
    shortly learn how effective the overall upgrade process is. However,
    special attention should be given to the proposed versions strings. In
    particular, the transition from `0.81.x` to `1.0.x` maintained
    compatibility with APP_VERSION 9, and that may present difficulties
    during the upgrade for nodes on one or the other version.
    
    - [x] I have added guiding text to explain how a reviewer should test
    these changes.
    
    - [x] If this code contains consensus-breaking changes, I have added the
    "consensus-breaking" label. Otherwise, I declare my belief that there
    are not consensus-breaking changes, for the following reason:
    
    > explicitly denotes consensus-breaking changes, and therefore targets a
    protocol feature branch, rather than main

--- crates/core/app/src/app_version.rs
+++ crates/core/app/src/app_version.rs
@@ -1,6 +1,6 @@
 /// Representation of the Penumbra application version. Notably, this is distinct
 /// from the crate version(s). This number should only ever be incremented.
-pub const APP_VERSION: u64 = 9;
+pub const APP_VERSION: u64 = 10;
 
 cfg_if::cfg_if! {
     if #[cfg(feature="component")] {
