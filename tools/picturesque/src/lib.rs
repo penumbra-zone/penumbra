393ddd766125c18b622e92f1dcbf00e8667abb2e
Lucas Meier <lucas@cronokirby.com>
2025-02-11 13:38:20 -0800

    picturesque: separate commands

--- tools/picturesque/src/lib.rs
+++ tools/picturesque/src/lib.rs
@@ -5,6 +5,31 @@ mod cometbft;
 mod pd;
 mod postgres;
 
+#[derive(Clone, Debug)]
+struct Context {
+    directory: PathBuf,
+    log_level: tracing::Level,
+}
+
+#[derive(Clone, Debug, Parser)]
+pub enum Command {
+    /// Create a new blank slate for the devnet.
+    ///
+    /// This will have no permanent state, but be ready to edit for changing params.
+    ///
+    /// In most cases, you'll want to edit it, and then save a copy for later,
+    /// before then spinning up the devnet.
+    Create {
+        /// How long epochs last, in blocks.
+        #[clap(long)]
+        epoch_duration: u32,
+    },
+    /// Start up a devnet.
+    ///
+    /// The working directory should have been initialized with the `create` command before.
+    Start,
+}
+
 /// This struct represents the command-line options
 #[derive(Clone, Debug, Parser)]
 #[clap(name = "picturesque", about = "a simple tool to run devnets", version)]
@@ -12,22 +37,54 @@ pub struct Options {
     /// The base of operations for the tool.
     #[clap(short, long)]
     pub directory: PathBuf,
+    /// The log level for this program, and the child processes.
+    #[clap(long, env = "RUST_LOG")]
+    pub log_level: tracing::Level,
+    #[clap(subcommand)]
+    pub command: Command,
 }
 
-#[tracing::instrument]
-pub async fn run_devnet(options: Options) -> anyhow::Result<()> {
+impl Options {
+    fn context(&self) -> Context {
+        Context {
+            directory: self.directory.clone(),
+            log_level: self.log_level,
+        }
+    }
+
+    pub async fn run(self) -> anyhow::Result<()> {
+        let ctx = self.context();
+        match self.command {
+            Command::Create { epoch_duration } => create_devnet(ctx, epoch_duration).await?,
+            Command::Start => run_devnet(ctx).await?,
+        }
+        Ok(())
+    }
+}
+
+#[tracing::instrument(skip_all)]
+async fn run_devnet(ctx: Context) -> anyhow::Result<()> {
     tracing::info!("spawning postgres");
-    let root = options.directory.canonicalize()?;
+    let root = ctx.directory.canonicalize()?;
     let postgres = tokio::spawn(postgres::run(root.clone()));
     let cometbft = tokio::spawn(cometbft::run(
         root.clone(),
         Some(Duration::from_millis(4000)),
     ));
-    let pd = tokio::spawn(pd::run(root.clone(), Some(Duration::from_millis(8000))));
+    let pd = tokio::spawn(pd::run(
+        root.clone(),
+        ctx.log_level,
+        Some(Duration::from_millis(8000)),
+    ));
     tokio::select! {
         x = postgres => x??,
         x = cometbft => x??,
         x = pd => x??,
     };
     Ok(())
+}
+
+#[tracing::instrument(skip(_ctx))]
+async fn create_devnet(_ctx: Context, _epoch_duration: u32) -> anyhow::Result<()> {
+    todo!()
 }
--- tools/picturesque/src/main.rs
+++ tools/picturesque/src/main.rs
@@ -1,16 +1,18 @@
 use std::io::IsTerminal as _;
 
 use clap::Parser;
-use picturesque::{run_devnet, Options};
+use picturesque::Options;
 use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _, EnvFilter};
 
-fn setup_tracing() -> anyhow::Result<()> {
+fn setup_tracing(log_level: tracing::Level) -> anyhow::Result<()> {
     // The `FmtLayer` is used to print to the console.
     let fmt_layer = tracing_subscriber::fmt::layer()
         .with_ansi(std::io::stdout().is_terminal())
         .with_target(true);
     // The `EnvFilter` layer is used to filter events based on `RUST_LOG`.
-    let filter_layer = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
+    let filter_layer = EnvFilter::builder()
+        .with_default_directive(log_level.into())
+        .parse("")?;
 
     // Register the tracing subscribers.
     let registry = tracing_subscriber::registry()
@@ -21,7 +23,8 @@ fn setup_tracing() -> anyhow::Result<()> {
 }
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    setup_tracing()?;
-    run_devnet(Options::parse()).await?;
+    let opts = Options::parse();
+    setup_tracing(opts.log_level)?;
+    opts.run().await?;
     Ok(())
 }
