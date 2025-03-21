d0b2c4d9345131734ab56d9b2d92b7ff9f200c16
Lucas Meier <lucas@cronokirby.com>
2025-03-21 16:36:34 -0700

    Add integrity check for missing events in cometindex

--- crates/bin/pindexer/src/main.rs
+++ crates/bin/pindexer/src/main.rs
@@ -6,10 +6,10 @@ use pindexer::{Indexer, IndexerExt as _, Options};
 async fn main() -> Result<()> {
     let opts = Options::parse();
     let cometindex_opts = opts.cometindex.clone();
+    tracing_subscriber::fmt::init();
     match cometindex_opts.command {
         cometindex::opt::Command::Index(index_options) => {
             Indexer::new(cometindex_opts.src_database_url, index_options)
-                .with_default_tracing()
                 .with_default_penumbra_app_views(&opts)
                 .run()
                 .await?;
--- crates/util/cometindex/src/integrity.rs
+++ crates/util/cometindex/src/integrity.rs
@@ -2,7 +2,7 @@ use sqlx::PgPool;
 
 use crate::database::read_only_db;
 
-pub async fn missing_blocks(name: &'static str, db: PgPool) -> anyhow::Result<()> {
+pub async fn missing_blocks(name: &'static str, db: PgPool) -> anyhow::Result<bool> {
     let mut dbtx = db.begin().await?;
     let res: Option<(i64, i64)> = sqlx::query_as(
         "
@@ -25,15 +25,44 @@ pub async fn missing_blocks(name: &'static str, db: PgPool) -> anyhow::Result<()
     .fetch_optional(dbtx.as_mut())
     .await?;
     dbtx.rollback().await?;
-    match res {
-        Some((current, next)) => {
-            eprintln!("{}:", name);
-            eprintln!("missing blocks between heights {} and {}", current, next);
-            anyhow::bail!("{} check failed", name);
-        }
-        None => {}
+    if let Some((current, next)) = res {
+        println!("{}:", name);
+        println!("missing blocks between heights {} and {}", current, next);
+        return Ok(false);
     }
-    Ok(())
+    Ok(true)
+}
+
+async fn missing_events(name: &'static str, db: PgPool) -> anyhow::Result<bool> {
+    let mut dbtx = db.begin().await?;
+    let res: Option<(i64, i64, i64)> = sqlx::query_as(
+        "
+      WITH event_heights AS (
+        SELECT e.rowid, b.height,
+               LEAD(e.rowid) OVER (PARTITION BY b.height ORDER BY e.rowid) AS next_rowid
+        FROM events e
+        JOIN blocks b ON e.block_id = b.rowid
+      )
+      SELECT height, rowid, next_rowid
+      FROM event_heights
+      WHERE next_rowid IS NOT NULL
+      AND next_rowid - rowid <> 1
+      ORDER BY height ASC, rowid ASC
+      LIMIT 1;
+    ",
+    )
+    .fetch_optional(dbtx.as_mut())
+    .await?;
+    if let Some((height, next, current)) = res {
+        println!("{}:", name);
+        println!(
+            "missing events at height {}: missing between event #{} and #{}",
+            height, current, next
+        );
+        return Ok(false);
+    };
+    dbtx.rollback().await?;
+    Ok(true)
 }
 
 pub async fn integrity_check(src_database_url: &str) -> anyhow::Result<()> {
@@ -43,10 +72,13 @@ pub async fn integrity_check(src_database_url: &str) -> anyhow::Result<()> {
         let db = db.clone();
         async move { missing_blocks("# 000 Missing Blocks", db).await }
     }));
+    tasks.push(tokio::spawn({
+        let db = db.clone();
+        async move { missing_events("# 001 Missing Events", db).await }
+    }));
     let mut failed = false;
     for task in tasks {
-        let res = task.await?;
-        failed |= res.is_err();
+        failed |= !task.await??;
     }
     if failed {
         anyhow::bail!("integrity checks failed, check logs");
