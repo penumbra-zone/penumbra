570935df8b9b1af4afb62d5f3fcbdeafb95635a9
Lucas Meier <lucas@cronokirby.com>
2025-04-18 13:35:40 -0700

    elcuity: implement continuous voting command

--- crates/bin/elcuity/src/lib.rs
+++ crates/bin/elcuity/src/lib.rs
@@ -1,21 +1,16 @@
 use clap::{Parser, Subcommand};
-use penumbra_sdk_proto::{box_grpc_svc, view::v1::view_service_client::ViewServiceClient};
-use penumbra_sdk_view::ViewClient;
 
+mod clients;
 mod vote;
-
-async fn create_view_client(url: String) -> anyhow::Result<Box<dyn ViewClient>> {
-    let endpoint = tonic::transport::Endpoint::new(url)?;
-    Ok(Box::new(ViewServiceClient::new(
-        box_grpc_svc::connect(endpoint).await?,
-    )))
-}
 
 /// A suite for automated tournament actions.
 ///
 /// Relies on an external view and custody service.
 #[derive(Debug, Parser)]
 pub struct Opt {
+    /// A URL for the GRPC endpoint.
+    #[clap(long)]
+    grpc_url: String,
     /// A URL for the view service.
     #[clap(long)]
     view_service: String,
@@ -32,9 +27,9 @@ pub enum Command {
 impl Opt {
     /// Run the command with the parsed options
     pub async fn run(self) -> anyhow::Result<()> {
-        let mut view_client = create_view_client(self.view_service).await?;
+        let clients = clients::Clients::init(self.grpc_url, self.view_service).await?;
         match self.command {
-            Command::Vote(opt) => opt.run(view_client.as_mut()).await?,
+            Command::Vote(opt) => opt.run(&clients).await?,
         }
         Ok(())
     }
