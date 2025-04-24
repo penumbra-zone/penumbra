7da31118a77514cbd211c40ee7d1d7220b63cec7
Conor Schaefer <conor@penumbralabs.xyz>
2025-04-24 16:38:44 -0700

    elcuity: enable info logging in headless environments

--- crates/bin/elcuity/src/main.rs
+++ crates/bin/elcuity/src/main.rs
@@ -1,15 +1,32 @@
 use clap::Parser;
 use elcuity::Opt;
 use rustls::crypto::aws_lc_rs;
-use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+use std::io::IsTerminal;
+use tracing_subscriber::{prelude::*, EnvFilter};
+
+/// Configure tracing_subscriber for logging messages
+fn init_tracing() -> anyhow::Result<()> {
+    // Instantiate tracing layers.
+    // The `FmtLayer` is used to print to the console.
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(std::io::stdout().is_terminal())
+        .with_writer(std::io::stderr)
+        .with_target(true);
+    // The `EnvFilter` layer is used to filter events based on `RUST_LOG`.
+    let filter_layer = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
+
+    // Register the tracing subscribers.
+    let registry = tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer);
+    registry.init();
+    Ok(())
+}
 
 #[tokio::main()]
 async fn main() -> anyhow::Result<()> {
     // Initialize tracing
-    tracing_subscriber::registry()
-        .with(fmt::layer())
-        .with(EnvFilter::from_default_env())
-        .init();
+    init_tracing()?;
 
     // Parse command line options
     let opt = Opt::parse();
