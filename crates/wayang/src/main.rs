47a68faac84226308be25cb76ee961ba6583d30f
Lucas Meier <lucas@cronokirby.com>
2025-06-25 13:43:26 -0700

    feat: wayang: create default config in path

--- crates/wayang/src/main.rs
+++ crates/wayang/src/main.rs
@@ -1,12 +1,22 @@
+use anyhow::Result;
+use clap::Parser;
+use penumbra_sdk_wayang::{config::Config, rhythm_and_feeler, Move};
 use std::{
     path::{Path, PathBuf},
     str::FromStr,
 };
+use tokio::fs;
+use tokio::task::JoinHandle;
 
-use anyhow::Result;
-use clap::Parser;
-use penumbra_sdk_wayang::{config::Config, rhythm_and_feeler, Move};
-use tokio::{fs, task::JoinHandle};
+async fn read_config(path: &Path) -> anyhow::Result<Config> {
+    let data = fs::read_to_string(path).await?;
+    Config::from_str(&data)
+}
+
+async fn write_example_config(path: &Path) -> anyhow::Result<()> {
+    fs::write(path, Config::EXAMPLE_STR.as_bytes()).await?;
+    Ok(())
+}
 
 #[derive(Parser)]
 struct Args {
@@ -15,15 +25,21 @@ struct Args {
     config: PathBuf,
 }
 
-async fn read_config(path: &Path) -> anyhow::Result<Config> {
-    let data = fs::read_to_string(path).await?;
-    Config::from_str(&data)
+impl Args {
+    /// Read the config from the path provided in the arguments, or create a default one.
+    async fn fetch_config(&self) -> anyhow::Result<Config> {
+        let path = &self.config;
+        if !fs::try_exists(path).await? {
+            write_example_config(path).await?;
+        }
+        Ok(read_config(path).await?)
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
-    let config = read_config(&args.config).await?;
+    let config = args.fetch_config().await?;
     let (mut rhythm, feeler) = rhythm_and_feeler(&config)?;
     let rhythm_task: JoinHandle<anyhow::Result<()>> = tokio::spawn(async move {
         loop {
