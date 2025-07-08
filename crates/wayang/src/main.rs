30fd4ba8d533f0a86b7fa695992ac4cffad3f3e3
Lucas Meier <lucas@cronokirby.com>
2025-07-07 17:43:45 -0700

    feat: wayang: parse orders from stdin
    Eventually this will be a more sophisticated interface.

--- crates/wayang/src/main.rs
+++ crates/wayang/src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
 use clap::Parser;
-use penumbra_sdk_wayang::{config::Config, init_tracing, rhythm_and_feeler, Move, PositionShape};
-use std::path::PathBuf;
+use penumbra_sdk_wayang::{config::Config, init_tracing, rhythm_and_feeler, Move, Position};
+use std::{path::PathBuf, str::FromStr};
+use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::task::JoinHandle;
 
 #[derive(Parser)]
@@ -19,24 +20,27 @@ async fn main() -> Result<()> {
     let config = Config::fetch(&args.config).await?;
     let (mut rhythm, feeler) = rhythm_and_feeler(&config)?;
     let rhythm_task: JoinHandle<anyhow::Result<()>> = tokio::spawn(async move {
+        let stdin = tokio::io::stdin();
+        let mut reader = BufReader::new(stdin);
+        let mut line = String::new();
+
         loop {
             let status = rhythm.sense().await?;
-            let price = status
-                .and_then(|x| {
-                    x.positions
-                        .get(0)
-                        .map(|x| (x.shape.upper_price + x.shape.lower_price) / 2.0)
-                })
-                .unwrap_or_default()
-                + 0.0001;
-            let shape = PositionShape {
-                upper_price: 1.01 * price,
-                lower_price: 0.99 * price,
-                base_liquidity: 1.0,
-                quote_liquidity: 1.0,
+            tracing::info!(?status, "current status");
+
+            let position = loop {
+                line.clear();
+                reader.read_line(&mut line).await?;
+                let trimmed = line.trim();
+                if let Ok(position) = Position::from_str(trimmed) {
+                    break position;
+                } else {
+                    tracing::info!("invalid position: '{trimmed}'");
+                }
             };
-            tracing::info!(?shape, "submitting position move");
-            rhythm.do_move(Move { shape }).await?;
+
+            tracing::info!(%position, "submitting position move");
+            rhythm.do_move(Move { position }).await?;
         }
     });
     let feeler_task = tokio::spawn(feeler.run());
